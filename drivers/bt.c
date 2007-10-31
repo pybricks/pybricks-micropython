925250d293dc818234eaebf72e96bc860d98fbd1
Jérôme Flesch <jflesch@gmail.com>
2007-10-31 21:54:16 +0100

    BT/UART : It can discover other devices, but just once

--- drivers/bt.c
+++ drivers/bt.c
@@ -7,13 +7,18 @@
 #include "base/drivers/bt.h"
 
 
+/* to remove : */
 #ifdef UART_DEBUG
+#include "base/display.h"
 #include "base/drivers/usb.h"
 #include "base/util.h"
+#define CMDS_BUFSIZE 128
 #define USB_SEND(txt) usb_send((U8*)txt, strlen(txt))
 #else
+#define CMDS_BUFSIZE 0
 #define USB_SEND(txt)
 #endif
+
 
 #define BT_RST_PIN AT91C_PIO_PA11
 #define BT_ARM_CMD_PIN AT91C_PIO_PA27
@@ -21,14 +26,75 @@
 #define BT_CS_PIN   AT91C_PIO_PA31
 
 
-/* to remove: */
-#define BT_CMD_BUF 128
+typedef enum {
+  /* AMR7 -> BC4 */
+  BT_MSG_BEGIN_INQUIRY = 0x00,
+  BT_MSG_CANCEL_INQUIRY = 0x01,
+  BT_MSG_CONNECT = 0x02,
+  BT_MSG_OPEN_PORT = 0x03,
+  BT_MSG_LOOKUP_NAME = 0x04,
+  BT_MSG_ADD_DEVICE = 0x05,
+  BT_MSG_REMOVE_DEVICE = 0x06,
+  BT_MSG_DUMP_LIST = 0x07,
+  BT_MSG_CLOSE_CONNECTION = 0x08,
+  BT_MSG_ACCEPT_CONNECTION = 0x09,
+  BT_MSG_PIN_CODE = 0x0A,
+  BT_MSG_OPEN_STREAM = 0x0B,
+  BT_MSG_START_HEART = 0x0C,
+  BT_MSG_SET_DISCOVERABLE = 0x1C,
+  BT_MSG_CLOSE_PORT = 0x1D,
+  BT_MSG_SET_FRIENDLY_NAME = 0x21,
+  BT_MSG_GET_LINK_QUALITY = 0x23,
+  BT_MSG_SET_FACTORY_SETTINGS = 0x25,
+  BT_MSG_GET_LOCAL_ADDR = 0x26,
+  BT_MSG_GET_FRIENDLY_NAME = 0x29,
+  BT_MSG_GET_DISCOVERABLE = 0x2A,
+  BT_MSG_GET_PORT_OPEN = 0x2B,
+  BT_MSG_GET_VERSION = 0x2F,
+  BT_MSG_GET_BRICK_STATUS_BYTE = 0x33,
+  BT_MSG_SET_BRICK_STATUS_BYTE = 0x34,
+  BT_MSG_GET_OPERATING_MODE = 0x35,
+  BT_MSG_SET_OPERATING_MODE = 0x36,
+  BT_MSG_GET_CONNECTION_STATUS = 0x38,
+  BT_MSG_GOTO_DFU_MODE = 0x3A,
 
+  /* BC4 -> ARM7 */
+  BT_MSG_HEARTBEAT = 0x0D,
+  BT_MSG_INQUIRY_RUNNING = 0x0E,
+  BT_MSG_INQUIRY_RESULT = 0x0F,
+  BT_MSG_INQUIRY_STOPPED = 0x10,
+  BT_MSG_LOOKUP_NAME_RESULT = 0x11,
+  BT_MSG_LOOKUP_NAME_FAILURE = 0x12,
+  BT_MSG_CONNECT_RESULT = 0x13,
+  BT_MSG_RESET_INDICATION = 0x14,
+  BT_MSG_REQUEST_PIN_CODE = 0x15,
+  BT_MSG_REQUEST_CONNECTION = 0x16,
+  BT_MSG_LIST_RESULT = 0x17,
+  BT_MSG_LIST_ITEM = 0x18,
+  BT_MSG_LIST_DUMP_STOPPED = 0x19,
+  BT_MSG_CLOSE_CONNECTION_RESULT = 0x1A,
+  BT_MSG_PORT_OPEN_RESULT = 0x1B,
+  BT_MSG_CLOSE_PORT_RESULT = 0x1E,
+  BT_MSG_PIN_CODE_ACK = 0x1F,
+  BT_MSG_SET_DISCOVERABLE_ACK = 0x20,
+  BT_MSG_SET_FRIENDLY_NAME_ACK = 0x22,
+  BT_MSG_LINK_QUALITY_RESULT = 0x24,
+  BT_MSG_SET_FACTORY_SETTINGS_ACK = 0x26,
+  BT_MSG_GET_LOCAL_ADDR_RESULT = 0x28,
+  BT_MSG_GET_FRIENDLY_NAME_RESULT = 0x2C,
+  BT_MSG_GET_DISCOVERABLE_RESULT = 0x2D,
+  BT_MSG_GET_PORT_OPEN_RESULT = 0x2E,
+  BT_MSG_GET_VERSION_RESULT = 0X30,
+  BT_MSG_GET_BRICK_STATUS_BYTE_RESULT = 0x31,
+  BT_MSG_SET_BRICK_STATUS_BYTE_RESULT = 0x32,
+  BT_MSG_OPERATING_MODE_RESULT = 0x37,
+  BT_MSG_CONNECTION_STATUS_RESULT = 0x39,
+} bt_msg_t;
 
 
 static const U8 bt_msg_start_heart[] = {
   0x03, /* length */
-  0x0C, /* start heart */
+  BT_MSG_START_HEART,
   0xFF, /* checksum (high) */
   0xF4  /* checksum (low) */
 };
@@ -36,7 +102,7 @@ static const U8 bt_msg_start_heart[] = {
 
 static const U8 bt_msg_get_version[] = {
   0x03, /* length */
-  0x2F, /* get version */
+  BT_MSG_GET_VERSION,
   0xFF, /* checksum (hi) */
   0xD1  /* checksum (lo) */
 };
@@ -44,7 +110,7 @@ static const U8 bt_msg_get_version[] = {
 
 static const U8 bt_set_discoverable_true[] = {
   0x04, /* length */
-  0x1C, /* set discoverable */
+  BT_MSG_SET_DISCOVERABLE,
   0x01, /* => true */
   0xFF,
   0xE3
@@ -52,19 +118,38 @@ static const U8 bt_set_discoverable_true[] = {
 
 static const U8 bt_set_discoverable_false[] = {
   0x04, /* length */
-  0x1C, /* set discoverable */
+  BT_MSG_SET_DISCOVERABLE, /* set discoverable */
   0x00, /* => true */
   0xFF,
   0xE4
 };
 
 
-static volatile struct {
-  U32 state; /* not used atm */
 
-  /* to remove */
-  U16 cmd_pos;
-  U8 cmds[BT_CMD_BUF];
+typedef enum {
+  BT_STATE_WAITING = 0,
+  BT_STATE_INQUIRING,
+} bt_state_t;
+
+
+static volatile struct {
+  bt_state_t state; /* not used atm */
+
+  /* see systick_get_ms() */
+  U32 last_heartbeat;
+
+  /* used for inquiring */
+
+  /* TODO : Figure why there is this InquiryStopped to ignore */
+  bool first_stop;
+  U8 bt_remote_id;
+  bt_device_t bt_remote_device;
+
+#ifdef UART_DEBUG
+  /* to remove: */
+  U8 cmds[CMDS_BUFSIZE];
+  U8 cmds_pos;
+#endif
 
 } bt_state = {
   0
@@ -100,7 +185,7 @@ static U16 bt_get_checksum(U8 *msg, U8 len, bool count_len)
 /* len => length excepted, but checksum included
  * two last bytes will be set
  */
-void bt_set_checksum(U8 *msg, U8 len) {
+static void bt_set_checksum(U8 *msg, U8 len) {
   U16 checksum = bt_get_checksum(msg, len, FALSE);
 
   msg[len] = ((checksum >> 8) & 0xFF);
@@ -123,19 +208,97 @@ static bool bt_check_checksum(U8 *msg, U8 len) {
 }
 
 
+static void bt_begin_inquiry(U8 max_devices,
+                             U8 timeout,
+                             U8 bt_remote_class[4])
+{
+  int i;
+  U8 packet[10];
+
+  packet[0] = 10;      /* length */
+  packet[1] = BT_MSG_BEGIN_INQUIRY;    /* begin inquiry */
+  packet[2] = max_devices;
+  packet[3] = 0;       /* timeout (hi) */
+  packet[4] = timeout; /* timeout (lo) */
+
+  for (i = 0 ; i < 4 ; i++)
+    packet[5+i] = bt_remote_class[i];
+
+  bt_set_checksum(packet+1, 10);
+
+  uart_write(packet, 19);
+
+  while(uart_is_writing()); /* to avoid some stack issues */
+
+  bt_state.state = BT_STATE_INQUIRING;
+}
+
+static void bt_reseted()
+{
+  bt_state.first_stop = FALSE;
+  bt_state.state = BT_STATE_WAITING;
+}
+
+
 
 static void bt_uart_callback(U8 *msg, U8 len)
 {
+  int i;
+
   /* we check first the checksum and ignore the message if the checksum is invalid */
-  if (!bt_check_checksum(msg, len))
+  if (len < 1 || !bt_check_checksum(msg, len))
     return;
 
-  /* to remove: */
-  if (bt_state.cmd_pos < BT_CMD_BUF) {
-    bt_state.cmds[bt_state.cmd_pos] = msg[0];
-    bt_state.cmd_pos++;
+#ifdef UART_DEBUG
+  if (bt_state.cmds_pos < CMDS_BUFSIZE) {
+    bt_state.cmds[bt_state.cmds_pos] = msg[0];
+    bt_state.cmds_pos++;
   }
+#endif
+
+
+  if (msg[0] == BT_MSG_HEARTBEAT) {
+    bt_state.last_heartbeat = systick_get_ms();
+    return;
+  }
+
+  if (msg[0] == BT_MSG_INQUIRY_RUNNING) {
+    bt_state.state = BT_STATE_INQUIRING;
+    return;
+  }
+
+  if (msg[0] == BT_MSG_INQUIRY_RESULT) {
+    /* we've found a device => we extract the fields */
+
+    for (i = 0 ; i < BT_ADDR_SIZE ; i++)
+      bt_state.bt_remote_device.addr[i] = msg[1+i];
+
+    for (i = 0 ; i < BT_NAME_MAX_LNG ; i++)
+      bt_state.bt_remote_device.name[i] = msg[1+7+i];
+    bt_state.bt_remote_device.name[BT_NAME_MAX_LNG+1] = '\0';
+
+    for (i = 0 ; i < BT_CLASS_SIZE ; i++)
+      bt_state.bt_remote_device.class[i] = msg[1+7+16+i];
+
+    bt_state.bt_remote_id++;
+    return;
+  }
+
+  if (msg[0] == BT_MSG_RESET_INDICATION) {
+    bt_reseted();
+    return;
+  }
+
+  if (msg[0] == BT_MSG_INQUIRY_STOPPED) {
+    if (!bt_state.first_stop)
+      bt_state.first_stop = TRUE;
+    else
+      bt_reseted();
+    return;
+  }
+
 }
+
 
 
 
@@ -171,12 +334,12 @@ void bt_set_friendly_name(char *name)
   U8 packet[20] = { 0 };
 
   packet[0] = 19; /* length */
-  packet[1] = 0x21; /* set friendly name */
+  packet[1] = BT_MSG_SET_FRIENDLY_NAME;
 
   for (i = 0 ; i < 16 && name[i] != '\0' ; i++)
     packet[i+2] = name[i];
 
-  bt_set_checksum(packet, 19);
+  bt_set_checksum(packet+1, 19);
 
   uart_write(packet, 19);
 
@@ -193,11 +356,45 @@ void bt_set_discoverable(bool d)
 }
 
 
-/* TO REMOVE : */
 
+
+void bt_inquiry(bt_inquiry_callback_t callback,
+                U8 max_devices,
+                U8 timeout,
+                U8 bt_remote_class[4])
+{
+  U8 last_id = 0;
+  U32 start_time;
+
+  if (timeout < 1 || timeout > 0x30)
+    return;
+
+  bt_begin_inquiry(max_devices, timeout, bt_remote_class);
+
+  start_time = systick_get_ms();
+
+  while(bt_state.state == BT_STATE_INQUIRING)
+    {
+      if (last_id != bt_state.bt_remote_id)
+        {
+          last_id = bt_state.bt_remote_id;
+          callback((bt_device_t *)&bt_state.bt_remote_device);
+        }
+
+      if (start_time + (2*timeout*1000) < systick_get_ms()) {
+        /* argh, sometimes went wrong */
+        break;
+      }
+    }
+}
+
+
+
+/* to remove: */
 void bt_debug()
 {
-  USB_SEND("===");
+  display_uint(bt_state.last_heartbeat);
+  display_end_line();
   USB_SEND((char *)bt_state.cmds);
 }
 
--- drivers/bt.h
+++ drivers/bt.h
@@ -5,6 +5,17 @@
 
 #include "base/drivers/uart.h"
 
+#define BT_ADDR_SIZE  7
+#define BT_CLASS_SIZE 4
+#define BT_NAME_MAX_LNG 16
+
+typedef struct bt_device {
+  U8 addr[BT_ADDR_SIZE];
+  char name[17];
+  U8 class[BT_CLASS_SIZE];
+} bt_device_t;
+
+
 /*
  * It will only initialize the communication with the bluetooth
  * coprocessor.
@@ -16,6 +27,15 @@ void bt_init();
  */
 void bt_set_friendly_name(char *name);
 void bt_set_discoverable(bool d);
+
+
+typedef void (*bt_inquiry_callback_t)(bt_device_t *remote);
+
+/* timeout unit: 1.28s ; min : 0x01 (1.28s) ; max: 0x30 (61.44s) */
+void bt_inquiry(bt_inquiry_callback_t callback,
+                U8 max_devices,
+                U8 timeout,
+                U8 bt_remote_class[4]);
 
 /* to remove */
 void bt_debug();
