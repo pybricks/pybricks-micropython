dd991b1b1ce0d291c1e3a6d3f66f466469f1f141
David Anderson <dave@natulte.net>
2007-10-31 15:36:52 +0100

    Add a structure declaration to ease access to saved task state.

--- interrupts.h
+++ interrupts.h
@@ -12,4 +12,37 @@ void default_irq();
 void default_fiq();
 void spurious_irq();
 
+/* The following structure describes the order in which registers are
+ * pushed on the system stack when the IRQ handler interrupts a
+ * user/system mode task. If you map a pointer to this structure on the
+ * stack pointer during an interrupt, you will get all the registers
+ * mapped properly in the struct.
+ *
+ * Yes, the structure is messy and disorganized, but the order is a
+ * direct result of the order in which the IRQ handler has to push
+ * things.
+ *
+ * This is not used in the base code, but is very useful if you want to
+ * implement a scheduler that pokes around in task states.
+ */
+struct task_state {
+  U32 cpsr;
+  U32 pc;
+  U32 spsr;
+  U32 r0;
+  U32 r1;
+  U32 r2;
+  U32 r3;
+  U32 r4;
+  U32 r5;
+  U32 r6;
+  U32 r7;
+  U32 r8;
+  U32 r9;
+  U32 r10;
+  U32 r11;
+  U32 r12;
+  U32 lr;
+};
+
 #endif
