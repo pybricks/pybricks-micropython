896f9b06e60652e37286e993aac1fad6129c3bd8
David Anderson <dave@natulte.net>
2007-11-04 03:42:33 +0100

    Make base/util.[ch] conform to the nascent style guidelines.
    The documentation is maybe a little much for such trivial functions, but this
    component should set an example for styling the rest of the code.

--- util.c
+++ util.c
@@ -1,7 +1,9 @@
-/* Various utility functions.
+/* Copyright (C) 2007 the NxOS developers
  *
- * Most of these functions are defined in the libc. However, pulling
- * the libc in is quite a big hit on kernel size in some cases.
+ * See AUTHORS for a full list of the developers.
+ *
+ * Redistribution of this file is permitted under
+ * the terms of the GNU Public License (GPL) version 2.
  */
 
 #include "base/types.h"
@@ -22,9 +24,8 @@ void memset(void *dest, const U8 val, U32 len) {
   }
 }
 
-
-U16 strlen(const char *str) {
-  U16 i = 0;
+U32 strlen(const char *str) {
+  U32 i = 0;
 
   while (*str++)
     i++;
@@ -32,8 +33,8 @@ U16 strlen(const char *str) {
   return i;
 }
 
-U8 strncmp(const char *a, const char *b, U32 n) {
-  U8 i;
+U32 strncmp(const char *a, const char *b, U32 n) {
+  U32 i;
 
   for (i=0 ; i<n ; i++) {
     if (a[i] < b[i]) {
@@ -46,6 +47,6 @@ U8 strncmp(const char *a, const char *b, U32 n) {
   return 0;
 }
 
-U8 strcmp(const char *a, const char *b) {
-  return strncmp(a, b, MIN(strlen(a), strlen(b)));
+U32 strcmp(const char *a, const char *b) {
+  return strncmp(a, b, MIN(strlen(a), strlen(b)+1));
 }
