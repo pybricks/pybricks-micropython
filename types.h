fba993bc3e2714260b0581703ec587c8c9d5faf1
David Anderson <dave@natulte.net>
2007-11-05 15:53:33 +0100

    Document base/types.h.

--- types.h
+++ types.h
@@ -1,23 +1,37 @@
-#ifndef __NXOS_MYTYPES_H__
-#define __NXOS_MYTYPES_H__
+/** @file types.h
+ *  @brief Basic type definitions for the Arm7 platform
+ */
 
-typedef unsigned char U8;
-typedef signed char S8;
-typedef unsigned short U16;
-typedef signed short S16;
-typedef unsigned long U32;
-typedef signed long S32;
+/* Copyright (C) 2007 the NxOS developers
+ *
+ * See AUTHORS for a full list of the developers.
+ *
+ * Redistribution of this file is permitted under
+ * the terms of the GNU Public License (GPL) version 2.
+ */
 
-typedef U32 size_t;
-typedef U8 bool;
+#ifndef __NXOS_BASE_TYPES_H__
+#define __NXOS_BASE_TYPES_H__
 
-#define FALSE 0
-#define TRUE (!FALSE)
+typedef unsigned char U8; /**< Unsigned 8-bit integer. */
+typedef signed char S8; /**< Signed 8-bit integer. */
+typedef unsigned short U16; /**< Unsigned 16-bit integer. */
+typedef signed short S16; /**< Signed 16-bit integer. */
+typedef unsigned long U32; /**< Unsigned 32-bit integer. */
+typedef signed long S32; /**< Signed 32-bit integer. */
+
+typedef U32 size_t; /**< Abstract size type, needed by the memory allocator. */
+
+typedef U8 bool; /**< Boolean data type. */
+#define FALSE 0 /**< False boolean value. */
+#define TRUE (!FALSE) /**< True boolean value. */
 
 #ifndef NULL
-#define NULL 0
+/** Definition of the NULL pointer. */
+#define NULL ((void*)0)
 #endif
 
+/** A function that takes no arguments and returns nothing. */
 typedef void (*nx_closure_t)(void);
 
 #endif
