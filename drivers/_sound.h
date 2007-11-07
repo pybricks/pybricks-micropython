814b77d5cb9299fcd09c356942a4517a8f1697fd
David Anderson <dave@natulte.net>
2007-11-07 21:45:39 +0100

    Document _sound.h according to the style guidelines.

--- drivers/_sound.h
+++ drivers/_sound.h
@@ -1,8 +1,30 @@
+/** @file _sound.h
+ *  @brief Sound system internal interface.
+ */
+
+/* Copyright (C) 2007 the NxOS developers
+ *
+ * See AUTHORS for a full list of the developers.
+ *
+ * Redistribution of this file is permitted under
+ * the terms of the GNU Public License (GPL) version 2.
+ */
+
 #ifndef __NXOS_BASE_DRIVERS__SOUND_H__
 #define __NXOS_BASE_DRIVERS__SOUND_H__
 
 #include "base/drivers/sound.h"
 
+/** @addtogroup driverinternal */
+/*@{*/
+
+/** @defgroup soundinternal Sound */
+/*@{*/
+
+/** Initialize the sound driver. */
 void nx__sound_init();
 
-#endif
+/*@}*/
+/*@}*/
+
+#endif /* __NXOS_BASE_DRIVERS__SOUND_H__ */
