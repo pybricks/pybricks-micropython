cd270ff9a0f4ee2b1ae383942b0dcbcdc8259b79
David Anderson <dave@natulte.net>
2007-11-07 22:41:15 +0100

    Document _sensors.h according to the style guidelines.

--- drivers/_sensors.h
+++ drivers/_sensors.h
@@ -1,8 +1,30 @@
+/** @file _sensors.h
+ *  @brief Sensor internal interface.
+ */
+
+/* Copyright (C) 2007 the NxOS developers
+ *
+ * See AUTHORS for a full list of the developers.
+ *
+ * Redistribution of this file is permitted under
+ * the terms of the GNU Public License (GPL) version 2.
+ */
+
 #ifndef __NXOS_BASE_DRIVERS__SENSORS_H__
 #define __NXOS_BASE_DRIVERS__SENSORS_H__
 
 #include "base/drivers/sensors.h"
 
+/** @addtogroup driverinternal */
+/*@{*/
+
+/** @defgroup sensorsinternal Sensor interface */
+/*@{*/
+
+/** Initialize the sensors driver. */
 void nx__sensors_init();
 
-#endif
+/*@}*/
+/*@}*/
+
+#endif /* __NXOS_BASE_DRIVERS__SENSORS_H__ */
