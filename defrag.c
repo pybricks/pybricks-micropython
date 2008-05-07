1ab9c3a2f546d96679ff8b1a24f29cfc4c8d6279
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2008-05-07 10:57:48 +0200

    Stub for the defrag feature.

---
+++ defrag.c
@@ -0,0 +1,28 @@
+/* Copyright (C) 2008 the NxOS developers
+ *
+ * See AUTHORS for a full list of the developers.
+ *
+ * Redistribution of this file is permitted under
+ * the terms of the GNU Public License (GPL) version 2.
+ */
+
+#include "base/at91sam7s256.h"
+
+#include "base/types.h"
+#include "base/nxt.h"
+#include "base/interrupts.h"
+#include "base/assert.h"
+#include "base/util.h"
+
+static defrag_err_t nx_defrag_move_region(U32 source, U32 dest, U32 len) {
+}
+
+defrag_err_t nx_defrag_simple(void) {
+}
+
+defrag_err_t nx_defrag_for_file_by_name(char *name) {
+  
+}
+
+defrag_err_t nx_defrag_for_file_by_origin(U32 origin) {
+}
