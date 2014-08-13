cetic/contiki

Drop packet if there are not enough free buffers to perform fragmentation

core/lib/memb.h
@@ -130,6 +130,7 @@ char  memb_free(struct memb *m, void *ptr);
 
 int memb_inmemb(struct memb *m, void *ptr);
 
+int  memb_count(struct memb *m);
 
 /** @} */
 /** @} */

pybricks/pybricks-micropython

memb.h
@@ -130,6 +130,7 @@ char  memb_free(struct memb *m, void *ptr);
 
 int memb_inmemb(struct memb *m, void *ptr);
 
+int  memb_numfree(struct memb *m);
 
 /** @} */
 /** @} */