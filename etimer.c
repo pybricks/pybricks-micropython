cmorty/contiki

Add function etimer_reset_set. This new function is similar to reset, but allows to also set a new timeout. Thus long-term accuracy with changing timeouts is now possible.

core/sys/etimer.c
@@ -180,6 +180,16 @@ etimer_set(struct etimer *et, clock_time_t interval)
   add_timer(et);
 }
 /*---------------------------------------------------------------------------*/
+
+void
+etimer_reset_set(struct etimer *et, clock_time_t interval)
+{
+  timer_reset(&et->timer);
+  et->timer.interval = interval;
+  add_timer(et);
+}
+/*---------------------------------------------------------------------------*/
+
 void
 etimer_reset(struct etimer *et)
 {

pybricks/pybricks-micropython

etimer.c
@@ -181,6 +181,14 @@ etimer_set(struct etimer *et, clock_time_t interval)
 }
 /*---------------------------------------------------------------------------*/
 void
+etimer_reset_with_new_interval(struct etimer *et, clock_time_t interval)
+{
+  timer_reset(&et->timer);
+  et->timer.interval = interval;
+  add_timer(et);
+}
+/*---------------------------------------------------------------------------*/
+void
 etimer_reset(struct etimer *et)
 {
   timer_reset(&et->timer);