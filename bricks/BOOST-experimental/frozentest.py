824c732f9324aea74d8dc475d4126d576f007409
Laurens Valk <laurens@pybricks.com>
2018-08-09 20:35:45 +0200

    Use lists of pin numbers in pin test functions

--- bricks/BOOST-experimental/frozentest.py
+++ bricks/BOOST-experimental/frozentest.py
@@ -1,26 +1,27 @@
 from mymodule import gpios
 
-def gpio_init_in(port, pin):
-    gpios(256*0+(ord(port)-65)*16+pin)
+def gpio_init_in_up(port, pins):
+    for pin in pins:
+        gpios(256*0+(ord(port)-65)*16+pin) 
 
-def gpio_init_out(port, pin):
-    gpios(256*1+(ord(port)-65)*16+pin)    
+def gpio_init_out(port, pins):
+    for pin in pins:
+        gpios(256*1+(ord(port)-65)*16+pin)    
 
-def gpio_read(port, pin):
-    return gpios(256*2+(ord(port)-65)*16+pin)        
+def gpio_read(port, pins):
+    return [gpios(256*2+(ord(port)-65)*16+pin) for pin in pins]
 
-def gpio_low(port, pin):
-    gpios(256*3+(ord(port)-65)*16+pin)    
+def gpio_low(port, pins):
+    for pin in pins:
+        gpios(256*3+(ord(port)-65)*16+pin)    
 
-def gpio_high(port, pin):
-    gpios(256*4+(ord(port)-65)*16+pin)
+def gpio_high(port, pins):
+    for pin in pins:
+        gpios(256*4+(ord(port)-65)*16+pin)
 
-def all_in(port):
-    for i in range(16):
-        gpio_init_in(port, i)
-
-def read_all(port):
-    return [gpio_read(port, pin) for pin in range(16)]
+def gpio_init_in_down(port, pins):
+    for pin in pins:
+        gpios(256*5+(ord(port)-65)*16+pin)   
 
 def off():
     gpio_low('B',11)
