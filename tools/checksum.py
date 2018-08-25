bdbee91fff57649749e4ebc52595d18f5930df6f
David Lechner <david@pybricks.com>
2018-08-25 11:47:31 -0500

    MOVEHUB: integrate checksum into build process

---
+++ tools/checksum.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""Calculate checksum of firmware file in the same way that the bootloader on
+the MOVEHUB does."""
+
+from __future__ import print_function
+
+import struct
+import sys
+
+# TODO: probably want to pass this as argument
+MAX_SIZE = 106 * 1024
+
+if len(sys.argv) < 2:
+    print("Missing arguments", file=sys.stderr)
+    sys.exit(1)
+
+checksum = 0
+size = 0
+
+with open(sys.argv[1], 'rb') as f:
+    while True:
+        word = f.read(4)
+        if not word:
+            break
+        checksum += struct.unpack('I', word)[0]
+        size += 4
+
+if size > MAX_SIZE:
+    print("File is too large", file=sys.stderr)
+    sys.exit(1)
+
+for _ in range(size, MAX_SIZE, 4):
+    checksum += 0xffffffff
+
+checksum &= 0xffffffff
+correction = checksum and (1 << 32) - checksum or 0
+
+print(hex(correction))
