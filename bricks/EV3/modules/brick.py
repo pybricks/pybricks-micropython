8e85c1d29f02d15feed5308729d50f7bc624e0af
Laurens Valk <laurens@pybricks.com>
2018-08-30 14:16:46 +0200

    EV3: Device modules

---
+++ bricks/EV3/modules/brick.py
@@ -0,0 +1,14 @@
+"""LEGO MINDSTORMS EV3 Programmable Brick.
+
+Contained in set:
+31313: LEGO MINDSTORMS EV3 (2013)
+45544: LEGO MINDSTORMS Education EV3 Core Set (2013)
+45500: Separate part (2013)
+
+LEGO ID: 95646
+"""
+
+
+def beep():
+    """Make a beep sound."""
+    pass
---
+++ bricks/EV3/modules/ev3devices.py
@@ -0,0 +1,127 @@
+"""Classes for LEGO MINDSTORMS EV3 Devices."""
+
+from motor import EncodedMotor
+
+
+class LargeMotor(EncodedMotor):
+    """LEGO MINDSTORMS EV3 Large Motor.
+
+    Contained in set:
+    31313: LEGO MINDSTORMS EV3 (2013)
+    45544: LEGO MINDSTORMS Education EV3 Core Set (2013)
+    45502: Separate part (2013)
+
+    LEGO ID: 95658/6148278
+
+    Compatible with:
+    Pybricks for LEGO MINDSTORMS NXT
+    Pybricks for LEGO MINDSTORMS EV3
+    """
+
+    pass
+
+
+class MediumMotor(EncodedMotor):
+    """LEGO MINDSTORMS EV3 Medium Motor.
+
+    Contained in set:
+    31313: LEGO MINDSTORMS EV3 (2013)
+    45544: LEGO MINDSTORMS Education EV3 Core Set (2013)
+    45502: Separate part (2013)
+
+    LEGO ID: 99455/6148292
+
+    Compatible with:
+    Pybricks for LEGO MINDSTORMS NXT
+    Pybricks for LEGO MINDSTORMS EV3
+    """
+
+    pass
+
+
+class AnalogSensor():
+    """Generic class for LEGO MINDSTORMS NXT Analog Sensors. Serves as base class for several LEGO MINDSTORMS EV3 Sensors."""
+
+    pass
+
+
+class TouchSensor():
+    """LEGO MINDSTORMS EV3 Touch Sensor.
+
+    Contained in set:
+    31313: LEGO MINDSTORMS EV3 (2013)
+    45544: LEGO MINDSTORMS Education EV3 Core Set (2013)
+    45507: Separate part (2013)
+
+    LEGO ID: 95648/6138404
+
+    Compatible with:
+    Pybricks for LEGO MINDSTORMS EV3
+    """
+
+    pass
+
+
+class ColorSensor():
+    """LEGO MINDSTORMS EV3 Color Sensor.
+
+    Contained in set:
+    31313: LEGO MINDSTORMS EV3 (2013)
+    45544: LEGO MINDSTORMS Education EV3 Core Set (2013)
+    45506: Separate part (2013)
+
+    LEGO ID: 95650/6128869
+
+    Compatible with:
+    Pybricks for LEGO MINDSTORMS EV3
+    """
+
+    pass
+
+
+class InfraredSensor():
+    """LEGO MINDSTORMS EV3 Infrared Sensor and Beacon.
+
+    Contained in set:
+    31313: LEGO MINDSTORMS EV3 (2013)
+    45509 and 45508: Separate parts (2013)
+
+    LEGO ID: 95654/6132629 and 72156/6127283
+
+    Compatible with:
+    Pybricks for LEGO MINDSTORMS EV3
+    """
+
+    pass
+
+
+class GyroSensor():
+    """LEGO MINDSTORMS EV3 Gyro Sensor.
+
+    Contained in set:
+    45544: LEGO MINDSTORMS Education EV3 Core Set (2013)
+    45505: Separate part (2013)
+
+    LEGO ID: 99380/6138411
+
+    Compatible with:
+    Pybricks for LEGO MINDSTORMS EV3
+    """
+
+    pass
+
+
+class UltrasonicSensor():
+    """LEGO MINDSTORMS EV3 Ultrasonic Sensor.
+
+    Contained in set:
+    45544: LEGO MINDSTORMS Education EV3 Core Set (2013)
+    45504: Separate part (2013)
+
+    LEGO ID: 95652/6138403
+
+    Compatible with:
+    Pybricks for LEGO MINDSTORMS EV3
+    """
+
+    pass
