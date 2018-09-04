f09545255905c1f82be1bc3333e70ecf275ff5e7
Laurens Valk <laurens@pybricks.com>
2018-09-04 15:56:01 +0200

    Drivers: begin cleaning modmotor

--- drivers/modmotor.c
+++ drivers/modmotor.c
@@ -1,41 +1,11 @@
 #include "modmotor.h"
-int global_test;
-
-/*-----------------DcMotor Class----------------------*/
-
 
 //
-// DcMotor Class: new object and self print function
+// DcMotor class methods
 //
 
-// C-structure for object
-typedef struct _motor_DcMotor_obj_t {
-    mp_obj_base_t base;
-    // Port
-    uint8_t DcMotor_port;
-    // Reverse
-    bool DcMotor_inverted;
-} motor_DcMotor_obj_t;
 
-STATIC void motor_DcMotor_print( const mp_print_t *print,
-                                  mp_obj_t self_in,
-                                  mp_print_kind_t kind ) {
-    // get a pointer to self
-    motor_DcMotor_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    // print the attributes
-    printf("Port: %c\n", self->DcMotor_port);
-    printf("Positive speed means: ");
-    if(!self->DcMotor_inverted){
-        printf("counter");
-    }
-    printf("clockwise");
-    
-}
-
-mp_obj_t motor_DcMotor_make_new( const mp_obj_type_t *type,
-                                  size_t n_args,
-                                  size_t n_kw,
-                                  const mp_obj_t *args ) {
+mp_obj_t motor_DcMotor_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args ) {
     // check the number of argument
     mp_arg_check_num(n_args, n_kw, 1, 2, false);
     // create a new C-struct type object
@@ -43,21 +13,31 @@ mp_obj_t motor_DcMotor_make_new( const mp_obj_type_t *type,
     // give it a type
     self->base.type = &motor_DcMotor_type;
     // set the member number with the first argument of the constructor
-    self->DcMotor_port = mp_obj_get_int(args[0]); // TODO: Check valid range using mp_arg_parse_all
+    self->port = mp_obj_get_int(args[0]); // TODO: Check valid range using mp_arg_parse_all
     // set the inverted member if given
-    self->DcMotor_inverted = (n_args == 2) ? mp_obj_is_true(args[1]) : false;
+    self->inverted = (n_args == 2) ? mp_obj_is_true(args[1]) : false;
     // Apply settings to the motor
-    pbio_motor_set_direction(self->DcMotor_port, self->DcMotor_inverted);
+    pbio_motor_set_direction(self->port, self->inverted);
     return MP_OBJ_FROM_PTR(self);
 }
 
-//
-// DcMotor Class: Methods
-//
+STATIC void motor_DcMotor_print(const mp_print_t *print,  mp_obj_t self_in, mp_print_kind_t kind ) {
+    motor_DcMotor_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    // Print port
+    printf("Port: %c\n", self->port);
+    // Print inverted (yes or no)
+    printf("Inverted: ");
+    if (self->inverted) {
+        printf("yes");
+    }
+    else {
+        printf("no");
+    }
+}
 
 STATIC mp_obj_t motor_DcMotor_coast(mp_obj_t self_in) {
     motor_DcMotor_obj_t *self = MP_OBJ_TO_PTR(self_in);    
-    pbio_motor_coast(self->DcMotor_port);
+    pbio_motor_coast(self->port);
     return mp_const_none;
 }
 MP_DEFINE_CONST_FUN_OBJ_1(motor_DcMotor_coast_obj,
@@ -65,24 +45,22 @@ MP_DEFINE_CONST_FUN_OBJ_1(motor_DcMotor_coast_obj,
 
 STATIC mp_obj_t motor_DcMotor_brake(mp_obj_t self_in) {
     motor_DcMotor_obj_t *self = MP_OBJ_TO_PTR(self_in);    
-    pbio_motor_set_duty_cycle(self->DcMotor_port, 0);
+    pbio_motor_set_duty_cycle(self->port, 0);
     return mp_const_none;
 }
-MP_DEFINE_CONST_FUN_OBJ_1(motor_DcMotor_brake_obj,
-                          motor_DcMotor_brake);
+MP_DEFINE_CONST_FUN_OBJ_1(motor_DcMotor_brake_obj, motor_DcMotor_brake);
 
 
 STATIC mp_obj_t motor_DcMotor_duty(mp_obj_t self_in, mp_obj_t duty) {
     motor_DcMotor_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    pbio_motor_set_duty_cycle(self->DcMotor_port, (int) mp_obj_get_float(duty));
+    pbio_motor_set_duty_cycle(self->port, (int) mp_obj_get_float(duty));
     return mp_const_none;
 }
-MP_DEFINE_CONST_FUN_OBJ_2(motor_DcMotor_duty_obj,
-                          motor_DcMotor_duty);
+MP_DEFINE_CONST_FUN_OBJ_2(motor_DcMotor_duty_obj, motor_DcMotor_duty);
 
 
 //
-// DcMotor Class: Tables
+// DcMotor class tables
 //
 
 // creating the table of members
@@ -92,8 +70,7 @@ STATIC const mp_rom_map_elem_t motor_DcMotor_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_duty), MP_ROM_PTR(&motor_DcMotor_duty_obj) },
 };
 
-STATIC MP_DEFINE_CONST_DICT(motor_DcMotor_locals_dict,
-                            motor_DcMotor_locals_dict_table);
+STATIC MP_DEFINE_CONST_DICT(motor_DcMotor_locals_dict, motor_DcMotor_locals_dict_table);
 
 // create the class-object itself
 const mp_obj_type_t motor_DcMotor_type = {
@@ -110,27 +87,11 @@ const mp_obj_type_t motor_DcMotor_type = {
 };
 
 
-/*--------------EncodedMotor Class--------------------*/
-
-
-
 //
-// EncodedMotor Class: new object and self print function
+// EncodedMotor class methods
 //
 
-// C-structure for object
-typedef struct _motor_EncodedMotor_obj_t {
-    mp_obj_base_t base;
-    // Port
-    uint8_t EncodedMotor_port;
-    // Reverse
-    bool EncodedMotor_inverted;
-} motor_EncodedMotor_obj_t;
-
-mp_obj_t motor_EncodedMotor_make_new( const mp_obj_type_t *type,
-                                  size_t n_args,
-                                  size_t n_kw,
-                                  const mp_obj_t *args ) {
+mp_obj_t motor_EncodedMotor_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args){
     // check the number of argument
     mp_arg_check_num(n_args, n_kw, 1, 2, false);
     // create a new C-struct type object
@@ -138,39 +99,35 @@ mp_obj_t motor_EncodedMotor_make_new( const mp_obj_type_t *type,
     // give it a type
     self->base.type = &motor_EncodedMotor_type;
     // set the member number with the first argument of the constructor
-    self->EncodedMotor_port = mp_obj_get_int(args[0]); // TODO: Check valid range using mp_arg_parse_all
+    self->port = mp_obj_get_int(args[0]); // TODO: Check valid range using mp_arg_parse_all
     // set the inverted member if given
-    self->EncodedMotor_inverted = (n_args == 2) ? mp_obj_is_true(args[1]) : false;
+    self->inverted = (n_args == 2) ? mp_obj_is_true(args[1]) : false;
     // Apply settings to the motor
-    pbio_motor_set_direction(self->EncodedMotor_port, self->EncodedMotor_inverted);
+    pbio_motor_set_direction(self->port, self->inverted);
     return MP_OBJ_FROM_PTR(self);
 }
 
-STATIC void motor_EncodedMotor_print( const mp_print_t *print,
-                                  mp_obj_t self_in,
-                                  mp_print_kind_t kind ) {
+STATIC void motor_EncodedMotor_print(const mp_print_t *print,  mp_obj_t self_in, mp_print_kind_t kind){
     // get a pointer to self
     motor_DcMotor_obj_t *self = MP_OBJ_TO_PTR(self_in);
     // print the DC attributes
     motor_DcMotor_print(print, self_in, kind);
     // Print other attributes
     printf("\nOther stuff\n");
-    printf("Port: %c", self->DcMotor_port);
+    printf("Port: %c", self->port);
 }
 
 //
-// EncodedMotor Class: Tables and  class object
+// EncodedMotor class tables
 //
 
-// creating the table of members
 STATIC const mp_rom_map_elem_t motor_EncodedMotor_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_coast), MP_ROM_PTR(&motor_DcMotor_coast_obj) },
     { MP_ROM_QSTR(MP_QSTR_brake), MP_ROM_PTR(&motor_DcMotor_brake_obj) },
     { MP_ROM_QSTR(MP_QSTR_duty), MP_ROM_PTR(&motor_DcMotor_duty_obj) },
 };
 
-STATIC MP_DEFINE_CONST_DICT(motor_EncodedMotor_locals_dict,
-                            motor_EncodedMotor_locals_dict_table);
+STATIC MP_DEFINE_CONST_DICT(motor_EncodedMotor_locals_dict, motor_EncodedMotor_locals_dict_table);
 
 const mp_obj_type_t motor_EncodedMotor_type = {
     { &mp_type_type },
@@ -182,10 +139,8 @@ const mp_obj_type_t motor_EncodedMotor_type = {
     .locals_dict = (mp_obj_dict_t*)&motor_EncodedMotor_locals_dict,
 };
 
-/*-----------------Motor Module-----------------------*/
-
 //
-// Motor Module tables
+// Motor Module tables: put the above classes in a module
 //
 
 STATIC const mp_map_elem_t motor_globals_table[] = {
@@ -194,10 +149,7 @@ STATIC const mp_map_elem_t motor_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR_EncodedMotor), (mp_obj_t)&motor_EncodedMotor_type},
 };
 
-STATIC MP_DEFINE_CONST_DICT (
-    mp_module_motor_globals,
-    motor_globals_table
-);
+STATIC MP_DEFINE_CONST_DICT (mp_module_motor_globals, motor_globals_table);
 
 const mp_obj_module_t mp_module_motor = {
     .base = { &mp_type_module },
--- drivers/modmotor.h
+++ drivers/modmotor.h
@@ -7,5 +7,19 @@
 #include <pbio/motorcontrol.h>
 #include <pbio/motor.h>
 
+// Class structure for DC Motors
+typedef struct _motor_DcMotor_obj_t {
+    mp_obj_base_t base;
+    pbio_port_t port;
+    pbio_motor_dir_t inverted;
+} motor_DcMotor_obj_t;
+
+// Class structure for Encoded Motors
+typedef struct _motor_EncodedMotor_obj_t {
+    mp_obj_base_t base;
+    uint8_t port;
+    pbio_motor_dir_t inverted;
+} motor_EncodedMotor_obj_t;
+
 extern const mp_obj_type_t motor_DcMotor_type;
 extern const mp_obj_type_t motor_EncodedMotor_type;
