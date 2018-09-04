a214d0350e5c1d25a9e6328fd7d46c2b503ffe0f
David Lechner <david@pybricks.com>
2018-09-04 11:12:31 -0500

    MOVEHUB: implement keyboard interrupt on UART REPL

--- bricks/MOVEHUB/mpconfigport.h
+++ bricks/MOVEHUB/mpconfigport.h
@@ -55,6 +55,9 @@
 #define MICROPY_CPYTHON_COMPAT      (0)
 #define MICROPY_LONGINT_IMPL        (MICROPY_LONGINT_IMPL_NONE)
 #define MICROPY_FLOAT_IMPL          (MICROPY_FLOAT_IMPL_FLOAT)
+#define MICROPY_KBD_EXCEPTION       (1)
+#define MICROPY_ENABLE_SCHEDULER    (1)
+
 
 // type definitions for the specific machine
 
--- bricks/MOVEHUB/uart_core.c
+++ bricks/MOVEHUB/uart_core.c
@@ -1,9 +1,37 @@
 #include <unistd.h>
 #include "py/mpconfig.h"
+#include "py/runtime.h"
+#include "lib/utils/interrupt_char.h"
 
 #include "stm32f070xb.h"
 
 #include "uartadr.h"
+
+#define RX_BUF_SIZE 32  // must be power of 2!
+
+static uint8_t rx_buf[RX_BUF_SIZE]; 
+static volatile uint8_t rx_buf_head;
+static volatile uint8_t rx_buf_tail;
+
+void USART3_6_IRQHandler(void) {
+    uint8_t c;
+
+    while (USART_REPL->ISR & USART_ISR_RXNE) {
+        c = USART_REPL->RDR;
+        if (c == mp_interrupt_char) {
+            mp_keyboard_interrupt();
+        } else {
+            uint8_t new_head = (rx_buf_head + 1) & (RX_BUF_SIZE - 1);
+            if (new_head == rx_buf_tail) {
+                // buffer overrun
+                // REVISIT: ignoring for now - will lose characters
+                return;
+            }
+            rx_buf[new_head] = c;
+            rx_buf_head = new_head;
+        }
+    }
+}
 
 /*
  * Core UART functions to implement for a port
@@ -11,12 +39,16 @@
 
 // Receive single character
 int mp_hal_stdin_rx_chr(void) {
-    unsigned char c = 0;
+    uint8_t c;
 
-    // wait for RXNE
-    while ((USART_REPL->ISR & USART_ISR_RXNE) == 0) {
+    // wait for rx interrupt
+    while (rx_buf_head == rx_buf_tail) {
+        mp_handle_pending();
+        __WFI();
     }
-    c = USART_REPL->RDR;
+
+    rx_buf_tail = (rx_buf_tail + 1) & (RX_BUF_SIZE - 1);
+    c = rx_buf[rx_buf_tail];
 
     return c;
 }
