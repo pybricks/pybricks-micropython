6489106d84b45422ea8c6be772ab4f9f197cc813
Laurens Valk <laurens@pybricks.com>
2018-09-07 19:59:15 +0200

    Drivers: move non-public control settings structure type

--- lib/pbio/include/pbio/motorcontrol.h
+++ lib/pbio/include/pbio/motorcontrol.h
@@ -33,23 +33,6 @@ typedef enum {
     PBIO_MOTOR_WAIT_NONE,       /**< Execute run command in the background and proceed with user program */
 } pbio_motor_wait_t;
 
-/**
- * Control settings for an encoded motor
- */
-typedef struct _pbio_motor_control_settings_t {
-    int16_t counts_per_output_unit; /**< Encoder counts per output unit, including optional gear train (counts per degree for rotational motors, counts per cm for a linear motor) */
-    int16_t max_speed;              /**< Soft limit on the reference speed in all run commands */
-    int16_t tolerance;              /**< Allowed deviation (deg) from target before motion is considered complete */
-    int16_t acceleration_start;     /**< Acceleration when beginning to move. Positive value in degrees per second per second */
-    int16_t acceleration_end;       /**< Deceleration when stopping. Positive value in degrees per second per second */
-    int16_t tight_loop_time_ms;     /**< When a run function is called twice in this interval, assume that the user is doing their own speed control.  */
-    int16_t pid_kp;                 /**< Proportional position control constant (and integral speed control constant) */
-    int16_t pid_ki;                 /**< Integral position control constant */
-    int16_t pid_kd;                 /**< Derivative position control constant (and proportional speed control constant) */
-} pbio_motor_control_settings_t;
-
-pbio_motor_control_settings_t motor_control_settings[PBIO_CONFIG_MAX_MOTORS];
-
 pbio_error_t pbio_motor_control_set_constant_settings(pbio_port_t port, int16_t counts_per_unit, float_t gear_ratio);
 
 pbio_error_t pbio_motor_control_set_variable_settings(pbio_port_t port, int16_t max_speed, int16_t tolerance, int16_t acceleration_start, int16_t acceleration_end, int16_t tight_loop_time_ms, int16_t pid_kp, int16_t pid_ki, int16_t pid_kd);
