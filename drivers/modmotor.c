af096d8e7391a7ed28fc6dbaef47af9d909d2a62
Laurens Valk <laurens@pybricks.com>
2018-09-13 20:33:57 +0200

    MOVEHUB: Put code back to working state.

--- drivers/modmotor.c
+++ drivers/modmotor.c
@@ -444,3 +444,16 @@ const mp_obj_module_t mp_module_motor = {
     .base = { &mp_type_module },
     .globals = (mp_obj_dict_t*)&mp_module_motor_globals,
 };
+
+
+/**** The code below should actually be in motordevices.c, but then it doesn't work. Putting it back here until we figure it out. ****/
+
+const mp_obj_type_id_t motor_MovehubMotor_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_MovehubMotor,
+    .print = motor_DCMotor_print,
+    .make_new = motor_DCMotor_make_new,
+    .parent = &motor_DCMotor_type,
+    .locals_dict = (mp_obj_dict_t*)&motor_DCMotor_locals_dict,
+    .device_id = PBIO_ID_PUP_MOVEHUB_MOTOR,
+};
--- drivers/motordevices.h
+++ drivers/motordevices.h
@@ -1,4 +1,1 @@
 #include "modmotor.h"
-
-const mp_obj_type_id_t motor_MovehubMotor_type;
-const mp_obj_type_id_t motor_External_type;
