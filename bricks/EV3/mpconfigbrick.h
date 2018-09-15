492ce296750f6ab82c2ac51e135fc04f0dde0a29
Laurens Valk <laurens@pybricks.com>
2018-09-15 19:55:16 +0200

    EV3: Revert to plain main.c from unix port. \nMicroPython offers custom de/init mechanism.

--- bricks/EV3/mpconfigbrick.h
+++ bricks/EV3/mpconfigbrick.h
@@ -1,5 +1,10 @@
+#include "pybricks.h"
+
 #define PYBRICKS_BRICK_EV3
 #define PYBRICKS_BRICK_NAME             "LEGO MINDSTORMS EV3 Intelligent Brick"
+
+#define MICROPY_PORT_INIT_FUNC pybricks_init()
+#define MICROPY_PORT_DEINIT_FUNC pybricks_deinit()
 
 extern const struct _mp_obj_module_t mp_module_constants;
 extern const struct _mp_obj_module_t mp_module_ev3devices;
