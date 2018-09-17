b907fc8e9926d4b4ed8129cb39e695009d4640be
Laurens Valk <laurens@pybricks.com>
2018-09-17 17:14:56 +0200

    Drivers: return time directly Fixes comment on f6ac2618733d14e15e6b06ac1607069f3579271d

--- lib/pbio/drv/ev3dev_stretch/time.c
+++ lib/pbio/drv/ev3dev_stretch/time.c
@@ -1,9 +1,8 @@
 #include <pbdrv/time.h>
 #include <time.h>
 
-pbio_error_t pbdrv_get_time_usec(uint32_t *time_now){
+uint32_t pbdrv_get_time_usec(){
     struct timespec time_val;
     clock_gettime(CLOCK_MONOTONIC, &time_val);
-    *time_now = time_val.tv_sec*1000000 + time_val.tv_nsec / 1000;
-    return PBIO_SUCCESS;
+    return (time_val.tv_sec*1000000 + time_val.tv_nsec / 1000);
 }
--- lib/pbio/include/pbdrv/time.h
+++ lib/pbio/include/pbdrv/time.h
@@ -4,9 +4,6 @@
 
 #include <stdint.h>
 
-#include <pbdrv/config.h>
-#include <pbio/error.h>
-
 /**
  * \addtogroup TimeDriver Time I/O driver
  * @{
@@ -16,10 +13,9 @@
  * Gets the time with units of microseconds. The time value has no specific starting point and should only be used to 
  * measure durations of processess less than 2^32 microseconds, or about an hour.
  * @param [in]  time    Pointer to time variable to be set
- * @return              ::PBIO_SUCCESS if the call was successful
- *                      ::PBIO_ERROR_IO if there was an I/O error
+ * @return              ::Time with units of microseconds
  */
-pbio_error_t pbdrv_get_time_usec(uint32_t *time);
+uint32_t pbdrv_get_time_usec();
 
 /** @}*/
 
