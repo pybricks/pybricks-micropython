6bd219431181d1f1bde7a88db477f4817467ac83
Laurens Valk <laurens@pybricks.com>
2018-09-25 10:56:50 +0200

    pbio: add PBIO_ERROR_NOT_SUPPORTED Fixes 02a72d10456cabf9f6ea55a50e5170afac14337c

--- drivers/pberror.c
+++ drivers/pberror.c
@@ -9,7 +9,7 @@ void pb_raise_pbio_error(pbio_error_t error){
             break;
         case PBIO_ERROR_INVALID_ARG:
             mp_raise_ValueError("Invalid argument");
-            break;            
+            break;
         case PBIO_ERROR_INVALID_PORT:
             mp_raise_ValueError("Invalid port");
             break;
@@ -17,13 +17,13 @@ void pb_raise_pbio_error(pbio_error_t error){
             mp_raise_msg(&mp_type_OSError, "I/O error");
             break;
         case PBIO_ERROR_NO_DEV:
-            mp_raise_ValueError("Device is not connected");
+            mp_raise_msg(&mp_type_RuntimeError, "Device is not connected");
             break;
         case PBIO_ERROR_NOT_IMPLEMENTED:
-            mp_raise_ValueError("Requested feature is not (yet) available for this device");
-            break;            
-        default:
-            mp_raise_msg(&mp_type_RuntimeError, "Unknown error");
+            mp_raise_msg(&mp_type_RuntimeError, "This feature is not yet implemented on this device");
             break;
-    }  
+        case PBIO_ERROR_NOT_SUPPORTED:
+            mp_raise_msg(&mp_type_RuntimeError, "This feature is not supported on this device");
+            break;
+    }
 }
