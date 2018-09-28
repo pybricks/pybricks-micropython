451121bc3abe222a596d727156423e035bcd130f
Laurens Valk <laurens@pybricks.com>
2018-09-28 15:22:36 +0200

    MOVEHUB: Position control test

--- bricks/MOVEHUB/frozentest.py
+++ bricks/MOVEHUB/frozentest.py
@@ -1,14 +1,16 @@
-from hub import PORT_B
+from hub import PORT_B, PORT_A
 from hub import MovehubMotor
 # from MovehubMotor import STOP_COAST, STOP_HOLD, STOP_BRAKE, DIR_NORMAL, DIR_INVERTED // This import doesn't work yet
 from time import sleep
 
 motor = MovehubMotor(PORT_B)
-motor.duty(50)
-sleep(0.2)
-print(motor.angle())
 
-motor.run_time(-500, 2, 0, False)
+motor.run_angle(500, 270, 2, False)
+
+sleep(3)
+
+motor.run_target(1000, 0, 0, False)
+
 
 # Wait a while before restarting the script
 sleep(5)
