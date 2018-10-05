02766b5aa4c58cf2ce14b90e831ea1c4129beb18
Laurens Valk <laurens@pybricks.com>
2018-10-05 14:49:11 +0200

    MOVEHUB: change main.py so it can run without motors

--- bricks/MOVEHUB/modules/main.py
+++ bricks/MOVEHUB/modules/main.py
@@ -1,15 +1,7 @@
-from hub import PORT_B, PORT_A
-from hub import MovehubMotor
-
-# from MovehubMotor import STOP_COAST, STOP_HOLD, STOP_BRAKE, DIR_NORMAL, DIR_INVERTED // This import doesn't work yet
 from time import sleep
 
-motor = MovehubMotor(PORT_B)
+print("User script says: started")
 
-motor.run_angle(500, 270, 2, False)
+sleep (3)
 
-sleep(3)
-
-motor.run_target(1000, 0, 0, False)
-
-sleep(3)
+print("User script says: complete")
