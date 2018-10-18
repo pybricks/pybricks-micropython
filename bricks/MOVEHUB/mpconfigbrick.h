540b86144efa56daa2bb07c5f06af7db013e984f
Laurens Valk <laurens@pybricks.com>
2018-10-18 12:11:25 +0200

    MOVEHUB: config for reference fw size Enable motors and floats Disable REPL as size starting reference fw size = 97816

--- bricks/MOVEHUB/mpconfigbrick.h
+++ bricks/MOVEHUB/mpconfigbrick.h
@@ -6,12 +6,12 @@
 // config option to 0.  If you do this then you won't get a REPL prompt, but you
 // will still be able to execute pre-compiled scripts, compiled with mpy-cross.
 // Requires about 19K (19568) of flash
-#define MICROPY_ENABLE_COMPILER         (1)
+#define MICROPY_ENABLE_COMPILER         (0)
 
 // Set to MICROPY_FLOAT_IMPL_FLOAT to enable floating point support in user code or
 // set to MICROPY_FLOAT_IMPL_NONE to disable floating point support in user code
 // Requires about 20K (21312) of flash
-#define MICROPY_FLOAT_IMPL              (MICROPY_FLOAT_IMPL_NONE)
+#define MICROPY_FLOAT_IMPL              (MICROPY_FLOAT_IMPL_FLOAT)
 
 extern const struct _mp_obj_module_t mp_module_hub;
 
