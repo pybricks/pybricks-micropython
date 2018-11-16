6496f0309a83c1e10defad31151a1e28272e24c1
Laurens Valk <laurens@pybricks.com>
2018-11-16 16:57:11 +0100

    EV3: button support

--- bricks/EV3/modev3brick.c
+++ bricks/EV3/modev3brick.c
@@ -1,4 +1,5 @@
 #include <pbio/port.h>
+#include <pbio/button.h>
 
 #include "pbobj.h"
 #include "extmod/utime_mphal.h"
@@ -14,9 +15,32 @@ STATIC const mp_rom_map_elem_t motor_Port_enum_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR_S1),   MP_OBJ_NEW_SMALL_INT(PBIO_PORT_1) },
     { MP_OBJ_NEW_QSTR(MP_QSTR_S2),   MP_OBJ_NEW_SMALL_INT(PBIO_PORT_2) },
     { MP_OBJ_NEW_QSTR(MP_QSTR_S3),   MP_OBJ_NEW_SMALL_INT(PBIO_PORT_3) },
-    { MP_OBJ_NEW_QSTR(MP_QSTR_S4),   MP_OBJ_NEW_SMALL_INT(PBIO_PORT_4) },    
+    { MP_OBJ_NEW_QSTR(MP_QSTR_S4),   MP_OBJ_NEW_SMALL_INT(PBIO_PORT_4) },
 };
 STATIC PB_DEFINE_CONST_ENUM(motor_Port_enum, motor_Port_enum_table);
+
+/* Button enum */
+
+STATIC const mp_rom_map_elem_t ev3brick_button_enum_table[] = {
+    { MP_OBJ_NEW_QSTR(MP_QSTR_up),     MP_OBJ_NEW_SMALL_INT(PBIO_BUTTON_UP)     },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_down),   MP_OBJ_NEW_SMALL_INT(PBIO_BUTTON_DOWN)   },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_left),   MP_OBJ_NEW_SMALL_INT(PBIO_BUTTON_LEFT)   },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_right),  MP_OBJ_NEW_SMALL_INT(PBIO_BUTTON_RIGHT)  },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_center), MP_OBJ_NEW_SMALL_INT(PBIO_BUTTON_CENTER) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_back),   MP_OBJ_NEW_SMALL_INT(PBIO_BUTTON_STOP)   },
+};
+PB_DEFINE_CONST_ENUM(ev3brick_button_enum, ev3brick_button_enum_table);
+
+/* Return a list of pressed buttons (EV3/ev3dev can detect only one button at a time). */
+
+STATIC mp_obj_t ev3brick_buttons(void) {
+    pbio_button_flags_t pressed = 0;
+    pbdrv_button_is_pressed(PBIO_PORT_SELF, &pressed);
+    mp_obj_t button_list[1];
+    button_list[0] = mp_obj_new_int(pressed);
+    return mp_obj_new_list(pressed ? 1 : 0, button_list);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(ev3brick_buttons_obj, ev3brick_buttons);
 
 /* Module table */
 
@@ -24,7 +48,9 @@ STATIC const mp_map_elem_t ev3brick_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_ev3brick_c)   },
     { MP_OBJ_NEW_QSTR(MP_QSTR_Port), (mp_obj_t)&motor_Port_enum },
     { MP_OBJ_NEW_QSTR(MP_QSTR_Color), (mp_obj_t)&pb_Color_enum },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_Button), (mp_obj_t)&ev3brick_button_enum },
     { MP_ROM_QSTR(MP_QSTR_wait), (mp_obj_t)&mp_utime_sleep_ms_obj },
+    { MP_ROM_QSTR(MP_QSTR_buttons), (mp_obj_t)&ev3brick_buttons_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_light), (mp_obj_t)&hub_set_light_obj },
 };
  STATIC MP_DEFINE_CONST_DICT (pb_module_ev3brick_globals, ev3brick_globals_table);
--- lib/pbio/drv/ev3dev_stretch/button.c
+++ lib/pbio/drv/ev3dev_stretch/button.c
@@ -1,17 +1,59 @@
+#include <linux/input.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include <pbio/button.h>
 #include <pbio/error.h>
 #include <pbio/port.h>
 
+#define EV3KEY_UP (103)
+#define EV3KEY_CENTER (28)
+#define EV3KEY_DOWN (108)
+#define EV3KEY_RIGHT (106)
+#define EV3KEY_LEFT (105)
+#define EV3KEY_BACK (14)
+
+uint8_t f_btn; // Button file descriptor
+
 void _pbdrv_button_init(void) {
-    // TODO
+    f_btn = open("/dev/input/by-path/platform-gpio_keys-event", O_RDONLY);
 }
 
 #ifdef PBIO_CONFIG_ENABLE_DEINIT
-void _pbdrv_button_deinit(void) { }
+void _pbdrv_button_deinit(void) {
+    close(f_btn); 
+ }
 #endif
 
+bool check(uint8_t *buffer, uint8_t key){
+    return buffer[key>>3] & (1 << (key % 8));
+}
+
 pbio_error_t pbdrv_button_is_pressed(pbio_port_t port, pbio_button_flags_t *pressed) {
-    // TODO
+    if (port != PBIO_PORT_SELF) {
+        return PBIO_ERROR_INVALID_PORT;
+    }
+
+    uint8_t buffer[(KEY_CNT + 8 - 1 ) / 8];
+    ioctl(f_btn, EVIOCGKEY(sizeof(buffer)), &buffer);
+    
+    if(check(buffer, EV3KEY_UP)) {
+        *pressed = PBIO_BUTTON_UP;
+    }
+    else if(check(buffer, EV3KEY_CENTER)) {
+        *pressed = PBIO_BUTTON_CENTER;
+    }    
+    else if(check(buffer, EV3KEY_DOWN)) {
+        *pressed = PBIO_BUTTON_DOWN;
+    }  
+    else if(check(buffer, EV3KEY_RIGHT)) {
+        *pressed = PBIO_BUTTON_RIGHT;
+    }  
+    else if(check(buffer, EV3KEY_LEFT)) {
+        *pressed = PBIO_BUTTON_LEFT;
+    }  
+    else if(check(buffer, EV3KEY_BACK)) {
+        *pressed = PBIO_BUTTON_STOP;
+    } 
     return PBIO_SUCCESS;
 }
