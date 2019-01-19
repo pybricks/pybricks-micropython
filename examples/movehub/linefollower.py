98dcbe4e3b8b118cf4837647da10740722c542e8
Laurens Valk <laurens@pybricks.com>
2019-01-19 18:15:03 +0100

    doc: remove examples

--- examples/movehub/linefollower.py
+++
@@ -1,8 +0,0 @@
-left = Motor(Port.A)
-right = Motor(Port.B)
-base = DriveBase(left, right)
-sensor = ColorDistanceSensor(Port.D)
-
-while True:
-    steering = ((sensor.reflection() - 45)*2)//3
-    base.drive(60, steering)
