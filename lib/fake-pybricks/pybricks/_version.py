16420353423cca6c59097b726a5b1f0d243a5e42
David Lechner <david@pybricks.com>
2019-01-24 19:51:33 -0600

    fake-pybricks: don't quote strings in setup.cfg
    this is an ini file, not a python file
    
    also run `python3 versioneer.py setup` to apply changes

--- lib/fake-pybricks/pybricks/_version.py
+++ lib/fake-pybricks/pybricks/_version.py
@@ -41,8 +41,8 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "'v'"
-    cfg.parentdir_prefix = "'fake-pybricks-'"
+    cfg.tag_prefix = "v"
+    cfg.parentdir_prefix = "fake-pybricks-"
     cfg.versionfile_source = "pybricks/_version.py"
     cfg.verbose = False
     return cfg
