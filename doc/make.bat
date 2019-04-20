7081aa02eb716f1b327baf803de5b0cbfbff0500
Laurens Valk <laurens@pybricks.com>
2019-04-20 12:22:15 +0200

    doc: windows build script update (untested)

--- doc/make.bat
+++ doc/make.bat
@@ -8,7 +8,7 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set SOURCEDIR=source
-set BUILDDIR=build
+set BUILDDIR=%SOURCEDIR%/build
 set SPHINXPROJ=Pybricks
 
 if "%1" == "" goto help
