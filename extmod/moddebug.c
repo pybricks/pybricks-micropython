f322cfbebd63f223198d815176ed207e58ce0a76
David Lechner <david@pybricks.com>
2019-05-20 17:09:13 -0500

    extmod/moddebug: add function for getting buttons

--- extmod/moddebug.c
+++ extmod/moddebug.c
@@ -6,24 +6,35 @@
 #if PYBRICKS_PY_DEBUG
 
 #include <pbdrv/adc.h>
+#include <pbdrv/button.h>
 
 #include "py/obj.h"
 
 #include "pberror.h"
 
 
-STATIC mp_obj_t hub_read_adc(mp_obj_t ch) {
+STATIC mp_obj_t debug_read_adc(mp_obj_t ch) {
     uint16_t value;
 
     pb_assert(pbdrv_adc_get_ch(mp_obj_get_int(ch), &value));
 
     return mp_obj_new_int(value);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(hub_read_adc_obj, hub_read_adc);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(debug_read_adc_obj, debug_read_adc);
+
+STATIC mp_obj_t debug_read_buttons() {
+    pbio_button_flags_t flags;
+
+    pb_assert(pbdrv_button_is_pressed(PBIO_PORT_SELF, &flags));
+
+    return mp_obj_new_int(flags);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(debug_read_buttons_obj, debug_read_buttons);
 
 STATIC const mp_rom_map_elem_t debug_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___name__),    MP_ROM_QSTR(MP_QSTR_debug)          },
-    { MP_ROM_QSTR(MP_QSTR_read_adc),    MP_ROM_PTR(&hub_read_adc_obj)       },
+    { MP_ROM_QSTR(MP_QSTR___name__),        MP_ROM_QSTR(MP_QSTR_debug)          },
+    { MP_ROM_QSTR(MP_QSTR_read_adc),        MP_ROM_PTR(&debug_read_adc_obj)     },
+    { MP_ROM_QSTR(MP_QSTR_read_buttons),    MP_ROM_PTR(&debug_read_buttons_obj) },
 };
 STATIC MP_DEFINE_CONST_DICT(pb_module_debug_globals, debug_globals_table);
 
