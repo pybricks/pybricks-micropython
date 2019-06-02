01f34f06439070469d0fd916edfd6bdbc66d24dc
David Lechner <david@pybricks.com>
2019-06-02 15:40:29 -0500

    platform/debug: enable floating point unit
    On Cortex M4, the floating point unit must be enabled before using floating point instructions.

--- lib/pbio/platform/debug/sys.c
+++ lib/pbio/platform/debug/sys.c
@@ -188,8 +188,11 @@ uint32_t SystemCoreClock = 16000000;
 const uint8_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
 const uint8_t APBPrescTable[8] = {0, 0, 0, 0, 1, 2, 3, 4};
 
-// speci
 void SystemInit(void) {
+#if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
+    SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
+#endif
+
     // basic MCU config
     RCC->CR |= RCC_CR_HSION;
     RCC->CFGR = 0; // reset all
