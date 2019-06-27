d1b6786e77d64ff1366f088f65a42349ac15aadd
David Lechner <david@pybricks.com>
2019-06-27 14:11:08 -0500

    tests: helper script for when tests fail
    This adds a new helper script to print the output when tests fail.

---
+++ tests/dump-out-files.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Run this script when tests fail to see if there is any useful output.
+
+SEP="------------------------------------------------------------------------"
+TEST_DIR=$(readlink -f $(dirname $(readlink -f $0))/../../../tests)
+
+find ${TEST_DIR} -name *.out -exec echo {} \; -exec echo ${SEP} \; \
+    -exec cat {} \; -exec echo \; -exec echo ${SEP} \; -exec echo \;
