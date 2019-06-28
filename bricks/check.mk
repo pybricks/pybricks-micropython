pybricks/pybricks-micropython

commit 11ad060f335b3d81f83a14b97d8b164b85270e48

bricks/check.mk: supress error when git not found
If the git command is not found, then we can assume that the code was
not checked out with git and so we don't need to do this check anyway,
so just supress any errors.

bricks/check.mk
@@ -4,7 +4,7 @@
 
 # This is a test to ensure that the parent micropython git tree is checked
 # out to the correct commit.
-ifeq ($(shell git rev-parse --is-inside-work-tree),true)
+ifeq ($(shell gitx rev-parse --is-inside-work-tree 2>/dev/null),true)
 HEAD = $(shell git --git-dir=../../../../.git rev-parse HEAD)
 TAG = $(shell cat ../../micropython-tag)
 HASH = $(shell git --git-dir=../../../../.git rev-parse $(TAG))

commit efceebe1983d8a5213b87bf92b267b951bd9eb15

bricks/check.mk
Fix typo introduced in 11ad060f

bricks/check.mk
@@ -4,7 +4,7 @@
 
 # This is a test to ensure that the parent micropython git tree is checked
 # out to the correct commit.
-ifeq ($(shell gitx rev-parse --is-inside-work-tree 2>/dev/null),true)
+ifeq ($(shell git rev-parse --is-inside-work-tree 2>/dev/null),true)
 HEAD = $(shell git --git-dir=../../../../.git rev-parse HEAD)
 TAG = $(shell cat ../../micropython-tag)
 HASH = $(shell git --git-dir=../../../../.git rev-parse $(TAG))