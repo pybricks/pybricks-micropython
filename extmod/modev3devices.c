b86c21e3ad15d89a7134ed15cc12772943c5377d
Laurens Valk <laurens@pybricks.com>
2019-07-17 15:49:55 +0200

    extmod/ev3devices: add InfraredSensor.beacon

--- extmod/modev3devices.c
+++ extmod/modev3devices.c
@@ -1,21 +1,23 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2018 Laurens Valk
 
-#include "py/obj.h"
 #include "modmotor.h"
 #include "py/mphal.h"
 #include "py/runtime.h"
+
+#include "pbobj.h"
 
 #include <pbio/iodev.h>
 #include <pbio/ev3device.h>
 #include <pberror.h>
 
-// InfraredSensor
+// pybricks.ev3devices.InfraredSensor class object
 typedef struct _ev3devices_InfraredSensor_obj_t {
     mp_obj_base_t base;
     ev3_iodev_t *iodev;
 } ev3devices_InfraredSensor_obj_t;
 
+// pybricks.ev3devices.InfraredSensor.__init__
 STATIC mp_obj_t ev3devices_InfraredSensor_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args ) {
     mp_arg_check_num(n_args, n_kw, 1, 1, false);
     ev3devices_InfraredSensor_obj_t *self = m_new_obj(ev3devices_InfraredSensor_obj_t);
@@ -24,12 +26,14 @@ STATIC mp_obj_t ev3devices_InfraredSensor_make_new(const mp_obj_type_t *type, si
     return MP_OBJ_FROM_PTR(self);
 }
 
+// pybricks.ev3devices.InfraredSensor.__str__
 STATIC void ev3devices_InfraredSensor_print(const mp_print_t *print,  mp_obj_t self_in, mp_print_kind_t kind) {
     ev3devices_InfraredSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
     mp_printf(print, qstr_str(MP_QSTR_InfraredSensor));
     mp_printf(print, " on Port.S%c",  self->iodev->port);
 }
 
+// pybricks.ev3devices.InfraredSensor.distance
 STATIC mp_obj_t ev3devices_InfraredSensor_distance(mp_obj_t self_in) {
     ev3devices_InfraredSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
     int8_t distance;
@@ -38,8 +42,47 @@ STATIC mp_obj_t ev3devices_InfraredSensor_distance(mp_obj_t self_in) {
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(ev3devices_InfraredSensor_distance_obj, ev3devices_InfraredSensor_distance);
 
+// pybricks.ev3devices.InfraredSensor.beacon
+STATIC mp_obj_t ev3devices_InfraredSensor_beacon(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+
+    ev3devices_InfraredSensor_obj_t *self = MP_OBJ_TO_PTR(pos_args[0]);
+
+    STATIC const mp_arg_t allowed_args[] = {
+        PB_ARG_REQUIRED(channel),
+    };
+    PB_PARSE_ARGS_METHOD(args, n_args, pos_args, kw_args, allowed_args);
+
+    mp_int_t channel = pb_obj_get_int(args[0].u_obj);
+
+    if (channel < 1 || channel > 4) {
+        pb_assert(PBIO_ERROR_INVALID_ARG);
+    }
+
+    int8_t beacon_data[8];
+    pb_assert(ev3device_get_values_at_mode(self->iodev, PBIO_IODEV_MODE_ID_EV3_IR_SENSOR__IR_SEEK, beacon_data));
+
+    mp_int_t heading = beacon_data[channel*2-2]*3;
+    mp_int_t distance = beacon_data[channel*2-1];
+    
+    mp_obj_t ret[2];
+    
+    if (distance == -128) {
+        ret[0] = mp_const_none;
+        ret[1] = mp_const_none;
+    }
+    else {
+        ret[0] = mp_obj_new_int(distance);
+        ret[1] = mp_obj_new_int(heading);
+    }
+
+    return mp_obj_new_tuple(2, ret);
+}
+MP_DEFINE_CONST_FUN_OBJ_KW(ev3devices_InfraredSensor_beacon_obj, 0, ev3devices_InfraredSensor_beacon);
+
+// dir(pybricks.ev3devices.InfraredSensor)
 STATIC const mp_rom_map_elem_t ev3devices_InfraredSensor_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_distance), MP_ROM_PTR(&ev3devices_InfraredSensor_distance_obj) },
+    { MP_ROM_QSTR(MP_QSTR_beacon),   MP_ROM_PTR(&ev3devices_InfraredSensor_beacon_obj) },
 };
 STATIC MP_DEFINE_CONST_DICT(ev3devices_InfraredSensor_locals_dict, ev3devices_InfraredSensor_locals_dict_table);
 
