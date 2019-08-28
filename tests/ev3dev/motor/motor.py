5aa72bdc43e99f737fbb45904874b6fe7a1e8a5c
David Lechner <david@pybricks.com>
2019-08-28 12:38:24 -0500

    tests/ev3dev: add motor tests
    A few basic tests to get started
    
    Requires ev3dev-mocks >= 1.3.0 and umockdev with "Add all bus names from Linux kernel 5.3" patch

---
+++ tests/ev3dev/motor/motor.py
@@ -0,0 +1,77 @@
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+
+IIO_BASE = ('/sys/devices/platform/soc@1c00000/ti-pruss/1c32000.pru1'
+            '/remoteproc/remoteproc0/virtio0/virtio0.ev3-tacho-rpmsg.-1.0'
+            '/iio:device1/')
+TACHO_BASE = ('/sys/devices/platform/ev3-ports/ev3-ports:outA/lego-port'
+              '/port4/ev3-ports:outA:lego-ev3-l-motor/tacho-motor/motor0/')
+
+
+def write_iio(attr, value):
+    with open(IIO_BASE + attr, 'w') as f:
+        f.write(value + '\n')
+
+
+def print_iio(attr):
+    with open(IIO_BASE + attr, 'r') as f:
+        print(f.read().strip())
+
+
+def write_tacho(attr, value):
+    with open(TACHO_BASE + attr, 'w') as f:
+        f.write(value + '\n')
+
+
+def print_tacho(attr):
+    with open(TACHO_BASE + attr, 'r') as f:
+        print(f.read().strip())
+
+
+m = Motor(Port.A)
+
+
+# testing angle
+
+print(m.angle())  # expect 0
+
+write_iio('in_count0_raw', '360')
+print(m.angle())  # expect 180
+
+
+# testing speed
+
+print(m.speed())  # expect 0
+
+write_iio('in_frequency0_input', '1000')
+print(m.speed())  # expect 500
+
+
+# testing stalled
+
+print(m.stalled())  # expect False
+
+
+# testing direct control of duty cycle
+
+m.dc(50)
+print_tacho('command')  # expect "run-direct"
+print_tacho('duty_cycle_sp')  # expect 50
+
+m.dc(100)
+print_tacho('command')  # expect "run-direct"
+print_tacho('duty_cycle_sp')  # expect 100
+
+# duty cycle > 100 is clipped
+m.dc(101)
+print_tacho('command')  # expect "run-direct"
+print_tacho('duty_cycle_sp')  # expect 100
+
+m.dc(-100)
+print_tacho('command')  # expect "run-direct"
+print_tacho('duty_cycle_sp')  # expect -100
+
+# duty cycle less than -100 is clipped
+m.dc(-101)
+print_tacho('command')  # expect "run-direct"
+print_tacho('duty_cycle_sp')  # expect -100
--- tests/ev3dev/test-wrapper.sh
+++ tests/ev3dev/test-wrapper.sh
@@ -4,4 +4,6 @@
 
 DIR="$(dirname $(readlink -f $0))"
 
+export EV3DEV_MOCKS_UMOCKDEV_RUN_ARGS="-d $DIR/lego-ev3-large-motor-port-a.umockdev"
+
 exec ev3dev-mocks-run "$DIR/../../bricks/ev3dev/pybricks-micropython" "$@"
