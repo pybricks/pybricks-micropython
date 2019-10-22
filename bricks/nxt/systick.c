876bf8509e99a1c481599b07054b6f4fb7164bb9
David Lechner <david@pybricks.com>
2019-10-22 17:13:20 -0500

    bricks/nxt/systick: use public systick_get_ms()
    This uses the public systick_get_ms() function instead of private
    systick_ms variable.
    
    nxt fw size +0

--- bricks/nxt/systick.c
+++ bricks/nxt/systick.c
@@ -11,18 +11,15 @@
 
 #include <nxt/systick.h>
 
-// using private nxt driver variable
-extern volatile uint32_t systick_ms;
-
 // Core delay function that does an efficient sleep and may switch thread context.
 // If IRQs are enabled then we must have the GIL.
 void mp_hal_delay_ms(mp_uint_t Delay) {
     if (interrupts_get_and_disable() != 0) {
         interrupts_enable();
         // IRQs enabled, so can use systick counter to do the delay
-        uint32_t start = systick_ms;
+        uint32_t start = systick_get_ms();
         // Wraparound of tick is taken care of by 2's complement arithmetic.
-        while (systick_ms - start < Delay) {
+        while (systick_get_ms() - start < Delay) {
             // This macro will execute the necessary idle behaviour.  It may
             // raise an exception, switch threads or enter sleep mode (waiting for
             // (at least) the SysTick interrupt).
