fdaacde1ea4a498522d5ff613157b471ea416d5e
David Lechner <david@pybricks.com>
2019-10-22 21:32:38 -0500

    platform/nxt/clock: copy clock delay function from contiki

--- bricks/nxt/mphalport.h
+++ bricks/nxt/mphalport.h
@@ -1,2 +1,10 @@
-static inline mp_uint_t mp_hal_ticks_ms(void) { return 0; }
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018 David Lechner
+
+#include "sys/clock.h"
+
 void mp_hal_set_interrupt_char(int c);
+#define mp_hal_ticks_ms clock_time
+#define mp_hal_ticks_us clock_usecs
+#define mp_hal_delay_us clock_delay_usec
+#define mp_hal_ticks_cpu() 0
--- lib/pbio/platform/nxt/clock.c
+++ lib/pbio/platform/nxt/clock.c
@@ -6,6 +6,7 @@
 #include "sys/clock.h"
 #include "sys/etimer.h"
 
+#include <nxt/at91sam7.h>
 #include <nxt/systick.h>
 
 clock_time_t clock_time() {
@@ -18,4 +19,18 @@ uint32_t clock_usecs(void) {
 
 void clock_init(void) {
     // TODO: Split NXT init into clock_init etc
+}
+
+/* The inner loop takes 4 cycles. The outer 5+SPIN_COUNT*4. */
+
+#define SPIN_TIME 2 /* us */
+#define SPIN_COUNT (((CLOCK_FREQUENCY*SPIN_TIME/1000000)-5)/4)
+
+void clock_delay_usec(uint16_t t) {
+#ifdef __THUMBEL__
+    __asm volatile("1: mov r1,%2\n2:\tsub r1,#1\n\tbne 2b\n\tsub %0,#1\n\tbne 1b\n"
+        : "=l"(t) : "0"(t), "l"(SPIN_COUNT));
+#else
+#error Must be compiled in thumb mode
+#endif
 }
