c120f455e345841b370fefdf20a1123ff36ffccb
Laurens Valk <laurens@pybricks.com>
2019-10-24 17:07:10 +0200

    ev3sensor: fix gyro ID
    It was listed as an ultrasonic sensor

--- lib/pbio/drv/ev3dev_stretch/ev3sensor.c
+++ lib/pbio/drv/ev3dev_stretch/ev3sensor.c
@@ -176,7 +176,7 @@ pbio_error_t pbdrv_ev3_sensor_get_id(pbdrv_ev3_sensor_t *sensor, pbio_iodev_type
         *id = PBIO_IODEV_TYPE_ID_EV3_ULTRASONIC_SENSOR;
     }
     else if (!strcmp(driver_name, "lego-ev3-gyro")) {
-        *id = PBIO_IODEV_TYPE_ID_EV3_ULTRASONIC_SENSOR;
+        *id = PBIO_IODEV_TYPE_ID_EV3_GYRO_SENSOR;
     }
     else {
         return PBIO_ERROR_IO;
