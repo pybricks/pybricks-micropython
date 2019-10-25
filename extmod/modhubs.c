9af7a8de851772f1831896b30c35dd3a5fec906a
Laurens Valk <laurens@pybricks.com>
2019-10-25 10:51:04 +0200

    modhubs/EV3Brick: Add Port Enum
    In the 2.0 API, ports are accessed through the hub, not the parameters module.

--- extmod/modhubs.c
+++ extmod/modhubs.c
@@ -4,6 +4,8 @@
 #include <pbio/port.h>
 #include <pbio/button.h>
 
+#include "modparameters.h"
+
 #include "pberror.h"
 #include "pbobj.h"
 
@@ -32,7 +34,8 @@ EV3Brick class tables
 */
 STATIC const mp_rom_map_elem_t hubs_EV3Brick_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_buttons),     MP_ROM_PTR(&pb_module_buttons)      },
-    { MP_ROM_QSTR(MP_QSTR_light),       MP_ROM_PTR(&pb_module_colorlight)   }, 
+    { MP_ROM_QSTR(MP_QSTR_Port),        MP_ROM_PTR(&pb_enum_type_Port)      },
+    { MP_ROM_QSTR(MP_QSTR_light),       MP_ROM_PTR(&pb_module_colorlight)   },
     { MP_ROM_QSTR(MP_QSTR_battery),     MP_ROM_PTR(&pb_module_battery)      },
 };
 STATIC MP_DEFINE_CONST_DICT(hubs_EV3Brick_locals_dict, hubs_EV3Brick_locals_dict_table);
