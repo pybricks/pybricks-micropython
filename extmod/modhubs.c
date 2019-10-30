3b5020b2b7848b16b5f357f3f2c0337b837c2941
Laurens Valk <laurens@pybricks.com>
2019-10-30 11:22:19 +0100

    modhubs: add CityHub class, drop cityhub module

--- extmod/modhubs.c
+++ extmod/modhubs.c
@@ -93,6 +93,46 @@ STATIC const mp_obj_type_t hubs_MoveHub_type = {
 
 #endif // PBIO_IODEV_TYPE_ID_MOVEHUB
 
+#ifdef PBDRV_CONFIG_HUB_CITYHUB
+
+// Class structure for CityHub
+typedef struct _hubs_CityHub_obj_t {
+    mp_obj_base_t base;
+} hubs_CityHub_obj_t;
+
+STATIC void hubs_CityHub_print(const mp_print_t *print,  mp_obj_t self_in, mp_print_kind_t kind) {
+    mp_printf(print, qstr_str(MP_QSTR_CityHub));
+}
+
+STATIC mp_obj_t hubs_CityHub_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args ) {
+    hubs_CityHub_obj_t *self = m_new_obj(hubs_CityHub_obj_t);
+    self->base.type = (mp_obj_type_t*) type;
+    return MP_OBJ_FROM_PTR(self);
+}
+
+/*
+CityHub class tables
+*/
+STATIC const mp_rom_map_elem_t hubs_CityHub_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_Port),        MP_ROM_PTR(&pb_enum_type_Port)    },
+    { MP_ROM_QSTR(MP_QSTR_light),       MP_ROM_PTR(&pb_module_colorlight) },
+    { MP_ROM_QSTR(MP_QSTR_battery),     MP_ROM_PTR(&pb_module_battery)    },
+    { MP_ROM_QSTR(MP_QSTR_shutdown),    MP_ROM_PTR(&hub_shutdown_obj)     },
+    { MP_ROM_QSTR(MP_QSTR_reboot),      MP_ROM_PTR(&hub_reboot_obj)       },
+    { MP_ROM_QSTR(MP_QSTR_update),      MP_ROM_PTR(&hub_update_obj)       },
+};
+STATIC MP_DEFINE_CONST_DICT(hubs_CityHub_locals_dict, hubs_CityHub_locals_dict_table);
+
+STATIC const mp_obj_type_t hubs_CityHub_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_CityHub,
+    .print = hubs_CityHub_print,
+    .make_new = hubs_CityHub_make_new,
+    .locals_dict = (mp_obj_dict_t*)&hubs_CityHub_locals_dict,
+};
+
+#endif // PBIO_IODEV_TYPE_ID_CITYHUB
+
 /* Module table */
 
 STATIC const mp_rom_map_elem_t hubs_globals_table[] = {
@@ -102,6 +142,9 @@ STATIC const mp_rom_map_elem_t hubs_globals_table[] = {
 #endif
 #ifdef PBDRV_CONFIG_HUB_MOVEHUB
     { MP_ROM_QSTR(MP_QSTR_MoveHub),    MP_ROM_PTR(&hubs_MoveHub_type) },
+#endif
+#ifdef PBDRV_CONFIG_HUB_CITYHUB
+    { MP_ROM_QSTR(MP_QSTR_CityHub),    MP_ROM_PTR(&hubs_CityHub_type) },
 #endif
 };
 STATIC MP_DEFINE_CONST_DICT(pb_module_hubs_globals, hubs_globals_table);
