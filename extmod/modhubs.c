5cf8f47d15a3e280f766a5ee32d451e4200fea8e
Laurens Valk <laurens@pybricks.com>
2019-10-30 11:25:07 +0100

    modhubs: add CPlusHub class, drop cplushub module

--- extmod/modhubs.c
+++ extmod/modhubs.c
@@ -51,7 +51,7 @@ STATIC const mp_obj_type_t hubs_EV3Brick_type = {
     .locals_dict = (mp_obj_dict_t*)&hubs_EV3Brick_locals_dict,
 };
 
-#endif // PBIO_IODEV_TYPE_ID_EV3_BRICK
+#endif // PBDRV_CONFIG_HUB_EV3BRICK
 
 #ifdef PBDRV_CONFIG_HUB_MOVEHUB
 
@@ -91,7 +91,7 @@ STATIC const mp_obj_type_t hubs_MoveHub_type = {
     .locals_dict = (mp_obj_dict_t*)&hubs_MoveHub_locals_dict,
 };
 
-#endif // PBIO_IODEV_TYPE_ID_MOVEHUB
+#endif // PBDRV_CONFIG_HUB_MOVEHUB
 
 #ifdef PBDRV_CONFIG_HUB_CITYHUB
 
@@ -131,7 +131,47 @@ STATIC const mp_obj_type_t hubs_CityHub_type = {
     .locals_dict = (mp_obj_dict_t*)&hubs_CityHub_locals_dict,
 };
 
-#endif // PBIO_IODEV_TYPE_ID_CITYHUB
+#endif // PBDRV_CONFIG_HUB_CITYHUB
+
+#ifdef PBDRV_CONFIG_HUB_CPLUSHUB
+
+// Class structure for CPlusHub
+typedef struct _hubs_CPlusHub_obj_t {
+    mp_obj_base_t base;
+} hubs_CPlusHub_obj_t;
+
+STATIC void hubs_CPlusHub_print(const mp_print_t *print,  mp_obj_t self_in, mp_print_kind_t kind) {
+    mp_printf(print, qstr_str(MP_QSTR_CPlusHub));
+}
+
+STATIC mp_obj_t hubs_CPlusHub_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args ) {
+    hubs_CPlusHub_obj_t *self = m_new_obj(hubs_CPlusHub_obj_t);
+    self->base.type = (mp_obj_type_t*) type;
+    return MP_OBJ_FROM_PTR(self);
+}
+
+/*
+CPlusHub class tables
+*/
+STATIC const mp_rom_map_elem_t hubs_CPlusHub_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_Port),        MP_ROM_PTR(&pb_enum_type_Port)    },
+    { MP_ROM_QSTR(MP_QSTR_light),       MP_ROM_PTR(&pb_module_colorlight) },
+    { MP_ROM_QSTR(MP_QSTR_battery),     MP_ROM_PTR(&pb_module_battery)    },
+    { MP_ROM_QSTR(MP_QSTR_shutdown),    MP_ROM_PTR(&hub_shutdown_obj)     },
+    { MP_ROM_QSTR(MP_QSTR_reboot),      MP_ROM_PTR(&hub_reboot_obj)       },
+    { MP_ROM_QSTR(MP_QSTR_update),      MP_ROM_PTR(&hub_update_obj)       },
+};
+STATIC MP_DEFINE_CONST_DICT(hubs_CPlusHub_locals_dict, hubs_CPlusHub_locals_dict_table);
+
+STATIC const mp_obj_type_t hubs_CPlusHub_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_CPlusHub,
+    .print = hubs_CPlusHub_print,
+    .make_new = hubs_CPlusHub_make_new,
+    .locals_dict = (mp_obj_dict_t*)&hubs_CPlusHub_locals_dict,
+};
+
+#endif // PBDRV_CONFIG_HUB_CPLUSHUB
 
 /* Module table */
 
@@ -145,6 +185,9 @@ STATIC const mp_rom_map_elem_t hubs_globals_table[] = {
 #endif
 #ifdef PBDRV_CONFIG_HUB_CITYHUB
     { MP_ROM_QSTR(MP_QSTR_CityHub),    MP_ROM_PTR(&hubs_CityHub_type) },
+#endif
+#ifdef PBDRV_CONFIG_HUB_CPLUSHUB
+    { MP_ROM_QSTR(MP_QSTR_CPlusHub),    MP_ROM_PTR(&hubs_CPlusHub_type) },
 #endif
 };
 STATIC MP_DEFINE_CONST_DICT(pb_module_hubs_globals, hubs_globals_table);
