a37bbb9c5084ee3dbb8f6679de10ef453e8d277f
Laurens Valk <laurens@pybricks.com>
2019-11-05 19:22:11 +0100

    modnxtdevices: Add NXT Sound Sensor t EV3

--- extmod/modnxtdevices.c
+++ extmod/modnxtdevices.c
@@ -175,6 +175,100 @@ STATIC const mp_obj_type_t nxtdevices_TouchSensor_type = {
     .locals_dict = (mp_obj_dict_t*)&nxtdevices_TouchSensor_locals_dict,
 };
 
+// pybricks.nxtdevices.SoundSensor class object
+typedef struct _nxtdevices_SoundSensor_obj_t {
+    mp_obj_base_t base;
+#ifdef PBDRV_CONFIG_HUB_EV3BRICK
+    pbio_ev3iodev_t *iodev;
+#else
+    pbio_port_t port;
+#endif
+} nxtdevices_SoundSensor_obj_t;
+
+// pybricks.nxtdevices.SoundSensor.__init__
+STATIC mp_obj_t nxtdevices_SoundSensor_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args ) {
+    PB_PARSE_ARGS_CLASS(n_args, n_kw, args,
+        PB_ARG_REQUIRED(port)
+    );
+
+    nxtdevices_SoundSensor_obj_t *self = m_new_obj(nxtdevices_SoundSensor_obj_t);
+    self->base.type = (mp_obj_type_t*) type;
+
+    mp_int_t port_num = enum_get_value_maybe(port, &pb_enum_type_Port);
+#ifdef PBDRV_CONFIG_HUB_EV3BRICK
+    // Get the device and assert that it is of the right type
+    pb_assert(ev3device_get_device(&self->iodev, PBIO_IODEV_TYPE_ID_NXT_ANALOG, port_num));
+#else
+    self->port = port_num;
+    pb_assert(PBIO_ERROR_NOT_IMPLEMENTED);
+#endif
+    return MP_OBJ_FROM_PTR(self);
+}
+
+// pybricks.nxtdevices.SoundSensor.__str__
+STATIC void nxtdevices_SoundSensor_print(const mp_print_t *print,  mp_obj_t self_in, mp_print_kind_t kind) {
+    nxtdevices_SoundSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    mp_printf(print, qstr_str(MP_QSTR_SoundSensor));
+    pbio_port_t port;
+#ifdef PBDRV_CONFIG_HUB_EV3BRICK
+    port = self->iodev->port;
+#else
+    port = self->port;
+#endif
+    mp_printf(print, " on Port.S%c",  port);
+}
+
+#define SOUND_VOLT_MIN (650)
+#define SOUND_VOLT_MAX (4860)
+
+STATIC int32_t analog_sound(int32_t mvolts) {
+    int32_t sound = 100-(100*(mvolts-SOUND_VOLT_MIN))/(SOUND_VOLT_MAX-SOUND_VOLT_MIN);
+    return max(-100, min(sound, 100));
+}
+
+// pybricks.nxtdevices.SoundSensor.db
+STATIC mp_obj_t nxtdevices_SoundSensor_db(mp_obj_t self_in) {
+    nxtdevices_SoundSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    int32_t analog;
+#ifdef PBDRV_CONFIG_HUB_EV3BRICK
+    pb_assert(ev3device_get_values_at_mode(self->iodev, PBIO_IODEV_MODE_NXT_ANALOG__PASSIVE, &analog));
+#else
+    analog = self->port;
+    pb_assert(PBIO_ERROR_NOT_IMPLEMENTED);
+#endif
+    return mp_obj_new_int(analog_sound(analog));
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(nxtdevices_SoundSensor_db_obj, nxtdevices_SoundSensor_db);
+
+// pybricks.nxtdevices.SoundSensor.dba
+STATIC mp_obj_t nxtdevices_SoundSensor_dba(mp_obj_t self_in) {
+    nxtdevices_SoundSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    int32_t analog;
+#ifdef PBDRV_CONFIG_HUB_EV3BRICK
+    pb_assert(ev3device_get_values_at_mode(self->iodev, PBIO_IODEV_MODE_NXT_ANALOG__ACTIVE, &analog));
+#else
+    analog = self->port;
+    pb_assert(PBIO_ERROR_NOT_IMPLEMENTED);
+#endif
+    return mp_obj_new_int(analog_sound(analog));
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(nxtdevices_SoundSensor_dba_obj, nxtdevices_SoundSensor_dba);
+
+// dir(pybricks.ev3devices.SoundSensor)
+STATIC const mp_rom_map_elem_t nxtdevices_SoundSensor_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_db),  MP_ROM_PTR(&nxtdevices_SoundSensor_db_obj ) },
+    { MP_ROM_QSTR(MP_QSTR_dba), MP_ROM_PTR(&nxtdevices_SoundSensor_dba_obj) },
+};
+STATIC MP_DEFINE_CONST_DICT(nxtdevices_SoundSensor_locals_dict, nxtdevices_SoundSensor_locals_dict_table);
+
+// type(pybricks.ev3devices.SoundSensor)
+STATIC const mp_obj_type_t nxtdevices_SoundSensor_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_SoundSensor,
+    .print = nxtdevices_SoundSensor_print,
+    .make_new = nxtdevices_SoundSensor_make_new,
+    .locals_dict = (mp_obj_dict_t*)&nxtdevices_SoundSensor_locals_dict,
+};
 // dir(pybricks.nxtdevices)
 STATIC const mp_rom_map_elem_t nxtdevices_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__),         MP_ROM_QSTR(MP_QSTR_nxtdevices)              },
@@ -182,6 +276,7 @@ STATIC const mp_rom_map_elem_t nxtdevices_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_Motor),            MP_ROM_PTR(&motor_Motor_type)                },
 #endif
     { MP_ROM_QSTR(MP_QSTR_TouchSensor),      MP_ROM_PTR(&nxtdevices_TouchSensor_type)     },
+    { MP_ROM_QSTR(MP_QSTR_SoundSensor),      MP_ROM_PTR(&nxtdevices_SoundSensor_type)     },
     { MP_ROM_QSTR(MP_QSTR_UltrasonicSensor), MP_ROM_PTR(&nxtdevices_UltrasonicSensor_type)},
 };
 
