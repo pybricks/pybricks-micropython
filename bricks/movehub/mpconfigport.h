a6b1f882f6715ae10c2193af228cee599ef59244
Laurens Valk <laurens@pybricks.com>
2019-11-22 11:17:19 +0100

    bricks/movehub: disable REPL, enable float
    Because we now have a download-and-run functionality for running pre-compiled MPY files, the REPL is no longer essential. By disabling it, we free up space to include MicroPython floating point support. This will offer a consistent experience across all Pybricks compatible hubs.
    
    movehub fw size -1332
    
    At this point in time, the config options have these build sizes:
    COMPILER 0 FLOAT 0: 84708
    COMPILER 0 FLOAT 1: 105328
    COMPILER 1 FLOAT 0: 106660

--- bricks/movehub/mpconfigport.h
+++ bricks/movehub/mpconfigport.h
@@ -18,12 +18,12 @@
 // You can disable the built-in MicroPython compiler by setting the following
 // config option to 0.  If you do this then you won't get a REPL prompt, but you
 // will still be able to execute pre-compiled scripts, compiled with mpy-cross.
-// Requires about 19K (19568) of flash
-#define MICROPY_ENABLE_COMPILER         (1)
+// Requires about 21.5K (21952) of flash
+#define MICROPY_ENABLE_COMPILER         (0)
 
 // Set to MICROPY_FLOAT_IMPL_FLOAT to enable floating point support in user code or
 // set to MICROPY_FLOAT_IMPL_NONE to disable floating point support in user code
-// Requires about 20K (21312) of flash
-#define MICROPY_FLOAT_IMPL              (MICROPY_FLOAT_IMPL_NONE)
+// Requires about 20.1K (20620) of flash
+#define MICROPY_FLOAT_IMPL              (MICROPY_FLOAT_IMPL_FLOAT)
 
 #include "../stm32configport.h"
