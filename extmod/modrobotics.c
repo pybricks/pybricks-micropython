56bc1d78f5c299dc8ac6eb25292dc1382e31df2e
Laurens Valk <laurens@pybricks.com>
2019-12-20 11:09:02 +0100

    modrobotics: drivebase backwards compatibility
    This adds the two methods of the legacy API.

--- extmod/modrobotics.c
+++ extmod/modrobotics.c
@@ -83,6 +83,67 @@ STATIC mp_obj_t robotics_DriveBase_start(size_t n_args, const mp_obj_t *pos_args
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(robotics_DriveBase_start_obj, 0, robotics_DriveBase_start);
 
+// LEGACY METHODS AVAILABLE ON EV3 ONLY
+#if PYBRICKS_PY_EV3DEVICES 
+
+// pybricks.robotics.DriveBase.drive (Legacy function for 1.0 API compatibility)
+STATIC mp_obj_t robotics_DriveBase_drive(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+    PB_PARSE_ARGS_METHOD(n_args, pos_args, kw_args,
+        PB_ARG_REQUIRED(speed),
+        PB_ARG_REQUIRED(steering)
+    );
+    robotics_DriveBase_obj_t *self = MP_OBJ_TO_PTR(pos_args[0]);
+
+    // Get wheel diameter and axle track dimensions
+    int32_t speed_val = pb_obj_get_int(speed);
+    int32_t turn_rate_val = pb_obj_get_int(steering);
+
+    pbio_error_t err;
+    pb_thread_enter();    
+    err = pbio_drivebase_start(self->drivebase, speed_val, turn_rate_val);
+    pb_thread_exit();
+    pb_assert(err);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_KW(robotics_DriveBase_drive_obj, 0, robotics_DriveBase_drive);
+
+// pybricks.robotics.DriveBase.drive_time (Legacy function for 1.0 API compatibility)
+STATIC mp_obj_t robotics_DriveBase_drive_time(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+    PB_PARSE_ARGS_METHOD(n_args, pos_args, kw_args,
+        PB_ARG_REQUIRED(speed),
+        PB_ARG_REQUIRED(steering),
+        PB_ARG_REQUIRED(time)
+    );
+    robotics_DriveBase_obj_t *self = MP_OBJ_TO_PTR(pos_args[0]);
+
+    // Get wheel diameter and axle track dimensions
+    int32_t speed_val = pb_obj_get_int(speed);
+    int32_t turn_rate_val = pb_obj_get_int(steering);
+    mp_int_t duration = pb_obj_get_int(time);
+    if (duration < 0) {
+        pb_assert(PBIO_ERROR_INVALID_ARG);
+    }
+
+    pbio_error_t err;
+    pb_thread_enter();    
+    err = pbio_drivebase_start(self->drivebase, speed_val, turn_rate_val);
+    pb_thread_exit();
+    pb_assert(err);
+
+    mp_hal_delay_ms(duration);
+
+    pb_thread_enter();    
+    err = pbio_drivebase_stop(self->drivebase, PBIO_ACTUATION_COAST);
+    pb_thread_exit();
+    pb_assert(err);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_KW(robotics_DriveBase_drive_time_obj, 0, robotics_DriveBase_drive_time);
+
+#endif // LEGACY METHODS AVAILABLE ON EV3 ONLY
+
 // pybricks.robotics.DriveBase.stop
 STATIC mp_obj_t robotics_DriveBase_stop(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
     PB_PARSE_ARGS_METHOD(n_args, pos_args, kw_args,
@@ -107,6 +168,10 @@ STATIC const mp_rom_map_elem_t robotics_DriveBase_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_right), MP_ROM_ATTRIBUTE_OFFSET(robotics_DriveBase_obj_t, right) },
     { MP_ROM_QSTR(MP_QSTR_start), MP_ROM_PTR(&robotics_DriveBase_start_obj) },
     { MP_ROM_QSTR(MP_QSTR_stop), MP_ROM_PTR(&robotics_DriveBase_stop_obj) },
+#if PYBRICKS_PY_EV3DEVICES // LEGACY METHODS AVAILABLE ON EV3 ONLY
+    { MP_ROM_QSTR(MP_QSTR_drive), MP_ROM_PTR(&robotics_DriveBase_drive_obj) },
+    { MP_ROM_QSTR(MP_QSTR_drive_time), MP_ROM_PTR(&robotics_DriveBase_drive_time_obj) },
+#endif
 };
 STATIC MP_DEFINE_CONST_DICT(robotics_DriveBase_locals_dict, robotics_DriveBase_locals_dict_table);
 
