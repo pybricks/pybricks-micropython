cd944bef4d6b8ccb24918b4e165afc1629b311d3
Laurens Valk <laurens@pybricks.com>
2019-12-21 23:18:29 +0100

    pbio/integrator: add file structure
    This will contain exact and numeric integrators that are currently built into the controllers.

---
+++ lib/pbio/include/pbio/integrator.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018-2019 Laurens Valk
+// Copyright (c) 2019 LEGO System A/S
+
+#ifndef _PBIO_INTEGRATOR_H_
+#define _PBIO_INTEGRATOR_H_
+
+#include <stdint.h>
+#include <stdio.h>
+
+#endif // _PBIO_INTEGRATOR_H_
---
+++ lib/pbio/src/integrator.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018-2019 Laurens Valk
+// Copyright (c) 2019 LEGO System A/S
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <contiki.h>
+
+#include <pbio/math.h>
+#include <pbio/integrator.h>
+
