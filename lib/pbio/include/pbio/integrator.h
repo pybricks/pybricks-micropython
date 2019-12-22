e01697199d9e6a9c0dfa0b57f22b6e493d062b46
Laurens Valk <laurens@pybricks.com>
2019-12-22 20:24:17 +0100

    pbio/integrator: implement count error integrator
     This will be used to replace pbio_control_status_anglular_t, for use in position control

--- lib/pbio/include/pbio/integrator.h
+++ lib/pbio/include/pbio/integrator.h
@@ -32,4 +32,26 @@ void pbio_rate_integrator_get_errors(pbio_rate_integrator_t *itg,
 
 bool pbio_rate_integrator_stalled(pbio_rate_integrator_t *itg, int32_t time_now, int32_t rate, int32_t time_stall, int32_t rate_stall);
 
+typedef struct _pbio_count_integrator_t {
+    bool running; // Whether the integrator is running (1) or paused (0)
+    int32_t time_paused; // Time at which we began pausing most recently, stopping integration
+    int32_t time_waited; // Total time we spent in a paused state
+    count_t count_err_prev; // Position error in the previous control iteration
+    ustime_t time_prev; // Time at the previous control iteratiom
+    int32_t count_err_integral; // Ongoing integral of position error
+    int32_t count_err_integral_max; // Maximum value of integrator
+} pbio_count_integrator_t;
+
+int32_t pbio_count_integrator_get_ref_time(pbio_count_integrator_t *itg, int32_t time_now);
+
+void pbio_count_integrator_pause(pbio_count_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref);
+
+void pbio_count_integrator_resume(pbio_count_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref);
+
+void pbio_count_integrator_reset(pbio_count_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref, int32_t max);
+
+void pbio_count_integrator_update(pbio_count_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref, int32_t *count_err, int32_t *count_err_integral);
+
+bool pbio_count_integrator_stalled(pbio_count_integrator_t *itg, int32_t time_now, int32_t rate, int32_t time_stall, int32_t rate_stall);
+
 #endif // _PBIO_INTEGRATOR_H_
--- lib/pbio/src/integrator.c
+++ lib/pbio/src/integrator.c
@@ -12,6 +12,8 @@
 #include <pbio/trajectory.h>
 #include <pbio/integrator.h>
 
+/* Rate integrator used for speed-based control */
+
 void pbio_rate_integrator_pause(pbio_rate_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref) {
 
     // Pause only if running
@@ -49,6 +51,9 @@ void pbio_rate_integrator_reset(pbio_rate_integrator_t *itg, int32_t time_now, i
 
     // Set integral to 0
     itg->rate_err_integral_paused = 0;
+
+    // Set state to paused
+    itg->running = false;
 
     // Resume integration
     pbio_rate_integrator_resume(itg, time_now, count, count_ref);
@@ -94,3 +99,98 @@ bool pbio_rate_integrator_stalled(pbio_rate_integrator_t *itg, int32_t time_now,
     // All checks have failed, so we are stalled
     return true; 
 }
+
+/* Count integrator used for position-based control */
+
+int32_t pbio_count_integrator_get_ref_time(pbio_count_integrator_t *itg, int32_t time_now) {
+    // The wall time at which we are is either the current time, or whenever we stopped last
+    int32_t real_time = itg->running ? time_now : itg->time_paused;
+
+    // But we want to evaluate the reference compensating for the time we spent waiting
+    return real_time - itg->time_waited;
+}
+
+void pbio_count_integrator_pause(pbio_count_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref) {
+
+    // Return if already paused
+    if (!itg->running) {
+        return;
+    }
+
+    // Disable the integrator
+    itg->running = false;
+    itg->time_paused = time_now;
+}
+
+void pbio_count_integrator_resume(pbio_count_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref) {
+
+    // Return if already running
+    if (itg->running) {
+        return;
+    }
+
+    // Then we must restart the time
+    itg->running = true;
+
+    // Increment total wait time by time elapsed since we started pausing
+    itg->time_waited += time_now - itg->time_paused;
+
+}
+
+void pbio_count_integrator_reset(pbio_count_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref, int32_t max) {
+
+    // Reset integrator state variables
+    itg->count_err_integral = 0;
+    itg->time_waited = 0;
+    itg->time_prev = time_now;
+    itg->time_paused = time_now;
+    itg->count_err_prev = 0;
+    itg->running = false;
+    itg->count_err_integral_max = max;
+
+    // Resume integration
+    pbio_count_integrator_resume(itg, time_now, count, count_ref);
+
+}
+
+void pbio_count_integrator_update(pbio_count_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref, int32_t *count_err, int32_t *count_err_integral) {
+    // Integrate and update position error
+    if (itg->running) {
+        itg->count_err_integral += itg->count_err_prev*(time_now - itg->time_prev);
+
+        if (itg->count_err_integral > itg->count_err_integral_max) {
+            itg->count_err_integral = itg->count_err_integral_max;
+        }
+        if (itg->count_err_integral < -itg->count_err_integral_max) {
+            itg->count_err_integral = -itg->count_err_integral_max;
+        }
+    }
+
+    // Calculate current error state
+    *count_err = count_ref - count;
+    *count_err_integral = itg->count_err_integral;
+
+    // Keep the error and time for use in next update
+    itg->count_err_prev = *count_err;
+    itg->time_prev = time_now;
+}
+
+bool pbio_count_integrator_stalled(pbio_count_integrator_t *itg, int32_t time_now, int32_t rate, int32_t time_stall, int32_t rate_stall) {
+    // If we're running and the integrator is not saturated, we're not stalled
+    if (itg->running && abs(itg->count_err_integral) < itg->count_err_integral_max) {
+        return false;
+    }
+
+    // If we're still running faster than the stall limit, we're certainly not stalled.
+    if (abs(rate) > rate_stall) {
+        return false;
+    }
+
+    // If the integrator is paused for less than the stall time, we're still not stalled for now.
+    if (time_now - itg->time_paused < time_stall) {
+        return false;
+    }
+
+    // All checks have failed, so we are stalled
+    return true; 
+};
