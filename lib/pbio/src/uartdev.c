pybricks/pybricks-micropython

commit cd6d687f6748fbf173438040f154020047bba4be

src/uartdev: remove check for unexpected mode
It is possible that a data message was already in progress when the mode
is changed, which could lead to an error condition. Since this is a
normal occurance, don't cause an error, but rather only update the
data received if the mode is the expected mode. If the mode of the
data messages does not change, a timeout will occur later anyway
because data->data_rec is not being updated.

lib/pbio/src/uartdev.c
@@ -697,21 +697,14 @@ static void pbio_uartdev_parse_msg(uartdev_port_data_t *data) {
                 DBG_ERR(data->last_err = "Invalid mode received");
                 goto err;
             }
-            if (mode != data->iodev.mode) {
-                if (mode == data->new_mode) {
-                    data->iodev.mode = mode;
-                    // TODO: notify that mode has changed
-                } else {
-                    DBG_ERR(data->last_err = "Unexpected mode");
-                    goto err;
+            data->iodev.mode = mode;
+            if (mode == data->new_mode) {
+                memcpy(data->iodev.bin_data, data->rx_msg + 1, msg_size - 2);
+                data->data_rec = 1;
+                if (data->num_data_err) {
+                    data->num_data_err--;
                 }
             }
-            memcpy(data->iodev.bin_data, data->rx_msg + 1, msg_size - 2);
-        }
-
-        data->data_rec = 1;
-        if (data->num_data_err) {
-            data->num_data_err--;
         }
         break;
     }

commit 4ed20f35613262a26f173fffd93af14d9387171b

src/uartdev: fix motors not working
This fixes a regression that caused motors to stop working because they
though they were not receiving any DATA messges ("No data since last
keepalive").

Also use stdbool macros instead of 0/1 for data_rec everywhere.

Fixes: cd6d687f6748fbf173438040f154020047bba4be

lib/pbio/src/uartdev.c
@@ -752,11 +752,12 @@ static void pbio_uartdev_parse_msg(uartdev_port_data_t *data) {
             data->iodev.mode = mode;
             if (mode == data->new_mode) {
                 memcpy(data->iodev.bin_data, data->rx_msg + 1, msg_size - 2);
-                data->data_rec = 1;
-                if (data->num_data_err) {
-                    data->num_data_err--;
-                }
             }
+        }
+
+        data->data_rec = true;
+        if (data->num_data_err) {
+            data->num_data_err--;
         }
         break;
     }
@@ -922,7 +923,7 @@ static PT_THREAD(pbio_uartdev_update(uartdev_port_data_t *data)) {
 
     data->type_id = data->rx_msg[1];
     data->info_flags = EV3_UART_INFO_FLAG_CMD_TYPE;
-    data->data_rec = 0;
+    data->data_rec = false;
     data->num_data_err = 0;
     data->status = PBIO_UARTDEV_STATUS_INFO;
     debug_pr("type id: %d\n", data->type_id);
@@ -1039,7 +1040,7 @@ static PT_THREAD(pbio_uartdev_update(uartdev_port_data_t *data)) {
                 data->status = PBIO_UARTDEV_STATUS_ERR;
             }
         }
-        data->data_rec = 0;
+        data->data_rec = false;
 
         // send keepalive
         PT_WAIT_WHILE(&data->pt, data->tx_busy);
@@ -1154,14 +1155,14 @@ static pbio_error_t ev3_uart_set_mode_end(pbio_iodev_t *iodev) {
         }
 
         if (err == PBIO_SUCCESS) {
-            port_data->data_rec = 0;
+            port_data->data_rec = false;
             return PBIO_ERROR_AGAIN;
         }
 
         return err;
     }
 
-    if (!port_data->data_rec) {
+    if (!port_data->data_rec || port_data->iodev.mode != port_data->new_mode) {
         return PBIO_ERROR_AGAIN;
     }
 