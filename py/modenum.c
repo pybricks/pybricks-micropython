39638aae22c1e046ec0008f25beaef80687fd796
David Lechner <david@pybricks.com>
2020-01-01 21:21:43 -0600

    py/modenum: raise TypeError when MICROPY_ERROR_REPORTING_TERSE
    This ensures that we raise the same error type regardless of MICROPY_ERROR_REPORTING_TERSE.  PBIO_ERROR_INVALID_ARG raises ValueError.

--- py/modenum.c
+++ py/modenum.c
@@ -16,12 +16,11 @@ void enum_class_print(const mp_print_t *print,  mp_obj_t self_in, mp_print_kind_
 }
 
 mp_int_t enum_get_value_maybe(mp_obj_t enum_elem, const mp_obj_type_t *valid_type) {
-
     pb_obj_enum_elem_t *elem = enum_elem;
 
     if (!mp_obj_is_type(enum_elem, valid_type)) {
         if (MICROPY_ERROR_REPORTING == MICROPY_ERROR_REPORTING_TERSE) {
-            pb_assert(PBIO_ERROR_INVALID_ARG);
+            mp_raise_TypeError("requires enum");
         } else {
             nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_TypeError,
                 "can't convert %s to %s", mp_obj_get_type_str(enum_elem), qstr_str(valid_type->name)));
