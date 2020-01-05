4f16f7977d16c7eac90c2cb77150771073653142
Laurens Valk <laurens@pybricks.com>
2020-01-05 21:09:12 +0100

    extmod/pupdevices: rename ColorDistanceSensor func
    This renames the C functions only. This has been called ColorDistanceSensor in the API for a long time already, so make the C functions match.

--- extmod/modpupdevices.c
+++ extmod/modpupdevices.c
@@ -19,19 +19,19 @@
 #include "modmotor.h"
 
 // Class structure for ColorDistanceSensor
-typedef struct _pupdevices_ColorAndDistSensor_obj_t {
+typedef struct _pupdevices_ColorDistanceSensor_obj_t {
     mp_obj_base_t base;
     mp_obj_t light;
     pbdevice_t *pbdev;
-} pupdevices_ColorAndDistSensor_obj_t;
+} pupdevices_ColorDistanceSensor_obj_t;
 
-// pybricks.pupdevices_ColorDistanceSensor.__init__
-STATIC mp_obj_t pupdevices_ColorAndDistSensor_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args ) {
+// pybricks.pupdevices.ColorDistanceSensor.__init__
+STATIC mp_obj_t pupdevices_ColorDistanceSensor_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args ) {
     PB_PARSE_ARGS_CLASS(n_args, n_kw, args,
         PB_ARG_REQUIRED(port)
     );
 
-    pupdevices_ColorAndDistSensor_obj_t *self = m_new_obj(pupdevices_ColorAndDistSensor_obj_t);
+    pupdevices_ColorDistanceSensor_obj_t *self = m_new_obj(pupdevices_ColorDistanceSensor_obj_t);
     self->base.type = (mp_obj_type_t*) type;
 
     mp_int_t port_num = pb_type_enum_get_value(port, &pb_enum_type_Port);
@@ -44,9 +44,9 @@ STATIC mp_obj_t pupdevices_ColorAndDistSensor_make_new(const mp_obj_type_t *type
     return MP_OBJ_FROM_PTR(self);
 }
 
-// pybricks.pupdevices_ColorDistanceSensor.color
-STATIC mp_obj_t pupdevices_ColorAndDistSensor_color(mp_obj_t self_in) {
-    pupdevices_ColorAndDistSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
+// pybricks.pupdevices.ColorDistanceSensor.color
+STATIC mp_obj_t pupdevices_ColorDistanceSensor_color(mp_obj_t self_in) {
+    pupdevices_ColorDistanceSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
 
     int8_t data[4];
     pbdevice_get_values(self->pbdev, PBIO_IODEV_MODE_PUP_COLOR_DISTANCE_SENSOR__SPEC1, data);
@@ -62,38 +62,38 @@ STATIC mp_obj_t pupdevices_ColorAndDistSensor_color(mp_obj_t self_in) {
         default: return mp_const_none;
     }
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(pupdevices_ColorAndDistSensor_color_obj, pupdevices_ColorAndDistSensor_color);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(pupdevices_ColorDistanceSensor_color_obj, pupdevices_ColorDistanceSensor_color);
 
-// pybricks.pupdevices_ColorDistanceSensor.distance
-STATIC mp_obj_t pupdevices_ColorAndDistSensor_distance(mp_obj_t self_in) {
-    pupdevices_ColorAndDistSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
+// pybricks.pupdevices.ColorDistanceSensor.distance
+STATIC mp_obj_t pupdevices_ColorDistanceSensor_distance(mp_obj_t self_in) {
+    pupdevices_ColorDistanceSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
     int8_t data[4];
     pbdevice_get_values(self->pbdev, PBIO_IODEV_MODE_PUP_COLOR_DISTANCE_SENSOR__SPEC1, data);
     return mp_obj_new_int(data[1]*10);
 }
-MP_DEFINE_CONST_FUN_OBJ_1(pupdevices_ColorAndDistSensor_distance_obj, pupdevices_ColorAndDistSensor_distance);
+MP_DEFINE_CONST_FUN_OBJ_1(pupdevices_ColorDistanceSensor_distance_obj, pupdevices_ColorDistanceSensor_distance);
 
-// pybricks.pupdevices_ColorDistanceSensor.reflection
-STATIC mp_obj_t pupdevices_ColorAndDistSensor_reflection(mp_obj_t self_in) {
-    pupdevices_ColorAndDistSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
+// pybricks.pupdevices.ColorDistanceSensor.reflection
+STATIC mp_obj_t pupdevices_ColorDistanceSensor_reflection(mp_obj_t self_in) {
+    pupdevices_ColorDistanceSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
     int8_t data[4];
     pbdevice_get_values(self->pbdev, PBIO_IODEV_MODE_PUP_COLOR_DISTANCE_SENSOR__SPEC1, data);
     return mp_obj_new_int(data[3]);
 }
-MP_DEFINE_CONST_FUN_OBJ_1(pupdevices_ColorAndDistSensor_reflection_obj, pupdevices_ColorAndDistSensor_reflection);
+MP_DEFINE_CONST_FUN_OBJ_1(pupdevices_ColorDistanceSensor_reflection_obj, pupdevices_ColorDistanceSensor_reflection);
 
-// pybricks.pupdevices_ColorDistanceSensor.ambient
-STATIC mp_obj_t pupdevices_ColorAndDistSensor_ambient(mp_obj_t self_in) {
-    pupdevices_ColorAndDistSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
+// pybricks.pupdevices.ColorDistanceSensor.ambient
+STATIC mp_obj_t pupdevices_ColorDistanceSensor_ambient(mp_obj_t self_in) {
+    pupdevices_ColorDistanceSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
     int8_t ambient;
     pbdevice_get_values(self->pbdev, PBIO_IODEV_MODE_PUP_COLOR_DISTANCE_SENSOR__AMBI, &ambient);
     return mp_obj_new_int(ambient);
 }
-MP_DEFINE_CONST_FUN_OBJ_1(pupdevices_ColorAndDistSensor_ambient_obj, pupdevices_ColorAndDistSensor_ambient);
+MP_DEFINE_CONST_FUN_OBJ_1(pupdevices_ColorDistanceSensor_ambient_obj, pupdevices_ColorDistanceSensor_ambient);
 
-// pybricks.pupdevices_ColorDistanceSensor.rgb
-STATIC mp_obj_t pupdevices_ColorAndDistSensor_rgb(mp_obj_t self_in) {
-    pupdevices_ColorAndDistSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
+// pybricks.pupdevices.ColorDistanceSensor.rgb
+STATIC mp_obj_t pupdevices_ColorDistanceSensor_rgb(mp_obj_t self_in) {
+    pupdevices_ColorDistanceSensor_obj_t *self = MP_OBJ_TO_PTR(self_in);
 
     int16_t data[3];
     pbdevice_get_values(self->pbdev, PBIO_IODEV_MODE_PUP_COLOR_DISTANCE_SENSOR__RGB_I, data);
@@ -105,26 +105,26 @@ STATIC mp_obj_t pupdevices_ColorAndDistSensor_rgb(mp_obj_t self_in) {
     }
     return mp_obj_new_tuple(3, rgb);
 }
-MP_DEFINE_CONST_FUN_OBJ_1(pupdevices_ColorAndDistSensor_rgb_obj, pupdevices_ColorAndDistSensor_rgb);
+MP_DEFINE_CONST_FUN_OBJ_1(pupdevices_ColorDistanceSensor_rgb_obj, pupdevices_ColorDistanceSensor_rgb);
 
 /*
-ColorAndDistSensor class tables
+ColorDistanceSensor class tables
 */
-STATIC const mp_rom_map_elem_t pupdevices_ColorAndDistSensor_locals_dict_table[] = {
-    { MP_ROM_QSTR(MP_QSTR_color), MP_ROM_PTR(&pupdevices_ColorAndDistSensor_color_obj) },
-    { MP_ROM_QSTR(MP_QSTR_reflection), MP_ROM_PTR(&pupdevices_ColorAndDistSensor_reflection_obj) },
-    { MP_ROM_QSTR(MP_QSTR_ambient), MP_ROM_PTR(&pupdevices_ColorAndDistSensor_ambient_obj) },
-    { MP_ROM_QSTR(MP_QSTR_distance), MP_ROM_PTR(&pupdevices_ColorAndDistSensor_distance_obj) },
-    { MP_ROM_QSTR(MP_QSTR_rgb), MP_ROM_PTR(&pupdevices_ColorAndDistSensor_rgb_obj) },
-    { MP_ROM_QSTR(MP_QSTR_light), MP_ROM_ATTRIBUTE_OFFSET(pupdevices_ColorAndDistSensor_obj_t, light) },
+STATIC const mp_rom_map_elem_t pupdevices_ColorDistanceSensor_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_color),       MP_ROM_PTR(&pupdevices_ColorDistanceSensor_color_obj)                },
+    { MP_ROM_QSTR(MP_QSTR_reflection),  MP_ROM_PTR(&pupdevices_ColorDistanceSensor_reflection_obj)           },
+    { MP_ROM_QSTR(MP_QSTR_ambient),     MP_ROM_PTR(&pupdevices_ColorDistanceSensor_ambient_obj)              },
+    { MP_ROM_QSTR(MP_QSTR_distance),    MP_ROM_PTR(&pupdevices_ColorDistanceSensor_distance_obj)             },
+    { MP_ROM_QSTR(MP_QSTR_rgb),         MP_ROM_PTR(&pupdevices_ColorDistanceSensor_rgb_obj)                  },
+    { MP_ROM_QSTR(MP_QSTR_light),       MP_ROM_ATTRIBUTE_OFFSET(pupdevices_ColorDistanceSensor_obj_t, light) },
 };
-STATIC MP_DEFINE_CONST_DICT(pupdevices_ColorAndDistSensor_locals_dict, pupdevices_ColorAndDistSensor_locals_dict_table);
+STATIC MP_DEFINE_CONST_DICT(pupdevices_ColorDistanceSensor_locals_dict, pupdevices_ColorDistanceSensor_locals_dict_table);
 
-STATIC const mp_obj_type_t pupdevices_ColorAndDistSensor_type = {
+STATIC const mp_obj_type_t pupdevices_ColorDistanceSensor_type = {
     { &mp_type_type },
     .name = MP_QSTR_ColorDistanceSensor,
-    .make_new = pupdevices_ColorAndDistSensor_make_new,
-    .locals_dict = (mp_obj_dict_t*)&pupdevices_ColorAndDistSensor_locals_dict,
+    .make_new = pupdevices_ColorDistanceSensor_make_new,
+    .locals_dict = (mp_obj_dict_t*)&pupdevices_ColorDistanceSensor_locals_dict,
 };
 
 /*
@@ -134,7 +134,7 @@ STATIC const mp_rom_map_elem_t pupdevices_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__),            MP_ROM_QSTR(MP_QSTR_devices) },
     { MP_ROM_QSTR(MP_QSTR_Motor),               MP_ROM_PTR(&motor_Motor_type)   },
     { MP_ROM_QSTR(MP_QSTR_DCMotor),               MP_ROM_PTR(&motor_DCMotor_type)   },
-    { MP_ROM_QSTR(MP_QSTR_ColorDistanceSensor), MP_ROM_PTR(&pupdevices_ColorAndDistSensor_type) },
+    { MP_ROM_QSTR(MP_QSTR_ColorDistanceSensor), MP_ROM_PTR(&pupdevices_ColorDistanceSensor_type) },
 };
 
 STATIC MP_DEFINE_CONST_DICT (
