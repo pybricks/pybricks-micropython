pybricks/pybricks-micropython

commit d3f8c30a8d95bd0ca75066110c888903d8bd0c00

pbio/trajectory: fix duration of patched maneuver
This was broken by baf891897b10812fe2f7c2948150d180a9d4c5e1

Because the starting point is shifted to the past to intersect with ongoing maneuver, we must compensate that difference in the duration. That way, the end time is indeed the current time plus the duration.

lib/pbio/src/trajectory_ext.c
@@ -66,7 +66,7 @@ static pbio_error_t pbio_trajectory_patch(pbio_trajectory_t *ref, bool time_base
         // Now we can make the new trajectory with a starting point coincident
         // with a point on the existing trajectory
         if (time_based) {
-            return pbio_trajectory_make_time_based(ref, t0, duration, th0, th0_ext, w0, wt, wmax, a);
+            return pbio_trajectory_make_time_based(ref, t0, duration + (nominal.t0 - t0), th0, th0_ext, w0, wt, wmax, a);
         }
         else {
             return pbio_trajectory_make_angle_based(ref, t0, th0, th3, w0, wt, wmax, a);

commit 652f050c88f74e52611dc35fde4d813162e73fd0

pbio/trajectory: fix forever for patched maneuver
This was broken by d3f8c30a8d95bd0ca75066110c888903d8bd0c00

Infinity shifted by a small amount is still infinity.

lib/pbio/src/trajectory_ext.c
@@ -63,10 +63,16 @@ static pbio_error_t pbio_trajectory_patch(pbio_trajectory_t *ref, bool time_base
             th0 = ref->th3;
             th0_ext = ref->th3_ext;
         }
+
+        // We shifted the start time into the past, so we must adjust duration accordingly. But forever remains forever.
+        if (duration != DURATION_FOREVER) {
+            duration += (nominal.t0 - t0);
+        }
+
         // Now we can make the new trajectory with a starting point coincident
         // with a point on the existing trajectory
         if (time_based) {
-            return pbio_trajectory_make_time_based(ref, t0, duration + (nominal.t0 - t0), th0, th0_ext, w0, wt, wmax, a);
+            return pbio_trajectory_make_time_based(ref, t0, duration, th0, th0_ext, w0, wt, wmax, a);
         }
         else {
             return pbio_trajectory_make_angle_based(ref, t0, th0, th3, w0, wt, wmax, a);