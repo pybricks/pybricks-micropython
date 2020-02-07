pybricks/pybricks-micropython

commit baf891897b10812fe2f7c2948150d180a9d4c5e1

pbio/trajectory: drop forever arg
use duration instead.

This makes it easier to see what the functions are doing.

lib/pbio/src/trajectory_ext.c
@@ -9,7 +9,7 @@
 #include <pbio/math.h>
 #include <pbio/trajectory.h>
 
-static pbio_error_t pbio_trajectory_patch(pbio_trajectory_t *ref, bool time_based, bool forever, int32_t t0, int32_t t3, int32_t th3, int32_t wt, int32_t wmax, int32_t a) {
+static pbio_error_t pbio_trajectory_patch(pbio_trajectory_t *ref, bool time_based, int32_t t0, int32_t duration, int32_t th3, int32_t wt, int32_t wmax, int32_t a) {
 
     // Get current reference point and acceleration, which will be the 0-point for the new trajectory
     int32_t th0;
@@ -22,7 +22,7 @@ static pbio_error_t pbio_trajectory_patch(pbio_trajectory_t *ref, bool time_base
     pbio_error_t err;
     pbio_trajectory_t nominal;
     if (time_based) {
-        err = pbio_trajectory_make_time_based(&nominal, forever, t0, t3, th0, th0_ext, w0, wt, wmax, a);    
+        err = pbio_trajectory_make_time_based(&nominal, t0, duration, th0, th0_ext, w0, wt, wmax, a);    
     }
     else {
         err = pbio_trajectory_make_angle_based(&nominal, t0, th0, th3, w0, wt, wmax, a);
@@ -66,7 +66,7 @@ static pbio_error_t pbio_trajectory_patch(pbio_trajectory_t *ref, bool time_base
         // Now we can make the new trajectory with a starting point coincident
         // with a point on the existing trajectory
         if (time_based) {
-            return pbio_trajectory_make_time_based(ref, forever, t0, t3, th0, th0_ext, w0, wt, wmax, a);
+            return pbio_trajectory_make_time_based(ref, t0, duration, th0, th0_ext, w0, wt, wmax, a);
         }
         else {
             return pbio_trajectory_make_angle_based(ref, t0, th0, th3, w0, wt, wmax, a);
@@ -80,10 +80,10 @@ static pbio_error_t pbio_trajectory_patch(pbio_trajectory_t *ref, bool time_base
     }
 }
 
-pbio_error_t pbio_trajectory_make_time_based_patched(pbio_trajectory_t *ref, bool forever, int32_t t0, int32_t t3, int32_t wt, int32_t wmax, int32_t a) {
-    return pbio_trajectory_patch(ref, true, forever, t0, t3, 0, wt, wmax, a);
+pbio_error_t pbio_trajectory_make_time_based_patched(pbio_trajectory_t *ref, int32_t t0, int32_t duration, int32_t wt, int32_t wmax, int32_t a) {
+    return pbio_trajectory_patch(ref, true, t0, duration, 0, wt, wmax, a);
 }
 
 pbio_error_t pbio_trajectory_make_angle_based_patched(pbio_trajectory_t *ref, int32_t t0, int32_t th3, int32_t wt, int32_t wmax, int32_t a) {
-    return pbio_trajectory_patch(ref, false, false, t0, 0, th3, wt, wmax, a);
+    return pbio_trajectory_patch(ref, false, t0, 0, th3, wt, wmax, a);
 }

lib/pbio/include/pbio/trajectory.h
@@ -17,6 +17,8 @@
 #define US_PER_MS (1000)
 #define US_PER_SECOND (1000000)
 
+#define DURATION_FOREVER (-1)
+
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
@@ -54,7 +56,7 @@ typedef struct _pbio_trajectory_t {
 
 void pbio_trajectory_make_stationary(pbio_trajectory_t *ref, int32_t t0, int32_t th0);
 
-pbio_error_t pbio_trajectory_make_time_based(pbio_trajectory_t *ref, bool forever, int32_t t0, int32_t t3, int32_t th0, int32_t th0_ext, int32_t w0, int32_t wt, int32_t wmax, int32_t a);
+pbio_error_t pbio_trajectory_make_time_based(pbio_trajectory_t *ref, int32_t t0, int32_t duration, int32_t th0, int32_t th0_ext, int32_t w0, int32_t wt, int32_t wmax, int32_t a);
 
 pbio_error_t pbio_trajectory_make_angle_based(pbio_trajectory_t *ref, int32_t t0, int32_t th0, int32_t th3, int32_t w0, int32_t wt, int32_t wmax, int32_t a);
 
@@ -62,7 +64,7 @@ void pbio_trajectory_get_reference(pbio_trajectory_t *traject, int32_t time_ref,
 
 // Extended and patched trajectories
 
-pbio_error_t pbio_trajectory_make_time_based_patched(pbio_trajectory_t *ref, bool forever, int32_t t0, int32_t t3, int32_t wt, int32_t wmax, int32_t a);
+pbio_error_t pbio_trajectory_make_time_based_patched(pbio_trajectory_t *ref, int32_t t0, int32_t t3, int32_t wt, int32_t wmax, int32_t a);
 
 pbio_error_t pbio_trajectory_make_angle_based_patched(pbio_trajectory_t *ref, int32_t t0, int32_t th3, int32_t wt, int32_t wmax, int32_t a);
 

lib/pbio/src/drivebase.c
@@ -279,9 +279,8 @@ static pbio_error_t pbio_drivebase_signal_run(pbio_control_t *ctl, int32_t targe
         // Make the new trajectory and try to patch
         err = pbio_trajectory_make_time_based_patched(
             &ctl->trajectory,
-            true,
             time_now,
-            time_now,
+            DURATION_FOREVER,
             target_rate,
             ctl->settings.max_rate,
             ctl->settings.abs_acceleration);
@@ -294,9 +293,8 @@ static pbio_error_t pbio_drivebase_signal_run(pbio_control_t *ctl, int32_t targe
         // Based on the current time and current state
         err = pbio_trajectory_make_time_based(
             &ctl->trajectory,
-            true,
             time_now,
-            time_now,
+            DURATION_FOREVER,
             count_now,
             0,
             rate_now,

lib/pbio/src/servo.c
@@ -352,16 +352,13 @@ pbio_error_t pbio_servo_stop(pbio_servo_t *srv, pbio_actuation_t after_stop) {
     return pbio_servo_actuate(srv, after_stop, control);
 }
 
-static pbio_error_t pbio_servo_run_time_common(pbio_servo_t *srv, int32_t speed, bool forever, int32_t duration, pbio_control_done_t stop_func, pbio_actuation_t after_stop) {
+static pbio_error_t pbio_servo_run_time_common(pbio_servo_t *srv, int32_t rate_ref, int32_t duration, pbio_control_done_t stop_func, pbio_actuation_t after_stop) {
 
     // Get the intitial state based on physical motor state.
     int32_t time_start;
     int32_t count_now;
     int32_t rate_now;
     pbio_error_t err;
-
-    // Get the target rate
-    int32_t rate_ref = pbio_math_mul_i32_fix16(speed, srv->control.settings.counts_per_unit);
 
     // Get the current state and time
     err = servo_get_state(srv, &time_start, &count_now, &rate_now);
@@ -379,9 +376,8 @@ static pbio_error_t pbio_servo_run_time_common(pbio_servo_t *srv, int32_t speed,
         // Make the new trajectory and try to patch
         err = pbio_trajectory_make_time_based_patched(
             &srv->control.trajectory,
-            forever,
             time_start,
-            time_start + duration,
+            duration,
             rate_ref,
             srv->control.settings.max_rate,
             srv->control.settings.abs_acceleration);
@@ -395,9 +391,8 @@ static pbio_error_t pbio_servo_run_time_common(pbio_servo_t *srv, int32_t speed,
         // Based on the current time and current state
         err = pbio_trajectory_make_time_based(
             &srv->control.trajectory,
-            forever,
             time_start,
-            time_start + duration,
+            duration,
             count_now,
             0,
             rate_now,
@@ -419,7 +414,8 @@ static pbio_error_t pbio_servo_run_time_common(pbio_servo_t *srv, int32_t speed,
 }
 
 pbio_error_t pbio_servo_run(pbio_servo_t *srv, int32_t speed) {
-    return pbio_servo_run_time_common(srv, speed, true, 0, pbio_control_never_done, PBIO_ACTUATION_COAST);
+    int32_t target_rate = pbio_control_user_to_counts(&srv->control.settings, speed);
+    return pbio_servo_run_time_common(srv, target_rate, DURATION_FOREVER, pbio_control_never_done, PBIO_ACTUATION_COAST);
 }
 
 static bool run_time_is_done_func(pbio_trajectory_t *trajectory, pbio_control_settings_t *settings, int32_t time, int32_t count, int32_t rate, bool stalled) {
@@ -427,7 +423,8 @@ static bool run_time_is_done_func(pbio_trajectory_t *trajectory, pbio_control_se
 }
 
 pbio_error_t pbio_servo_run_time(pbio_servo_t *srv, int32_t speed, int32_t duration, pbio_actuation_t after_stop) {
-    return pbio_servo_run_time_common(srv, speed, false, duration*US_PER_MS, run_time_is_done_func, after_stop);
+    int32_t target_rate = pbio_control_user_to_counts(&srv->control.settings, speed);
+    return pbio_servo_run_time_common(srv, target_rate, duration*US_PER_MS, run_time_is_done_func, after_stop);
 }
 
 static bool run_until_stalled_is_done_func(pbio_trajectory_t *trajectory, pbio_control_settings_t *settings, int32_t time, int32_t count, int32_t rate, bool stalled) {
@@ -435,7 +432,8 @@ static bool run_until_stalled_is_done_func(pbio_trajectory_t *trajectory, pbio_c
 }
 
 pbio_error_t pbio_servo_run_until_stalled(pbio_servo_t *srv, int32_t speed, pbio_actuation_t after_stop) {
-    return pbio_servo_run_time_common(srv, speed, true, 0, run_until_stalled_is_done_func, after_stop);
+    int32_t target_rate = pbio_control_user_to_counts(&srv->control.settings, speed);
+    return pbio_servo_run_time_common(srv, target_rate, DURATION_FOREVER, run_until_stalled_is_done_func, after_stop);
 }
 
 static bool run_target_is_done_func(pbio_trajectory_t *trajectory, pbio_control_settings_t *settings, int32_t time, int32_t count, int32_t rate, bool stalled) {

lib/pbio/src/trajectory.c
@@ -83,7 +83,7 @@ static int64_t x_time2(int32_t b, int32_t t) {
     return x_time(x_time(b, t), t)/(2*US_PER_MS);
 }
 
-pbio_error_t pbio_trajectory_make_time_based(pbio_trajectory_t *ref, bool forever, int32_t t0, int32_t t3, int32_t th0, int32_t th0_ext, int32_t w0, int32_t wt, int32_t wmax, int32_t a) {
+pbio_error_t pbio_trajectory_make_time_based(pbio_trajectory_t *ref, int32_t t0, int32_t duration, int32_t th0, int32_t th0_ext, int32_t w0, int32_t wt, int32_t wmax, int32_t a) {
 
     // Work with time intervals instead of absolute time. Read 'm' as '-'.
     int32_t t3mt0;
@@ -92,7 +92,7 @@ pbio_error_t pbio_trajectory_make_time_based(pbio_trajectory_t *ref, bool foreve
     int32_t t1mt0;
 
     // Duration of the maneuver
-    if (forever) {
+    if (duration == DURATION_FOREVER) {
         // In case of forever, we set the duration to a fictitious 60 seconds.
         t3mt0 = 60*US_PER_SECOND;
         // This is an infinite maneuver. (This means we'll just ignore the deceleration
@@ -100,8 +100,8 @@ pbio_error_t pbio_trajectory_make_time_based(pbio_trajectory_t *ref, bool foreve
         ref->forever = true; 
     }
     else {
-        // Otherwise, the duration is just the end time minus start time
-        t3mt0 = t3-t0;
+        // Otherwise, the interval is just the duration
+        t3mt0 = duration;
         // This is a finite maneuver
         ref->forever = false;
     }
@@ -173,7 +173,7 @@ pbio_error_t pbio_trajectory_make_time_based(pbio_trajectory_t *ref, bool foreve
     ref->t0 = t0;
     ref->t1 = t0 + t1mt0;
     ref->t2 = t0 + t1mt0 + t2mt1;
-    ref->t3 = t3;
+    ref->t3 = t0 + t3mt0;
 
     // Corresponding angle values with millicount/millideg precision
     int64_t mth0 = as_mcount(th0, th0_ext);

commit d3f8c30a8d95bd0ca75066110c888903d8bd0c00

pbio/trajectory: fix duration of patched maneuver
This was broken by baf891897b10812fe2f7c2948150d180a9d4c5e1

Because the starting point is shifted to the past to intersect with ongoing maneuver, we must compensate that difference in the duration. That way, the end time is indeed the current time plus the duration.

lib/pbio/src/trajectory_ext.c
@@ -66,7 +66,7 @@ static pbio_error_t pbio_trajectory_patch(pbio_trajectory_t *ref, bool time_base
         // Now we can make the new trajectory with a starting point coincident
         // with a point on the existing trajectory
         if (time_based) {
-            return pbio_trajectory_make_time_based(ref, t0, duration, th0, th0_ext, w0, wt, wmax, a);
+            return pbio_trajectory_make_time_based(ref, t0, duration + (nominal.t0 - t0), th0, th0_ext, w0, wt, wmax, a);
         }
         else {
             return pbio_trajectory_make_angle_based(ref, t0, th0, th3, w0, wt, wmax, a);