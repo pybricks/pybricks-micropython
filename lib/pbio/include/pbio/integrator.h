acd578afdfc7cca39d2fa763aeb95f39eeacbcbc
Laurens Valk <laurens@pybricks.com>
2020-02-09 13:06:25 +0100

    pbio/integrator: rename internal variables
    Make the names a bit more specific to distinguish from upcoming new fields.

--- lib/pbio/include/pbio/integrator.h
+++ lib/pbio/include/pbio/integrator.h
@@ -10,7 +10,7 @@
 
 typedef struct _pbio_rate_integrator_t {
     bool running; // Whether the integrator is running (1) or paused (0)
-    int32_t time_paused; // Time at which we began pausing, stopping integration
+    int32_t time_pause_begin; // Time at which we began pausing, stopping integration
     int32_t count_resumed; // Count at which rate integration resumed again
     int32_t count_ref_resumed; // Idealized reference count at restart
     int32_t rate_err_integral_paused; // Total integrated state up to previous pause.
@@ -33,9 +33,9 @@ void pbio_rate_integrator_get_errors(pbio_rate_integrator_t *itg,
 bool pbio_rate_integrator_stalled(pbio_rate_integrator_t *itg, int32_t time_now, int32_t rate, int32_t time_stall, int32_t rate_stall);
 
 typedef struct _pbio_count_integrator_t {
-    bool running; // Whether the integrator is running (1) or paused (0)
-    int32_t time_paused; // Time at which we began pausing most recently, stopping integration
-    int32_t time_waited; // Total time we spent in a paused state
+    bool trajectory_running; // Whether the trajectory is running (1) or paused (0)
+    int32_t time_pause_begin; // Time at which we began pausing most recently, stopping integration
+    int32_t time_paused_total; // Total time we spent in a paused state
     int32_t count_err_prev; // Position error in the previous control iteration
     int32_t time_prev; // Time at the previous control iteratiom
     int32_t count_err_integral; // Ongoing integral of position error
--- lib/pbio/src/integrator.c
+++ lib/pbio/src/integrator.c
@@ -28,7 +28,7 @@ void pbio_rate_integrator_pause(pbio_rate_integrator_t *itg, int32_t time_now, i
     itg->rate_err_integral_paused += count_ref - itg->count_ref_resumed - count + itg->count_resumed;
 
     // Store time at which we started pausing
-    itg->time_paused = time_now;
+    itg->time_pause_begin = time_now;
 }
 
 void pbio_rate_integrator_resume(pbio_rate_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref) {
@@ -92,7 +92,7 @@ bool pbio_rate_integrator_stalled(pbio_rate_integrator_t *itg, int32_t time_now,
     }
 
     // If the integrator is paused for less than the stall time, we're still not stalled for now.
-    if (time_now - itg->time_paused < time_stall) {
+    if (time_now - itg->time_pause_begin < time_stall) {
         return false;
     }
 
@@ -104,36 +104,36 @@ bool pbio_rate_integrator_stalled(pbio_rate_integrator_t *itg, int32_t time_now,
 
 int32_t pbio_count_integrator_get_ref_time(pbio_count_integrator_t *itg, int32_t time_now) {
     // The wall time at which we are is either the current time, or whenever we stopped last
-    int32_t real_time = itg->running ? time_now : itg->time_paused;
+    int32_t real_time = itg->trajectory_running ? time_now : itg->time_pause_begin;
 
     // But we want to evaluate the reference compensating for the time we spent waiting
-    return real_time - itg->time_waited;
+    return real_time - itg->time_paused_total;
 }
 
 void pbio_count_integrator_pause(pbio_count_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref) {
 
     // Return if already paused
-    if (!itg->running) {
+    if (!itg->trajectory_running) {
         return;
     }
 
     // Disable the integrator
-    itg->running = false;
-    itg->time_paused = time_now;
+    itg->trajectory_running = false;
+    itg->time_pause_begin = time_now;
 }
 
 void pbio_count_integrator_resume(pbio_count_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref) {
 
-    // Return if already running
-    if (itg->running) {
+    // Return if already trajectory_running
+    if (itg->trajectory_running) {
         return;
     }
 
     // Then we must restart the time
-    itg->running = true;
+    itg->trajectory_running = true;
 
     // Increment total wait time by time elapsed since we started pausing
-    itg->time_waited += time_now - itg->time_paused;
+    itg->time_paused_total += time_now - itg->time_pause_begin;
 
 }
 
@@ -141,11 +141,11 @@ void pbio_count_integrator_reset(pbio_count_integrator_t *itg, int32_t time_now,
 
     // Reset integrator state variables
     itg->count_err_integral = 0;
-    itg->time_waited = 0;
+    itg->time_paused_total = 0;
     itg->time_prev = time_now;
-    itg->time_paused = time_now;
+    itg->time_pause_begin = time_now;
     itg->count_err_prev = 0;
-    itg->running = false;
+    itg->trajectory_running = false;
     itg->count_err_integral_max = max;
 
     // Resume integration
@@ -155,7 +155,7 @@ void pbio_count_integrator_reset(pbio_count_integrator_t *itg, int32_t time_now,
 
 void pbio_count_integrator_update(pbio_count_integrator_t *itg, int32_t time_now, int32_t count, int32_t count_ref, int32_t *count_err, int32_t *count_err_integral) {
     // Integrate and update position error
-    if (itg->running) {
+    if (itg->trajectory_running) {
         itg->count_err_integral += itg->count_err_prev*(time_now - itg->time_prev);
 
         if (itg->count_err_integral > itg->count_err_integral_max) {
@@ -177,7 +177,7 @@ void pbio_count_integrator_update(pbio_count_integrator_t *itg, int32_t time_now
 
 bool pbio_count_integrator_stalled(pbio_count_integrator_t *itg, int32_t time_now, int32_t rate, int32_t time_stall, int32_t rate_stall) {
     // If we're running and the integrator is not saturated, we're not stalled
-    if (itg->running && abs(itg->count_err_integral) < itg->count_err_integral_max) {
+    if (itg->trajectory_running && abs(itg->count_err_integral) < itg->count_err_integral_max) {
         return false;
     }
 
@@ -187,7 +187,7 @@ bool pbio_count_integrator_stalled(pbio_count_integrator_t *itg, int32_t time_no
     }
 
     // If the integrator is paused for less than the stall time, we're still not stalled for now.
-    if (time_now - itg->time_paused < time_stall) {
+    if (time_now - itg->time_pause_begin < time_stall) {
         return false;
     }
 
