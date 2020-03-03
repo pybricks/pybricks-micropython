b3e71823875944f3ded5b267e0851f0a81705baf
Laurens Valk <laurens@pybricks.com>
2020-03-03 17:52:01 +0100

    bricks/ev3dev/modules: rename boot.py to core.py
    We are not using it in the same was as MicroPython "boot.py", so we rename it to avoid confusion.
    
    It is merely a shortcut to import core functionality quickly in the REPL; it does not run automatically on boot.
    
    Also, make the imports match the imports of the new project template. This covers core functionality, which are common classes/functions that cover most examples and typical robot projects that use one EV3 kit.

---
+++ bricks/ev3dev/modules/core.py
@@ -0,0 +1,10 @@
+"""Conveniently import core functionality for use on the REPL."""
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import ImageFile, SoundFile
+
+# Create an instance of the EV3 Brick
+ev3 = EV3Brick()
