82be85ef32fa4da47ba256e97b23c1007584a294
Laurens Valk <laurens@pybricks.com>
2020-03-17 19:43:20 +0100

    py/pberror.h generalize type assertion
    This is useful outside the enum type, so share it.

--- py/pb_type_enum.c
+++ py/pb_type_enum.c
@@ -14,14 +14,7 @@ void pb_type_enum_print(const mp_print_t *print,  mp_obj_t self_in, mp_print_kin
 }
 
 mp_int_t pb_type_enum_get_value(mp_obj_t obj, const mp_obj_type_t *type) {
-    if (!mp_obj_is_type(obj, type)) {
-        if (MICROPY_ERROR_REPORTING == MICROPY_ERROR_REPORTING_TERSE) {
-            mp_raise_TypeError("requires enum");
-        } else {
-            nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_TypeError,
-                "can't convert %s to %s", mp_obj_get_type_str(obj), qstr_str(type->name)));
-        }
-    }
+    pb_assert_type(obj, type);
     pb_obj_enum_member_t *member = MP_OBJ_TO_PTR(obj);
     return member->value;
 }
--- py/pberror.c
+++ py/pberror.c
@@ -72,3 +72,14 @@ void pb_assert(pbio_error_t error) {
 
     mp_raise_OSError(os_err);
 }
+
+void pb_assert_type(mp_obj_t obj, const mp_obj_type_t *type) {
+    if (!mp_obj_is_type(obj, type)) {
+        #if MICROPY_ERROR_REPORTING == MICROPY_ERROR_REPORTING_TERSE
+        mp_raise_TypeError(NULL);
+        #else
+        nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_TypeError,
+            "can't convert %s to %s", mp_obj_get_type_str(obj), qstr_str(type->name)));
+        #endif
+    }
+}
