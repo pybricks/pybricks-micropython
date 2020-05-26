8a9be36696de8cf17b6c2bdd79f5c30de6252304
David Lechner <david@pybricks.com>
2020-05-25 20:50:09 -0500

    npm/firmware: new package for Pybricks firmware
    This is a node package that will be used to distribute firmware blobs.

---
+++ npm/firmware/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Name of the Technic Control+ hub firmware file.
+ */
+export const cplushub = 'cplushub.zip'
+
+/**
+ * Name of the BOOST Move hub firmware file.
+ */
+export const movehub = 'movehub.zip'
