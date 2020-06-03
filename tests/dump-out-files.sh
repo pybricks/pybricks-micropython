9b1dd98b0be78719ff1a82569bf3ece79ca651bf
David Lechner <david@pybricks.com>
2020-06-03 12:28:10 -0500

    tests/dump-out-files: fix relative path
    This was missed when making micropython a submodule.

--- tests/dump-out-files.sh
+++ tests/dump-out-files.sh
@@ -3,7 +3,7 @@
 # Run this script when tests fail to see if there is any useful output.
 
 SEP="------------------------------------------------------------------------"
-TEST_DIR=$(readlink -f $(dirname $(readlink -f $0))/../../../tests)
+TEST_DIR=$(readlink -f $(dirname $(readlink -f $0))/../micropython/tests)
 
 find ${TEST_DIR} -name "*.out" -exec echo {} \; -exec echo ${SEP} \; \
     -exec cat {} \; -exec echo \; -exec echo ${SEP} \; -exec echo \;
