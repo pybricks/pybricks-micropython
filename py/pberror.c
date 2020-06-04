4f5de1df6717794973720a8a7a0510f9bfb8c5de
David Lechner <david@pybricks.com>
2020-06-04 14:23:38 -0500

    py/pberror: make use of new mp_raise_msg_varg()
    This is a new upstream function that make the call a bit less verbose.

--- py/pberror.c
+++ py/pberror.c
@@ -142,8 +142,8 @@ void pb_assert_type(mp_obj_t obj, const mp_obj_type_t *type) {
         #if MICROPY_ERROR_REPORTING == MICROPY_ERROR_REPORTING_TERSE
         mp_raise_TypeError(NULL);
         #else
-        nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_TypeError,
-            "can't convert %s to %s", mp_obj_get_type_str(obj), qstr_str(type->name)));
+        mp_raise_msg_varg(&mp_type_TypeError, "can't convert %s to %s",
+            mp_obj_get_type_str(obj), qstr_str(type->name));
         #endif
     }
 }
