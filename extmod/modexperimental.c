6cd52d916450d97a206beec5b10987089de450cf
David Lechner <david@pybricks.com>
2020-06-04 16:19:45 -0500

    extmod/modexperimental: prepare for use on other hubs
    This rearranges things a bit so other hubs don't have to enable
    MICROPY_MODULE_BUILTIN_INIT in order to use this module.

--- extmod/modexperimental.c
+++ extmod/modexperimental.c
@@ -8,32 +8,28 @@
 
 #if PYBRICKS_PY_EXPERIMENTAL
 
-#if !MICROPY_MODULE_BUILTIN_INIT
-#error "pybricks.experimental module requires that MICROPY_MODULE_BUILTIN_INIT is enabled"
-#endif
-
-#if PYBRICKS_HUB_EV3
-#include <signal.h>
-#endif // PYBRICKS_HUB_EV3
-
-#include "py/mpthread.h"
 #include "py/obj.h"
 #include "py/runtime.h"
 
 #if PYBRICKS_HUB_EV3
+#if !MICROPY_MODULE_BUILTIN_INIT
+#error "pybricks.experimental module requires that MICROPY_MODULE_BUILTIN_INIT is enabled"
+#endif
+
+#include <signal.h>
+
+#include "py/mpthread.h"
+
 STATIC void sighandler() {
     // we just want the signal to interrupt system calls
 }
-#endif // PYBRICKS_HUB_EV3
 
 STATIC mp_obj_t mod_experimental___init__() {
-    #if PYBRICKS_HUB_EV3
     struct sigaction sa;
     sa.sa_flags = 0;
     sa.sa_handler = sighandler;
     sigemptyset(&sa.sa_mask);
     sigaction(SIGUSR2, &sa, NULL);
-    #endif // PYBRICKS_HUB_EV3
 
     return mp_const_none;
 }
@@ -47,11 +43,14 @@ STATIC mp_obj_t mod_experimental_pthread_raise(mp_obj_t thread_id_in, mp_obj_t e
     return mp_obj_new_int(mp_thread_schedule_exception(thread_id, ex_in));
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(mod_experimental_pthread_raise_obj, mod_experimental_pthread_raise);
+#endif // PYBRICKS_HUB_EV3
 
 STATIC const mp_rom_map_elem_t mod_experimental_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_experimental_c) },
+    #if PYBRICKS_HUB_EV3
     { MP_ROM_QSTR(MP_QSTR___init__), MP_ROM_PTR(&mod_experimental___init___obj) },
     { MP_ROM_QSTR(MP_QSTR_pthread_raise), MP_ROM_PTR(&mod_experimental_pthread_raise_obj) },
+    #endif // PYBRICKS_HUB_EV3
 };
 STATIC MP_DEFINE_CONST_DICT(mod_experimental_globals, mod_experimental_globals_table);
 
