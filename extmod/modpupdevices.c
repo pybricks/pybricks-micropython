f17b7d1334580fe9ec48968b10ba8b6a98ab9f5d
Laurens Valk <laurens@pybricks.com>
2020-06-08 21:09:48 +0200

    extmod/modpupdevices: add Light
    This adds support for the Powered Up light.

--- extmod/modpupdevices.c
+++ extmod/modpupdevices.c
@@ -526,6 +526,64 @@ STATIC const mp_obj_type_t pupdevices_ForceSensor_type = {
     .locals_dict = (mp_obj_dict_t *)&pupdevices_ForceSensor_locals_dict,
 };
 
+// Class structure for Light
+typedef struct _pupdevices_Light_obj_t {
+    mp_obj_base_t base;
+    pbdevice_t *pbdev;
+} pupdevices_Light_obj_t;
+
+// pybricks.pupdevices.Light.__init__
+STATIC mp_obj_t pupdevices_Light_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
+    PB_PARSE_ARGS_CLASS(n_args, n_kw, args,
+        PB_ARG_REQUIRED(port));
+
+    pupdevices_Light_obj_t *self = m_new_obj(pupdevices_Light_obj_t);
+    self->base.type = (mp_obj_type_t *)type;
+
+    mp_int_t port_num = pb_type_enum_get_value(port, &pb_enum_type_Port);
+
+    // Get iodevices
+    self->pbdev = pbdevice_get_device(port_num, PBIO_IODEV_TYPE_ID_LPF2_LIGHT);
+
+    return MP_OBJ_FROM_PTR(self);
+}
+
+// pybricks.pupdevices.Light.on
+STATIC mp_obj_t pupdevices_Light_on(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+    PB_PARSE_ARGS_METHOD(n_args, pos_args, kw_args,
+        pupdevices_Light_obj_t, self,
+        PB_ARG_DEFAULT_INT(brightness, 100));
+
+    // Set the brightness
+    pbdevice_set_power_supply(self->pbdev, pb_obj_get_int(brightness));
+
+    return mp_const_none;
+}
+MP_DEFINE_CONST_FUN_OBJ_KW(pupdevices_Light_on_obj, 1, pupdevices_Light_on);
+
+// pybricks.pupdevices.Light.off
+STATIC mp_obj_t pupdevices_Light_off(mp_obj_t self_in) {
+    pupdevices_Light_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    pbdevice_set_power_supply(self->pbdev, 0);
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(pupdevices_Light_off_obj, pupdevices_Light_off);
+
+// dir(pybricks.pupdevices.Light)
+STATIC const mp_rom_map_elem_t pupdevices_Light_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_on),      MP_ROM_PTR(&pupdevices_Light_on_obj)           },
+    { MP_ROM_QSTR(MP_QSTR_off),     MP_ROM_PTR(&pupdevices_Light_off_obj)          },
+};
+STATIC MP_DEFINE_CONST_DICT(pupdevices_Light_locals_dict, pupdevices_Light_locals_dict_table);
+
+// type(pybricks.pupdevices.Light)
+STATIC const mp_obj_type_t pupdevices_Light_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_Light,
+    .make_new = pupdevices_Light_make_new,
+    .locals_dict = (mp_obj_dict_t *)&pupdevices_Light_locals_dict,
+};
+
 // dir(pybricks.pupdevices)
 STATIC const mp_rom_map_elem_t pupdevices_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__),            MP_ROM_QSTR(MP_QSTR_pupdevices)                  },
@@ -535,6 +593,7 @@ STATIC const mp_rom_map_elem_t pupdevices_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_ColorSensor),         MP_ROM_PTR(&pupdevices_ColorSensor_type)         },
     { MP_ROM_QSTR(MP_QSTR_UltrasonicSensor),    MP_ROM_PTR(&pupdevices_UltrasonicSensor_type)    },
     { MP_ROM_QSTR(MP_QSTR_ForceSensor),         MP_ROM_PTR(&pupdevices_ForceSensor_type)         },
+    { MP_ROM_QSTR(MP_QSTR_Light),               MP_ROM_PTR(&pupdevices_Light_type)               },
 };
 
 STATIC MP_DEFINE_CONST_DICT(
