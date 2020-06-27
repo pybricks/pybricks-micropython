62ecced801d980523041d958730a6b0e685b46f7
David Lechner <david@pybricks.com>
2020-06-26 20:41:47 -0500

    platform/city_hub: fix I/O port gpios

--- lib/pbio/platform/city_hub/platform.c
+++ lib/pbio/platform/city_hub/platform.c
@@ -24,8 +24,8 @@ const pbdrv_button_gpio_platform_t pbdrv_button_gpio_platform[PBDRV_CONFIG_BUTTO
 
 // Port A - USART3
 const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_0 = {
-    .id1 = { .bank = GPIOB, .pin = 1  },
-    .id2 = { .bank = GPIOC, .pin = 3  },
+    .id1 = { .bank = GPIOA, .pin = 1  },
+    .id2 = { .bank = GPIOA, .pin = 3  },
     .uart_buf = { .bank = GPIOB, .pin = 5  },
     .uart_tx = { .bank = GPIOC, .pin = 4  },
     .uart_rx = { .bank = GPIOC, .pin = 5  },
@@ -34,7 +34,7 @@ const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_0 = {
 
 // Port B - USART4
 const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_1 = {
-    .id1 = { .bank = GPIOB, .pin = 0  },
+    .id1 = { .bank = GPIOA, .pin = 0  },
     .id2 = { .bank = GPIOA, .pin = 2  },
     .uart_buf = { .bank = GPIOB, .pin = 4  },
     .uart_tx = { .bank = GPIOC, .pin = 10  },
