b6c59d5e3c020f0f484ce354ae7d27b9e24f5a80
David Lechner <david@pybricks.com>
2020-07-02 19:14:51 -0500

    @pybricks/firmare: add cityhub constant
    Firmware assets now include this hub, so we need to make it part of the API.

--- npm/firmware/index.ts
+++ npm/firmware/index.ts
@@ -4,6 +4,11 @@
 export const cplushub = 'cplushub.zip'
 
 /**
+ * Name of the 2-port Powered Up hub firmware file.
+ */
+export const cityhub = 'cityhub.zip'
+
+/**
  * Name of the BOOST Move hub firmware file.
  */
 export const movehub = 'movehub.zip'
