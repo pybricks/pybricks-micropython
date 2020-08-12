pybricks/pybricks-micropython

commit 712f8394f58ffeb6d6691622be37d9388e6073b6

drv/led: new LED drivers
This consolidates the platform-specific light drivers into a couple of
new LED drivers.

Instead of color lookup tables, drivers can now show the full spectrum
of hues in the HSV color model plus white.

Currently, the same calibration values are used on all platforms. The
values were derived from the Move/City hub LEDs so the colors are a bit
off on the Control+ hub since it has a different LED.

All LED PWM periods are now set to 10000 to match the Control+ hub.
This way we don't have to have platform data for different periods on
different devices. (Control+ hub shares LED PWMs with motor PWMs, so
it can't be changed.)

lib/pbio/src/light.c
@@ -7,16 +7,16 @@
 
 #include <stdbool.h>
 
+#include <pbdrv/led.h>
+
 #include <pbio/color.h>
 #include <pbio/error.h>
 #include <pbio/light.h>
 #include <pbio/port.h>
 
-#include <pbdrv/light.h>
-
 typedef struct {
     pbio_light_pattern_t pattern;
-    pbdrv_light_raw_rgb_t raw;
+    pbio_color_t color;
 } user_data_t;
 
 static user_data_t user_light_data;
@@ -38,7 +38,6 @@ static const uint8_t breathe_pattern_data[64] = {
 
 pbio_error_t _pbio_light_on(pbio_port_t port, pbio_color_t color, pbio_light_pattern_t pattern) {
     user_data_t data;
-    pbio_error_t err;
 
     if (port != PBIO_PORT_SELF) {
         // TODO: handle lights on I/O ports
@@ -49,11 +48,7 @@ pbio_error_t _pbio_light_on(pbio_port_t port, pbio_color_t color, pbio_light_pat
         return PBIO_ERROR_INVALID_ARG;
     }
 
-    err = pbdrv_light_get_rgb_for_color(port, color, &data.raw);
-    if (err != PBIO_SUCCESS) {
-        return err;
-    }
-
+    data.color = color;
     data.pattern = pattern;
 
     user_light_data = data;
@@ -72,6 +67,9 @@ void _pbio_light_poll(uint32_t now) {
 
     data = user_light_data;
 
+    pbio_color_hsv_t hsv;
+    pbio_color_to_hsv(data.color, &hsv);
+
     switch (data.pattern) {
         case PBIO_LIGHT_PATTERN_NONE:
             break;
@@ -79,21 +77,20 @@ void _pbio_light_poll(uint32_t now) {
             // breathe pattern has 64 values over the course of two seconds (2048ms)
             idx = (now >> 5) & (64 - 1);
             scale = breathe_pattern_data[idx] + 1;
-            data.raw.r = data.raw.r * scale / 256;
-            data.raw.g = data.raw.g * scale / 256;
-            data.raw.b = data.raw.b * scale / 256;
+            hsv.v = 101 * scale / 256;
             break;
         case PBIO_LIGHT_PATTERN_FLASH:
             // flash pattern has 8 value over the course of two seconds (2048ms)
             idx = (now >> 8) & (8 - 1);
-            scale = flash_pattern_data[idx] + 1;
-            data.raw.r = data.raw.r * scale / 256;
-            data.raw.g = data.raw.g * scale / 256;
-            data.raw.b = data.raw.b * scale / 256;
+            scale = flash_pattern_data[idx];
+            hsv.v = 100 * scale;
             break;
     }
 
-    pbdrv_light_set_rgb(PBIO_PORT_SELF, &data.raw);
+    pbdrv_led_dev_t *led;
+    if (pbdrv_led_get_dev(0, &led) == PBIO_SUCCESS) {
+        pbdrv_led_set_hsv(led, &hsv);
+    }
 }
 
 /**

lib/pbio/drv/city_hub/light.c
@@ -1,68 +1,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2020 The Pybricks Authors
-
-#include <pbdrv/config.h>
-
-#if PBDRV_CONFIG_LIGHT
-
-#include <pbdrv/light.h>
-#include <pbdrv/pwm.h>
-#include <pbio/color.h>
-#include <pbio/config.h>
-#include <pbio/error.h>
-#include <pbio/port.h>
-
-pbio_error_t pbdrv_light_set_rgb(pbio_port_t port, const pbdrv_light_raw_rgb_t *raw) {
-    if (port == PBIO_PORT_B || port == PBIO_PORT_A) {
-        // TODO: check for Powered UP Lights connected to ports C/D
-        return PBIO_ERROR_NO_DEV;
-    }
-
-    if (port != PBIO_PORT_SELF) {
-        return PBIO_ERROR_INVALID_PORT;
-    }
-
-    pbdrv_pwm_dev_t *dev;
-    if (pbdrv_pwm_get_dev(2, &dev) == PBIO_SUCCESS) {
-        pbdrv_pwm_set_duty(dev, 1, raw->r);
-    }
-    if (pbdrv_pwm_get_dev(1, &dev) == PBIO_SUCCESS) {
-        pbdrv_pwm_set_duty(dev, 1, raw->g);
-        pbdrv_pwm_set_duty(dev, 2, raw->b);
-    }
-
-    return PBIO_SUCCESS;
-}
-
-pbio_error_t pbdrv_light_get_rgb_for_color(pbio_port_t port, pbio_color_t color,
-    pbdrv_light_raw_rgb_t *raw) {
-    if (port == PBIO_PORT_B || port == PBIO_PORT_A) {
-        // TODO: check for Powered UP Lights connected to ports C/D
-        return PBIO_ERROR_NO_DEV;
-    }
-
-    if (port != PBIO_PORT_SELF) {
-        return PBIO_ERROR_INVALID_PORT;
-    }
-
-    pbio_color_rgb_t rgb;
-    pbio_color_to_rgb(color, &rgb);
-
-    // Adjust for chromacity
-    uint32_t r = rgb.r * 1000;
-    uint32_t g = rgb.g * 270;
-    uint32_t b = rgb.b * 200;
-
-    // Adjust for apparent brightness
-    // These are basically the luminous intensity values from the datasheet
-    // with red multiplied by 0.35 (it has different resistor and voltage drop)
-    // + 1 protects against division by zero
-    uint32_t Y = ((174 * r + 1590 * g + 327 * b) >> 16) + 1;
-    raw->r = r / Y;
-    raw->g = g / Y;
-    raw->b = b / Y;
-
-    return PBIO_SUCCESS;
-}
-
-#endif // PBDRV_CONFIG_LIGHT

lib/pbio/drv/cplus_hub/light.c
@@ -1,60 +1,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2020 The Pybricks Authors
-
-#include <pbdrv/config.h>
-
-#if PBDRV_CONFIG_LIGHT
-
-#include <pbdrv/light.h>
-#include <pbdrv/pwm.h>
-#include <pbio/color.h>
-#include <pbio/config.h>
-#include <pbio/error.h>
-#include <pbio/port.h>
-
-pbio_error_t pbdrv_light_set_rgb(pbio_port_t port, const pbdrv_light_raw_rgb_t *raw) {
-    if (port != PBIO_PORT_SELF) {
-        return PBIO_ERROR_INVALID_PORT;
-    }
-
-    // Timers have period of 10000 and we want period / 5 as max brightness.
-
-    pbdrv_pwm_dev_t *dev;
-    if (pbdrv_pwm_get_dev(1, &dev) == PBIO_SUCCESS) {
-        pbdrv_pwm_set_duty(dev, 2, raw->r * 2000 / 256);
-    }
-    if (pbdrv_pwm_get_dev(0, &dev) == PBIO_SUCCESS) {
-        pbdrv_pwm_set_duty(dev, 4, raw->g * 2000 / 256);
-    }
-    if (pbdrv_pwm_get_dev(2, &dev) == PBIO_SUCCESS) {
-        pbdrv_pwm_set_duty(dev, 1, raw->b * 2000 / 256);
-    }
-
-    return PBIO_SUCCESS;
-}
-
-pbio_error_t pbdrv_light_get_rgb_for_color(pbio_port_t port, pbio_color_t color,
-    pbdrv_light_raw_rgb_t *raw) {
-    if (port != PBIO_PORT_SELF) {
-        return PBIO_ERROR_INVALID_PORT;
-    }
-
-    pbio_color_rgb_t rgb;
-    pbio_color_to_rgb(color, &rgb);
-
-    // Adjust for chromacity
-    uint32_t r = rgb.r * 1000;
-    uint32_t g = rgb.g * 270;
-    uint32_t b = rgb.b * 200;
-
-    // Adjust for apparent brightness
-    // + 1 protects against division by zero
-    uint32_t Y = ((174 * r + 1590 * g + 327 * b) >> 16) + 1;
-    raw->r = r / Y;
-    raw->g = g / Y;
-    raw->b = b / Y;
-
-    return PBIO_SUCCESS;
-}
-
-#endif // PBDRV_CONFIG_LIGHT

lib/pbio/drv/debug/light.c
@@ -1,58 +1,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2020 The Pybricks Authors
-
-#include <pbdrv/config.h>
-
-#if PBDRV_CONFIG_LIGHT
-
-#include <pbdrv/light.h>
-#include <pbdrv/pwm.h>
-#include <pbio/color.h>
-#include <pbio/config.h>
-#include <pbio/error.h>
-#include <pbio/port.h>
-
-pbio_error_t pbdrv_light_set_rgb(pbio_port_t port, const pbdrv_light_raw_rgb_t *raw) {
-    if (port != PBIO_PORT_SELF) {
-        return PBIO_ERROR_INVALID_PORT;
-    }
-
-    pbdrv_pwm_dev_t *dev;
-    if (pbdrv_pwm_get_dev(2, &dev) == PBIO_SUCCESS) {
-        pbdrv_pwm_set_duty(dev, 1, raw->r);
-    }
-    if (pbdrv_pwm_get_dev(0, &dev) == PBIO_SUCCESS) {
-        pbdrv_pwm_set_duty(dev, 3, raw->g);
-    }
-    if (pbdrv_pwm_get_dev(1, &dev) == PBIO_SUCCESS) {
-        pbdrv_pwm_set_duty(dev, 2, raw->b);
-    }
-
-    return PBIO_SUCCESS;
-}
-
-pbio_error_t pbdrv_light_get_rgb_for_color(pbio_port_t port, pbio_color_t color,
-    pbdrv_light_raw_rgb_t *raw) {
-    if (port != PBIO_PORT_SELF) {
-        return PBIO_ERROR_INVALID_PORT;
-    }
-
-    pbio_color_rgb_t rgb;
-    pbio_color_to_rgb(color, &rgb);
-
-    // Adjust for chromacity
-    uint32_t r = rgb.r * 1000;
-    uint32_t g = rgb.g * 270;
-    uint32_t b = rgb.b * 200;
-
-    // Adjust for apparent brightness
-    // + 1 protects against division by zero
-    uint32_t Y = ((174 * r + 1590 * g + 327 * b) >> 16) + 1;
-    raw->r = r / Y;
-    raw->g = g / Y;
-    raw->b = b / Y;
-
-    return PBIO_SUCCESS;
-}
-
-#endif // PBDRV_CONFIG_LIGHT

lib/pbio/drv/ev3dev_stretch/light.c
@@ -1,106 +1,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2020 The Pybricks Authors
-
-#include <pbdrv/config.h>
-
-#if PBDRV_CONFIG_LIGHT
-
-#include <stdio.h>
-
-#include <pbdrv/light.h>
-#include <pbio/color.h>
-#include <pbio/config.h>
-#include <pbio/error.h>
-#include <pbio/port.h>
-
-#define NLEDS 4
-
-static FILE *f_brightness[NLEDS];
-
-void _pbdrv_light_init(void) {
-
-    const char *const trigger_paths[NLEDS] = {
-        "/sys/class/leds/led0:red:brick-status/trigger",
-        "/sys/class/leds/led1:red:brick-status/trigger",
-        "/sys/class/leds/led0:green:brick-status/trigger",
-        "/sys/class/leds/led1:green:brick-status/trigger"
-    };
-
-    const char *const brightness_paths[NLEDS] = {
-        "/sys/class/leds/led0:red:brick-status/brightness",
-        "/sys/class/leds/led1:red:brick-status/brightness",
-        "/sys/class/leds/led0:green:brick-status/brightness",
-        "/sys/class/leds/led1:green:brick-status/brightness"
-    };
-
-    for (int led = 0; led < NLEDS; led++) {
-        FILE *f_trigger = fopen(trigger_paths[led], "w");
-        if (!f_trigger) {
-            continue;
-        }
-        fprintf(f_trigger, "none");
-        fclose(f_trigger);
-        f_brightness[led] = fopen(brightness_paths[led], "w");
-        if (f_brightness[led]) {
-            setbuf(f_brightness[led], NULL);
-        }
-    }
-}
-
-#if PBIO_CONFIG_ENABLE_DEINIT
-void _pbdrv_light_deinit(void) {
-    for (int led = 0; led < NLEDS; led++) {
-        if (f_brightness[led]) {
-            fclose(f_brightness[led]);
-        }
-    }
-}
-#endif
-
-pbio_error_t pbdrv_light_set_rgb(pbio_port_t port, const pbdrv_light_raw_rgb_t *raw) {
-    if (port != PBIO_PORT_SELF) {
-        return PBIO_ERROR_INVALID_PORT;
-    }
-    for (int led = 0; led < NLEDS; led++) {
-        if (f_brightness[led]) {
-            int ret;
-
-            ret = fseek(f_brightness[led], 0, SEEK_SET);
-            if (ret == -1) {
-                return PBIO_ERROR_IO;
-            }
-            ret = fprintf(f_brightness[led], "%d", led < 2 ? raw->r : raw->g);
-            if (ret < 0) {
-                return PBIO_ERROR_IO;
-            }
-        }
-    }
-    return PBIO_SUCCESS;
-}
-
-pbio_error_t pbdrv_light_get_rgb_for_color(pbio_port_t port, pbio_color_t color,
-    pbdrv_light_raw_rgb_t *raw) {
-
-    if (port != PBIO_PORT_SELF) {
-        return PBIO_ERROR_INVALID_PORT;
-    }
-
-    pbio_color_rgb_t rgb;
-    pbio_color_to_rgb(color, &rgb);
-
-    // Adjust for chromacity
-    uint32_t r = rgb.r * 1000;
-    uint32_t g = rgb.g * 270;
-    uint32_t b = rgb.b * 200;
-
-    // Adjust for apparent brightness
-    // + 1 protects against division by zero
-    uint32_t Y = ((174 * r + 1590 * g + 327 * b) >> 16) + 1;
-    raw->r = r / Y;
-    raw->g = g / Y;
-    raw->b = b / Y;
-
-    return PBIO_SUCCESS;
-}
-
-#endif // PBDRV_CONFIG_LIGHT

lib/pbio/drv/move_hub/light.c
@@ -1,68 +1,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2020 The Pybricks Authors
-
-#include <pbdrv/config.h>
-
-#if PBDRV_CONFIG_LIGHT
-
-#include <pbdrv/light.h>
-#include <pbdrv/pwm.h>
-#include <pbio/color.h>
-#include <pbio/config.h>
-#include <pbio/error.h>
-#include <pbio/port.h>
-
-pbio_error_t pbdrv_light_set_rgb(pbio_port_t port, const pbdrv_light_raw_rgb_t *rgb) {
-    if (port == PBIO_PORT_C || port == PBIO_PORT_D) {
-        // TODO: check for Powered UP Lights connected to ports C/D
-        return PBIO_ERROR_NO_DEV;
-    }
-
-    if (port != PBIO_PORT_SELF) {
-        return PBIO_ERROR_INVALID_PORT;
-    }
-
-    pbdrv_pwm_dev_t *dev;
-    if (pbdrv_pwm_get_dev(3, &dev) == PBIO_SUCCESS) {
-        pbdrv_pwm_set_duty(dev, 1, rgb->r);
-    }
-    if (pbdrv_pwm_get_dev(2, &dev) == PBIO_SUCCESS) {
-        pbdrv_pwm_set_duty(dev, 1, rgb->g);
-        pbdrv_pwm_set_duty(dev, 2, rgb->b);
-    }
-
-    return PBIO_SUCCESS;
-}
-
-pbio_error_t pbdrv_light_get_rgb_for_color(pbio_port_t port, pbio_color_t color,
-    pbdrv_light_raw_rgb_t *raw) {
-    if (port == PBIO_PORT_C || port == PBIO_PORT_D) {
-        // TODO: check for Powered UP Lights connected to ports C/D
-        return PBIO_ERROR_NO_DEV;
-    }
-
-    if (port != PBIO_PORT_SELF) {
-        return PBIO_ERROR_INVALID_PORT;
-    }
-
-    pbio_color_rgb_t rgb;
-    pbio_color_to_rgb(color, &rgb);
-
-    // Adjust for chromacity
-    uint32_t r = rgb.r * 1000;
-    uint32_t g = rgb.g * 270;
-    uint32_t b = rgb.b * 200;
-
-    // Adjust for apparent brightness
-    // These are basically the luminous intensity values from the datasheet
-    // with red multiplied by 0.35 (it has different resistor and voltage drop)
-    // + 1 protects against division by zero
-    uint32_t Y = ((174 * r + 1590 * g + 327 * b) >> 16) + 1;
-    raw->r = r / Y;
-    raw->g = g / Y;
-    raw->b = b / Y;
-
-    return PBIO_SUCCESS;
-}
-
-#endif // PBDRV_CONFIG_LIGHT

lib/pbio/drv/prime_hub/light.c
@@ -1,49 +1,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2020 The Pybricks Authors
-
-#include <pbdrv/light.h>
-#include <pbio/color.h>
-#include <pbio/config.h>
-#include <pbio/error.h>
-#include <pbio/port.h>
-
-// #include "stm32f413xx.h"
-
-// // setup LED PWMs and pins
-// void _pbdrv_light_init(void) {
-
-// }
-
-// #if PBIO_CONFIG_ENABLE_DEINIT
-// // turn off the light
-// void _pbdrv_light_deinit(void) {
-// }
-// #endif
-
-// pbio_error_t pbdrv_light_set_rgb(pbio_port_t port, uint16_t r, uint16_t g, uint16_t b) {
-//     return PBIO_SUCCESS;
-// }
-
-// pbio_error_t pbdrv_light_get_rgb_for_color(pbio_port_t port, pbio_color_t color,
-//                                            pbdrv_light_raw_rgb_t *raw) {
-//     if (port != PBIO_PORT_SELF) {
-//         return PBIO_ERROR_INVALID_PORT;
-//     }
-
-//     pbio_color_rgb_t rgb;
-//     pbio_color_to_rgb(color, &rgb);
-
-//     // Adjust for chromacity
-//     uint32_t r = rgb.r * 1000;
-//     uint32_t g = rgb.g * 270;
-//     uint32_t b = rgb.b * 200;
-
-//     // Adjust for apparent brightness
-//     // + 1 protects against division by zero
-//     uint32_t Y = ((174 * r + 1590 * g + 327 * b) >> 16) + 1;
-//     raw->r = r / Y;
-//     raw->g = g / Y;
-//     raw->b = b / Y;
-
-//     return PBIO_SUCCESS;
-// }

bricks/ev3dev/Makefile
@@ -297,10 +297,11 @@ PYBRICKS_LIB_SRC_C = $(addprefix lib/,\
 	pbio/drv/core.c \
 	pbio/drv/counter/counter_core.c \
 	pbio/drv/counter/counter_ev3dev_stretch_iio.c \
-	pbio/drv/ev3dev_stretch/light.c \
 	pbio/drv/ev3dev_stretch/motor.c \
 	pbio/drv/ev3dev_stretch/serial.c \
 	pbio/drv/ioport/ioport_ev3dev_stretch.c \
+	pbio/drv/led/led_core.c \
+	pbio/drv/led/led_ev3dev.c \
 	pbio/platform/ev3dev_stretch/clock.c \
 	pbio/src/color/conversion.c \
 	pbio/src/control.c \

commit 6548a8e09f618ba014f4310d9220f82112527031

src/light: fix brightness overflow
Since we were adding 1 to a value 0-255 and multiplying it by 101, we
could end up with V of 101 in HSV, which is not valid. This caused the
the breathe pattern to have a brightness of 1 instead of max brightness
at the peak.

Fixes: 712f839 "drv/led: new LED drivers"

lib/pbio/src/light.c
@@ -76,7 +76,7 @@ void _pbio_light_poll(uint32_t now) {
         case PBIO_LIGHT_PATTERN_BREATHE:
             // breathe pattern has 64 values over the course of two seconds (2048ms)
             idx = (now >> 5) & (64 - 1);
-            scale = breathe_pattern_data[idx] + 1;
+            scale = breathe_pattern_data[idx];
             hsv.v = 101 * scale / 256;
             break;
         case PBIO_LIGHT_PATTERN_FLASH: