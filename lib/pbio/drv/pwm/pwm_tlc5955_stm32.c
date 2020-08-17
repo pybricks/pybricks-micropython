91a688b02a5e6659cd866e9fd1da782ad38004a5
David Lechner <david@pybricks.com>
2020-08-17 14:13:01 -0500

    drv/pwm_tlc5955_stm32: add enum for max current
    This adds a new enum so we can know the meaning of the max current
    values without having to refer to the data sheet.
    
    Also add TLC5955 prefix to other macros related to this chip.

--- lib/pbio/drv/pwm/pwm_tlc5955_stm32.c
+++ lib/pbio/drv/pwm/pwm_tlc5955_stm32.c
@@ -23,10 +23,30 @@
 #include "./pwm_tlc5955_stm32.h"
 
 // size in bytes to hold 769-bit shift register data
-#define DATA_SIZE ((769 + 7) / 8)
+#define TLC5955_DATA_SIZE ((769 + 7) / 8)
 
 // number of PWM channels on TLC5955
-#define NUM_CHANNEL 48
+#define TLC5955_NUM_CHANNEL 48
+
+/** Values for TLC5955_CONTROL_DATA maximum current parameter. */
+enum {
+    /** Max current: 3.2 mA */
+    TLC5955_MC_3_2 = 0b000,
+    /** Max current: 8.0 mA */
+    TLC5955_MC_8_0 = 0b001,
+    /** Max current: 11.2 mA */
+    TLC5955_MC_11_2 = 0b010,
+    /** Max current: 15.9 mA */
+    TLC5955_MC_15_9 = 0b011,
+    /** Max current: 19.1 mA */
+    TLC5955_MC_19_1 = 0b100,
+    /** Max current: 23.9 mA */
+    TLC5955_MC_23_9 = 0b101,
+    /** Max current: 27.1 mA */
+    TLC5955_MC_27_1 = 0b110,
+    /** Max current: 31.9 mA */
+    TLC5955_MC_31_9 = 0b111,
+};
 
 /**
  * Control data latch initializer
@@ -44,8 +64,8 @@
  * @param espwm     ES-PWM mode enable (1-bit)
  * @param lsdvlt    LSD detection voltage selection (1-bit)
  */
-#define CONTROL_DATA(name, dc, mc, bc, dsprpt, tmgrst, rfresh, espwm, lsdvlt) \
-    uint8_t name[DATA_SIZE] = { \
+#define TLC5955_CONTROL_DATA(name, dc, mc, bc, dsprpt, tmgrst, rfresh, espwm, lsdvlt) \
+    uint8_t name[TLC5955_DATA_SIZE] = { \
         /* bit 768 */      [0] = 1, \
         /* bits 767-760 */ [1] = 0x96, \
         /* bits 370-368 */ [50] = (((lsdvlt) << 2) | ((espwm) << 1) | (rfresh)) & 0xff, \
@@ -120,16 +140,16 @@ static pbdrv_pwm_tlc5955_stm32_priv_t dev_priv[PBDRV_CONFIG_PWM_TLC5955_STM32_NU
 // Control latch data setting dot correction to 100%, max current to 19.1 mA,
 // global brightness to 100%, auto repeat enabled, display timing reset disabled,
 // auto refresh disabled, ES-PWM mode enabled, LSD detection voltage 90%.
-static const CONTROL_DATA(control_latch_3mA, 127, 4, 127, 1, 0, 0, 1, 1);
+static const TLC5955_CONTROL_DATA(control_latch_3mA, 127, TLC5955_MC_19_1, 127, 1, 0, 0, 1, 1);
 
-static uint8_t grayscale_latch[PBDRV_CONFIG_PWM_TLC5955_STM32_NUM_DEV][DATA_SIZE];
+static uint8_t grayscale_latch[PBDRV_CONFIG_PWM_TLC5955_STM32_NUM_DEV][TLC5955_DATA_SIZE];
 
 // channels are mapped to GS registers in reverse order. CH 0: GSB15, CH 1: GSG15,
 // CH 2: GSR15 ... CH 45: GSB0, CH 46: GSG0, CH 47: GSR0
 static pbio_error_t pbdrv_pwm_tlc5955_stm32_set_duty(pbdrv_pwm_dev_t *dev, uint32_t ch, uint32_t value) {
     pbdrv_pwm_tlc5955_stm32_priv_t *priv = dev->priv;
 
-    assert(ch < NUM_CHANNEL);
+    assert(ch < TLC5955_NUM_CHANNEL);
     assert(value <= UINT16_MAX);
 
     priv->grayscale_latch[ch * 2 + 1] = value >> 8;
@@ -233,12 +253,12 @@ static void pbdrv_pwm_tlc5955_toggle_latch(pbdrv_pwm_tlc5955_stm32_priv_t *priv)
 static PT_THREAD(pbdrv_pwm_tlc5955_stm32_handle_event(pbdrv_pwm_tlc5955_stm32_priv_t * priv, process_event_t ev)) {
     PT_BEGIN(&priv->pt);
 
-    HAL_SPI_Transmit_DMA(&priv->hspi, (uint8_t *)control_latch_3mA, DATA_SIZE);
+    HAL_SPI_Transmit_DMA(&priv->hspi, (uint8_t *)control_latch_3mA, TLC5955_DATA_SIZE);
     PT_WAIT_UNTIL(&priv->pt, priv->hspi.State == HAL_SPI_STATE_READY);
     pbdrv_pwm_tlc5955_toggle_latch(priv);
 
     // have to send twice for max current to take effect
-    HAL_SPI_Transmit_DMA(&priv->hspi, (uint8_t *)control_latch_3mA, DATA_SIZE);
+    HAL_SPI_Transmit_DMA(&priv->hspi, (uint8_t *)control_latch_3mA, TLC5955_DATA_SIZE);
     PT_WAIT_UNTIL(&priv->pt, priv->hspi.State == HAL_SPI_STATE_READY);
     pbdrv_pwm_tlc5955_toggle_latch(priv);
 
@@ -247,7 +267,7 @@ static PT_THREAD(pbdrv_pwm_tlc5955_stm32_handle_event(pbdrv_pwm_tlc5955_stm32_pr
 
     for (;;) {
         PT_WAIT_UNTIL(&priv->pt, priv->changed);
-        HAL_SPI_Transmit_DMA(&priv->hspi, priv->grayscale_latch, DATA_SIZE);
+        HAL_SPI_Transmit_DMA(&priv->hspi, priv->grayscale_latch, TLC5955_DATA_SIZE);
         priv->changed = false;
         PT_WAIT_UNTIL(&priv->pt, priv->hspi.State == HAL_SPI_STATE_READY);
         pbdrv_pwm_tlc5955_toggle_latch(priv);
