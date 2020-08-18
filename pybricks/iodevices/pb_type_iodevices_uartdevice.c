017c2fec8c0dbdd1027d540d96850427ee733ac7
Laurens Valk <laurens@pybricks.com>
2020-08-18 10:12:56 +0200

    pybricks/iodevices/UARTDevice: drop pbio static object abstraction
    We no longer need pbio-like per-port objects.
    The properties just become part of the class instance.

--- pybricks/iodevices/pb_type_iodevices_uartdevice.c
+++ pybricks/iodevices/pb_type_iodevices_uartdevice.c
@@ -24,61 +24,54 @@
 
 #include <contiki.h>
 
-// FIXME: Drop pbio separation and consolidate into class below
-typedef struct _pbio_serial_t {
+// pybricks.iodevices.UARTDevice class object
+typedef struct _iodevices_UARTDevice_obj_t {
+    mp_obj_base_t base;
+    pb_device_t *pbdev;
     pb_serial_t *dev;
     int32_t timeout;
     bool busy;
     unsigned long time_start;
     size_t remaining;
-} pbio_serial_t;
+} iodevices_UARTDevice_obj_t;
 
-pbio_serial_t serials[PBDRV_CONFIG_IOPORT_LPF2_LAST_PORT - PBDRV_CONFIG_IOPORT_LPF2_FIRST_PORT + 1];
-
-pbio_error_t pbio_serial_read(pbio_serial_t *ser, uint8_t *buf, size_t count) {
+pbio_error_t serial_read(iodevices_UARTDevice_obj_t *self, uint8_t *buf, size_t count) {
 
     pbio_error_t err;
 
-    if (!ser->busy) {
+    if (!self->busy) {
         // Reset state variables if we are yet to start
-        ser->busy = true;
-        ser->time_start = clock_usecs() / 1000;
-        ser->remaining = count;
+        self->busy = true;
+        self->time_start = clock_usecs() / 1000;
+        self->remaining = count;
     }
 
     // Read and keep track of how much was read
     size_t read_now;
-    err = pb_serial_read(ser->dev, &buf[count - ser->remaining], count, &read_now);
+    err = pb_serial_read(self->dev, &buf[count - self->remaining], count, &read_now);
     if (err != PBIO_SUCCESS) {
-        ser->busy = false;
+        self->busy = false;
         return err;
     }
 
     // Decrement remaining count
-    ser->remaining -= read_now;
+    self->remaining -= read_now;
 
     // If there is nothing remaining, we are done
-    if (ser->remaining == 0) {
-        ser->busy = false;
+    if (self->remaining == 0) {
+        self->busy = false;
         return PBIO_SUCCESS;
     }
 
     // If we have timed out, let the user know
-    if (ser->timeout >= 0 && clock_usecs() / 1000 - ser->time_start > (unsigned long)ser->timeout) {
-        ser->busy = false;
+    if (self->timeout >= 0 && clock_usecs() / 1000 - self->time_start > (unsigned long)self->timeout) {
+        self->busy = false;
         return PBIO_ERROR_TIMEDOUT;
     }
 
     // If we are here, we need to call this again
     return PBIO_ERROR_AGAIN;
 }
-
-// pybricks.iodevices.UARTDevice class object
-typedef struct _iodevices_UARTDevice_obj_t {
-    mp_obj_base_t base;
-    pb_device_t *pbdev;
-    pbio_serial_t *serial;
-} iodevices_UARTDevice_obj_t;
 
 // pybricks.iodevices.UARTDevice.__init__
 STATIC mp_obj_t iodevices_UARTDevice_make_new(const mp_obj_type_t *otype, size_t n_args, size_t n_kw, const mp_obj_t *args) {
@@ -96,10 +89,9 @@ STATIC mp_obj_t iodevices_UARTDevice_make_new(const mp_obj_type_t *otype, size_t
     self->pbdev = pb_device_get_device(port_num, PBIO_IODEV_TYPE_ID_CUSTOM_UART);
 
     // Initialize serial
-    self->serial = &serials[port_num - PBDRV_CONFIG_IOPORT_LPF2_FIRST_PORT];
-    self->serial->timeout = timeout == mp_const_none ? -1 : pb_obj_get_int(timeout);
-    pb_assert(pb_serial_get(&self->serial->dev, port_num, pb_obj_get_int(baudrate)));
-    pb_assert(pb_serial_clear(self->serial->dev));
+    self->timeout = timeout == mp_const_none ? -1 : pb_obj_get_int(timeout);
+    pb_assert(pb_serial_get(&self->dev, port_num, pb_obj_get_int(baudrate)));
+    pb_assert(pb_serial_clear(self->dev));
 
     return MP_OBJ_FROM_PTR(self);
 }
@@ -120,7 +112,7 @@ STATIC mp_obj_t iodevices_UARTDevice_write(size_t n_args, const mp_obj_t *pos_ar
     GET_STR_DATA_LEN(data, bytes, len);
 
     // Write data to serial
-    pb_assert(pb_serial_write(self->serial->dev, bytes, len));
+    pb_assert(pb_serial_write(self->dev, bytes, len));
 
     return mp_const_none;
 }
@@ -130,7 +122,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_KW(iodevices_UARTDevice_write_obj, 1, iodevices_U
 STATIC mp_obj_t iodevices_UARTDevice_waiting(mp_obj_t self_in) {
     iodevices_UARTDevice_obj_t *self = MP_OBJ_TO_PTR(self_in);
     size_t waiting;
-    pb_assert(pb_serial_in_waiting(self->serial->dev, &waiting));
+    pb_assert(pb_serial_in_waiting(self->dev, &waiting));
     return mp_obj_new_int(waiting);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(iodevices_UARTDevice_waiting_obj, iodevices_UARTDevice_waiting);
@@ -156,7 +148,7 @@ STATIC mp_obj_t iodevices_UARTDevice_read_internal(iodevices_UARTDevice_obj_t *s
 
     // Read up to the timeout
     pbio_error_t err;
-    while ((err = pbio_serial_read(self->serial, buf, len)) == PBIO_ERROR_AGAIN) {
+    while ((err = serial_read(self, buf, len)) == PBIO_ERROR_AGAIN) {
         mp_hal_delay_ms(10);
     }
     // Raise io/timeout error if needed.
@@ -188,7 +180,7 @@ STATIC mp_obj_t iodevices_UARTDevice_read_all(mp_obj_t self_in) {
     iodevices_UARTDevice_obj_t *self = MP_OBJ_TO_PTR(self_in);
 
     size_t len;
-    pb_assert(pb_serial_in_waiting(self->serial->dev, &len));
+    pb_assert(pb_serial_in_waiting(self->dev, &len));
 
     return iodevices_UARTDevice_read_internal(self, len);
 }
@@ -197,7 +189,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(iodevices_UARTDevice_read_all_obj, iodevices_UA
 // pybricks.iodevices.UARTDevice.clear
 STATIC mp_obj_t iodevices_UARTDevice_clear(mp_obj_t self_in) {
     iodevices_UARTDevice_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    pb_assert(pb_serial_clear(self->serial->dev));
+    pb_assert(pb_serial_clear(self->dev));
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(iodevices_UARTDevice_clear_obj, iodevices_UARTDevice_clear);
