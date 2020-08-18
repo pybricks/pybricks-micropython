334ca33b8fd171aa9c2da8dfda2ff9e31e6d2753
Laurens Valk <laurens@pybricks.com>
2020-08-18 10:36:19 +0200

    pybricks/iodevices/UARTDevice: simplify read logic
    This drops the legacy pbio pattern of calling a function until it no longer
    returns PBIO_ERROR_AGAIN. Since this part of a MicroPython module,
    it can simply wait using mp_hal_delay_ms. This way, we don't need any
    state variables to keep track of which start/busy/stop state we are in.

--- pybricks/iodevices/pb_type_iodevices_uartdevice.c
+++ pybricks/iodevices/pb_type_iodevices_uartdevice.c
@@ -25,50 +25,9 @@
 // pybricks.iodevices.UARTDevice class object
 typedef struct _iodevices_UARTDevice_obj_t {
     mp_obj_base_t base;
-    pb_serial_t *dev;
-    int32_t timeout;
-    bool busy;
-    unsigned long time_start;
-    size_t remaining;
+    pb_serial_t *serial;
+    mp_int_t timeout;
 } iodevices_UARTDevice_obj_t;
-
-pbio_error_t serial_read(iodevices_UARTDevice_obj_t *self, uint8_t *buf, size_t count) {
-
-    pbio_error_t err;
-
-    if (!self->busy) {
-        // Reset state variables if we are yet to start
-        self->busy = true;
-        self->time_start = mp_hal_ticks_ms();
-        self->remaining = count;
-    }
-
-    // Read and keep track of how much was read
-    size_t read_now;
-    err = pb_serial_read(self->dev, &buf[count - self->remaining], count, &read_now);
-    if (err != PBIO_SUCCESS) {
-        self->busy = false;
-        return err;
-    }
-
-    // Decrement remaining count
-    self->remaining -= read_now;
-
-    // If there is nothing remaining, we are done
-    if (self->remaining == 0) {
-        self->busy = false;
-        return PBIO_SUCCESS;
-    }
-
-    // If we have timed out, let the user know
-    if (self->timeout >= 0 && mp_hal_ticks_ms() - self->time_start > (unsigned long)self->timeout) {
-        self->busy = false;
-        return PBIO_ERROR_TIMEDOUT;
-    }
-
-    // If we are here, we need to call this again
-    return PBIO_ERROR_AGAIN;
-}
 
 // pybricks.iodevices.UARTDevice.__init__
 STATIC mp_obj_t iodevices_UARTDevice_make_new(const mp_obj_type_t *otype, size_t n_args, size_t n_kw, const mp_obj_t *args) {
@@ -87,8 +46,8 @@ STATIC mp_obj_t iodevices_UARTDevice_make_new(const mp_obj_type_t *otype, size_t
 
     // Initialize serial
     self->timeout = timeout == mp_const_none ? -1 : pb_obj_get_int(timeout);
-    pb_assert(pb_serial_get(&self->dev, port_num, pb_obj_get_int(baudrate)));
-    pb_assert(pb_serial_clear(self->dev));
+    pb_assert(pb_serial_get(&self->serial, port_num, pb_obj_get_int(baudrate)));
+    pb_assert(pb_serial_clear(self->serial));
 
     return MP_OBJ_FROM_PTR(self);
 }
@@ -109,7 +68,7 @@ STATIC mp_obj_t iodevices_UARTDevice_write(size_t n_args, const mp_obj_t *pos_ar
     GET_STR_DATA_LEN(data, bytes, len);
 
     // Write data to serial
-    pb_assert(pb_serial_write(self->dev, bytes, len));
+    pb_assert(pb_serial_write(self->serial, bytes, len));
 
     return mp_const_none;
 }
@@ -119,7 +78,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_KW(iodevices_UARTDevice_write_obj, 1, iodevices_U
 STATIC mp_obj_t iodevices_UARTDevice_waiting(mp_obj_t self_in) {
     iodevices_UARTDevice_obj_t *self = MP_OBJ_TO_PTR(self_in);
     size_t waiting;
-    pb_assert(pb_serial_in_waiting(self->dev, &waiting));
+    pb_assert(pb_serial_in_waiting(self->serial, &waiting));
     return mp_obj_new_int(waiting);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(iodevices_UARTDevice_waiting_obj, iodevices_UARTDevice_waiting);
@@ -143,13 +102,33 @@ STATIC mp_obj_t iodevices_UARTDevice_read_internal(iodevices_UARTDevice_obj_t *s
         pb_assert(PBIO_ERROR_FAILED);
     }
 
+    // Initial status
+    mp_uint_t time_start = mp_hal_ticks_ms();
+    size_t remaining = len;
+
     // Read up to the timeout
-    pbio_error_t err;
-    while ((err = serial_read(self, buf, len)) == PBIO_ERROR_AGAIN) {
+    while (true) {
+
+        // Read and keep track of how much was read
+        size_t read_now;
+        pb_assert(pb_serial_read(self->serial, &buf[len - remaining], len, &read_now));
+
+        // Decrement remaining count
+        remaining -= read_now;
+
+        // If there is nothing remaining, we are done
+        if (remaining == 0) {
+            break;
+        }
+
+        // If we have timed out, let the user know
+        if (self->timeout >= 0 && mp_hal_ticks_ms() - time_start > (mp_uint_t)self->timeout) {
+            pb_assert(PBIO_ERROR_TIMEDOUT);
+        }
+
+        // Wait before looking again
         mp_hal_delay_ms(10);
     }
-    // Raise io/timeout error if needed.
-    pb_assert(err);
 
     // Convert to bytes
     mp_obj_t ret = mp_obj_new_bytes(buf, len);
@@ -177,7 +156,7 @@ STATIC mp_obj_t iodevices_UARTDevice_read_all(mp_obj_t self_in) {
     iodevices_UARTDevice_obj_t *self = MP_OBJ_TO_PTR(self_in);
 
     size_t len;
-    pb_assert(pb_serial_in_waiting(self->dev, &len));
+    pb_assert(pb_serial_in_waiting(self->serial, &len));
 
     return iodevices_UARTDevice_read_internal(self, len);
 }
@@ -186,7 +165,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(iodevices_UARTDevice_read_all_obj, iodevices_UA
 // pybricks.iodevices.UARTDevice.clear
 STATIC mp_obj_t iodevices_UARTDevice_clear(mp_obj_t self_in) {
     iodevices_UARTDevice_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    pb_assert(pb_serial_clear(self->dev));
+    pb_assert(pb_serial_clear(self->serial));
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(iodevices_UARTDevice_clear_obj, iodevices_UARTDevice_clear);
