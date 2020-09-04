f367be64eb09080ff797ef0d5ad90bee66c73dd3
David Lechner <david@pybricks.com>
2020-09-04 13:42:29 -0500

    test/light_grid: new tests for light grid

--- lib/pbio/test/pbioconfig.h
+++ lib/pbio/test/pbioconfig.h
@@ -1,5 +1,6 @@
 
 #define PBIO_CONFIG_LIGHT                   (1)
+#define PBIO_CONFIG_LIGHT_GRID              (1)
 
 #define PBIO_CONFIG_UARTDEV                 (1)
 #define PBIO_CONFIG_UARTDEV_NUM_DEV         (1)
