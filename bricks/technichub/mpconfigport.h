d09ca3af9dce626420329af52b44b3be311144cb
David Lechner <david@pybricks.com>
2020-10-09 18:37:05 -0500

    bricks/technichub: set heap to 48K
    This still leaves 16K for static/global variables and stack.
    
    Also fix alignment while we are touching this code.

--- bricks/technichub/mpconfigport.h
+++ bricks/technichub/mpconfigport.h
@@ -6,9 +6,9 @@
 #define MICROPY_HW_BOARD_NAME           "LEGO Technic Hub"
 #define MICROPY_HW_MCU_NAME             "STM32L431RC"
 
-#define PYBRICKS_HEAP_KB                16
+#define PYBRICKS_HEAP_KB                48
 
-#define PYBRICKS_HUB_TECHNICHUB           (1)
+#define PYBRICKS_HUB_TECHNICHUB         (1)
 
 // Pybricks modules
 #define PYBRICKS_PY_COMMON              (1)
