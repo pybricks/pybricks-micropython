33a65a08962edbc7db1fad99c63d7da87793f966
Laurens Valk <laurens@pybricks.com>
2020-11-06 11:07:05 +0100

    pybricks.hubs.PrimeHub: rename Light Matrix instance object
    https://github.com/pybricks/pybricks-api/commit/ace5cb445b9cb0cdd288613d82ca558f24910f1d

--- pybricks/hubs/pb_type_primehub.c
+++ pybricks/hubs/pb_type_primehub.c
@@ -17,22 +17,22 @@
 typedef struct _hubs_PrimeHub_obj_t {
     mp_obj_base_t base;
     mp_obj_t light;
-    mp_obj_t grid;
+    mp_obj_t display;
 } hubs_PrimeHub_obj_t;
 
 STATIC mp_obj_t hubs_PrimeHub_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
     hubs_PrimeHub_obj_t *self = m_new_obj(hubs_PrimeHub_obj_t);
     self->base.type = (mp_obj_type_t *)type;
     self->light = common_ColorLight_internal_obj_new(pbsys_status_light);
-    self->grid = pb_type_LightGrid_obj_new(pbsys_hub_light_grid);
+    self->display = pb_type_LightGrid_obj_new(pbsys_hub_light_grid);
     return MP_OBJ_FROM_PTR(self);
 }
 
 STATIC const mp_rom_map_elem_t hubs_PrimeHub_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_battery),     MP_ROM_PTR(&pb_module_battery)    },
     { MP_ROM_QSTR(MP_QSTR_buttons),     MP_ROM_PTR(&pb_module_buttons)    },
-    { MP_ROM_QSTR(MP_QSTR_light),       MP_ROM_ATTRIBUTE_OFFSET(hubs_PrimeHub_obj_t, light) },
-    { MP_ROM_QSTR(MP_QSTR_grid),        MP_ROM_ATTRIBUTE_OFFSET(hubs_PrimeHub_obj_t, grid)  },
+    { MP_ROM_QSTR(MP_QSTR_light),       MP_ROM_ATTRIBUTE_OFFSET(hubs_PrimeHub_obj_t, light)   },
+    { MP_ROM_QSTR(MP_QSTR_display),     MP_ROM_ATTRIBUTE_OFFSET(hubs_PrimeHub_obj_t, display) },
 };
 STATIC MP_DEFINE_CONST_DICT(hubs_PrimeHub_locals_dict, hubs_PrimeHub_locals_dict_table);
 
