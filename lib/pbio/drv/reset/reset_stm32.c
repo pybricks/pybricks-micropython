78a0ec2de7799c02e49781bd48504cf507e63d92
David Lechner <david@pybricks.com>
2020-11-13 17:53:25 -0600

    drv/reset/reset_stm32: replace TODO comment
    It has been determined that it is not possible to reboot into update
    mode on SPIKE Prime without attaching a USB cable and holding the
    Bluetooth button.

--- lib/pbio/drv/reset/reset_stm32.c
+++ lib/pbio/drv/reset/reset_stm32.c
@@ -29,13 +29,13 @@ uint32_t pbdrv_reset_stm32_bootloader_selector __attribute__((section(".magic"))
 
 void pbdrv_reset(pbdrv_reset_action_t action) {
     switch (action) {
+        // Some platforms can't reboot in update mode. In those cases it will
+        // just shutdown instead so that update mode can be manually activated.
         #if PBDRV_CONFIG_RESET_STM32_HAS_BLE_BOOTLOADER
         case PBDRV_RESET_ACTION_RESET_IN_UPDATE_MODE:
             pbdrv_reset_stm32_bootloader_selector = BOOTLOADER_FIRMWARE_UPDATE_MODE;
             // fallthrough to PBDRV_RESET_ACTION_RESET
         #endif // PBDRV_CONFIG_RESET_STM32_HAS_BLE_BOOTLOADER
-
-        // TODO: find out if SPIKE Prime can reboot into DFU mode
 
         case PBDRV_RESET_ACTION_RESET:
             NVIC_SystemReset(); // does not return
