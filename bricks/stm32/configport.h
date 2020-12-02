ab782b6930d4c6e2f4a74a9f0e42b68f80d5b953
David Lechner <david@pybricks.com>
2020-12-02 15:56:18 -0600

    bricks/stm32: fix MICROPY_EVENT_POLL_HOOK not working
    We wrote `void pb_stm32_poll()` instead of `pb_stm32_poll()` which
    caused the function to be declared rather than called.
    
    Fixes 1c31b7b9

--- bricks/stm32/configport.h
+++ bricks/stm32/configport.h
@@ -151,7 +151,7 @@ static inline mp_uint_t disable_irq(void) {
 #define MICROPY_EVENT_POLL_HOOK \
     do { \
         extern void pb_stm32_poll(); \
-        void pb_stm32_poll(); \
+        pb_stm32_poll(); \
     } while (0);
 
 // We need to provide a declaration/definition of alloca()
