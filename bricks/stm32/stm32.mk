a346eaf2d1f6b8ac2521e07b157c415344b465f5
David Lechner <david@pybricks.com>
2020-12-03 16:59:26 -0600

    bricks/Makefile: add $(CFLAGS_EXTRA)
    This adds $(CFLAGS_EXTRA) similar to upstream MicroPython for passing
    one-off C compiler flags from the command line.

--- bricks/stm32/stm32.mk
+++ bricks/stm32/stm32.mk
@@ -121,7 +121,7 @@ TEXT0_ADDR ?= 0x08000000
 CFLAGS_MCU_F0 = -mthumb -mtune=cortex-m0 -mcpu=cortex-m0  -msoft-float
 CFLAGS_MCU_F4 = -mthumb -mtune=cortex-m4 -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard
 CFLAGS_MCU_L4 = -mthumb -mtune=cortex-m4 -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard
-CFLAGS = $(INC) -Wall -Werror -std=c99 -nostdlib -fshort-enums $(CFLAGS_MCU_$(PB_MCU_SERIES)) $(COPT)
+CFLAGS = $(INC) -Wall -Werror -std=c99 -nostdlib -fshort-enums $(CFLAGS_MCU_$(PB_MCU_SERIES)) $(COPT) $(CFLAGS_EXTRA)
 
 # define external oscillator frequency
 CFLAGS += -DHSE_VALUE=$(PB_MCU_EXT_OSC_HZ)
