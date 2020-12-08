2579aaa3aa31dd73c222df832ea25c24b8e5acd9
Laurens Valk <laurens@pybricks.com>
2020-12-08 09:32:03 +0100

    pbio/test/drv/battery: implement pbdrv_battery_get_voltage_now
    Return a voltage for the motor tests.

--- lib/pbio/test/drv/battery.c
+++ lib/pbio/test/drv/battery.c
@@ -3,5 +3,13 @@
 
 // Software battery implementation for simulating battery in tests
 
+#include <pbdrv/battery.h>
+#include <pbio/error.h>
+
 void pbdrv_battery_init(void) {
+}
+
+pbio_error_t pbdrv_battery_get_voltage_now(uint16_t *value) {
+    *value = 7200;
+    return PBIO_SUCCESS;
 }
