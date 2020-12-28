6378ad9082735606c8cea118215dd68a64923062
Laurens Valk <laurens@pybricks.com>
2020-12-28 11:33:15 +0100

    pybricks.common.IMU: copy from experimental
    This moves the experimental implementation to the hub class instance.

--- pybricks/common/pb_type_imu.c
+++ pybricks/common/pb_type_imu.c
@@ -6,16 +6,214 @@
 #if PYBRICKS_PY_COMMON && PYBRICKS_PY_COMMON_IMU
 
 #include "py/obj.h"
+#include "py/runtime.h"
+
+#include <lsm6ds3tr_c_reg.h>
+
+#include STM32_HAL_H
+
+#if PYBRICKS_HUB_TECHNICHUB
+#include <stm32l4xx_ll_i2c.h>
+#endif
 
 #include <pybricks/common.h>
 
-// pybricks._common.IMU class object
 typedef struct _common_IMU_obj_t {
     mp_obj_base_t base;
+    stmdev_ctx_t ctx;
 } common_IMU_obj_t;
+
+STATIC common_IMU_obj_t instance;
+STATIC I2C_HandleTypeDef hi2c;
+
+void mod_experimental_IMU_handle_i2c_er_irq(void) {
+    HAL_I2C_ER_IRQHandler(&hi2c);
+}
+
+void mod_experimental_IMU_handle_i2c_ev_irq(void) {
+    HAL_I2C_EV_IRQHandler(&hi2c);
+}
+
+void HAL_I2C_MemTxCpltCallback(I2C_HandleTypeDef *hi2c) {
+    instance.ctx.read_write_done = true;
+}
+
+void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c) {
+    instance.ctx.read_write_done = true;
+}
+
+// REVISIT: if there is ever an error the PT threads will stall since we aren't
+// handling the error callbacks.
+
+STATIC void mod_experimental_IMU_write_reg(void *handle, uint8_t reg, uint8_t *data, uint16_t len) {
+    HAL_I2C_Mem_Write_IT(&hi2c, LSM6DS3TR_C_I2C_ADD_L, reg, I2C_MEMADD_SIZE_8BIT, data, len);
+}
+
+STATIC void mod_experimental_IMU_read_reg(void *handle, uint8_t reg, uint8_t *data, uint16_t len) {
+    HAL_I2C_Mem_Read_IT(&hi2c, LSM6DS3TR_C_I2C_ADD_L, reg, I2C_MEMADD_SIZE_8BIT, data, len);
+}
+
+STATIC PT_THREAD(common_IMU_configure(struct pt *pt, stmdev_ctx_t *ctx)) {
+    static struct pt child;
+    static uint8_t id;
+    static uint8_t rst;
+
+    PT_BEGIN(pt);
+
+    PT_SPAWN(pt, &child, lsm6ds3tr_c_device_id_get(&child, ctx, &id));
+
+    if (id != LSM6DS3TR_C_ID) {
+        mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("incorrect device id"));
+    }
+
+    // Init based on data polling example
+
+    /*
+     *  Restore default configuration
+     */
+    PT_SPAWN(pt, &child, lsm6ds3tr_c_reset_set(&child, ctx, PROPERTY_ENABLE));
+    do {
+        PT_SPAWN(pt, &child, lsm6ds3tr_c_reset_get(&child, ctx, &rst));
+    } while (rst);
+    /*
+     *  Enable Block Data Update
+     */
+    PT_SPAWN(pt, &child, lsm6ds3tr_c_block_data_update_set(&child, ctx, PROPERTY_ENABLE));
+    /*
+     * Set Output Data Rate
+     */
+    PT_SPAWN(pt, &child, lsm6ds3tr_c_xl_data_rate_set(&child, ctx, LSM6DS3TR_C_XL_ODR_12Hz5));
+    PT_SPAWN(pt, &child, lsm6ds3tr_c_gy_data_rate_set(&child, ctx, LSM6DS3TR_C_GY_ODR_833Hz));
+    /*
+     * Set full scale
+     */
+    PT_SPAWN(pt, &child, lsm6ds3tr_c_xl_full_scale_set(&child, ctx, LSM6DS3TR_C_2g));
+    PT_SPAWN(pt, &child, lsm6ds3tr_c_gy_full_scale_set(&child, ctx, LSM6DS3TR_C_250dps));
+
+    /*
+     * Configure filtering chain(No aux interface)
+     */
+    /* Accelerometer - analog filter */
+    PT_SPAWN(pt, &child, lsm6ds3tr_c_xl_filter_analog_set(&child, ctx, LSM6DS3TR_C_XL_ANA_BW_400Hz));
+
+    /* Accelerometer - LPF1 path ( LPF2 not used )*/
+    // PT_SPAWN(pt, &child, lsm6ds3tr_c_xl_lp1_bandwidth_set(&child, ctx, LSM6DS3TR_C_XL_LP1_ODR_DIV_4));
+
+    /* Accelerometer - LPF1 + LPF2 path */
+    PT_SPAWN(pt, &child, lsm6ds3tr_c_xl_lp2_bandwidth_set(&child, ctx, LSM6DS3TR_C_XL_LOW_NOISE_LP_ODR_DIV_100));
+
+    /* Accelerometer - High Pass / Slope path */
+    // PT_SPAWN(pt, &child, lsm6ds3tr_c_xl_reference_mode_set(&child, ctx, PROPERTY_DISABLE));
+    // PT_SPAWN(pt, &child, lsm6ds3tr_c_xl_hp_bandwidth_set(&child, ctx, LSM6DS3TR_C_XL_HP_ODR_DIV_100));
+
+    /* Gyroscope - filtering chain */
+    PT_SPAWN(pt, &child, lsm6ds3tr_c_gy_band_pass_set(&child, ctx, LSM6DS3TR_C_HP_16mHz_LP1_LIGHT));
+
+    PT_END(pt);
+}
+
+STATIC mp_obj_t common_IMU_init(mp_obj_t self_in) {
+    common_IMU_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    struct pt pt;
+
+    self->ctx.write_reg = mod_experimental_IMU_write_reg;
+    self->ctx.read_reg = mod_experimental_IMU_read_reg;
+
+    if (hi2c.Instance == NULL) {
+        #if PYBRICKS_HUB_TECHNICHUB
+        hi2c.Instance = I2C1;
+        // Clock is 5MHz, so these timing come out to 1 usec. When combined with
+        // internal delays, this is slightly slower than 400kHz
+        hi2c.Init.Timing = __LL_I2C_CONVERT_TIMINGS(0, 0, 0, 4, 4);
+        #else
+        // On PrimeHub (STM32F4), we set clock speed directly
+        hi2c.Instance = I2C2;
+        hi2c.Init.ClockSpeed = 400000;
+        #endif // PYBRICKS_HUB_TECHNICHUB
+        hi2c.Init.OwnAddress1 = 0;
+        hi2c.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+        hi2c.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+        hi2c.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+        hi2c.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+        HAL_StatusTypeDef ret = HAL_I2C_Init(&hi2c);
+        if (ret != HAL_OK) {
+            hi2c.Instance = NULL;
+            mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("failed to init I2C"));
+        }
+    }
+
+    PT_INIT(&pt);
+    while (PT_SCHEDULE(common_IMU_configure(&pt, &self->ctx))) {
+        nlr_buf_t nlr;
+        if (nlr_push(&nlr) == 0) {
+            MICROPY_EVENT_POLL_HOOK
+            nlr_pop();
+        } else {
+            HAL_I2C_Master_Abort_IT(&hi2c, LSM6DS3TR_C_I2C_ADD_L);
+            nlr_jump(nlr.ret_val);
+        }
+    }
+
+    return MP_OBJ_FROM_PTR(self);
+}
+
+STATIC mp_obj_t mod_experimental_IMU_accel(mp_obj_t self_in) {
+    common_IMU_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    struct pt pt;
+    int16_t data[3];
+
+    PT_INIT(&pt);
+    while (PT_SCHEDULE(lsm6ds3tr_c_acceleration_raw_get(&pt, &self->ctx, (uint8_t *)data))) {
+        nlr_buf_t nlr;
+        if (nlr_push(&nlr) == 0) {
+            MICROPY_EVENT_POLL_HOOK
+            nlr_pop();
+        } else {
+            HAL_I2C_Master_Abort_IT(&hi2c, LSM6DS3TR_C_I2C_ADD_L);
+            nlr_jump(nlr.ret_val);
+        }
+    }
+
+    mp_obj_t values[3];
+    values[0] = mp_obj_new_float_from_f(lsm6ds3tr_c_from_fs2g_to_mg(data[0]) / 1000.0f);
+    values[1] = mp_obj_new_float_from_f(lsm6ds3tr_c_from_fs2g_to_mg(data[1]) / 1000.0f);
+    values[2] = mp_obj_new_float_from_f(lsm6ds3tr_c_from_fs2g_to_mg(data[2]) / 1000.0f);
+
+    return mp_obj_new_tuple(3, values);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_experimental_IMU_accel_obj, mod_experimental_IMU_accel);
+
+STATIC mp_obj_t mod_experimental_IMU_gyro(mp_obj_t self_in) {
+    common_IMU_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    struct pt pt;
+    int16_t data[3];
+
+    PT_INIT(&pt);
+    while (PT_SCHEDULE(lsm6ds3tr_c_angular_rate_raw_get(&pt, &self->ctx, (uint8_t *)data))) {
+        nlr_buf_t nlr;
+        if (nlr_push(&nlr) == 0) {
+            MICROPY_EVENT_POLL_HOOK
+            nlr_pop();
+        } else {
+            HAL_I2C_Master_Abort_IT(&hi2c, LSM6DS3TR_C_I2C_ADD_L);
+            nlr_jump(nlr.ret_val);
+        }
+    }
+
+    mp_obj_t values[3];
+    values[0] = mp_obj_new_float_from_f(lsm6ds3tr_c_from_fs250dps_to_mdps(data[0]) / 1000.0f);
+    values[1] = mp_obj_new_float_from_f(lsm6ds3tr_c_from_fs250dps_to_mdps(data[1]) / 1000.0f);
+    values[2] = mp_obj_new_float_from_f(lsm6ds3tr_c_from_fs250dps_to_mdps(data[2]) / 1000.0f);
+
+    return mp_obj_new_tuple(3, values);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_experimental_IMU_gyro_obj, mod_experimental_IMU_gyro);
+
 
 // dir(pybricks.common.IMU)
 STATIC const mp_rom_map_elem_t common_IMU_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_accel), MP_ROM_PTR(&mod_experimental_IMU_accel_obj) },
+    { MP_ROM_QSTR(MP_QSTR_gyro),  MP_ROM_PTR(&mod_experimental_IMU_gyro_obj)  },
 };
 STATIC MP_DEFINE_CONST_DICT(common_IMU_locals_dict, common_IMU_locals_dict_table);
 
@@ -28,8 +226,17 @@ STATIC const mp_obj_type_t pb_type_IMU = {
 
 // pybricks._common.IMU.__init__
 mp_obj_t pb_type_IMU_obj_new(void) {
-    common_IMU_obj_t *self = m_new_obj(common_IMU_obj_t);
+    common_IMU_obj_t *self = &instance;
+
+    // Return if already initialized
+    if (self->base.type) {
+        return MP_OBJ_FROM_PTR(self);
+    }
+
+    // Initialize IMU
     self->base.type = &pb_type_IMU;
+    common_IMU_init(self);
+
     return self;
 }
 
