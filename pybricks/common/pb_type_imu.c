909e89ac54bc81cacf164efc82e9adc7de7900b0
Laurens Valk <laurens@pybricks.com>
2020-12-30 16:25:26 +0100

    pybricks.common.IMU: pass generic axis
    Accept a generic input axis and obtain the scalar result through projection.
    
    Also share this projection between gyro and acceleration methods, which do the same.

--- pybricks/common/pb_type_imu.c
+++ pybricks/common/pb_type_imu.c
@@ -13,6 +13,7 @@
 #include <pybricks/geometry.h>
 
 #include <pybricks/util_pb/pb_imu.h>
+#include <pybricks/util_pb/pb_error.h>
 #include <pybricks/util_mp/pb_kwarg_helper.h>
 
 typedef struct _common_IMU_obj_t {
@@ -20,15 +21,15 @@ typedef struct _common_IMU_obj_t {
     pb_imu_dev_t *imu_dev;
 } common_IMU_obj_t;
 
-// pybricks._common.IMU.acceleration
-STATIC mp_obj_t common_IMU_acceleration(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
-    PB_PARSE_ARGS_METHOD(n_args, pos_args, kw_args,
-        common_IMU_obj_t, self,
-        PB_ARG_DEFAULT_NONE(axis));
 
-    float_t values[3];
-    pb_imu_accel_read(self->imu_dev, values);
+STATIC mp_obj_t common_IMU_project_3d_axis(mp_obj_t axis_in, float *values) {
 
+    // If no axis is specified, return a vector of values
+    if (axis_in == mp_const_none) {
+        return pb_type_Matrix_make_vector(3, values, false);
+    }
+
+    // If X, Y, or Z is specified, return value directly for efficiency in most cases
     if (axis_in == &pb_Axis_X_obj) {
         return mp_obj_new_float_from_f(values[0]);
     }
@@ -39,9 +40,32 @@ STATIC mp_obj_t common_IMU_acceleration(size_t n_args, const mp_obj_t *pos_args,
         return mp_obj_new_float_from_f(values[2]);
     }
 
-    // TODO: Handle other axes via projection and scale accordingly
+    // Argument must be a matrix
+    if (!mp_obj_is_type(axis_in, &pb_type_Matrix)) {
+        pb_assert(PBIO_ERROR_INVALID_ARG);
+    }
+    pb_type_Matrix_obj_t *axis = MP_OBJ_TO_PTR(axis_in);
 
-    return pb_type_Matrix_make_vector(3, values, false);
+    // Axis must be 1x3 or 3x1
+    if (axis->m * axis->n != 3) {
+        pb_assert(PBIO_ERROR_INVALID_ARG);
+    }
+
+    // Project data onto user specified axis and scale user axis to unit length
+    float scalar = (axis->data[0] * values[0] + axis->data[1] * values[1] + axis->data[2] * values[2]) * axis->scale;
+    return mp_obj_new_float_from_f(scalar / sqrtf(axis->data[0] * axis->data[0] + axis->data[1] * axis->data[1] + axis->data[2] * axis->data[2]));
+}
+
+// pybricks._common.IMU.acceleration
+STATIC mp_obj_t common_IMU_acceleration(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+    PB_PARSE_ARGS_METHOD(n_args, pos_args, kw_args,
+        common_IMU_obj_t, self,
+        PB_ARG_DEFAULT_NONE(axis));
+
+    float_t values[3];
+    pb_imu_accel_read(self->imu_dev, values);
+
+    return common_IMU_project_3d_axis(axis_in, values);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(common_IMU_acceleration_obj, 1, common_IMU_acceleration);
 
@@ -54,19 +78,7 @@ STATIC mp_obj_t common_IMU_gyro(size_t n_args, const mp_obj_t *pos_args, mp_map_
     float_t values[3];
     pb_imu_gyro_read(self->imu_dev, values);
 
-    if (axis_in == &pb_Axis_X_obj) {
-        return mp_obj_new_float_from_f(values[0]);
-    }
-    if (axis_in == &pb_Axis_Y_obj) {
-        return mp_obj_new_float_from_f(values[1]);
-    }
-    if (axis_in == &pb_Axis_Z_obj) {
-        return mp_obj_new_float_from_f(values[2]);
-    }
-
-    // TODO: Handle other axes via projection and scale accordingly
-
-    return pb_type_Matrix_make_vector(3, values, false);
+    return common_IMU_project_3d_axis(axis_in, values);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(common_IMU_gyro_obj, 1, common_IMU_gyro);
 
