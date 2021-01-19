pybricks/pybricks-micropython

commit 01f1e17d8e1136bbc4d525758a3d90ca71d2d20a

drv/ioport/ioport_lpf2: consolidate platform data
Previously we had individual platform data structures for each port.
This combines those into an array and adds an extra field for
controlling the port VCC, which will be used later.

lib/pbio/platform/prime_hub/platform.c
@@ -120,6 +120,59 @@ void USART2_IRQHandler(void) {
     pbdrv_bluetooth_btstack_uart_block_stm32_hal_handle_uart_irq();
 }
 
+// I/O ports
+
+const pbdrv_ioport_lpf2_platform_data_t pbdrv_ioport_lpf2_platform_data = {
+    .port_vcc = { .bank = GPIOA, .pin = 14 },
+    .ports = {
+        // Port A
+        {
+            .id1 = { .bank = GPIOD, .pin = 7  },
+            .id2 = { .bank = GPIOD, .pin = 8  },
+            .uart_buf = { .bank = GPIOA, .pin = 10 },
+            .uart_tx = { .bank = GPIOE, .pin = 8  },
+            .uart_rx = { .bank = GPIOE, .pin = 7  },
+            .alt = GPIO_AF8_UART7,
+        },
+        // Port B
+        {
+            .id1 = { .bank = GPIOD, .pin = 11 },
+            .id2 = { .bank = GPIOE, .pin = 4  },
+            .uart_buf = { .bank = GPIOE, .pin = 5  },
+            .uart_tx = { .bank = GPIOE, .pin = 1  },
+            .uart_rx = { .bank = GPIOE, .pin = 0  },
+            .alt = GPIO_AF8_UART4,
+        },
+        // Port D
+        {
+            .id1 = { .bank = GPIOC, .pin = 15 },
+            .id2 = { .bank = GPIOC, .pin = 14 },
+            .uart_buf = { .bank = GPIOB, .pin = 2  },
+            .uart_tx = { .bank = GPIOC, .pin = 12 },
+            .uart_rx = { .bank = GPIOD, .pin = 2  },
+            .alt = GPIO_AF8_UART5,
+        },
+        // Port E
+        {
+            .id1 = { .bank = GPIOC, .pin = 13 },
+            .id2 = { .bank = GPIOE, .pin = 12 },
+            .uart_buf = { .bank = GPIOB, .pin = 5  },
+            .uart_tx = { .bank = GPIOE, .pin = 3  },
+            .uart_rx = { .bank = GPIOE, .pin = 2  },
+            .alt = GPIO_AF11_UART10,
+        },
+        // Port F
+        {
+            .id1 = { .bank = GPIOC, .pin = 11 },
+            .id2 = { .bank = GPIOE, .pin = 6  },
+            .uart_buf = { .bank = GPIOC, .pin = 5  },
+            .uart_tx = { .bank = GPIOD, .pin = 15 },
+            .uart_rx = { .bank = GPIOD, .pin = 14 },
+            .alt = GPIO_AF11_UART9,
+        },
+    },
+};
+
 // LED
 
 const pbdrv_led_dual_platform_data_t pbdrv_led_dual_platform_data[PBDRV_CONFIG_LED_DUAL_NUM_DEV] = {
@@ -306,14 +359,14 @@ const pbdrv_pwm_tlc5955_stm32_platform_data_t
     },
 };
 
-// RESET
+// Reset
 
 void pbdrv_reset_stm32_power_off(void) {
     // setting PA13 low cuts the power
     GPIOA->BSRR = GPIO_BSRR_BR_13;
 }
 
-// SOUND
+// Sound
 
 const pbdrv_sound_stm32_hal_dac_platform_data_t pbdrv_sound_stm32_hal_dac_platform_data = {
     .enable_gpio_bank = GPIOC,
@@ -342,66 +395,6 @@ void DMA1_Stream5_IRQHandler(void) {
 }
 
 // UART
-
-// Port A - UART7
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_0 = {
-    .id1 = { .bank = GPIOD, .pin = 7  },
-    .id2 = { .bank = GPIOD, .pin = 8  },
-    .uart_buf = { .bank = GPIOA, .pin = 10 },
-    .uart_tx = { .bank = GPIOE, .pin = 8  },
-    .uart_rx = { .bank = GPIOE, .pin = 7  },
-    .alt = 8,
-};
-
-// Port B - UART4
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_1 = {
-    .id1 = { .bank = GPIOD, .pin = 9  },
-    .id2 = { .bank = GPIOD, .pin = 10 },
-    .uart_buf = { .bank = GPIOA, .pin = 8  },
-    .uart_tx = { .bank = GPIOD, .pin = 1  },
-    .uart_rx = { .bank = GPIOD, .pin = 0  },
-    .alt = 11,
-};
-
-// Port C - UART8
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_2 = {
-    .id1 = { .bank = GPIOD, .pin = 11 },
-    .id2 = { .bank = GPIOE, .pin = 4  },
-    .uart_buf = { .bank = GPIOE, .pin = 5  },
-    .uart_tx = { .bank = GPIOE, .pin = 1  },
-    .uart_rx = { .bank = GPIOE, .pin = 0  },
-    .alt = 8,
-};
-
-// Port D - UART5
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_3 = {
-    .id1 = { .bank = GPIOC, .pin = 15 },
-    .id2 = { .bank = GPIOC, .pin = 14 },
-    .uart_buf = { .bank = GPIOB, .pin = 2  },
-    .uart_tx = { .bank = GPIOC, .pin = 12 },
-    .uart_rx = { .bank = GPIOD, .pin = 2  },
-    .alt = 8,
-};
-
-// Port E - UART10
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_4 = {
-    .id1 = { .bank = GPIOC, .pin = 13 },
-    .id2 = { .bank = GPIOE, .pin = 12 },
-    .uart_buf = { .bank = GPIOB, .pin = 5  },
-    .uart_tx = { .bank = GPIOE, .pin = 3  },
-    .uart_rx = { .bank = GPIOE, .pin = 2  },
-    .alt = 11,
-};
-
-// Port F - UART9
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_5 = {
-    .id1 = { .bank = GPIOC, .pin = 11 },
-    .id2 = { .bank = GPIOE, .pin = 6  },
-    .uart_buf = { .bank = GPIOC, .pin = 5  },
-    .uart_tx = { .bank = GPIOD, .pin = 15 },
-    .uart_rx = { .bank = GPIOD, .pin = 14 },
-    .alt = 11,
-};
 
 const pbdrv_uart_stm32f4_ll_irq_platform_data_t
     pbdrv_uart_stm32f4_ll_irq_platform_data[PBDRV_CONFIG_UART_STM32F4_LL_IRQ_NUM_UART] = {

lib/pbio/platform/city_hub/platform.c
@@ -35,6 +35,8 @@ enum {
     UART_ID_1,
 };
 
+// button
+
 const pbdrv_button_gpio_platform_t pbdrv_button_gpio_platform[PBDRV_CONFIG_BUTTON_GPIO_NUM_BUTTON] = {
     [0] = {
         .gpio = { .bank = GPIOC, .pin = 13 },
@@ -42,6 +44,32 @@ const pbdrv_button_gpio_platform_t pbdrv_button_gpio_platform[PBDRV_CONFIG_BUTTO
         .button = PBIO_BUTTON_CENTER,
         .active_low = true,
     }
+};
+
+// I/O ports
+
+const pbdrv_ioport_lpf2_platform_data_t pbdrv_ioport_lpf2_platform_data = {
+    .port_vcc = { .bank = GPIOB, .pin = 2 },
+    .ports = {
+        // Port A
+        {
+            .id1 = { .bank = GPIOA, .pin = 1  },
+            .id2 = { .bank = GPIOA, .pin = 3  },
+            .uart_buf = { .bank = GPIOB, .pin = 5  },
+            .uart_tx = { .bank = GPIOC, .pin = 4  },
+            .uart_rx = { .bank = GPIOC, .pin = 5  },
+            .alt = 1, // USART3
+        },
+        // Port B
+        {
+            .id1 = { .bank = GPIOA, .pin = 0  },
+            .id2 = { .bank = GPIOA, .pin = 2  },
+            .uart_buf = { .bank = GPIOB, .pin = 4  },
+            .uart_tx = { .bank = GPIOC, .pin = 10  },
+            .uart_rx = { .bank = GPIOC, .pin = 11 },
+            .alt = 0, // USART4
+        },
+    },
 };
 
 // LED
@@ -141,26 +169,6 @@ void pbdrv_reset_stm32_power_off(void) {
     // prevent watchdog from resetting MCU
     IWDG->KR = 0xaaaa;
 }
-
-// Port A - USART3
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_0 = {
-    .id1 = { .bank = GPIOA, .pin = 1  },
-    .id2 = { .bank = GPIOA, .pin = 3  },
-    .uart_buf = { .bank = GPIOB, .pin = 5  },
-    .uart_tx = { .bank = GPIOC, .pin = 4  },
-    .uart_rx = { .bank = GPIOC, .pin = 5  },
-    .alt = 1,
-};
-
-// Port B - USART4
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_1 = {
-    .id1 = { .bank = GPIOA, .pin = 0  },
-    .id2 = { .bank = GPIOA, .pin = 2  },
-    .uart_buf = { .bank = GPIOB, .pin = 4  },
-    .uart_tx = { .bank = GPIOC, .pin = 10  },
-    .uart_rx = { .bank = GPIOC, .pin = 11 },
-    .alt = 0,
-};
 
 // UART
 

lib/pbio/platform/move_hub/platform.c
@@ -39,6 +39,8 @@ enum {
     UART_ID_1,
 };
 
+// button
+
 const pbdrv_button_gpio_platform_t pbdrv_button_gpio_platform[PBDRV_CONFIG_BUTTON_GPIO_NUM_BUTTON] = {
     [0] = {
         .gpio = { .bank = GPIOC, .pin = 13 },
@@ -63,6 +65,32 @@ const pbdrv_counter_stm32f0_gpio_quad_enc_platform_data_t
         .gpio_dir = { .bank = GPIOA, .pin = 1},
         .invert = false,
         .counter_id = COUNTER_PORT_B,
+    },
+};
+
+// I/O ports
+
+const pbdrv_ioport_lpf2_platform_data_t pbdrv_ioport_lpf2_platform_data = {
+    .port_vcc = { .bank = GPIOB, .pin = 2 },
+    .ports = {
+        // Port C
+        {
+            .id1 = { .bank = GPIOB, .pin = 7  },
+            .id2 = { .bank = GPIOC, .pin = 15 },
+            .uart_buf = { .bank = GPIOB, .pin = 4  },
+            .uart_tx = { .bank = GPIOC, .pin = 10 },
+            .uart_rx = { .bank = GPIOC, .pin = 11 },
+            .alt = 0, // USART4
+        },
+        // Port D
+        {
+            .id1 = { .bank = GPIOB, .pin = 10 },
+            .id2 = { .bank = GPIOA, .pin = 12 },
+            .uart_buf = { .bank = GPIOB, .pin = 0  },
+            .uart_tx = { .bank = GPIOC, .pin = 4  },
+            .uart_rx = { .bank = GPIOC, .pin = 5  },
+            .alt = 1, // USART3
+        },
     },
 };
 
@@ -191,26 +219,6 @@ void pbdrv_reset_stm32_power_off(void) {
     // setting PB11 low cuts the power
     GPIOB->BRR = GPIO_BRR_BR_11;
 }
-
-// Port C - USART4
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_0 = {
-    .id1 = { .bank = GPIOB, .pin = 7  },
-    .id2 = { .bank = GPIOC, .pin = 15 },
-    .uart_buf = { .bank = GPIOB, .pin = 4  },
-    .uart_tx = { .bank = GPIOC, .pin = 10 },
-    .uart_rx = { .bank = GPIOC, .pin = 11 },
-    .alt = 0,
-};
-
-// Port D - USART3
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_1 = {
-    .id1 = { .bank = GPIOB, .pin = 10 },
-    .id2 = { .bank = GPIOA, .pin = 12 },
-    .uart_buf = { .bank = GPIOB, .pin = 0  },
-    .uart_tx = { .bank = GPIOC, .pin = 4  },
-    .uart_rx = { .bank = GPIOC, .pin = 5  },
-    .alt = 1,
-};
 
 // UART
 

lib/pbio/platform/technic_hub/platform.c
@@ -62,44 +62,48 @@ const pbdrv_button_gpio_platform_t pbdrv_button_gpio_platform[PBDRV_CONFIG_BUTTO
     },
 };
 
-// Port A
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_0 = {
-    .id1 = { .bank = GPIOC, .pin = 5  },
-    .id2 = { .bank = GPIOB, .pin = 2  },
-    .uart_buf = { .bank = GPIOH, .pin = 0  },
-    .uart_tx = { .bank = GPIOB, .pin = 6  },
-    .uart_rx = { .bank = GPIOB, .pin = 7  },
-    .alt = GPIO_AF7_USART1,
-};
+// I/O ports
 
-// Port B
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_1 = {
-    .id1 = { .bank = GPIOC, .pin = 3  },
-    .id2 = { .bank = GPIOC, .pin = 0  },
-    .uart_buf = { .bank = GPIOH, .pin = 1  },
-    .uart_tx = { .bank = GPIOA, .pin = 2  },
-    .uart_rx = { .bank = GPIOA, .pin = 3  },
-    .alt = GPIO_AF7_USART2,
-};
-
-// Port C
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_2 = {
-    .id1 = { .bank = GPIOC, .pin = 4  },
-    .id2 = { .bank = GPIOA, .pin = 7  },
-    .uart_buf = { .bank = GPIOC, .pin = 8  },
-    .uart_tx = { .bank = GPIOC, .pin = 10 },
-    .uart_rx = { .bank = GPIOC, .pin = 11 },
-    .alt = GPIO_AF7_USART3,
-};
-
-// Port D
-const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_3 = {
-    .id1 = { .bank = GPIOA, .pin = 4  },
-    .id2 = { .bank = GPIOA, .pin = 5  },
-    .uart_buf = { .bank = GPIOC, .pin = 7  },
-    .uart_tx = { .bank = GPIOB, .pin = 11 },
-    .uart_rx = { .bank = GPIOB, .pin = 10  },
-    .alt = GPIO_AF8_LPUART1,
+const pbdrv_ioport_lpf2_platform_data_t pbdrv_ioport_lpf2_platform_data = {
+    .port_vcc = { .bank = GPIOB, .pin = 12 },
+    .ports = {
+        // Port A
+        {
+            .id1 = { .bank = GPIOC, .pin = 5  },
+            .id2 = { .bank = GPIOB, .pin = 2  },
+            .uart_buf = { .bank = GPIOH, .pin = 0  },
+            .uart_tx = { .bank = GPIOB, .pin = 6  },
+            .uart_rx = { .bank = GPIOB, .pin = 7  },
+            .alt = GPIO_AF7_USART1,
+        },
+        // Port B
+        {
+            .id1 = { .bank = GPIOC, .pin = 3  },
+            .id2 = { .bank = GPIOC, .pin = 0  },
+            .uart_buf = { .bank = GPIOH, .pin = 1  },
+            .uart_tx = { .bank = GPIOA, .pin = 2  },
+            .uart_rx = { .bank = GPIOA, .pin = 3  },
+            .alt = GPIO_AF7_USART2,
+        },
+        // Port C
+        {
+            .id1 = { .bank = GPIOC, .pin = 4  },
+            .id2 = { .bank = GPIOA, .pin = 7  },
+            .uart_buf = { .bank = GPIOC, .pin = 8  },
+            .uart_tx = { .bank = GPIOC, .pin = 10 },
+            .uart_rx = { .bank = GPIOC, .pin = 11 },
+            .alt = GPIO_AF7_USART3,
+        },
+        // Port D
+        {
+            .id1 = { .bank = GPIOA, .pin = 4  },
+            .id2 = { .bank = GPIOA, .pin = 5  },
+            .uart_buf = { .bank = GPIOC, .pin = 7  },
+            .uart_tx = { .bank = GPIOB, .pin = 11 },
+            .uart_rx = { .bank = GPIOB, .pin = 10  },
+            .alt = GPIO_AF8_LPUART1,
+        },
+    },
 };
 
 // LED
@@ -226,7 +230,7 @@ void TIM2_IRQHandler(void) {
     TIM2->SR = 0;
 }
 
-// RESET
+// Reset
 
 void pbdrv_reset_stm32_power_off(void) {
     // setting PC12 low cuts the power

lib/pbio/drv/ioport/ioport_lpf2.c
@@ -40,7 +40,7 @@ typedef struct _dcm_data_t {
 
 typedef struct {
     pbio_iodev_t *iodev;
-    const pbdrv_ioport_lpf2_platform_port_t *pins;
+    const pbdrv_ioport_lpf2_port_platform_data_t *pdata;
     dcm_data_t dcm;
     struct pt pt;
     pbio_iodev_type_id_t connected_type_id;
@@ -209,60 +209,32 @@ static const pbio_iodev_type_id_t ioport_type_id_lookup[3][3] = {
 
 PROCESS(pbdrv_ioport_lpf2_process, "I/O port");
 
-static ioport_dev_t ioport_devs[PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS] = {
-    #if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 0
-    [0] = {
-        .pins = &pbdrv_ioport_lpf2_platform_port_0,
-    },
-    #endif
-    #if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 1
-    [1] = {
-        .pins = &pbdrv_ioport_lpf2_platform_port_1,
-    },
-    #endif
-    #if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 2
-    [2] = {
-        .pins = &pbdrv_ioport_lpf2_platform_port_2,
-    },
-    #endif
-    #if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 3
-    [3] = {
-        .pins = &pbdrv_ioport_lpf2_platform_port_3,
-    },
-    #endif
-    #if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 4
-    [4] = {
-        .pins = &pbdrv_ioport_lpf2_platform_port_4,
-    },
-    #endif
-    #if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 5
-    [5] = {
-        .pins = &pbdrv_ioport_lpf2_platform_port_5,
-    },
-    #endif
-};
+static ioport_dev_t ioport_devs[PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS];
 
 static void ioport_enable_uart(ioport_dev_t *ioport) {
-    const pbdrv_ioport_lpf2_platform_port_t *pins = ioport->pins;
+    const pbdrv_ioport_lpf2_port_platform_data_t *pdata = ioport->pdata;
 
-    pbdrv_gpio_alt(&pins->uart_rx, pins->alt);
-    pbdrv_gpio_alt(&pins->uart_tx, pins->alt);
-    pbdrv_gpio_out_low(&pins->uart_buf);
+    pbdrv_gpio_alt(&pdata->uart_rx, pdata->alt);
+    pbdrv_gpio_alt(&pdata->uart_tx, pdata->alt);
+    pbdrv_gpio_out_low(&pdata->uart_buf);
 }
 
 static const pbio_iodev_ops_t basic_dev_ops = {
 };
 
 static void init_one(uint8_t ioport) {
-    const pbdrv_ioport_lpf2_platform_port_t *pins = ioport_devs[ioport].pins;
+    const pbdrv_ioport_lpf2_port_platform_data_t *pdata =
+        &pbdrv_ioport_lpf2_platform_data.ports[ioport];
+
+    ioport_devs[ioport].pdata = pdata;
 
     PT_INIT(&ioport_devs[ioport].pt);
 
-    pbdrv_gpio_input(&pins->id1);
-    pbdrv_gpio_input(&pins->id2);
-    pbdrv_gpio_input(&pins->uart_buf);
-    pbdrv_gpio_input(&pins->uart_tx);
-    pbdrv_gpio_input(&pins->uart_rx);
+    pbdrv_gpio_input(&pdata->id1);
+    pbdrv_gpio_input(&pdata->id2);
+    pbdrv_gpio_input(&pdata->uart_buf);
+    pbdrv_gpio_input(&pdata->uart_tx);
+    pbdrv_gpio_input(&pdata->uart_rx);
 
     basic_devs[ioport].port = PBDRV_CONFIG_IOPORT_LPF2_FIRST_PORT + ioport;
     basic_devs[ioport].ops = &basic_dev_ops;
@@ -288,7 +260,7 @@ pbio_error_t pbdrv_ioport_get_iodev(pbio_port_t port, pbio_iodev_t **iodev) {
 static PT_THREAD(poll_dcm(ioport_dev_t * ioport)) {
     struct pt *pt = &ioport->pt;
     dcm_data_t *data = &ioport->dcm;
-    const pbdrv_ioport_lpf2_platform_port_t pins = *ioport->pins;
+    const pbdrv_ioport_lpf2_port_platform_data_t pdata = *ioport->pdata;
 
     PT_BEGIN(pt);
 
@@ -296,24 +268,24 @@ static PT_THREAD(poll_dcm(ioport_dev_t * ioport)) {
     data->dev_id1_group = DEV_ID1_GROUP_OPEN;
 
     // set ID1 high
-    pbdrv_gpio_out_high(&pins.uart_tx);
-    pbdrv_gpio_out_low(&pins.uart_buf);
+    pbdrv_gpio_out_high(&pdata.uart_tx);
+    pbdrv_gpio_out_low(&pdata.uart_buf);
 
     // set ID2 as input
-    pbdrv_gpio_input(&pins.id2);
+    pbdrv_gpio_input(&pdata.id2);
 
     PT_YIELD(pt);
 
     // save current ID2 value
-    data->prev_gpio_value = pbdrv_gpio_input(&pins.id2);
+    data->prev_gpio_value = pbdrv_gpio_input(&pdata.id2);
 
     // set ID1 low
-    pbdrv_gpio_out_low(&pins.uart_tx);
+    pbdrv_gpio_out_low(&pdata.uart_tx);
 
     PT_YIELD(pt);
 
     // read ID2
-    data->gpio_value = pbdrv_gpio_input(&pins.id2);
+    data->gpio_value = pbdrv_gpio_input(&pdata.id2);
 
     // if ID2 changed from high to low
     if (data->prev_gpio_value == 1 && data->gpio_value == 0) {
@@ -321,29 +293,29 @@ static PT_THREAD(poll_dcm(ioport_dev_t * ioport)) {
         data->type_id = PBIO_IODEV_TYPE_ID_LPF2_TOUCH;
 
         // set ID1 as input
-        pbdrv_gpio_out_high(&pins.uart_buf);
-        pbdrv_gpio_input(&pins.uart_tx);
+        pbdrv_gpio_out_high(&pdata.uart_buf);
+        pbdrv_gpio_input(&pdata.uart_tx);
 
         PT_YIELD(pt);
 
         // ID1 is inverse of touch sensor value
         // TODO: save this value to sensor data
-        // sensor_data = !pbdrv_gpio_input(&pins.id1);
+        // sensor_data = !pbdrv_gpio_input(&pdata.id1);
     }
     // if ID2 changed from low to high
     else if (data->prev_gpio_value == 0 && data->gpio_value == 1) {
         data->type_id = PBIO_IODEV_TYPE_ID_LPF2_TPOINT;
     } else {
         // read ID1
-        data->prev_gpio_value = pbdrv_gpio_input(&pins.id1);
+        data->prev_gpio_value = pbdrv_gpio_input(&pdata.id1);
 
         // set ID1 high
-        pbdrv_gpio_out_high(&pins.uart_tx);
+        pbdrv_gpio_out_high(&pdata.uart_tx);
 
         PT_YIELD(pt);
 
         // read ID1
-        data->gpio_value = pbdrv_gpio_input(&pins.id1);
+        data->gpio_value = pbdrv_gpio_input(&pdata.id1);
 
         // if ID1 did not change and is high
         if (data->prev_gpio_value == 1 && data->gpio_value == 1) {
@@ -356,13 +328,13 @@ static PT_THREAD(poll_dcm(ioport_dev_t * ioport)) {
             data->dev_id1_group = DEV_ID1_GROUP_GND;
         } else {
             // set ID1 as input
-            pbdrv_gpio_out_high(&pins.uart_buf);
-            pbdrv_gpio_input(&pins.uart_tx);
+            pbdrv_gpio_out_high(&pdata.uart_buf);
+            pbdrv_gpio_input(&pdata.uart_tx);
 
             PT_YIELD(pt);
 
             // read ID1
-            if (pbdrv_gpio_input(&pins.id1) == 1) {
+            if (pbdrv_gpio_input(&pdata.id1) == 1) {
                 // we have ID1 == open
                 data->dev_id1_group = DEV_ID1_GROUP_OPEN;
             } else {
@@ -374,24 +346,24 @@ static PT_THREAD(poll_dcm(ioport_dev_t * ioport)) {
         PT_YIELD(pt);
 
         // set ID1 as input
-        pbdrv_gpio_out_high(&pins.uart_buf);
-        pbdrv_gpio_input(&pins.uart_tx);
+        pbdrv_gpio_out_high(&pdata.uart_buf);
+        pbdrv_gpio_input(&pdata.uart_tx);
 
         // set ID2 high
-        pbdrv_gpio_out_high(&pins.id2);
+        pbdrv_gpio_out_high(&pdata.id2);
 
         PT_YIELD(pt);
 
         // read ID1
-        data->prev_gpio_value = pbdrv_gpio_input(&pins.id1);
+        data->prev_gpio_value = pbdrv_gpio_input(&pdata.id1);
 
         // set ID2 low
-        pbdrv_gpio_out_low(&pins.id2);
+        pbdrv_gpio_out_low(&pdata.id2);
 
         PT_YIELD(pt);
 
         // read ID1
-        data->gpio_value = pbdrv_gpio_input(&pins.id1);
+        data->gpio_value = pbdrv_gpio_input(&pdata.id1);
 
         // if ID1 changed from high to low
         if (data->prev_gpio_value == 1 && data->gpio_value == 0) {
@@ -407,23 +379,23 @@ static PT_THREAD(poll_dcm(ioport_dev_t * ioport)) {
             data->type_id = PBIO_IODEV_TYPE_ID_LPF2_EXPLOD;
         } else {
             // set ID1 high
-            pbdrv_gpio_out_high(&pins.uart_tx);
-            pbdrv_gpio_out_low(&pins.uart_buf);
+            pbdrv_gpio_out_high(&pdata.uart_tx);
+            pbdrv_gpio_out_low(&pdata.uart_buf);
 
             // set ID2 high
-            pbdrv_gpio_out_high(&pins.id2);
+            pbdrv_gpio_out_high(&pdata.id2);
 
             PT_YIELD(pt);
 
             // if ID2 is high
-            if (pbdrv_gpio_input(&pins.uart_rx) == 1) {
+            if (pbdrv_gpio_input(&pdata.uart_rx) == 1) {
                 // set ID2 low
-                pbdrv_gpio_out_low(&pins.id2);
+                pbdrv_gpio_out_low(&pdata.id2);
 
                 PT_YIELD(pt);
 
                 // if ID2 is low
-                if (pbdrv_gpio_input(&pins.uart_rx) == 0) {
+                if (pbdrv_gpio_input(&pdata.uart_rx) == 0) {
                     if (data->dev_id1_group < 3) {
                         data->type_id = ioport_type_id_lookup[data->dev_id1_group][2];
                     }
@@ -446,11 +418,11 @@ static PT_THREAD(poll_dcm(ioport_dev_t * ioport)) {
     PT_YIELD(pt);
 
     // set ID2 as input
-    pbdrv_gpio_input(&pins.id2);
+    pbdrv_gpio_input(&pdata.id2);
 
     // set ID1 high
-    pbdrv_gpio_out_high(&pins.uart_tx);
-    pbdrv_gpio_out_low(&pins.uart_buf);
+    pbdrv_gpio_out_high(&pdata.uart_tx);
+    pbdrv_gpio_out_low(&pdata.uart_buf);
 
     if (data->type_id == data->prev_type_id) {
         if (++data->dev_id_match_count >= 20) {

lib/pbio/drv/ioport/ioport_lpf2.h
@@ -11,13 +11,20 @@
 
 // GPIOs associated with ID1 and ID2 pins
 typedef struct {
-    const pbdrv_gpio_t id1;        // pin 5 in
-    const pbdrv_gpio_t id2;        // pin 6 in/out
-    const pbdrv_gpio_t uart_buf;
-    const pbdrv_gpio_t uart_tx;    // pin 5 out
-    const pbdrv_gpio_t uart_rx;    // pin 6 in
-    const uint8_t alt;
-} pbdrv_ioport_lpf2_platform_port_t;
+    pbdrv_gpio_t id1;        // pin 5 in
+    pbdrv_gpio_t id2;        // pin 6 in/out
+    pbdrv_gpio_t uart_buf;
+    pbdrv_gpio_t uart_tx;    // pin 5 out
+    pbdrv_gpio_t uart_rx;    // pin 6 in
+    uint8_t alt;
+} pbdrv_ioport_lpf2_port_platform_data_t;
+
+typedef struct {
+    /** Supplies 3.3V power to pin 4 on all ports. */
+    pbdrv_gpio_t port_vcc;
+    /** Individual port data. */
+    pbdrv_ioport_lpf2_port_platform_data_t ports[PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS];
+} pbdrv_ioport_lpf2_platform_data_t;
 
 // platforom configuration checks
 #ifndef PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS
@@ -30,24 +37,7 @@ typedef struct {
 #error Platform must define PBDRV_CONFIG_IOPORT_LPF2_LAST_PORT
 #endif
 
-// These are defined in platform/*/platform.c
-#if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 0
-extern const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_0;
-#endif
-#if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 1
-extern const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_1;
-#endif
-#if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 2
-extern const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_2;
-#endif
-#if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 3
-extern const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_3;
-#endif
-#if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 4
-extern const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_4;
-#endif
-#if PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS > 5
-extern const pbdrv_ioport_lpf2_platform_port_t pbdrv_ioport_lpf2_platform_port_5;
-#endif
+// This is defined in platform/*/platform.c
+extern const pbdrv_ioport_lpf2_platform_data_t pbdrv_ioport_lpf2_platform_data;
 
 #endif // _INTERNAL_PBDRV_IOPORT_LPF2_H_

commit 087a11f77cffe0efd534925ab3b774533dd472a0

pbio/platform/prime_hub: fix missing ioport B
This was broken by https://github.com/pybricks/pybricks-micropython/commit/01f1e17d8e1136bbc4d525758a3d90ca71d2d20a

Fixes https://github.com/pybricks/support/issues/219

lib/pbio/platform/prime_hub/platform.c
@@ -134,6 +134,15 @@ const pbdrv_ioport_lpf2_platform_data_t pbdrv_ioport_lpf2_platform_data = {
             .uart_rx = { .bank = GPIOE, .pin = 7  },
             .alt = GPIO_AF8_UART7,
         },
+        // Port B
+        {
+            .id1 = { .bank = GPIOD, .pin = 9  },
+            .id2 = { .bank = GPIOD, .pin = 10 },
+            .uart_buf = { .bank = GPIOA, .pin = 8  },
+            .uart_tx = { .bank = GPIOD, .pin = 1  },
+            .uart_rx = { .bank = GPIOD, .pin = 0  },
+            .alt = GPIO_AF11_UART4,
+        },
         // Port C
         {
             .id1 = { .bank = GPIOD, .pin = 11 },