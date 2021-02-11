0d6046e960926ea47ebca13fab086ade89402940
David Lechner <david@pybricks.com>
2021-02-11 16:05:37 -0600

    test: add crash handling
    This adds handling for crashes in tests by using gdb to get a stack trace.

--- test-pbio.sh
+++ test-pbio.sh
@@ -2,7 +2,7 @@
 #
 # Shortcut for running tests on the pbio library.
 #
-# Run ``./test-pbio.sh --list-tests` for more info
+# Run `./test-pbio.sh --list-tests` for more info
 #
 
 set -e
@@ -12,4 +12,14 @@ SCRIPT_DIR=$(dirname "$0")
 export PBIO_TEST_RESULTS_DIR="${SCRIPT_DIR}/lib/pbio/test/results"
 rm -rf "${PBIO_TEST_RESULTS_DIR:?}"/*
 make -s -C "${SCRIPT_DIR}/lib/pbio/test"
+
+ulimit -S -c unlimited
+
+set +e
+
 "${SCRIPT_DIR}/lib/pbio/test/build/test-pbio" "$@"
+
+# use gdb to get backtrace on segfault
+if [ $? -eq 139 ]; then
+    gdb -q "${SCRIPT_DIR}/lib/pbio/test/build/test-pbio" "${PBIO_TEST_RESULTS_DIR}/core" -ex backtrace
+fi
