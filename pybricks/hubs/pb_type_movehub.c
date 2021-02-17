3f23016f0900a56b334ef3291993df92a2125dd5
Laurens Valk <laurens@pybricks.com>
2021-02-17 15:17:33 +0100

    pybricks.hubs.MoveHub: move imu from experimental

--- pybricks/hubs/pb_type_movehub.c
+++ pybricks/hubs/pb_type_movehub.c
@@ -5,21 +5,215 @@
 
 #if PYBRICKS_PY_HUBS && PYBRICKS_HUB_MOVEHUB
 
+#include "py/runtime.h"
+
 #include <pbdrv/reset.h>
 #include <pbsys/light.h>
 
 #include <pybricks/common.h>
 #include <pybricks/hubs.h>
 
+// LIS3DH motion sensor
+
+#include "stm32f070xb.h"
+
+#define LIS3DH_ID                   0x33
+
+#define WHO_AM_I                    0x0f
+
+#define CTRL_REG1                   0x20
+#define CTRL_REG1_ODR_POWER_DOWN    (0 << 4)
+#define CTRL_REG1_ODR_1HZ           (1 << 4)
+#define CTRL_REG1_ODR_10HZ          (2 << 4)
+#define CTRL_REG1_ODR_25HZ          (3 << 4)
+#define CTRL_REG1_ODR_50HZ          (4 << 4)
+#define CTRL_REG1_ODR_100HZ         (5 << 4)
+#define CTRL_REG1_ODR_200HZ         (6 << 4)
+#define CTRL_REG1_ODR_400HZ         (7 << 4)
+#define CTRL_REG1_ODR_1620HZ        (8 << 4)
+#define CTRL_REG1_ODR_5376HZ        (9 << 4)
+#define CTRL_REG1_LPEN              (1 << 3)
+#define CTRL_REG1_ZEN               (1 << 2)
+#define CTRL_REG1_YEN               (1 << 1)
+#define CTRL_REG1_XEN               (1 << 0)
+
+#define OUT_X_L                     0x28
+#define OUT_X_H                     0x29
+#define OUT_Y_L                     0x2A
+#define OUT_Y_H                     0x2B
+#define OUT_Z_L                     0x2C
+#define OUT_Z_H                     0x2D
+
+#define CLICK_CFG                   0x38
+#define CLICK_CFG_ZD                (1 << 5)
+#define CLICK_CFG_ZS                (1 << 4)
+#define CLICK_CFG_YD                (1 << 3)
+#define CLICK_CFG_YS                (1 << 2)
+#define CLICK_CFG_XD                (1 << 1)
+#define CLICK_CFG_XS                (1 << 0)
+
+#define CLICK_THS                   0x3a
+#define CLICK_THS_LIR_CLICK         (1 << 7)
+#define CLICK_THS_THS(n)            ((n) & 0x7f)
+
+#define TIME_LIMIT                  0x3b
+#define TIME_LIMIT_TLI(n)           ((n) & 0x7f)
+
+#define TIME_LATENCY                0x3c
+#define TIME_LATENCY_TLA(n)         ((n) & 0x7f)
+
+#define READ_FLAG                   0x80
+
+#define BYTE_ACCESS(r) (*(volatile uint8_t *)&(r))
+
+typedef struct {
+    mp_obj_base_t base;
+} hubs_MoveHub_IMU_obj_t;
+
+static void motion_spi_read(uint8_t reg, uint8_t *value) {
+    uint8_t dummy;
+
+    // set chip select low
+    GPIOA->BRR = GPIO_BRR_BR_4;
+
+    BYTE_ACCESS(SPI1->DR) = READ_FLAG | reg;
+
+    // busy wait
+    do {
+        while (!(SPI1->SR & SPI_SR_RXNE)) {
+            MICROPY_EVENT_POLL_HOOK;
+        }
+    } while (SPI1->SR & SPI_SR_BSY);
+
+    while (SPI1->SR & SPI_SR_RXNE) {
+        dummy = BYTE_ACCESS(SPI1->DR);
+        (void)dummy;
+        MICROPY_EVENT_POLL_HOOK;
+    }
+    BYTE_ACCESS(SPI1->DR) = 0;
+
+    // busy wait
+    do {
+        while (!(SPI1->SR & SPI_SR_RXNE)) {
+            MICROPY_EVENT_POLL_HOOK
+        }
+    } while (SPI1->SR & SPI_SR_BSY);
+
+    *value = BYTE_ACCESS(SPI1->DR);
+
+    // clear chip select
+    GPIOA->BSRR = GPIO_BSRR_BS_4;
+}
+
+static void motion_spi_write(uint8_t reg, uint8_t value) {
+    // set chip select low
+    GPIOA->BRR = GPIO_BRR_BR_4;
+
+    BYTE_ACCESS(SPI1->DR) = reg;
+
+    // busy wait
+    do {
+        while (!(SPI1->SR & SPI_SR_RXNE)) {
+            MICROPY_EVENT_POLL_HOOK;
+        }
+    } while (SPI1->SR & SPI_SR_BSY);
+
+    BYTE_ACCESS(SPI1->DR) = value;
+
+    // busy wait
+    do {
+        while (!(SPI1->SR & SPI_SR_RXNE)) {
+            MICROPY_EVENT_POLL_HOOK;
+        }
+    } while (SPI1->SR & SPI_SR_BSY);
+
+    // clear chip select
+    GPIOA->BSRR = GPIO_BSRR_BS_4;
+}
+
+STATIC mp_obj_t hubs_MoveHub_IMU_accel(mp_obj_t self_in) {
+    hubs_MoveHub_IMU_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    (void)self;
+
+    uint8_t data[3];
+    motion_spi_read(OUT_X_H, &data[0]);
+    motion_spi_read(OUT_Y_H, &data[1]);
+    motion_spi_read(OUT_Z_H, &data[2]);
+
+    mp_obj_t values[3];
+    values[0] = mp_obj_new_int((int8_t)data[0]);
+    values[1] = mp_obj_new_int((int8_t)data[1]);
+    values[2] = mp_obj_new_int((int8_t)data[2]);
+
+    return mp_obj_new_tuple(3, values);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(hubs_MoveHub_IMU_accel_obj, hubs_MoveHub_IMU_accel);
+
+STATIC const mp_rom_map_elem_t hubs_MoveHub_IMU_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_accel), MP_ROM_PTR(&hubs_MoveHub_IMU_accel_obj) },
+};
+STATIC MP_DEFINE_CONST_DICT(hubs_MoveHub_IMU_locals_dict, hubs_MoveHub_IMU_locals_dict_table);
+
+STATIC const mp_obj_type_t hubs_MoveHub_IMU_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_Motion,
+    .locals_dict = (mp_obj_dict_t *)&hubs_MoveHub_IMU_locals_dict,
+};
+
+STATIC mp_obj_t hubs_MoveHub_IMU_make_new() {
+    hubs_MoveHub_IMU_obj_t *self = m_new_obj(hubs_MoveHub_IMU_obj_t);
+
+    self->base.type = &hubs_MoveHub_IMU_type;
+
+    // PA4 gpio output - used for CS
+    GPIOA->BSRR = GPIO_BSRR_BS_4;
+    GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER4_Msk) | (1 << GPIO_MODER_MODER4_Pos);
+
+    // PA5, PA5, PA7 muxed as SPI1 pins
+    GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER5_Msk) | (2 << GPIO_MODER_MODER5_Pos);
+    GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL5;
+    GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER6_Msk) | (2 << GPIO_MODER_MODER6_Pos);
+    GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL6;
+    GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER7_Msk) | (2 << GPIO_MODER_MODER7_Pos);
+    GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL7;
+
+    // configure SPI1
+    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+    #define SPI_CR1_BR_DIV8 (SPI_CR1_BR_1)
+    SPI1->CR1 = SPI_CR1_CPHA | SPI_CR1_CPOL | SPI_CR1_MSTR | SPI_CR1_BR_DIV8;
+    #define SPI_CR2_DS_8BIT (SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2)
+    SPI1->CR2 = SPI_CR2_SSOE | SPI_CR2_DS_8BIT | SPI_CR2_FRXTH;
+    SPI1->CR1 |= SPI_CR1_SPE;
+
+    uint8_t id;
+    motion_spi_read(WHO_AM_I, &id);
+
+    if (id != LIS3DH_ID) {
+        mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("incorrect device id"));
+    }
+
+    // Init
+
+    motion_spi_write(CTRL_REG1, CTRL_REG1_XEN | CTRL_REG1_YEN | CTRL_REG1_ZEN | CTRL_REG1_ODR_5376HZ);
+    motion_spi_write(CLICK_CFG, CLICK_CFG_XS | CLICK_CFG_XD | CLICK_CFG_YS | CLICK_CFG_YD | CLICK_CFG_ZS | CLICK_CFG_ZD);
+    motion_spi_write(CLICK_THS, CLICK_THS_THS(127));
+    motion_spi_write(TIME_LIMIT, TIME_LIMIT_TLI(127));
+    motion_spi_write(TIME_LATENCY, TIME_LATENCY_TLA(127));
+
+    return MP_OBJ_FROM_PTR(self);
+}
+
 typedef struct _hubs_MoveHub_obj_t {
     mp_obj_base_t base;
     mp_obj_t light;
+    mp_obj_t imu;
 } hubs_MoveHub_obj_t;
 
 STATIC mp_obj_t hubs_MoveHub_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
     hubs_MoveHub_obj_t *self = m_new_obj(hubs_MoveHub_obj_t);
     self->base.type = (mp_obj_type_t *)type;
     self->light = common_ColorLight_internal_obj_new(pbsys_status_light);
+    self->imu = hubs_MoveHub_IMU_make_new();
     return MP_OBJ_FROM_PTR(self);
 }
 
@@ -46,6 +240,7 @@ STATIC const mp_rom_map_elem_t hubs_MoveHub_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_shutdown),    MP_ROM_PTR(&hubs_movehub_shutdown_obj)     },
     { MP_ROM_QSTR(MP_QSTR_reboot),      MP_ROM_PTR(&hubs_movehub_reboot_obj)       },
     { MP_ROM_QSTR(MP_QSTR_update),      MP_ROM_PTR(&hubs_movehub_update_obj)       },
+    { MP_ROM_QSTR(MP_QSTR_imu),         MP_ROM_ATTRIBUTE_OFFSET(hubs_MoveHub_obj_t, imu) },
     { MP_ROM_QSTR(MP_QSTR_light),       MP_ROM_ATTRIBUTE_OFFSET(hubs_MoveHub_obj_t, light) },
 };
 STATIC MP_DEFINE_CONST_DICT(hubs_MoveHub_locals_dict, hubs_MoveHub_locals_dict_table);
