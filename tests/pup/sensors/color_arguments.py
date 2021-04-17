42a7bf45d69a961f1b410e27bf77f63a6069cc45
Bert <BertLindeman@users.noreply.github.com>
2021-04-17 10:03:57 +0200

    Better variable names and comments like sentences

--- tests/pup/sensors/color_arguments.py
+++ tests/pup/sensors/color_arguments.py
@@ -1,35 +1,34 @@
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2020 The Pybricks Authors
+# Copyright (c) 2021 The Pybricks Authors
 
 """
 Hardware Module: 1
 
-Description: 
-	Verify that ambient() and reflection() refuse an argument
+Description: Verify that ambient() and reflection() refuse an argument.
 """
 
 from pybricks.pupdevices import ColorSensor
 from pybricks.parameters import Port
 
-# Initialize devices.
+# Initialize device.
 color_sensor = ColorSensor(Port.B)
 
-# Get the ambient light intensity.
-ambientD = color_sensor.ambient()
+# Get the ambient light intensity to verify that the default works.
+ambient_light_default = color_sensor.ambient()
 
-# verify an argument passed to ambient is refused
+# verify an argument passed to ambient is correctly refused.
 expected = "function doesn't take keyword arguments"
 try:
-    ambientT = color_sensor.ambient(surface=True)
+    ambient_light_surface_true = color_sensor.ambient(surface=True)
 except Exception as e:
     assert str(e) == expected, "Expected '{0}' == '{1}'".format(e, expected)
 
-# Get the reflected light intensity.
-reflectionD = color_sensor.reflection()
+# Get the reflected light intensity to verify that the default works.
+reflection_default = color_sensor.reflection()
 
-# verify an argument passed to ambient is refused
+# verify an argument passed to reflection is correctly refused.
 expected = "function doesn't take keyword arguments"
 try:
-    reflectionT = color_sensor.reflection(surface=True)
+    reflection_surface_true = color_sensor.reflection(surface=True)
 except Exception as e:
     assert str(e) == expected, "Expected '{0}' == '{1}'".format(e, expected)
