a463250890261e2a42962b71435de17dc11921ec
Laurens Valk <laurens@pybricks.com>
2021-06-08 13:21:24 +0200

    tests/pup/motors: Test that EBUSY is raised.

---
+++ tests/pup/motors/busy.py
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 The Pybricks Authors
+
+"""
+Hardware Module: 1
+
+Description: Checks that EBUSY is raised when changing settings while holding.
+"""
+
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port
+from uerrno import EBUSY
+
+# Initialize the motor.
+motor = Motor(Port.A)
+
+# Rotate the motor to base position and hold it there.
+motor.run_target(500, 0)
+
+# Try to change a setting. This should raise an error because the motor is busy.
+try:
+    motor.control.limits(duty=50)
+except Exception as e:
+    exception = e
+    pass
+
+# Assert that we got the expected exception.
+assert exception.args[0] == EBUSY, "Did not raise expected exception."
