f0b80f96ba356b92205a94ab6520e805f497f63c
Laurens Valk <laurens@pybricks.com>
2021-06-08 12:08:54 +0200

    tests/pup/basic: Automate system exit test.
    Allow this one to pass both with and without human interaction. If no button is pressed within 5 seconds, it just raises the SystemExit automatically.

--- tests/pup/basic/system_exit.py
+++ tests/pup/basic/system_exit.py
@@ -7,15 +7,19 @@ Hardware Module: Any hub.
 Description: Catch SystemExit from the stop button.
 """
 
-from pybricks.tools import wait
+from pybricks.tools import wait, StopWatch
+
 
 for i in range(3):
-    print("press stop button...")
+    print("Press stop button or wait 5 seconds...")
     try:
+        watch = StopWatch()
         while True:
             wait(10)
+            if watch.time() > 5000:
+                raise SystemExit
     except SystemExit:
-        print("caught SystemExit", i)
+        print("Caught SystemExit", i)
 
 # Should print this after pressing the stop button 3 times.
-print("...done")
+print("Done!")
