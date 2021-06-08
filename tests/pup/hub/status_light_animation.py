40a8f8f0d96a5112d730a4ea076e2bded9071d5c
Laurens Valk <laurens@pybricks.com>
2021-06-08 12:03:12 +0200

    tests/pup/hub/status_light: Use finite animation.
    Otherwise, this test never completes.

--- tests/pup/hub/status_light_animation.py
+++ tests/pup/hub/status_light_animation.py
@@ -11,7 +11,9 @@ hub = ThisHub()
 # Note: In real programs, use every 2 or 3 degrees to save memory.
 rainbow = [Color.BLUE >> d for d in range(360)]
 
-hub.light.animate(rainbow, 100)
+# Start the animation.
+INTERVAL = 50
+hub.light.animate(rainbow, INTERVAL)
 
-while True:
-    wait(10)
+# Wait for two whole cycles.
+wait(2 * INTERVAL * len(rainbow))
