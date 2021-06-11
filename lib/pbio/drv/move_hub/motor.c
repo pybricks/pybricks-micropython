pybricks/pybricks-micropython

commit fa08cb7a70766aef18aa8cd4e22ecca5dd1149ce

drv/move_hub/motor: use pbdrv GPIO APIs
This simplifies the code and reduces the firmware size.

lib/pbio/drv/move_hub/motor.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2020 The Pybricks Authors
+// Copyright (c) 2018-2021 The Pybricks Authors
 
 #include <pbdrv/config.h>
 
@@ -11,40 +11,41 @@
 
 #include "stm32f070xb.h"
 
+#include <pbdrv/gpio.h>
 #include <pbdrv/motor.h>
 #include <pbdrv/pwm.h>
 #include <pbio/error.h>
 #include <pbio/iodev.h>
 #include <pbio/port.h>
 
+static const pbdrv_gpio_t port_a_pin_1 = { .bank = GPIOA, .pin = 8 };
+static const pbdrv_gpio_t port_a_pin_2 = { .bank = GPIOA, .pin = 10 };
+static const pbdrv_gpio_t port_b_pin_1 = { .bank = GPIOA, .pin = 9 };
+static const pbdrv_gpio_t port_b_pin_2 = { .bank = GPIOA, .pin = 11 };
+static const pbdrv_gpio_t port_c_pin_1 = { .bank = GPIOC, .pin = 8 };
+static const pbdrv_gpio_t port_c_pin_2 = { .bank = GPIOC, .pin = 6 };
+static const pbdrv_gpio_t port_d_pin_1 = { .bank = GPIOC, .pin = 7 };
+static const pbdrv_gpio_t port_d_pin_2 = { .bank = GPIOC, .pin = 9 };
 
 pbio_error_t pbdrv_motor_coast(pbio_port_t port) {
+    // motor terminals will be floating when both pins are low
 
-    // set both port pins 1 and 2 to output low
     switch (port) {
         case PBIO_PORT_A:
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER8_Msk) | (1 << GPIO_MODER_MODER8_Pos);
-            GPIOA->BRR = GPIO_BRR_BR_8;
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER10_Msk) | (1 << GPIO_MODER_MODER10_Pos);
-            GPIOA->BRR = GPIO_BRR_BR_10;
+            pbdrv_gpio_out_low(&port_a_pin_1);
+            pbdrv_gpio_out_low(&port_a_pin_2);
             break;
         case PBIO_PORT_B:
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER9_Msk) | (1 << GPIO_MODER_MODER9_Pos);
-            GPIOA->BRR = GPIO_BRR_BR_9;
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER11_Msk) | (1 << GPIO_MODER_MODER11_Pos);
-            GPIOA->BRR = GPIO_BRR_BR_11;
+            pbdrv_gpio_out_low(&port_b_pin_1);
+            pbdrv_gpio_out_low(&port_b_pin_2);
             break;
         case PBIO_PORT_C:
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER6_Msk) | (1 << GPIO_MODER_MODER6_Pos);
-            GPIOC->BRR = GPIO_BRR_BR_6;
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER8_Msk) | (1 << GPIO_MODER_MODER8_Pos);
-            GPIOC->BRR = GPIO_BRR_BR_8;
+            pbdrv_gpio_out_low(&port_c_pin_1);
+            pbdrv_gpio_out_low(&port_c_pin_2);
             break;
         case PBIO_PORT_D:
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER7_Msk) | (1 << GPIO_MODER_MODER7_Pos);
-            GPIOC->BRR = GPIO_BRR_BR_7;
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER9_Msk) | (1 << GPIO_MODER_MODER9_Pos);
-            GPIOC->BRR = GPIO_BRR_BR_9;
+            pbdrv_gpio_out_low(&port_d_pin_1);
+            pbdrv_gpio_out_low(&port_d_pin_2);
             break;
         default:
             return PBIO_ERROR_INVALID_PORT;
@@ -54,31 +55,23 @@ pbio_error_t pbdrv_motor_coast(pbio_port_t port) {
 }
 
 static void pbdrv_motor_brake(pbio_port_t port) {
-    // set both port pins 1 and 2 to output high
+    // motor terminals will short-circuit when both pins are high
+
     switch (port) {
         case PBIO_PORT_A:
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER8_Msk) | (1 << GPIO_MODER_MODER8_Pos);
-            GPIOA->BSRR = GPIO_BSRR_BS_8;
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER10_Msk) | (1 << GPIO_MODER_MODER10_Pos);
-            GPIOA->BSRR = GPIO_BSRR_BS_10;
+            pbdrv_gpio_out_high(&port_a_pin_1);
+            pbdrv_gpio_out_high(&port_a_pin_2);
             break;
         case PBIO_PORT_B:
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER9_Msk) | (1 << GPIO_MODER_MODER9_Pos);
-            GPIOA->BSRR = GPIO_BSRR_BS_9;
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER11_Msk) | (1 << GPIO_MODER_MODER11_Pos);
-            GPIOA->BSRR = GPIO_BSRR_BS_11;
+            pbdrv_gpio_out_high(&port_b_pin_1);
+            pbdrv_gpio_out_high(&port_b_pin_2);
             break;
-        case PBIO_PORT_C:
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER6_Msk) | (1 << GPIO_MODER_MODER6_Pos);
-            GPIOC->BSRR = GPIO_BSRR_BS_6;
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER8_Msk) | (1 << GPIO_MODER_MODER8_Pos);
-            GPIOC->BSRR = GPIO_BSRR_BS_8;
+            pbdrv_gpio_out_high(&port_c_pin_1);
+            pbdrv_gpio_out_high(&port_c_pin_2);
             break;
         case PBIO_PORT_D:
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER7_Msk) | (1 << GPIO_MODER_MODER7_Pos);
-            GPIOC->BSRR = GPIO_BSRR_BS_7;
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER9_Msk) | (1 << GPIO_MODER_MODER9_Pos);
-            GPIOC->BSRR = GPIO_BSRR_BS_9;
+            pbdrv_gpio_out_high(&port_d_pin_1);
+            pbdrv_gpio_out_high(&port_d_pin_2);
             break;
         default:
             break;
@@ -91,36 +84,32 @@ static void pbdrv_motor_run_fwd(pbio_port_t port, int16_t duty_cycle) {
     // one pin as out, high and the other as PWM
     switch (port) {
         case PBIO_PORT_A:
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER10_Msk) | (1 << GPIO_MODER_MODER10_Pos);
-            GPIOA->BSRR = GPIO_BSRR_BS_10;
+            pbdrv_gpio_out_high(&port_a_pin_2);
             if (pbdrv_pwm_get_dev(0, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 1, 1000 - duty_cycle / 10);
             }
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER8_Msk) | (2 << GPIO_MODER_MODER8_Pos);
+            pbdrv_gpio_alt(&port_a_pin_1, 2);
             break;
         case PBIO_PORT_B:
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER11_Msk) | (1 << GPIO_MODER_MODER11_Pos);
-            GPIOA->BSRR = GPIO_BSRR_BS_11;
+            pbdrv_gpio_out_high(&port_b_pin_2);
             if (pbdrv_pwm_get_dev(0, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 2, 1000 - duty_cycle / 10);
             }
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER9_Msk) | (2 << GPIO_MODER_MODER9_Pos);
+            pbdrv_gpio_alt(&port_b_pin_1, 2);
             break;
         case PBIO_PORT_C:
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER6_Msk) | (1 << GPIO_MODER_MODER6_Pos);
-            GPIOC->BSRR = GPIO_BSRR_BS_6;
+            pbdrv_gpio_out_high(&port_c_pin_2);
             if (pbdrv_pwm_get_dev(1, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 3, 1000 - duty_cycle / 10);
             }
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER8_Msk) | (2 << GPIO_MODER_MODER8_Pos);
+            pbdrv_gpio_alt(&port_c_pin_1, 2);
             break;
         case PBIO_PORT_D:
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER9_Msk) | (1 << GPIO_MODER_MODER9_Pos);
-            GPIOC->BSRR = GPIO_BSRR_BS_9;
+            pbdrv_gpio_out_high(&port_d_pin_2);
             if (pbdrv_pwm_get_dev(1, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 2, 1000 - duty_cycle / 10);
             }
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER7_Msk) | (2 << GPIO_MODER_MODER7_Pos);
+            pbdrv_gpio_alt(&port_d_pin_1, 2);
             break;
         default:
             break;
@@ -133,36 +122,32 @@ static void pbdrv_motor_run_rev(pbio_port_t port, int16_t duty_cycle) {
     // one pin as out, high and the other as PWM
     switch (port) {
         case PBIO_PORT_A:
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER8_Msk) | (1 << GPIO_MODER_MODER8_Pos);
-            GPIOA->BSRR = GPIO_BSRR_BS_8;
+            pbdrv_gpio_out_high(&port_a_pin_1);
             if (pbdrv_pwm_get_dev(0, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 3, 1000 + duty_cycle / 10);
             }
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER10_Msk) | (2 << GPIO_MODER_MODER10_Pos);
+            pbdrv_gpio_alt(&port_a_pin_2, 2);
             break;
         case PBIO_PORT_B:
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER9_Msk) | (1 << GPIO_MODER_MODER9_Pos);
-            GPIOA->BSRR = GPIO_BSRR_BS_9;
+            pbdrv_gpio_out_high(&port_b_pin_1);
             if (pbdrv_pwm_get_dev(0, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 4, 1000 + duty_cycle / 10);
             }
-            GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODER11_Msk) | (2 << GPIO_MODER_MODER11_Pos);
+            pbdrv_gpio_alt(&port_b_pin_2, 2);
             break;
         case PBIO_PORT_C:
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER8_Msk) | (1 << GPIO_MODER_MODER8_Pos);
-            GPIOC->BSRR = GPIO_BSRR_BS_8;
+            pbdrv_gpio_out_high(&port_c_pin_1);
             if (pbdrv_pwm_get_dev(1, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 1, 1000 + duty_cycle / 10);
             }
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER6_Msk) | (2 << GPIO_MODER_MODER6_Pos);
+            pbdrv_gpio_alt(&port_c_pin_2, 2);
             break;
         case PBIO_PORT_D:
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER7_Msk) | (1 << GPIO_MODER_MODER7_Pos);
-            GPIOC->BSRR = GPIO_BSRR_BS_7;
+            pbdrv_gpio_out_high(&port_d_pin_1);
             if (pbdrv_pwm_get_dev(1, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 4, 1000 + duty_cycle / 10);
             }
-            GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER9_Msk) | (2 << GPIO_MODER_MODER9_Pos);
+            pbdrv_gpio_alt(&port_d_pin_2, 2);
             break;
         default:
             break;
@@ -173,16 +158,6 @@ pbio_error_t pbdrv_motor_set_duty_cycle(pbio_port_t port, int16_t duty_cycle) {
     if (port < PBIO_PORT_A || port > PBIO_PORT_D) {
         return PBIO_ERROR_INVALID_PORT;
     }
-
-    // if (port == PBIO_PORT_C || port == PBIO_PORT_D) {
-    //     pbio_iodev_t *iodev;
-
-    //     iodev = get_iodev(port);
-
-    //     if (!iodev) {
-    //         return PBIO_ERROR_NO_DEV;
-    //     }
-    // }
 
     if (duty_cycle < -10000 || duty_cycle > 10000) {
         return PBIO_ERROR_INVALID_ARG;

commit 921bf5e9b012e5ea4a6418d57b4338d8989866d3

drv/move_hub/motor: fix alt function on port C/D
This fixes a regression from fa08cb7a.

lib/pbio/drv/move_hub/motor.c
@@ -102,14 +102,14 @@ static void pbdrv_motor_run_fwd(pbio_port_t port, int16_t duty_cycle) {
             if (pbdrv_pwm_get_dev(1, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 3, 1000 - duty_cycle / 10);
             }
-            pbdrv_gpio_alt(&port_c_pin_1, 2);
+            pbdrv_gpio_alt(&port_c_pin_1, 0);
             break;
         case PBIO_PORT_D:
             pbdrv_gpio_out_high(&port_d_pin_2);
             if (pbdrv_pwm_get_dev(1, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 2, 1000 - duty_cycle / 10);
             }
-            pbdrv_gpio_alt(&port_d_pin_1, 2);
+            pbdrv_gpio_alt(&port_d_pin_1, 0);
             break;
         default:
             break;
@@ -140,14 +140,14 @@ static void pbdrv_motor_run_rev(pbio_port_t port, int16_t duty_cycle) {
             if (pbdrv_pwm_get_dev(1, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 1, 1000 + duty_cycle / 10);
             }
-            pbdrv_gpio_alt(&port_c_pin_2, 2);
+            pbdrv_gpio_alt(&port_c_pin_2, 0);
             break;
         case PBIO_PORT_D:
             pbdrv_gpio_out_high(&port_d_pin_1);
             if (pbdrv_pwm_get_dev(1, &dev) == PBIO_SUCCESS) {
                 pbdrv_pwm_set_duty(dev, 4, 1000 + duty_cycle / 10);
             }
-            pbdrv_gpio_alt(&port_d_pin_2, 2);
+            pbdrv_gpio_alt(&port_d_pin_2, 0);
             break;
         default:
             break;