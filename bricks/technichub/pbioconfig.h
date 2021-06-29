7fe0e06e270a7dddba647910d3c14f1af2c8f007
David Lechner <david@pybricks.com>
2021-06-29 16:18:24 -0500

    bricks/pbioconfig: remove unused option
    PBIO_CONFIG_IOPORT_LPF2 is not used anywhere, so it can be removed.

--- bricks/technichub/pbioconfig.h
+++ bricks/technichub/pbioconfig.h
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2019-2020 The Pybricks Authors
 
-#define PBIO_CONFIG_IOPORT_LPF2             (1)
-
 #define PBIO_CONFIG_DCMOTOR                 (1)
 #define PBIO_CONFIG_LIGHT                   (1)
 #define PBIO_CONFIG_TACHO                   (1)
