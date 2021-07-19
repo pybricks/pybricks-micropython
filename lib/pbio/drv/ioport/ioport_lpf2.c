pybricks/pybricks-micropython

commit 1473c4ba649fd38d07731c6104f66bf43b083d38

sys/io_ports: add new sys component to handle I/O ports
For now, this just powers down the ports when the hub is shut down.

Issue: https://github.com/pybricks/pybricks-micropython/issues/36

lib/pbio/drv/ioport/ioport_lpf2.c
@@ -139,6 +139,10 @@ void pbdrv_ioport_lpf2_init(void) {
     process_start(&pbdrv_ioport_lpf2_process, NULL);
 }
 
+void pbdrv_ioport_lpf2_shutdown(void) {
+    process_exit(&pbdrv_ioport_lpf2_process);
+}
+
 static void ioport_enable_uart(ioport_dev_t *ioport) {
     const pbdrv_ioport_lpf2_port_platform_data_t *pdata = ioport->pdata;
 

bricks/stm32/stm32.mk
@@ -452,6 +452,7 @@ PBIO_SRC_C = $(addprefix lib/pbio/,\
 	sys/bluetooth.c \
 	sys/command.c \
 	sys/hmi.c \
+	sys/io_ports.c \
 	sys/light_matrix.c \
 	sys/light.c \
 	sys/main.c \

lib/pbio/drv/ioport/ioport_lpf2.h
@@ -43,8 +43,11 @@ typedef struct {
 extern const pbdrv_ioport_lpf2_platform_data_t pbdrv_ioport_lpf2_platform_data;
 
 void pbdrv_ioport_lpf2_init(void);
+void pbdrv_ioport_lpf2_shutdown(void);
+
 #else // PBDRV_CONFIG_IOPORT_LPF2
 #define pbdrv_ioport_lpf2_init()
+#define pbdrv_ioport_lpf2_shutdown()
 #endif // PBDRV_CONFIG_IOPORT_LPF2
 
 #endif // _INTERNAL_PBDRV_IOPORT_LPF2_H_

lib/pbio/platform/city_hub/sys.c
@@ -11,6 +11,7 @@
 #include <pbsys/status.h>
 
 #include "../sys/hmi.h"
+#include "../sys/io_ports.h"
 #include "../sys/supervisor.h"
 #include "../sys/user_program.h"
 
@@ -33,6 +34,7 @@ PROCESS_THREAD(pbsys_process, ev, data) {
             etimer_reset(&timer);
             pbsys_battery_poll();
             pbsys_hmi_poll();
+            pbsys_io_ports_poll();
             pbsys_supervisor_poll();
             pbsys_user_program_poll();
         }

lib/pbio/platform/move_hub/sys.c
@@ -11,6 +11,7 @@
 #include <pbsys/status.h>
 
 #include "../sys/hmi.h"
+#include "../sys/io_ports.h"
 #include "../sys/supervisor.h"
 #include "../sys/user_program.h"
 
@@ -33,6 +34,7 @@ PROCESS_THREAD(pbsys_process, ev, data) {
             etimer_reset(&timer);
             pbsys_battery_poll();
             pbsys_hmi_poll();
+            pbsys_io_ports_poll();
             pbsys_supervisor_poll();
             pbsys_user_program_poll();
         }

lib/pbio/platform/prime_hub/sys.c
@@ -11,6 +11,7 @@
 #include <pbsys/status.h>
 
 #include "../sys/hmi.h"
+#include "../sys/io_ports.h"
 #include "../sys/supervisor.h"
 #include "../sys/user_program.h"
 
@@ -33,6 +34,7 @@ PROCESS_THREAD(pbsys_process, ev, data) {
             etimer_reset(&timer);
             pbsys_battery_poll();
             pbsys_hmi_poll();
+            pbsys_io_ports_poll();
             pbsys_supervisor_poll();
             pbsys_user_program_poll();
         }

lib/pbio/platform/technic_hub/sys.c
@@ -11,6 +11,7 @@
 #include <pbsys/status.h>
 
 #include "../sys/hmi.h"
+#include "../sys/io_ports.h"
 #include "../sys/supervisor.h"
 #include "../sys/user_program.h"
 
@@ -34,6 +35,7 @@ PROCESS_THREAD(pbsys_process, ev, data) {
             etimer_reset(&timer);
             pbsys_battery_poll();
             pbsys_hmi_poll();
+            pbsys_io_ports_poll();
             pbsys_supervisor_poll();
             pbsys_user_program_poll();
         }

lib/pbio/sys/io_ports.c
@@ -1,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2021 The Pybricks Authors
+
+// Manages I/O ports
+
+#include <pbsys/status.h>
+
+// TODO need to make this more generic - for now assuming LPF2 everywhere
+#include "../../drv/ioport/ioport_lpf2.h"
+
+void pbsys_io_ports_poll(void) {
+    if (pbsys_status_test(PBIO_PYBRICKS_STATUS_SHUTDOWN)) {
+        pbdrv_ioport_lpf2_shutdown();
+    }
+}

commit 2f52c34ada876fa9574b6766bd49fee27187377e

drv/ioport_lpf2: add move hub shutdown quirk
This adds a configuration option to handle a quirk of the BOOST Move
hub. For some reason, this hub turns back on when the power button is
released after shutdown if the port VCC is turned off.

Also added PROCESS_EXIT() to the exit handler just to be safe while we
are touching this (otherwise device connection manager might run one
more iteration).

Fixes: https://github.com/pybricks/support/issues/386
Fixes: 1473c4ba649fd38d07731c6104f66bf43b083d38

lib/pbio/drv/ioport/ioport_lpf2.c
@@ -409,8 +409,16 @@ PROCESS_THREAD(pbdrv_ioport_lpf2_process, ev, data) {
         // TODO: we need to ensure H-bridge power is off here to avoid potentially
         // damaging custom I/O devices.
 
+        #if PBDRV_CONFIG_IOPORT_LPF2_BOOST_SHUTDOWN_QUIRK
+        // The LEGO BOOST Move hub will turn right back on when the power button
+        // is released if we turn off VCC at shutdown for some reason (hardware bug?).
+        pbdrv_gpio_out_high(&pbdrv_ioport_lpf2_platform_data.port_vcc);
+        #else
         // Turn off power on pin 4 on all ports
         pbdrv_gpio_out_low(&pbdrv_ioport_lpf2_platform_data.port_vcc);
+        #endif
+
+        PROCESS_EXIT();
     });
 
     PROCESS_BEGIN();

lib/pbio/platform/move_hub/pbdrvconfig.h
@@ -41,6 +41,7 @@
 
 #define PBDRV_CONFIG_IOPORT                         (1)
 #define PBDRV_CONFIG_IOPORT_LPF2                    (1)
+#define PBDRV_CONFIG_IOPORT_LPF2_BOOST_SHUTDOWN_QUIRK (1)
 #define PBDRV_CONFIG_IOPORT_LPF2_NUM_PORTS          (2)
 #define PBDRV_CONFIG_IOPORT_LPF2_FIRST_PORT         PBIO_PORT_ID_C
 #define PBDRV_CONFIG_IOPORT_LPF2_LAST_PORT          PBIO_PORT_ID_D