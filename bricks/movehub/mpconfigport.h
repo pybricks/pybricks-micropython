58e882ebd76f9fc197c74220375ef089c90e6349
Laurens Valk <laurens@pybricks.com>
2021-07-23 14:24:10 +0200

    bricks/movehub: Disable iodevices module.
    This saves about 500 bytes now, and more considering future additions like UARTDevice and I2CDevice.

--- bricks/movehub/mpconfigport.h
+++ bricks/movehub/mpconfigport.h
@@ -22,7 +22,7 @@
 #define PYBRICKS_PY_EXPERIMENTAL        (0)
 #define PYBRICKS_PY_GEOMETRY            (0)
 #define PYBRICKS_PY_HUBS                (1)
-#define PYBRICKS_PY_IODEVICES           (1)
+#define PYBRICKS_PY_IODEVICES           (0)
 #define PYBRICKS_PY_MEDIA               (0)
 #define PYBRICKS_PY_PARAMETERS          (1)
 #define PYBRICKS_PY_PARAMETERS_BUTTON   (1)
