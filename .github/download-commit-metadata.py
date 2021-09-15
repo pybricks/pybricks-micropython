pybricks/pybricks-micropython

commit f8bdc6512e54951bc0fabe16aba2116e90803735

platform/essential_hub: add support for SPIKE Essential hub
This adds support for the new SPIKE Essential hub.

Issue: https://github.com/pybricks/support/issues/440

.github/download-commit-metadata.py
@@ -17,7 +17,7 @@ FIRMWARE_SIZE_TABLE = os.environ["FIRMWARE_SIZE_TABLE"]
 BUILD_DIR = os.environ.get("BUILD_DIR", "build")
 
 # all past and present hub names
-HUBS = ["cityhub", "cplushub", "technichub", "movehub", "primehub", "nxt"]
+HUBS = ["cityhub", "cplushub", "technichub", "movehub", "primehub", "essentialhub,", "nxt"]
 
 QUERY = """query {
     repository(owner:"pybricks", name:"pybricks-micropython") {

.github/build-each-commit.py
@@ -37,11 +37,11 @@ for commit in reversed(
     # update only required submodules
     pybricks.git.submodule("update", "--init", "micropython")
     pybricks.git.submodule("update", "--init", "lib/libfixmath")
-    if args.hub in ["cityhub", "movehub", "technichub", "primehub"]:
+    if args.hub in ["cityhub", "movehub", "technichub", "primehub", "essentialhub"]:
         pybricks.submodule("micropython").module().git.submodule(
             "update", "--init", "lib/stm32lib"
         )
-    if args.hub == "primehub":
+    if args.hub == "primehub" or args.hub == "essentialhub":
         pybricks.git.submodule("update", "--init", "--checkout", "lib/btstack")
     if args.hub == "nxt":
         pybricks.git.submodule(

.github/build-missing-commits.py
@@ -19,7 +19,7 @@ PYBRICKS_PATH = os.environ.get("PYBRICKS_PATH", ".")
 
 PYBRICKS_BRANCH = "origin/master"
 
-HUBS = ["movehub", "cityhub", "technichub", "primehub", "nxt"]
+HUBS = ["movehub", "cityhub", "technichub", "primehub", "essentialhub", "nxt"]
 
 print("Building commits...")
 

.github/build-stats-web.py
@@ -15,7 +15,7 @@ BUILD_DIR = os.environ.get("BUILD_DIR", "build")
 # Number of digits of hash to display
 HASH_SIZE = 7
 
-HUBS = ["cityhub", "technichub", "movehub", "primehub", "nxt"]
+HUBS = ["cityhub", "technichub", "movehub", "primehub", "essentialhub", "nxt"]
 
 GITHUB_REPO_URL = "https://github.com/pybricks/pybricks-micropython"
 

.github/workflows/build.yml
@@ -171,7 +171,7 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        hub: [cityhub, movehub, nxt, primehub, technichub]
+        hub: [cityhub, essentialhub, movehub, nxt, primehub, technichub]
     needs: [cross_compiler, mpy_cross]
     runs-on: ubuntu-20.04
     steps:
@@ -227,7 +227,7 @@ jobs:
           bricks/${{ matrix.hub }}/main.py
           bricks/${{ matrix.hub }}/ReadMe_OSS.txt
     - name: Upload install_pybricks.*
-      if: ${{ success() && matrix.hub == 'primehub' }}
+      if: ${{ success() && (matrix.hub == 'essentialhub' || matrix.hub == 'primehub') }}
       uses: actions/upload-artifact@v2
       with:
         name: ${{ matrix.hub }}-firmware

Makefile
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2019-2020 The Pybricks Authors
+# Copyright (c) 2019-2021 The Pybricks Authors
 
 # ensure micropython submodule is checked out for building mpy-cross
 ifeq ("$(wildcard micropython/README.md)","")
@@ -28,9 +28,9 @@ doc:
 clean-doc:
 	@$(MAKE) -C lib/pbio/doc clean
 
-all: movehub cityhub technichub primehub nxt ev3dev-armel doc
+all: movehub cityhub technichub primehub essentialhub nxt ev3dev-armel doc
 
-clean-all: clean-movehub clean-cityhub clean-technichub clean-primehub clean-nxt clean-ev3dev-armel clean-doc
+clean-all: clean-movehub clean-cityhub clean-technichub clean-primehub clean-essentialhub clean-nxt clean-ev3dev-armel clean-doc
 
 ifeq ($(HOST_OS),Linux)
 
@@ -105,6 +105,12 @@ primehub: mpy-cross
 
 clean-primehub: clean-mpy-cross
 	@$(MAKE) -C bricks/primehub clean
+
+essentialhub: mpy-cross
+	@$(MAKE) -C bricks/essentialhub
+
+clean-essentialhub: clean-mpy-cross
+	@$(MAKE) -C bricks/essentialhub clean
 
 mpy-cross:
 	@$(MAKE) -C micropython/mpy-cross CROSS_COMPILE=$(HOST_CROSS_COMPILE)

bricks/essentialhub/Makefile
@@ -1,0 +1,20 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2019-2021 The Pybricks Authors
+
+PBIO_PLATFORM = essential_hub
+PB_MCU_SERIES = F4
+PB_CMSIS_MCU = STM32F413xx
+PB_MCU_EXT_OSC_HZ = 16000000
+PB_FIRMWARE_MAX_SIZE = 524288
+PB_USE_HAL = 1
+PB_USE_LSM6DS3TR_C = 1
+PB_LIB_BLUENRG = 0
+PB_LIB_BTSTACK = 1
+PB_LIB_STM32_USB_DEVICE = 1
+PB_DUAL_BOOT = 1
+CHECKSUM_TYPE = crc32
+TEXT0_ADDR = 0x8008000
+DFU_VID = 0x0694
+DFU_PID = 0x000C
+
+include ../stm32/stm32.mk

bricks/essentialhub/essential_hub.ld
@@ -1,0 +1,20 @@
+/* SPDX-License-Identifier: MIT */
+/* Copyright (c) 2013, 2014 Damien P. George */
+/* Copyright (c) 2018-2021 The Pybricks Authors */
+
+/*
+    GNU linker script for LEGO SPIKE Essential hub
+*/
+
+/* Specify the memory areas */
+MEMORY
+{
+    /* Flash size is 1M. Bootloader resides in first 32K. When DUAL_BOOT is
+     * defined, we put the Pybricks firmware in the last 256K of the flash,
+     * reserving the rest for the official LEGO firmware. */
+    FLASH (rx)      : ORIGIN = DEFINED(DUAL_BOOT) ? 0x080C0000 : 0x08008000, LENGTH = DEFINED(DUAL_BOOT) ? 256K : 992K
+    /* SRAM1 (256K) and SRAM2 (64K) are treated as one continuous block */
+    RAM (xrw)       : ORIGIN = 0x20000000, LENGTH = 320K
+}
+
+_minimum_stack_size = 8K;

commit c4599bcd4c3eae0f9657a83c89c01770f73cd813

github: fix typo in download-commit-metadata.py
There was an extra trailing comma inside the string "essentialhub".

This was causing the "Stats" CI task to fail with:

    Client-Request-ID=827c8092-1630-11ec-8f90-57a053a78575 Retry policy did not allow for a retry: Server-Timestamp=Wed, 15 Sep 2021 14:23:34 GMT, Server-Request-ID=28a86b1b-6002-009a-073d-aa1971000000, HTTP status code=400, Exception=Bad Request{"odata.error":{"code":"InvalidInput","message":{"lang":"en-US","value":"An identifier was expected at position 65.\nRequestId:28a86b1b-6002-009a-073d-aa1971000000\nTime:2021-09-15T14:23:34.8381644Z"}}}.
    Traceback (most recent call last):
    File ".github/download-commit-metadata.py", line 96, in <module>
        main()
    File ".github/download-commit-metadata.py", line 53, in main
        for item in service.query_entities(
    File "/home/david/work/pybricks/pybricks-micropython/.venv/lib/python3.8/site-packages/azure/cosmosdb/table/tableservice.py", line 738, in query_entities
        resp = self._query_entities(*args, **kwargs)
    File "/home/david/work/pybricks/pybricks-micropython/.venv/lib/python3.8/site-packages/azure/cosmosdb/table/tableservice.py", line 801, in _query_entities
        return self._perform_request(request, _convert_json_response_to_entities,
    File "/home/david/work/pybricks/pybricks-micropython/.venv/lib/python3.8/site-packages/azure/cosmosdb/table/tableservice.py", line 1106, in _perform_request
        return super(TableService, self)._perform_request(request, parser, parser_args, operation_context)
    File "/home/david/work/pybricks/pybricks-micropython/.venv/lib/python3.8/site-packages/azure/cosmosdb/table/common/storageclient.py", line 430, in _perform_request
        raise ex
    File "/home/david/work/pybricks/pybricks-micropython/.venv/lib/python3.8/site-packages/azure/cosmosdb/table/common/storageclient.py", line 358, in _perform_request
        raise ex
    File "/home/david/work/pybricks/pybricks-micropython/.venv/lib/python3.8/site-packages/azure/cosmosdb/table/common/storageclient.py", line 343, in _perform_request
        _http_error_handler(
    File "/home/david/work/pybricks/pybricks-micropython/.venv/lib/python3.8/site-packages/azure/cosmosdb/table/common/_error.py", line 115, in _http_error_handler
        raise ex
    azure.common.AzureHttpError: Bad Request
    {"odata.error":{"code":"InvalidInput","message":{"lang":"en-US","value":"An identifier was expected at position 65.\nRequestId:28a86b1b-6002-009a-073d-aa1971000000\nTime:2021-09-15T14:23:34.8381644Z"}}}

Fixes: f8bdc651

.github/download-commit-metadata.py
@@ -17,7 +17,15 @@ FIRMWARE_SIZE_TABLE = os.environ["FIRMWARE_SIZE_TABLE"]
 BUILD_DIR = os.environ.get("BUILD_DIR", "build")
 
 # all past and present hub names
-HUBS = ["cityhub", "cplushub", "technichub", "movehub", "primehub", "essentialhub,", "nxt"]
+HUBS = [
+    "cityhub",
+    "cplushub",
+    "technichub",
+    "movehub",
+    "primehub",
+    "essentialhub",
+    "nxt",
+]
 
 QUERY = """query {
     repository(owner:"pybricks", name:"pybricks-micropython") {