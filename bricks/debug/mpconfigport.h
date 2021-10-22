f20c99764f65d39a030f6f5eda0bc63c68e133c9
David Lechner <david@pybricks.com>
2021-10-22 15:13:10 -0500

    bricks/mpconfigport: include all PYBRICKS_PY_* options
    This adds all possible PYBRICKS_PY_* options to all config files. This
    makes it easier to compare features of each port and makes it trivial
    to copy a config to a new brick without missing features.

--- bricks/debug/mpconfigport.h
+++ bricks/debug/mpconfigport.h
@@ -12,15 +12,25 @@
 
 // Pybricks modules
 #define PYBRICKS_PY_COMMON              (1)
+#define PYBRICKS_PY_COMMON_IMU          (0)
 #define PYBRICKS_PY_COMMON_KEYPAD       (1)
+#define PYBRICKS_PY_COMMON_LIGHT_MATRIX (0)
 #define PYBRICKS_PY_COMMON_MOTORS       (0)
+#define PYBRICKS_PY_COMMON_SPEAKER      (0)
+#define PYBRICKS_PY_COMMON_SYSTEM       (0)
+#define PYBRICKS_PY_EV3DEVICES          (0)
+#define PYBRICKS_PY_EXPERIMENTAL        (0)
+#define PYBRICKS_PY_GEOMETRY            (0)
 #define PYBRICKS_PY_HUBS                (0)
 #define PYBRICKS_PY_IODEVICES           (0)
 #define PYBRICKS_PY_MEDIA               (0)
+#define PYBRICKS_PY_MEDIA_EV3DEV        (0)
+#define PYBRICKS_PY_NXTDEVICES          (0)
 #define PYBRICKS_PY_PARAMETERS          (1)
 #define PYBRICKS_PY_PARAMETERS_BUTTON   (1)
 #define PYBRICKS_PY_PARAMETERS_ICON     (0)
 #define PYBRICKS_PY_PUPDEVICES          (1)
+#define PYBRICKS_PY_ROBOTICS            (0)
 #define PYBRICKS_PY_TOOLS               (1)
 
 #include "../stm32/configport.h"
