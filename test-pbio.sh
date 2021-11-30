fcd91c1897939aeaf9761bccb0787bdf1c891c8f
Laurens Valk <laurens@pybricks.com>
2021-11-30 08:21:33 +0100

    test-pbio: Run it faster.
    This can compile and run much faster on most machines by doing things in parallel.

--- test-pbio.sh
+++ test-pbio.sh
@@ -11,7 +11,7 @@ SCRIPT_DIR=$(dirname "$0")
 
 export PBIO_TEST_RESULTS_DIR="${SCRIPT_DIR}/lib/pbio/test/results"
 rm -rf "${PBIO_TEST_RESULTS_DIR:?}"/*
-make -s -C "${SCRIPT_DIR}/lib/pbio/test"
+make -s -C "${SCRIPT_DIR}/lib/pbio/test" -j$(nproc)
 
 ulimit -S -c unlimited
 
