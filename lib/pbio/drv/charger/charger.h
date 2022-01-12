pybricks/pybricks-micropython

commit 472eb7dc8f76005b0d02c7e246847b831bdf2cbe

drv/charger_mp2639a: implement ISET control
The SPIKE Prime hardware uses a PWM output to simulate a resistor
value on the MP2639A ISET pin to control the charging current limit.

We use the type of charger connected to set the current limit.

lib/pbio/drv/charger/charger_mp2639a.c
@@ -22,7 +22,7 @@
 #include <pbdrv/adc.h>
 #include <pbdrv/charger.h>
 #include <pbdrv/gpio.h>
-#if PBDRV_CONFIG_CHARGER_MP2639A_MODE_PWM
+#if PBDRV_CONFIG_CHARGER_MP2639A_MODE_PWM | PBDRV_CONFIG_CHARGER_MP2639A_ISET_PWM
 #include <pbdrv/pwm.h>
 #endif
 #if PBDRV_CONFIG_CHARGER_MP2639A_CHG_RESISTOR_LADDER
@@ -43,6 +43,9 @@ static process_event_t usb_event;
 #if PBDRV_CONFIG_CHARGER_MP2639A_MODE_PWM
 static pbdrv_pwm_dev_t *mode_pwm;
 #endif
+#if PBDRV_CONFIG_CHARGER_MP2639A_ISET_PWM
+static pbdrv_pwm_dev_t *iset_pwm;
+#endif
 
 static pbdrv_charger_status_t pbdrv_charger_status;
 static bool mode_pin_is_low;
@@ -59,6 +62,34 @@ pbdrv_charger_usb_type_t pbdrv_charger_get_usb_type(void) {
 
 void pbdrv_charger_set_usb_type(pbdrv_charger_usb_type_t type) {
     usb_type = type;
+
+    #if PBDRV_CONFIG_CHARGER_MP2639A_ISET_PWM
+    // Set the current limit (ISET) based on the type of charger attached.
+    // The duty cycle values are hard-coded for SPIKE Prime hardware.
+
+    uint32_t duty_cycle;
+    switch (type) {
+        case PBDRV_CHARGER_USB_TYPE_STANDARD_DOWNSTREAM:
+            // REVISIT: Do we need to wait for negotiation to determine if 500mA
+            // is actually available? The chip monitors VBUS and will reduce the
+            // current being used if it starts to pull down VBUS anyway.
+            // duty_cycle = 2; // 100mA
+            duty_cycle = 15; // 500mA
+            break;
+        case PBDRV_CHARGER_USB_TYPE_CHARGING_DOWNSTREAM:
+        case PBDRV_CHARGER_USB_TYPE_DEDICATED_CHARGING:
+            // Allow max charging current (1.5A max according to spec).
+            duty_cycle = 100;
+            break;
+        default:
+            duty_cycle = 0;
+            break;
+    }
+
+    pbdrv_pwm_set_duty(iset_pwm, platform.iset_pwm_ch, duty_cycle);
+
+    #endif
+
     process_post(&pbdrv_charger_mp2639a_process, usb_event, NULL);
 }
 
@@ -118,6 +149,12 @@ PROCESS_THREAD(pbdrv_charger_mp2639a_process, ev, data) {
 
     #if PBDRV_CONFIG_CHARGER_MP2639A_MODE_PWM
     while (pbdrv_pwm_get_dev(platform.mode_pwm_id, &mode_pwm) != PBIO_SUCCESS) {
+        PROCESS_PAUSE();
+    }
+    #endif
+
+    #if PBDRV_CONFIG_CHARGER_MP2639A_ISET_PWM
+    while (pbdrv_pwm_get_dev(platform.iset_pwm_id, &iset_pwm) != PBIO_SUCCESS) {
         PROCESS_PAUSE();
     }
     #endif

lib/pbio/drv/charger/charger_mp2639a.h
@@ -39,7 +39,12 @@ typedef struct {
     #endif
     /** ADC channel connected to the IB pin. */
     uint8_t ib_adc_ch;
-    // TODO: add optional ISET output
+    #if PBDRV_CONFIG_CHARGER_MP2639A_ISET_PWM
+    /** ID of PWM device connected to the ISET pin. */
+    uint8_t iset_pwm_id;
+    /** Channel of PWM device connected to the ISET pin. */
+    uint8_t iset_pwm_ch;
+    #endif
 } pbdrv_charger_mp2639a_platform_data_t;
 
 extern const pbdrv_charger_mp2639a_platform_data_t pbdrv_charger_mp2639a_platform_data;

lib/pbio/platform/prime_hub/pbdrvconfig.h
@@ -40,6 +40,7 @@
 #define PBDRV_CONFIG_CHARGER_MP2639A                (1)
 #define PBDRV_CONFIG_CHARGER_MP2639A_MODE_PWM       (1)
 #define PBDRV_CONFIG_CHARGER_MP2639A_CHG_RESISTOR_LADDER (1)
+#define PBDRV_CONFIG_CHARGER_MP2639A_ISET_PWM       (1)
 
 #define PBDRV_CONFIG_CLOCK                          (1)
 #define PBDRV_CONFIG_CLOCK_STM32                    (1)
@@ -69,9 +70,9 @@
 #define PBDRV_CONFIG_LED_ARRAY_PWM_NUM_DEV          (1)
 
 #define PBDRV_CONFIG_PWM                            (1)
-#define PBDRV_CONFIG_PWM_NUM_DEV                    (6)
+#define PBDRV_CONFIG_PWM_NUM_DEV                    (7)
 #define PBDRV_CONFIG_PWM_STM32_TIM                  (1)
-#define PBDRV_CONFIG_PWM_STM32_TIM_NUM_DEV          (5)
+#define PBDRV_CONFIG_PWM_STM32_TIM_NUM_DEV          (6)
 #define PBDRV_CONFIG_PWM_STM32_TIM_EXTRA_FLAGS      (1)
 #define PBDRV_CONFIG_PWM_TLC5955_STM32              (1)
 #define PBDRV_CONFIG_PWM_TLC5955_STM32_NUM_DEV      (1)

lib/pbio/platform/prime_hub/platform.c
@@ -54,6 +54,7 @@ enum {
     PWM_DEV_3_TIM12,
     PWM_DEV_4_TIM8,
     PWM_DEV_5_TLC5955,
+    PWM_DEV_6_TIM5,
 };
 
 enum {
@@ -138,6 +139,8 @@ const pbdrv_charger_mp2639a_platform_data_t pbdrv_charger_mp2639a_platform_data 
     .chg_resistor_ladder_id = RESISTOR_LADDER_DEV_0,
     .chg_resistor_ladder_ch = PBDRV_RESISTOR_LADDER_CH_2,
     .ib_adc_ch = 3,
+    .iset_pwm_id = PWM_DEV_6_TIM5,
+    .iset_pwm_ch = 1,
 };
 
 // I/O ports
@@ -309,6 +312,17 @@ static void pwm_dev_4_platform_init(void) {
     TIM8->CCR4 = TIM8->ARR / 2;
 }
 
+static void pwm_dev_6_platform_init(void) {
+    GPIO_InitTypeDef gpio_init;
+
+    gpio_init.Pin = GPIO_PIN_0;
+    gpio_init.Mode = GPIO_MODE_AF_PP;
+    gpio_init.Pull = GPIO_NOPULL;
+    gpio_init.Speed = GPIO_SPEED_FREQ_LOW;
+    gpio_init.Alternate = GPIO_AF2_TIM5;
+    HAL_GPIO_Init(GPIOA, &gpio_init);
+}
+
 // NOTE: Official LEGO firmware uses 1.2 kHz PWM for motors. We have changed to
 // 12 kHz to reduce the unpleasant noise (similar to the frequency used by the
 // official EV3 firmware).
@@ -368,6 +382,15 @@ const pbdrv_pwm_stm32_tim_platform_data_t
         .id = PWM_DEV_4_TIM8,
         // channel 4: Bluetooth 32.768 kHz clock
         .channels = PBDRV_PWM_STM32_TIM_CHANNEL_4_ENABLE,
+    },
+    {
+        .platform_init = pwm_dev_6_platform_init,
+        .TIMx = TIM5,
+        .prescalar = 10, // results in 9.6 MHz clock
+        .period = 100, // 9.6 MHz divided by 100 makes 96 kHz PWM
+        .id = PWM_DEV_6_TIM5,
+        // channel 1: Battery charger ISET
+        .channels = PBDRV_PWM_STM32_TIM_CHANNEL_1_ENABLE,
     },
 };
 
@@ -784,8 +807,8 @@ void SystemInit(void) {
         RCC_AHB1ENR_GPIODEN | RCC_AHB1ENR_GPIOEEN | RCC_AHB1ENR_DMA1EN | RCC_AHB1ENR_DMA2EN;
     RCC->APB1ENR |= RCC_APB1ENR_USART2EN | RCC_APB1ENR_UART4EN | RCC_APB1ENR_UART5EN |
         RCC_APB1ENR_UART7EN | RCC_APB1ENR_UART8EN | RCC_APB1ENR_TIM2EN | RCC_APB1ENR_TIM3EN |
-        RCC_APB1ENR_TIM4EN | RCC_APB1ENR_TIM6EN | RCC_APB1ENR_TIM12EN | RCC_APB1ENR_I2C2EN |
-        RCC_APB1ENR_DACEN;
+        RCC_APB1ENR_TIM4EN | RCC_APB1ENR_TIM5EN | RCC_APB1ENR_TIM6EN | RCC_APB1ENR_TIM12EN |
+        RCC_APB1ENR_I2C2EN | RCC_APB1ENR_DACEN;
     RCC->APB2ENR |= RCC_APB2ENR_TIM1EN | RCC_APB2ENR_TIM8EN | RCC_APB2ENR_UART9EN |
         RCC_APB2ENR_UART10EN | RCC_APB2ENR_ADC1EN | RCC_APB2ENR_SPI1EN | RCC_APB2ENR_SYSCFGEN;
     RCC->AHB2ENR |= RCC_AHB2ENR_OTGFSEN;

commit 556ad3568be4a702979e6f9e550abd2af9117720

drv/{charger,usb}: separate USB from charger
This does a better job isolating the drivers as abstractions of only
the specific chip/subsystem that they encapsulate and lets the pbsys
layer do the gluing together instead.

This also fixes a crash introduced in commit 472eb7dc "drv/charger_mp2639a:
implement ISET control" that was caused by pbdrv_charger_set_usb_type()
accessing an initialized pointer because the USB driver was calling
it before the charger driver was done initializing.

lib/pbio/doc/doxygen.conf
@@ -2136,6 +2136,7 @@ PREDEFINED = \
     PBDRV_CONFIG_LED=1 \
     PBDRV_CONFIG_PWM=1 \
     PBDRV_CONFIG_RESET=1 \
+    PBDRV_CONFIG_USB=1 \
     PBDRV_CONFIG_WATCHDOG=1 \
     PBDRV_CONFIG_HAS_PORT_A=1 \
     PBDRV_CONFIG_HAS_PORT_B=1 \

lib/pbio/drv/charger/charger.h
@@ -12,12 +12,9 @@
 
 void pbdrv_charger_init(void);
 
-void pbdrv_charger_set_usb_type(pbdrv_charger_usb_type_t type);
-
 #else // PBDRV_CONFIG_CHARGER
 
 #define pbdrv_charger_init()
-#define pbdrv_charger_set_usb_type(type)
 
 #endif // PBDRV_CONFIG_CHARGER
 

lib/pbio/drv/charger/charger_mp2639a.c
@@ -37,8 +37,6 @@
 #define platform pbdrv_charger_mp2639a_platform_data
 
 PROCESS(pbdrv_charger_mp2639a_process, "MP2639A");
-static pbdrv_charger_usb_type_t usb_type;
-static process_event_t usb_event;
 
 #if PBDRV_CONFIG_CHARGER_MP2639A_MODE_PWM
 static pbdrv_pwm_dev_t *mode_pwm;
@@ -52,45 +50,7 @@ static bool mode_pin_is_low;
 
 void pbdrv_charger_init(void) {
     pbdrv_init_busy_up();
-    usb_event = process_alloc_event();
     process_start(&pbdrv_charger_mp2639a_process);
-}
-
-pbdrv_charger_usb_type_t pbdrv_charger_get_usb_type(void) {
-    return usb_type;
-}
-
-void pbdrv_charger_set_usb_type(pbdrv_charger_usb_type_t type) {
-    usb_type = type;
-
-    #if PBDRV_CONFIG_CHARGER_MP2639A_ISET_PWM
-    // Set the current limit (ISET) based on the type of charger attached.
-    // The duty cycle values are hard-coded for SPIKE Prime hardware.
-
-    uint32_t duty_cycle;
-    switch (type) {
-        case PBDRV_CHARGER_USB_TYPE_STANDARD_DOWNSTREAM:
-            // REVISIT: Do we need to wait for negotiation to determine if 500mA
-            // is actually available? The chip monitors VBUS and will reduce the
-            // current being used if it starts to pull down VBUS anyway.
-            // duty_cycle = 2; // 100mA
-            duty_cycle = 15; // 500mA
-            break;
-        case PBDRV_CHARGER_USB_TYPE_CHARGING_DOWNSTREAM:
-        case PBDRV_CHARGER_USB_TYPE_DEDICATED_CHARGING:
-            // Allow max charging current (1.5A max according to spec).
-            duty_cycle = 100;
-            break;
-        default:
-            duty_cycle = 0;
-            break;
-    }
-
-    pbdrv_pwm_set_duty(iset_pwm, platform.iset_pwm_ch, duty_cycle);
-
-    #endif
-
-    process_post(&pbdrv_charger_mp2639a_process, usb_event, NULL);
 }
 
 pbio_error_t pbdrv_charger_get_current_now(uint16_t *current) {
@@ -108,17 +68,46 @@ pbdrv_charger_status_t pbdrv_charger_get_status(void) {
     return pbdrv_charger_status;
 }
 
-void pbdrv_charger_enable(bool enable) {
+void pbdrv_charger_enable(bool enable, pbdrv_charger_limit_t limit) {
+    #if PBDRV_CONFIG_CHARGER_MP2639A_ISET_PWM
+
+    // Set the current limit (ISET) based on the type of charger attached.
+    // The duty cycle values are hard-coded for SPIKE Prime hardware.
+
+    uint32_t duty_cycle;
+    switch (limit) {
+        case PBDRV_CHARGER_LIMIT_CHARGING:
+            duty_cycle = 100; // max duty cycle
+            break;
+        case PBDRV_CHARGER_LIMIT_STD_MAX:
+            duty_cycle = 15; // 500 mA
+            break;
+        case PBDRV_CHARGER_LIMIT_STD_MIN:
+            duty_cycle = 2; // 100 mA
+            break;
+        default:
+            duty_cycle = 0;
+            break;
+    }
+
+    pbdrv_pwm_set_duty(iset_pwm, platform.iset_pwm_ch, duty_cycle);
+
+    #endif // PBDRV_CONFIG_CHARGER_MP2639A_ISET_PWM
+
     #if PBDRV_CONFIG_CHARGER_MP2639A_MODE_PWM
+
     // REVISIT: only known use has max duty cycle of UINT16_MAX
     pbdrv_pwm_set_duty(mode_pwm, platform.mode_pwm_ch, enable ? 0 : UINT16_MAX);
-    #else
+
+    #else // PBDRV_CONFIG_CHARGER_MP2639A_MODE_PWM
+
     if (enable) {
         pbdrv_gpio_out_low(&platform.mode_gpio);
     } else {
         pbdrv_gpio_out_high(&platform.mode_gpio);
     }
-    #endif
+
+    #endif // PBDRV_CONFIG_CHARGER_MP2639A_MODE_PWM
 
     // Need to keep track of MODE pin state for charging logic since /ACOK pin
     // is not wired up.
@@ -159,7 +148,7 @@ PROCESS_THREAD(pbdrv_charger_mp2639a_process, ev, data) {
     }
     #endif
 
-    pbdrv_charger_enable(false);
+    pbdrv_charger_enable(false, PBDRV_CHARGER_LIMIT_NONE);
 
     #if !PBDRV_CONFIG_CHARGER_MP2639A_CHG_RESISTOR_LADDER
     // /CHG pin is pulled low or open drain.

lib/pbio/drv/usb/usb_stm32.c
@@ -14,7 +14,7 @@
 #include <stm32f4xx_hal_pcd_ex.h>
 #include <usbd_core.h>
 
-#include <pbdrv/charger.h>
+#include <pbdrv/usb.h>
 #include <pbio/util.h>
 
 #include "../charger/charger.h"
@@ -22,9 +22,10 @@
 
 PROCESS(pbdrv_usb_process, "USB");
 
-static volatile bool vbus_active;
 static USBD_HandleTypeDef husbd;
 static PCD_HandleTypeDef hpcd;
+static volatile bool vbus_active;
+static pbdrv_usb_bcd_t pbdrv_usb_bcd;
 
 /**
  * Battery charger detection task.
@@ -52,7 +53,7 @@ static PT_THREAD(pbdrv_usb_stm32_bcd_detect(struct pt *pt)) {
         if (etimer_expired(&timer)) {
             USBx->GCCFG &= ~USB_OTG_GCCFG_DCDEN;
             HAL_PCDEx_DeActivateBCD(&hpcd);
-            pbdrv_charger_set_usb_type(PBDRV_CHARGER_USB_TYPE_NONSTANDARD);
+            pbdrv_usb_bcd = PBDRV_USB_BCD_NONSTANDARD;
             PT_EXIT(pt);
         }
         PT_YIELD(pt);
@@ -73,7 +74,7 @@ static PT_THREAD(pbdrv_usb_stm32_bcd_detect(struct pt *pt)) {
     if (!(USBx->GCCFG & USB_OTG_GCCFG_PDET)) {
         /* Case of Standard Downstream Port */
         USBx->GCCFG &= ~USB_OTG_GCCFG_PDEN;
-        pbdrv_charger_set_usb_type(PBDRV_CHARGER_USB_TYPE_STANDARD_DOWNSTREAM);
+        pbdrv_usb_bcd = PBDRV_USB_BCD_STANDARD_DOWNSTREAM;
     } else {
         /* start secondary detection to check connection to Charging Downstream
         Port or Dedicated Charging Port */
@@ -85,10 +86,10 @@ static PT_THREAD(pbdrv_usb_stm32_bcd_detect(struct pt *pt)) {
 
         if ((USBx->GCCFG) & USB_OTG_GCCFG_SDET) {
             /* case Dedicated Charging Port  */
-            pbdrv_charger_set_usb_type(PBDRV_CHARGER_USB_TYPE_DEDICATED_CHARGING);
+            pbdrv_usb_bcd = PBDRV_USB_BCD_DEDICATED_CHARGING;
         } else {
             /* case Charging Downstream Port  */
-            pbdrv_charger_set_usb_type(PBDRV_CHARGER_USB_TYPE_CHARGING_DOWNSTREAM);
+            pbdrv_usb_bcd = PBDRV_USB_BCD_CHARGING_DOWNSTREAM;
         }
 
         USBx->GCCFG &= ~USB_OTG_GCCFG_SDEN;
@@ -134,6 +135,10 @@ void pbdrv_usb_init(void) {
     process_poll(&pbdrv_usb_process);
 }
 
+pbdrv_usb_bcd_t pbdrv_usb_get_bcd(void) {
+    return pbdrv_usb_bcd;
+}
+
 // Event loop
 
 PROCESS_THREAD(pbdrv_usb_process, ev, data) {
@@ -144,7 +149,7 @@ PROCESS_THREAD(pbdrv_usb_process, ev, data) {
 
     PROCESS_POLLHANDLER({
         if (!bcd_busy && pbio_oneshot(!vbus_active, &no_vbus_oneshot)) {
-            pbdrv_charger_set_usb_type(PBDRV_CHARGER_USB_TYPE_NONE);
+            pbdrv_usb_bcd = PBDRV_USB_BCD_NONE;
         }
 
         // pbdrv_usb_stm32_bcd_detect() needs to run completely to the end,

lib/pbio/include/pbdrv/charger.h
@@ -27,21 +27,34 @@ typedef enum {
     PBDRV_CHARGER_STATUS_FAULT,
 } pbdrv_charger_status_t;
 
-/**
- * Indicates the type of the connected USB port.
- */
+/** Battery charger current limit selection. */
 typedef enum {
-    /** The USB cable is not connected (no VBUS). */
-    PBDRV_CHARGER_USB_TYPE_NONE,
-    /** The USB cable is connected to a non-standard charger or PS/2 port. */
-    PBDRV_CHARGER_USB_TYPE_NONSTANDARD,
-    /** The USB cable is connected to standard downstream port. */
-    PBDRV_CHARGER_USB_TYPE_STANDARD_DOWNSTREAM,
-    /** The USB cable is connected to charging downstream port. */
-    PBDRV_CHARGER_USB_TYPE_CHARGING_DOWNSTREAM,
-    /** The USB cable is connected to dedicated charging port. */
-    PBDRV_CHARGER_USB_TYPE_DEDICATED_CHARGING,
-} pbdrv_charger_usb_type_t;
+    /**
+     * The charger should be limited to 0 mA.
+     *
+     * This can be used when the charger is not charging.
+     */
+    PBDRV_CHARGER_LIMIT_NONE,
+    /**
+     * The charger should be limited to 100 mA.
+     *
+     * This is the standard for USB 2.0 ports.
+     */
+    PBDRV_CHARGER_LIMIT_STD_MIN,
+    /**
+     * The charger should be limited to 500 mA.
+     *
+     * This is the standard for USB 2.0 ports after negotiation.
+     */
+    PBDRV_CHARGER_LIMIT_STD_MAX,
+    /**
+     * The charger should be limited to 1.5A.
+     *
+     * This is the standard for both downstream charging ports and dedicated
+     * charging ports.
+     */
+    PBDRV_CHARGER_LIMIT_CHARGING,
+} pbdrv_charger_limit_t;
 
 #if PBDRV_CONFIG_CHARGER
 
@@ -60,16 +73,11 @@ pbio_error_t pbdrv_charger_get_current_now(uint16_t *current);
 pbdrv_charger_status_t pbdrv_charger_get_status(void);
 
 /**
- * Gets the USB charger type.
- * @return              The type.
- */
-pbdrv_charger_usb_type_t pbdrv_charger_get_usb_type(void);
-
-/**
  * Enables or disables charging.
  * @param [in]  enable  True to enable charging or false for discharging.
+ * @param [in]  limit   The current limit for the charging rate.
  */
-void pbdrv_charger_enable(bool enable);
+void pbdrv_charger_enable(bool enable, pbdrv_charger_limit_t limit);
 
 #else
 
@@ -82,11 +90,7 @@ static inline pbdrv_charger_status_t pbdrv_charger_get_status(void) {
     return PBDRV_CHARGER_STATUS_FAULT;
 }
 
-static inline pbdrv_charger_usb_type_t pbdrv_charger_get_usb_type(void) {
-    return PBDRV_CHARGER_USB_TYPE_NONE;
-}
-
-static inline void pbdrv_charger_enable(bool enable) {
+static inline void pbdrv_charger_enable(bool enable, pbdrv_charger_limit_t limit) {
 }
 
 #endif

lib/pbio/include/pbdrv/usb.h
@@ -1,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2022 The Pybricks Authors
+
+/**
+ * @addtogroup UsbDriver Driver: USB
+ * @{
+ */
+
+#ifndef _PBDRV_USB_H_
+#define _PBDRV_USB_H_
+
+#include <pbdrv/config.h>
+
+/**
+ * Indicates battery charging capabilites that were detected on a USB port.
+ */
+typedef enum {
+    /** The USB cable is not connected (no VBUS). */
+    PBDRV_USB_BCD_NONE,
+    /** The USB cable is connected to a non-standard charger or PS/2 port. */
+    PBDRV_USB_BCD_NONSTANDARD,
+    /** The USB cable is connected to standard downstream port. */
+    PBDRV_USB_BCD_STANDARD_DOWNSTREAM,
+    /** The USB cable is connected to charging downstream port. */
+    PBDRV_USB_BCD_CHARGING_DOWNSTREAM,
+    /** The USB cable is connected to dedicated charging port. */
+    PBDRV_USB_BCD_DEDICATED_CHARGING,
+} pbdrv_usb_bcd_t;
+
+#if PBDRV_CONFIG_USB
+
+/**
+ * Gets the result of the USB battery charger detection.
+ * @return              The result.
+ */
+pbdrv_usb_bcd_t pbdrv_usb_get_bcd(void);
+
+#else // PBDRV_CONFIG_USB
+
+static inline pbdrv_usb_bcd_t pbdrv_usb_get_bcd(void) {
+    return PBDRV_USB_BCD_NONE;
+}
+
+#endif // PBDRV_CONFIG_USB
+
+#endif // _PBDRV_USB_H_
+
+/** @} */

lib/pbio/sys/battery.c
@@ -10,6 +10,7 @@
 #include <pbdrv/charger.h>
 #include <pbdrv/config.h>
 #include <pbdrv/clock.h>
+#include <pbdrv/usb.h>
 #include <pbsys/status.h>
 
 // period over which the battery voltage is averaged (in milliseconds)
@@ -100,6 +101,27 @@ void pbsys_battery_poll(void) {
 
     // REVISIT: we should be able to make this event driven rather than polled
     #if PBDRV_CONFIG_CHARGER
-    pbdrv_charger_enable(pbdrv_charger_get_usb_type() != PBDRV_CHARGER_USB_TYPE_NONE);
-    #endif
+
+    pbdrv_usb_bcd_t bcd = pbdrv_usb_get_bcd();
+    bool enable = bcd != PBDRV_USB_BCD_NONE;
+    pbdrv_charger_limit_t limit;
+
+    // REVISIT: The only current battery charger chip will automatically monitor
+    // VBUS and limit the current if the VBUS voltage starts to drop, so these
+    // limits are a bit looser than they could be.
+    switch (bcd) {
+        case PBDRV_USB_BCD_NONE:
+            limit = PBDRV_CHARGER_LIMIT_NONE;
+            break;
+        case PBDRV_USB_BCD_STANDARD_DOWNSTREAM:
+            limit = PBDRV_CHARGER_LIMIT_STD_MAX;
+            break;
+        default:
+            limit = PBDRV_CHARGER_LIMIT_CHARGING;
+            break;
+    }
+
+    pbdrv_charger_enable(enable, limit);
+
+    #endif // PBDRV_CONFIG_CHARGER
 }

pybricks/common/pb_type_charger.c
@@ -6,6 +6,7 @@
 #if PYBRICKS_PY_COMMON_CHARGER
 
 #include <pbdrv/charger.h>
+#include <pbdrv/usb.h>
 
 #include "py/obj.h"
 #include "py/runtime.h"
@@ -45,16 +46,16 @@ STATIC mp_obj_t Charger_status(mp_obj_t self_in) {
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(Charger_status_obj, Charger_status);
 
 STATIC mp_obj_t Charger_usb(mp_obj_t self_in) {
-    switch (pbdrv_charger_get_usb_type()) {
-        case PBDRV_CHARGER_USB_TYPE_NONE:
+    switch (pbdrv_usb_get_bcd()) {
+        case PBDRV_USB_BCD_NONE:
             return mp_const_none;
-        case PBDRV_CHARGER_USB_TYPE_NONSTANDARD:
+        case PBDRV_USB_BCD_NONSTANDARD:
             return MP_OBJ_NEW_QSTR(MP_QSTR_nonstandard);
-        case PBDRV_CHARGER_USB_TYPE_STANDARD_DOWNSTREAM:
+        case PBDRV_USB_BCD_STANDARD_DOWNSTREAM:
             return MP_OBJ_NEW_QSTR(MP_QSTR_standard);
-        case PBDRV_CHARGER_USB_TYPE_CHARGING_DOWNSTREAM:
+        case PBDRV_USB_BCD_CHARGING_DOWNSTREAM:
             return MP_OBJ_NEW_QSTR(MP_QSTR_charging);
-        case PBDRV_CHARGER_USB_TYPE_DEDICATED_CHARGING:
+        case PBDRV_USB_BCD_DEDICATED_CHARGING:
             return MP_OBJ_NEW_QSTR(MP_QSTR_dedicated);
         default:
             mp_raise_NotImplementedError(MP_ERROR_TEXT("unknown"));