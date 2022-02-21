046b6b5e055050b036afcb1c2e3d5d6962d66c3d
David Lechner <david@pybricks.com>
2022-02-21 12:49:24 -0600

    test/drv/battery: implement pbdrv_battery_get_type()
    This fixes a linker error about a missing symbol when compiling pbio
    tests. For some reason, this error is only showing up on Cygwin/GCC 11.

--- lib/pbio/test/drv/battery.c
+++ lib/pbio/test/drv/battery.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2020 The Pybricks Authors
+// Copyright (c) 2020-2022 The Pybricks Authors
 
 // Software battery implementation for simulating battery in tests
 
@@ -11,5 +11,10 @@ void pbdrv_battery_init(void) {
 
 pbio_error_t pbdrv_battery_get_voltage_now(uint16_t *value) {
     *value = 7200;
+    return PBIO_SUCCESS;
+}
+
+pbio_error_t pbdrv_battery_get_type(pbdrv_battery_type_t *value) {
+    *value = PBDRV_BATTERY_TYPE_LIION;
     return PBIO_SUCCESS;
 }
