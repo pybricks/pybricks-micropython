882bbf94593feaf5da553e8637600c9f8b4032c3
David Lechner <david@pybricks.com>
2022-04-15 15:31:53 -0500

    pbio_virtual.drv.clock: new CountingClock class
    This adds a new CountingClock class that allows running the virtual
    clock as fast as possible. This is useful for testing/simulation where
    we don't want to wait in real time for tests that depend on the passage
    of time.
    
    The class also contains several features to help prevent accidentally
    developing code that depends on a "perfect" clock.

--- lib/pbio/cpython/pbio_virtual/drv/clock.py
+++ lib/pbio/cpython/pbio_virtual/drv/clock.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2022 The Pybricks Authors
 
+import random
 import signal
 import threading
 import time
@@ -57,3 +58,63 @@ class WallClock(VirtualClock):
         while True:
             time.sleep(0.001)
             self.interrupt()
+
+
+class CountingClock(VirtualClock):
+    """
+    Clock implementation that increases the current time by *step* microseconds
+    each time :meth:`tick` is called.
+
+    To create a clock that runs as fast as possible use the class like this::
+
+        class Platform(VirtualPlatform):
+            def __init__(self):
+                super().__init__()
+                ...
+                self.clock[-1] = CountingClock()
+                self.subscribe_poll(self.clock[-1].tick)
+                ...
+    """
+
+    def __init__(
+        self, start: int = (2 ** 32 - 3) * 1000, step: int = 1000, fuzz: int = 100
+    ) -> None:
+        """
+        Args:
+            start:
+                The starting time in microseconds. The default value is chosen
+                so that the 32-bit millisecond clock and the 32-bit microsecond
+                clock will both roll over in two virtual seconds. This is done
+                to help expose code that may not be computing time differences
+                correctly.
+            step:
+                The number of microseconds to increase the clock time by on
+                each :meth:`tick`. The default value advances the virtual clock
+                by 1 millisecond on each tick.
+            fuzz:
+                The amount of random variation to apply to *step* at each tick.
+                This is done to help expose code that may not correctly handle
+                small variations in timestamps. Setting to 0 will disable
+                fuzzing.
+        """
+        super().__init__()
+        # convert microseconds to nanoseconds
+        self.nanoseconds = start * 1000
+        self._step_ns = step * 1000
+        self._fuzz_ns = fuzz * 1000
+
+    def tick(self, *args):
+        """
+        Increases the clock time by *step* +/- *fuzz* and triggers the clock interupt.
+
+        This method has unused *args so that it can be passed directly to the
+        :class:`VirtualPlatform` poll subscribe method.
+        """
+        if self._fuzz_ns:
+            self.nanoseconds += random.randint(
+                self._step_ns - self._fuzz_ns, self._step_ns + self._fuzz_ns
+            )
+        else:
+            self.nanoseconds += self._step_ns
+
+        self.interrupt()
