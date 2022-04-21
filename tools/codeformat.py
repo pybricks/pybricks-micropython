f69bd694ba481563c980e09fae41b60c0ecea4fc
Laurens Valk <laurens@pybricks.com>
2022-04-21 20:07:46 +0200

    tools/codeformat: Exclude generated test scripts.

--- tools/codeformat.py
+++ tools/codeformat.py
@@ -29,6 +29,7 @@ codeformat.EXCLUSIONS = [
     "bricks/nxt/nxt-firmware-drivers/**",
     "bricks/**/build*/**",
     "micropython/**",
+    "tests/**/build/**",
 ]
 
 codeformat.TOP = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
