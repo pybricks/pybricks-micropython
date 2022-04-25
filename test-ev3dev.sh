cc48f3d6a7f61068f8de498b1550acfb0dd540ec
David Lechner <david@pybricks.com>
2022-04-25 14:03:15 -0500

    test-ev3dev: build in separate directory
    This changes test-ev3dev.sh to build in a separate build-test directory.
    This ensures that it is build with DEBUG=1 and won't accidentally get
    written over by other local builds. This makes it easier to debug when
    tests fail.

--- test-ev3dev.sh
+++ test-ev3dev.sh
@@ -11,10 +11,10 @@ SCRIPT_DIR=$(dirname "$0")
 BRICK_DIR="$SCRIPT_DIR/bricks/ev3dev"
 MP_TEST_DIR="$SCRIPT_DIR/micropython/tests"
 PB_TEST_DIR=$(readlink -f "$SCRIPT_DIR/tests")
-BUILD_DIR=$(readlink -f "$BRICK_DIR/build")
+BUILD_DIR=$(readlink -f "$BRICK_DIR/build-test")
 
 rm -f "$BRICK_DIR/pybricks-micropython"
-make -s -j $(nproc --all) -C "$BRICK_DIR" pybricks-micropython build/libgrx-3.0-vdriver-test.so CROSS_COMPILE=
+make -s -j $(nproc --all) -C "$BRICK_DIR" pybricks-micropython build-test/libgrx-3.0-vdriver-test.so CROSS_COMPILE= DEBUG=1 BUILD=build-test
 
 export MICROPY_MICROPYTHON="$PB_TEST_DIR/ev3dev/test-wrapper.sh"
 export GRX_PLUGIN_PATH="$BUILD_DIR"
