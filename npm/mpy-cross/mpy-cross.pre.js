f5603728d3bceb2c625cb10b968087a7e7bb32f9
David Lechner <david@pybricks.com>
2022-06-16 17:28:30 -0500

    npm/mpy-cross: automatic code formatting

--- npm/mpy-cross/mpy-cross.pre.js
+++ npm/mpy-cross/mpy-cross.pre.js
@@ -1,34 +1,38 @@
-
 const args = Module['arguments'];
 // input file name is last argument
 const inputFileName = args[args.length - 1];
 const outputOptionFlagIndex = args.indexOf('-o');
 // if output option was given, use that as output file name, otherwise use
 // input file name with .mpy file extension
-const outputFileName = outputOptionFlagIndex >= 0 ?
-  args[outputOptionFlagIndex + 1] : inputFileName.replace('.py', '.mpy');
+const outputFileName =
+    outputOptionFlagIndex >= 0
+        ? args[outputOptionFlagIndex + 1]
+        : inputFileName.replace('.py', '.mpy');
 
 Module['preRun'] = () => {
-  FS.writeFile(inputFileName, Module['inputFileContents']);
+    FS.writeFile(inputFileName, Module['inputFileContents']);
 };
 
 const collectedOut = [];
 
 Module['print'] = (out) => {
-  collectedOut.push(out);
-}
+    collectedOut.push(out);
+};
 
 const collectedErr = [];
 
 Module['printErr'] = (err) => {
-  collectedErr.push(err);
-}
+    collectedErr.push(err);
+};
 
 Module['onExit'] = (status) => {
-  const mpy = status === 0 ? FS.readFile(outputFileName, { encoding: 'binary' }) : undefined;
-  Module['callback'](status, mpy, collectedOut, collectedErr);
-}
+    const mpy =
+        status === 0
+            ? FS.readFile(outputFileName, { encoding: 'binary' })
+            : undefined;
+    Module['callback'](status, mpy, collectedOut, collectedErr);
+};
 
 Module['quit'] = () => {
-  // prevent node from calling process.exit()
-}
+    // prevent node from calling process.exit()
+};
