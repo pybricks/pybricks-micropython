deead2344bc6b61dc84eb6f3eff466a9731be39f
Laurens Valk <laurens@pybricks.com>
2022-06-20 15:44:34 +0200

    drv/counter_lpf2: Refactor to prepare for unit change.

--- lib/pbio/drv/counter/counter_lpf2.c
+++ lib/pbio/drv/counter/counter_lpf2.c
@@ -24,15 +24,25 @@
 #include "counter_lpf2.h"
 
 typedef struct {
+    pbdrv_counter_dev_t *dev;
+    /**
+     * Whether the motor can measure an absolute angle.
+     */
+    bool supports_abs_angle;
+    /**
+     * Millidegrees within the circle, 0--360 000
+     */
+    int32_t millidegrees;
+    /**
+     * Whole rotation counter. At 1000 deg/s, overflows after 24 years.
+     */
     int32_t rotations;
-    int32_t last_abs_pos;
 } private_data_t;
-
-#define PBDRV_COUNTER_UNKNOWN_ABS_ANGLE (INT32_MAX)
 
 static private_data_t private_data[PBDRV_CONFIG_COUNTER_LPF2_NUM_DEV];
 
-static pbio_error_t pbdrv_counter_lpf2_get_count(pbdrv_counter_dev_t *dev, int32_t *count) {
+// Parses latest LPF2 message and stores result in private data.
+static pbio_error_t pbdrv_counter_lpf2_update(pbdrv_counter_dev_t *dev) {
 
     private_data_t *priv = dev->priv;
     const pbdrv_counter_lpf2_platform_data_t *pdata = dev->pdata;
@@ -49,64 +59,96 @@ static pbio_error_t pbdrv_counter_lpf2_get_count(pbdrv_counter_dev_t *dev, int32
         return PBIO_ERROR_NO_DEV;
     }
 
+    // Check if device can measure absolute angles.
+    priv->supports_abs_angle = PBIO_IODEV_IS_ABS_MOTOR(iodev);
+
     // Ensure that we are on expected mode.
-    uint8_t mode_id = PBIO_IODEV_IS_ABS_MOTOR(iodev) ?
+    uint8_t mode_id = priv->supports_abs_angle ?
         PBIO_IODEV_MODE_PUP_ABS_MOTOR__CALIB:
         PBIO_IODEV_MODE_PUP_REL_MOTOR__POS;
     if (iodev->mode != mode_id) {
         return PBIO_ERROR_INVALID_OP;
     }
 
-    // Get LPF2 data buffer.
+    // Get pointer to LPF2 data buffer.
     uint8_t *data;
     err = pbio_iodev_get_data(iodev, &data);
     if (err != PBIO_SUCCESS) {
         return err;
     }
 
-    // For incremental encoders, we can just return the LPF2 data directly.
-    if (!PBIO_IODEV_IS_ABS_MOTOR(iodev)) {
-        *count = pbio_get_uint32_le(data);
-        priv->last_abs_pos = PBDRV_COUNTER_UNKNOWN_ABS_ANGLE;
+    // For incremental encoders, we read data in degrees.
+    if (!priv->supports_abs_angle) {
+        // At max speed (1000 deg/s), degrees overflows after 24 days, so we
+        // don't bother with additional buffering and resetting.
+        int32_t degrees = pbio_get_uint32_le(data);
+
+        // Store as (rotations, millidegree) pair. This is slightly redundant,
+        // but this makes it easy to deal with both motor types consistently.
+        // In practice, this is only used on the BOOST interactive motor, so
+        // the overall overhead is minimal.
+        int32_t degrees_abs = degrees % 360;
+        if (degrees_abs < 0) {
+            degrees_abs += 360;
+        }
+        priv->millidegrees = degrees_abs * 1000;
+        priv->rotations = (degrees - degrees_abs) / 360;
+
         return PBIO_SUCCESS;
     }
 
     // For absolute encoders, we need to keep track of whole rotations.
-    int32_t abs_pos = (int16_t)pbio_get_uint16_le(data + 2);
+    // First, read value in tenths of degrees.
+    int32_t abs_now = (int16_t)pbio_get_uint16_le(data + 2);
+    int32_t abs_prev = priv->millidegrees / 100;
+
+    // Store measured millidegree state value.
+    priv->millidegrees = abs_now * 100;
 
     // Update rotation counter as encoder passes through 0
-    if (priv->last_abs_pos > 2700 && abs_pos < 900) {
+    if (abs_prev > 2700 && abs_now < 900) {
         priv->rotations += 1;
     }
-    if (priv->last_abs_pos < 900 && abs_pos > 2700) {
+    if (abs_prev < 900 && abs_now > 2700) {
         priv->rotations -= 1;
     }
-    priv->last_abs_pos = abs_pos;
-
-    // The total count is the position in this circle plus full rotations.
-    *count = priv->rotations * 360 + (abs_pos + 5) / 10;
     return PBIO_SUCCESS;
 }
 
-static pbio_error_t pbdrv_counter_lpf2_get_abs_count(pbdrv_counter_dev_t *dev, int32_t *count) {
-    private_data_t *priv = dev->priv;
+static pbio_error_t pbdrv_counter_lpf2_get_count(pbdrv_counter_dev_t *dev, int32_t *count) {
 
-    // Get total count value, which also updates absolute count value.
-    pbio_error_t err = pbdrv_counter_lpf2_get_count(dev, count);
+    // Read sensor to update position buffer.
+    pbio_error_t err = pbdrv_counter_lpf2_update(dev);
     if (err != PBIO_SUCCESS) {
         return err;
     }
 
-    if (priv->last_abs_pos == PBDRV_COUNTER_UNKNOWN_ABS_ANGLE) {
+    // Return total angle.
+    private_data_t *priv = dev->priv;
+    *count = priv->rotations * 360 + (priv->millidegrees + 500) / 1000;
+    return PBIO_SUCCESS;
+}
+
+static pbio_error_t pbdrv_counter_lpf2_get_abs_count(pbdrv_counter_dev_t *dev, int32_t *count) {
+
+    private_data_t *priv = dev->priv;
+
+    // Read sensor to update position buffer.
+    pbio_error_t err = pbdrv_counter_lpf2_update(dev);
+    if (err != PBIO_SUCCESS) {
+        return err;
+    }
+
+    // Return error if absolute angle not available.
+    if (!priv->supports_abs_angle) {
         return PBIO_ERROR_NOT_SUPPORTED;
     }
 
-    // Convert last absolute angle to signed angle in (-180, 179).
-    *count = (priv->last_abs_pos + 5) / 10;
+    // Convert absolute angle to signed angle in (-180, 179).
+    *count = (priv->millidegrees + 500) / 1000;
     if (*count >= 180) {
         *count -= 360;
     }
-
     return PBIO_SUCCESS;
 }
 
