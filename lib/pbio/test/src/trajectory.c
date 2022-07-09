9e77e27f7de7c04e94740fbcbccd34d531b08e74
Laurens Valk <laurens@pybricks.com>
2022-07-09 10:29:04 +0200

    pbio/test/trajectory: Avoid deeply nested loops.
    Instead of nesting loops to get all permutations, get all parameters
    from a single permutation index. This lets us use the same permutations
    for other tests.

--- lib/pbio/test/src/trajectory.c
+++ lib/pbio/test/src/trajectory.c
@@ -2,6 +2,7 @@
 // Copyright (c) 2022 The Pybricks Authors
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <pbio/trajectory.h>
 #include <pbio/util.h>
@@ -15,16 +16,20 @@
 #define TICKS_PER_MS (10)
 #define DDEGS_PER_DEGS (10)
 
-static pbio_angle_t origin = {
-    .rotations = 0,
-    .millidegrees = 0,
-};
-
+/**
+ * This tests one trajectory with simple numbers. If there is a regression,
+ * this one is easy to debug.
+ */
 static void test_simple_trajectory(void *env) {
 
     // Command: Run for 10000 degrees at 1000 deg/s with a = 2000 deg/s/s.
     // Ramping up and down takes 500 ms this way, during which we travel 250
     // degrees, so overal expected duration is 10500 ms.
+
+    static pbio_angle_t start = {
+        .rotations = 0,
+        .millidegrees = 0,
+    };
 
     pbio_angle_t end = {
         .rotations = 27,
@@ -33,7 +38,7 @@ static void test_simple_trajectory(void *env) {
 
     pbio_trajectory_command_t command = {
         .time_start = 0,
-        .position_start = origin,
+        .position_start = start,
         .position_end = end,
         .speed_start = 0,
         .speed_target = 1000 * MDEG_PER_DEG,
@@ -60,72 +65,96 @@ static void test_simple_trajectory(void *env) {
     tt_want_int_op(trj.a2, ==, -command.deceleration / MDEG_PER_DEG);
 }
 
+// Start and end angles in millidegrees.
+static const pbio_angle_t angles[] = {
+    {.rotations = 0,             .millidegrees = 0 },
+    {.rotations = -123,          .millidegrees = 456 },
+    {.rotations = 0,             .millidegrees = INT32_MAX / 4 },
+    {.rotations = INT32_MAX / 4, .millidegrees = 0 },
+};
+
+// Acceleration and deceleration test range in deg/s^2.
+static const int32_t accelerations[] = {
+    10, 50, 100, 500, 1000, 2000, 5000, 10000, 20000, 40000,
+};
+
+// Start and target speed range deg/s.
+static const int32_t speeds[] = {
+    -2000, -500, 0, 1, 10, 500, 1000, 2000,
+};
+
+// Start time in clock ticks.
+static const int32_t start_times[] = {
+    INT32_MIN, INT32_MIN / 2, -1, 0, 1, INT32_MAX / 2, INT32_MAX
+};
+
+// Number of permutations for infinite trajectories.
+static const uint32_t num_infinite_trajectories =
+    PBIO_ARRAY_SIZE(angles) * // For starting angle. End angle unused.
+    PBIO_ARRAY_SIZE(accelerations) * // For acceleration. Deceleration unused.
+    PBIO_ARRAY_SIZE(speeds) * // For start speed.
+    PBIO_ARRAY_SIZE(speeds) * // For target speed.
+    PBIO_ARRAY_SIZE(start_times);
+
+/**
+ * Get one command for testing an infinite trajectory.
+ */
+static void get_infinite_command(uint32_t index, pbio_trajectory_command_t *c) {
+
+    c->duration = DURATION_FOREVER_TICKS;
+    c->speed_max = 1000 * DDEGS_PER_DEGS;
+    c->continue_running = true;
+
+    c->position_start = angles[index % PBIO_ARRAY_SIZE(angles)];
+    index /= PBIO_ARRAY_SIZE(angles);
+
+    c->time_start = start_times[index % PBIO_ARRAY_SIZE(start_times)];
+    index /= PBIO_ARRAY_SIZE(start_times);
+
+    c->acceleration = accelerations[index % PBIO_ARRAY_SIZE(accelerations)] * MDEG_PER_DEG;
+    c->deceleration = c->acceleration;
+    index /= PBIO_ARRAY_SIZE(accelerations);
+
+    c->speed_start = speeds[index % PBIO_ARRAY_SIZE(speeds)] * DDEGS_PER_DEGS;
+    index /= PBIO_ARRAY_SIZE(speeds);
+
+    c->speed_target = speeds[index % PBIO_ARRAY_SIZE(speeds)] * DDEGS_PER_DEGS;
+    index /= PBIO_ARRAY_SIZE(speeds);
+}
+
 static void test_infinite_trajectory(void *env) {
 
-    int32_t accelerations[] = {
-        10, 50, 100, 500, 1000, 2000, 5000, 10000, 20000, 40000
-    };
+    pbio_trajectory_command_t command;
 
-    int32_t speeds[] = {
-        -2000, -1000, -500, -10, 0, 10, 500, 1000, 2000,
-    };
+    for (uint32_t i = 0; i < num_infinite_trajectories; i++) {
+        get_infinite_command(i, &command);
 
-    int32_t times[] = {
-        INT32_MIN, INT32_MIN / 2, -1, 0, 1, INT32_MAX / 2, INT32_MAX
-    };
+        // Calculate the trajectory.
+        pbio_trajectory_t trj;
+        pbio_error_t err = pbio_trajectory_new_time_command(&trj, &command);
+        tt_want_int_op(err, ==, PBIO_SUCCESS);
 
-    // Go through range of accelerations.
-    for (int a = 0; a < PBIO_ARRAY_SIZE(accelerations); a++) {
-        // Go through range of target speeds.
-        for (int wt = 0; wt < PBIO_ARRAY_SIZE(speeds); wt++) {
-            // Go through range of initial speeds.
-            for (int w0 = 0; w0 < PBIO_ARRAY_SIZE(speeds); w0++) {
-                // Go through range of initial times.
-                for (int t = 0; t < PBIO_ARRAY_SIZE(times); t++) {
-                    // Define the command for this permutation of parameters.
-                    pbio_trajectory_command_t command = {
-                        .time_start = times[t],
-                        .duration = DURATION_FOREVER_TICKS,
-                        .position_start = origin,
-                        .position_end = origin,
-                        .speed_start = speeds[w0] * DDEGS_PER_DEGS,
-                        .speed_target = speeds[wt] * DDEGS_PER_DEGS,
-                        .speed_max = 1000 * DDEGS_PER_DEGS,
-                        .acceleration = accelerations[a] * MDEG_PER_DEG,
-                        .deceleration = accelerations[a] * MDEG_PER_DEG,
-                        .continue_running = true,
-                    };
+        // Verify that we maintain a constant speed when done.
+        tt_want_int_op(trj.w1, ==, trj.w3);
 
-                    // Calculate the trajectory.
-                    pbio_trajectory_t trj;
-                    pbio_error_t err = pbio_trajectory_new_time_command(&trj, &command);
-                    tt_want_int_op(err, ==, PBIO_SUCCESS);
+        // Initial speed may now be bounded. Verify that the sign
+        // is the same and that it did not grow in size.
+        if (command.speed_start > 0) {
+            tt_want_int_op(trj.w0 / DDEGS_PER_DEGS, <=, command.speed_start / MDEG_PER_DEG);
+        } else if (command.speed_start < 0) {
+            tt_want_int_op(trj.w0 / DDEGS_PER_DEGS, >=, command.speed_start / MDEG_PER_DEG);
+        } else {
+            tt_want_int_op(trj.w0 / DDEGS_PER_DEGS, ==, command.speed_start / MDEG_PER_DEG);
+        }
 
-                    // Verify that we maintain a constant speed when done.
-                    tt_want_int_op(trj.w1, ==, trj.w3);
-
-                    // Initial speed may now be bounded. Verify that the sign
-                    // is the same and that it did not grow in size.
-                    if (command.speed_start > 0) {
-                        tt_want_int_op(trj.w0 / DDEGS_PER_DEGS, <=, command.speed_start / MDEG_PER_DEG);
-                    }
-                    if (command.speed_start < 0) {
-                        tt_want_int_op(trj.w0 / DDEGS_PER_DEGS, >=, command.speed_start / MDEG_PER_DEG);
-                    } else {
-                        tt_want_int_op(trj.w0 / DDEGS_PER_DEGS, ==, command.speed_start / MDEG_PER_DEG);
-                    }
-
-                    // Verify that the target speed is reached, which should
-                    // always be the case in an infinite maneuver.
-                    if (command.speed_target > command.speed_max) {
-                        tt_want_int_op(trj.w1 / DDEGS_PER_DEGS, ==, command.speed_max / MDEG_PER_DEG);
-                    } else if (command.speed_target < -command.speed_max) {
-                        tt_want_int_op(trj.w1 / DDEGS_PER_DEGS, ==, -command.speed_max / MDEG_PER_DEG);
-                    } else {
-                        tt_want_int_op(trj.w1 / DDEGS_PER_DEGS, ==, command.speed_target / MDEG_PER_DEG);
-                    }
-                }
-            }
+        // Verify that the target speed is reached, which should
+        // always be the case in an infinite maneuver.
+        if (command.speed_target > command.speed_max) {
+            tt_want_int_op(trj.w1 / DDEGS_PER_DEGS, ==, command.speed_max / MDEG_PER_DEG);
+        } else if (command.speed_target < -command.speed_max) {
+            tt_want_int_op(trj.w1 / DDEGS_PER_DEGS, ==, -command.speed_max / MDEG_PER_DEG);
+        } else {
+            tt_want_int_op(trj.w1 / DDEGS_PER_DEGS, ==, command.speed_target / MDEG_PER_DEG);
         }
     }
 }
