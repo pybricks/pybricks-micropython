42a14e78e7aee52eb4b4ccdccbb687b09c6fa0ba
Laurens Valk <laurens@pybricks.com>
2022-08-08 20:44:46 +0200

    pbsys/main: Drop shutdown longjmp.
    Since the main application is cancellable from pbsys and the download
    process was recently moved to pbsys, we don't need this anymore.
    
    This simplifies the main code and it saves a consirable amount of space.

--- lib/pbio/include/pbio/main.h
+++ lib/pbio/include/pbio/main.h
@@ -6,14 +6,8 @@
 
 #include "pbio/config.h"
 
-/**
- * Event loop hook function.
- */
-typedef void (*pbio_event_hook_t)(void);
-
 void pbio_init(void);
 void pbio_stop_all(void);
 int pbio_do_one_event(void);
-void pbio_set_event_hook(pbio_event_hook_t hook);
 
 #endif // _PBIO_MAIN_H_
--- lib/pbio/sys/main.c
+++ lib/pbio/sys/main.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2021 The Pybricks Authors
+// Copyright (c) 2022 The Pybricks Authors
 
 #include <stdint.h>
 
@@ -13,49 +13,26 @@
 #include <pbsys/user_program.h>
 #include "user_program.h"
 
-static void *pbsys_main_jmp_buf[5];
-
-static void pb_sys_main_check_for_shutdown(void) {
-    if (pbsys_status_test(PBIO_PYBRICKS_STATUS_SHUTDOWN)) {
-        pbio_set_event_hook(NULL);
-        __builtin_longjmp(pbsys_main_jmp_buf, 1);
-    }
-}
-
 /**
- * Initializes the PBIO library and runs custom main program.
- *
- * The main program may be abruptly ended when shutting down the hub.
+ * Initializes the PBIO library, runs custom main program, and handles shutdown.
  *
  * @param [in]  main    The main program.
  */
 int main(int argc, char **argv) {
     pbio_init();
 
-    // REVISIT: __builtin_setjmp() only saves a couple registers, so using it
-    // could cause problems if we add more to this function. However, since we
-    // use -nostdlib compile flag, we don't have setjmp(). We should be safe
-    // for now though since we don't use any local variables after the longjmp.
-    if (__builtin_setjmp(pbsys_main_jmp_buf) == 0) {
-        // REVISIT: we could save a few CPU cycles on each call to pbio_do_one_event()
-        // if we don't set this until shutdown is actually requested
-        pbio_set_event_hook(pb_sys_main_check_for_shutdown);
-        for (;;) {
-            // Load program info and start process to wait for program.
-            pbsys_user_program_info_t *info;
-            pbsys_user_program_process_start(&info);
+    while (!pbsys_status_test(PBIO_PYBRICKS_STATUS_SHUTDOWN)) {
+        // Load program info and start process to wait for program.
+        pbsys_user_program_info_t *info;
+        pbsys_user_program_process_start(&info);
 
-            // Wait for program process to complete.
-            while (!pbsys_user_program_process_complete()) {
-                pbio_do_one_event();
-            }
-
-            // Run the main application.
-            pbsys_user_program_application_main(info);
+        // Wait for program receive process to complete.
+        while (!pbsys_user_program_process_complete()) {
+            pbio_do_one_event();
         }
-    } else {
-        // in case we jumped out of the middle of a user program
-        pbsys_user_program_unprepare();
+
+        // Run the main application.
+        pbsys_user_program_application_main(info);
     }
 
     // The power could be held on due to someone pressing the center button
