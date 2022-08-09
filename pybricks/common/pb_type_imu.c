f9e42c1b74ff2991b3d9502a6914cd3fc0c2a618
David Lechner <david@pybricks.com>
2022-08-09 12:04:37 -0500

    pybricks.common.IMU: fix error handling during init
    This fixes the output arg of pbdrv_imu_get_imu() not being discarded
    when it returns an error.

--- pybricks/common/pb_type_imu.c
+++ pybricks/common/pb_type_imu.c
@@ -239,8 +239,10 @@ mp_obj_t pb_type_IMU_obj_new(mp_obj_t top_side_axis, mp_obj_t front_side_axis) {
 
     // Initialized if not done so already
     if (!self->imu_dev) {
-        pb_assert(pbdrv_imu_get_imu(&self->imu_dev));
+        pbdrv_imu_dev_t *imu_dev;
+        pb_assert(pbdrv_imu_get_imu(&imu_dev));
         self->base.type = &pb_type_IMU;
+        self->imu_dev = imu_dev;
     }
 
     // Check if we use the default orientation.
