926d1bde757e3c11ee57b0d76fbf7217c0dcb152
Laurens Valk <laurens@pybricks.com>
2022-08-26 11:01:44 +0200

    lib: Drop littlefs.
    This has been replaced by a generic raw block device that can be read asynchronously on all hub types.
    
    We may still use littlefs in the future for user file access, but this should use the implementation and littlefs library provided by MicroPython.

--- bricks/stm32/stm32.mk
+++ bricks/stm32/stm32.mk
@@ -39,15 +39,6 @@ $(error failed)
 endif
 endif
 endif
-ifeq ($(PB_LIB_LITTLEFS),1)
-ifeq ("$(wildcard ../../lib/littlefs/README.md)","")
-$(info GIT cloning littlefs submodule)
-$(info $(shell cd ../.. && git submodule update --checkout --init lib/littlefs))
-ifeq ("$(wildcard ../../lib/littlefs/README.md)","")
-$(error failed)
-endif
-endif
-endif
 
 # lets micropython make files work with external files
 USER_C_MODULES = $(PBTOP)
@@ -102,9 +93,6 @@ endif
 ifeq ($(PB_LIB_BTSTACK),1)
 INC += -I$(PBTOP)/lib/btstack/chipset/cc256x
 INC += -I$(PBTOP)/lib/btstack/src
-endif
-ifeq ($(PB_LIB_LITTLEFS),1)
-INC += -I$(PBTOP)/lib/littlefs
 endif
 ifeq ($(PB_USE_LSM6DS3TR_C),1)
 INC += -I$(PBTOP)/lib/lsm6ds3tr_c_STdC/driver
@@ -329,13 +317,6 @@ BTSTACK_SRC_C += $(addprefix lib/btstack/src/ble/,\
 
 BTSTACK_SRC_C += $(addprefix lib/btstack/chipset/cc256x/,\
 	btstack_chipset_cc256x.c \
-	)
-
-# Littlefs
-
-LITTLEFS_SRC_C = $(addprefix lib/littlefs/,\
-	lfs_util.c \
-	lfs.c \
 	)
 
 # Contiki
@@ -567,10 +548,6 @@ OBJ += $(addprefix $(BUILD)/, $(BLE5STACK_SRC_C:.c=.o))
 endif
 ifeq ($(PB_LIB_BTSTACK),1)
 OBJ += $(addprefix $(BUILD)/, $(BTSTACK_SRC_C:.c=.o))
-endif
-ifeq ($(PB_LIB_LITTLEFS),1)
-CFLAGS+= -DLFS_NO_ASSERT -DLFS_NO_MALLOC -DLFS_NO_DEBUG -DLFS_NO_WARN -DLFS_NO_ERROR
-OBJ += $(addprefix $(BUILD)/, $(LITTLEFS_SRC_C:.c=.o))
 endif
 ifeq ($(PB_USE_HAL),1)
 OBJ += $(addprefix $(BUILD)/, $(HAL_SRC_C:.c=.o))
