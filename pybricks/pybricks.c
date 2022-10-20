pybricks/pybricks-micropython

commit cd16b4c2df0810164254fd52133c6c30e21993da

pybricks: Use standard way to register modules.
This uses the MP_REGISTER_MODULE macro to register the modules. To make
them part of the pybricks "package", the module names are prefixed with
pybricks_dot_, and the real names with dots are added to the qstrdefs.

This method uses standard MicroPython tools, but it is a bit more
fragile since it relies implicitly on the order of the py/builtinimport.
It works on embedded ports if external imports are disabled. This is
no loss of generality since there aren't any modules or files.

On ev3dev, this almost works as well. Because the builtin modules
take precedence, we have to take care to give them a different name
if there is a Python file with the same name.

Another advantage is that the __init__ method now gets called properly
if it exists, which lets us remove an old workaround on ev3dev.

pybricks/pybricks.c
@@ -51,39 +51,6 @@ STATIC const mp_rom_map_elem_t pybricks_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__),            MP_ROM_QSTR(MP_QSTR_pybricks) },
     { MP_ROM_QSTR(MP_QSTR___init__),            MP_ROM_PTR(&pb_package_pybricks_init_obj)},
     { MP_ROM_QSTR(MP_QSTR_version),             MP_ROM_PTR(&pybricks_info_obj)},
-    #if PYBRICKS_PY_GEOMETRY
-    { MP_ROM_QSTR(MP_QSTR_geometry),            MP_ROM_PTR(&pb_module_geometry)},
-    #endif
-    #if PYBRICKS_PY_EV3DEVICES
-    { MP_ROM_QSTR(MP_QSTR_ev3devices),          MP_ROM_PTR(&pb_module_ev3devices)},
-    #endif
-    #if PYBRICKS_PY_EXPERIMENTAL
-    { MP_ROM_QSTR(MP_QSTR_experimental),        MP_ROM_PTR(&pb_module_experimental)},
-    #endif
-    #if PYBRICKS_PY_HUBS
-    { MP_ROM_QSTR(MP_QSTR_hubs),                MP_ROM_PTR(&pb_module_hubs)        },
-    #endif
-    #if PYBRICKS_PY_IODEVICES
-    { MP_ROM_QSTR(MP_QSTR_iodevices),           MP_ROM_PTR(&pb_module_iodevices)},
-    #endif
-    #if PYBRICKS_PY_MEDIA
-    { MP_ROM_QSTR(MP_QSTR_media),               MP_ROM_PTR(&pb_module_media)    },
-    #endif
-    #if PYBRICKS_PY_NXTDEVICES
-    { MP_ROM_QSTR(MP_QSTR_nxtdevices),          MP_ROM_PTR(&pb_module_nxtdevices)},
-    #endif
-    #if PYBRICKS_PY_PARAMETERS
-    { MP_ROM_QSTR(MP_QSTR_parameters),          MP_ROM_PTR(&pb_module_parameters)  },
-    #endif
-    #if PYBRICKS_PY_PUPDEVICES
-    { MP_ROM_QSTR(MP_QSTR_pupdevices),          MP_ROM_PTR(&pb_module_pupdevices)},
-    #endif
-    #if PYBRICKS_PY_TOOLS
-    { MP_ROM_QSTR(MP_QSTR_tools),               MP_ROM_PTR(&pb_module_tools)  },
-    #endif
-    #if PYBRICKS_PY_ROBOTICS
-    { MP_ROM_QSTR(MP_QSTR_robotics),            MP_ROM_PTR(&pb_module_robotics)},
-    #endif
 };
 STATIC MP_DEFINE_CONST_DICT(pb_package_pybricks_globals, pybricks_globals_table);
 

bricks/ev3dev/modules/pybricks/ev3devices.py
@@ -1,14 +1,0 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2018-2020 The Pybricks Authors
-
-"""Classes for LEGO MINDSTORMS EV3 Devices."""
-
-# import those ev3devices that are already written in MicroPython-style C code.
-from pybricks_c.ev3devices import (
-    InfraredSensor,
-    ColorSensor,
-    TouchSensor,
-    UltrasonicSensor,
-    GyroSensor,
-    Motor,
-)

bricks/ev3dev/modules/pybricks/experimental.py
@@ -1,14 +1,9 @@
 """The experimental module contains unstable APIs for development and testing.
 """
 
-from pybricks_c.experimental import pthread_raise
+from _experimental import pthread_raise
 from _thread import start_new_thread, get_ident, allocate_lock
 from usignal import pthread_kill, SIGUSR2
-
-# FIXME: Make generic fix for running init of module from builtin package
-from pybricks_c.experimental import __init__ as experimental_init
-
-experimental_init()
 
 
 def run_parallel(*args):

bricks/ev3dev/modules/pybricks/geometry.py
@@ -1,1 +1,0 @@
-from pybricks_c.geometry import Matrix, vector

bricks/ev3dev/modules/pybricks/hubs.py
@@ -1,4 +1,0 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2018-2020 The Pybricks Authors
-
-from pybricks_c.hubs import EV3Brick

bricks/ev3dev/modules/pybricks/iodevices.py
@@ -1,6 +1,0 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2018-2020 The Pybricks Authors
-
-"""Classes for generic I/O devices."""
-
-from pybricks_c.iodevices import *

bricks/ev3dev/modules/pybricks/nxtdevices.py
@@ -3,8 +3,7 @@
 
 """Classes for LEGO MINDSTORMS NXT Devices."""
 
-
-from pybricks_c.nxtdevices import *
+from _nxtdevices import *
 from pybricks.iodevices import AnalogSensor
 
 

bricks/ev3dev/modules/pybricks/parameters.py
@@ -1,6 +1,0 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2018-2020 The Pybricks Authors
-
-"""Enum classes of parameters, used by modules in the Pybricks package."""
-
-from pybricks_c.parameters import Direction, Stop, Color, Button, Port

commit 4e1eac238f45769a74fe79b3c00e8ac74a9fe83d

pybricks/pybricks: Fix auto-import in REPL.
Fixes https://github.com/pybricks/support/issues/741

This was broken by https://github.com/pybricks/pybricks-micropython/commit/cd16b4c2df0810164254fd52133c6c30e21993da

pybricks/pybricks.c
@@ -62,18 +62,17 @@ MP_REGISTER_MODULE(MP_QSTR_pybricks, pb_package_pybricks);
 #endif
 
 /**
- * Import all pybricks.* modules.
+ * import * from all builtin Pybricks and MicroPython modules.
  */
 static void pb_package_import_all(void) {
 
-    // Go through each module in the package.
-    for (size_t i = 0; i < MP_ARRAY_SIZE(pybricks_globals_table); i++) {
-        mp_rom_obj_t module = pybricks_globals_table[i].value;
-        if (mp_obj_is_type(module, &mp_type_module)) {
-            // Import everything from the module.
-            mp_import_all((mp_obj_t)module);
-        }
+    // Go through all modules in mp_builtin_module_map.
+    for (size_t i = 0; i < mp_builtin_module_map.used; i++) {
+        // This is a constant map of modules, so we can skip checks for
+        // filled slots and module types.
+        mp_import_all(mp_builtin_module_map.table[i].value);
     }
+
     #if PYBRICKS_PY_HUBS
     // Initialize hub instance
     const mp_obj_t args;