pybricks/pybricks-micropython

commit fa37d043d7003bdb508f78bd88bf3baa2a954598

pybricks.common.Control.limits: Set deceleration.
This makes the previously added pbio setting configurable
from the Pybricks API. For backwards compatibility, distinct
acceleration and deceleration values must be given as tuple.

pybricks/common/pb_type_control.c
@@ -63,17 +63,43 @@ STATIC mp_obj_t common_Control_limits(size_t n_args, const mp_obj_t *pos_args, m
     // If all given values are none, return current values
     if (speed_in == mp_const_none && acceleration_in == mp_const_none && torque_in == mp_const_none) {
         mp_obj_t ret[3];
+        mp_obj_t accel[2];
         ret[0] = mp_obj_new_int(speed);
-        ret[1] = mp_obj_new_int(acceleration);
         ret[2] = mp_obj_new_int(torque);
+        // For backwards compatibility, return acceleration and deceleration
+        // as a single integer if they are equal. Otherwise, return as tuple.
+        if (acceleration == deceleration) {
+            ret[1] = mp_obj_new_int(acceleration);
+        } else {
+            accel[0] = mp_obj_new_int(acceleration);
+            accel[1] = mp_obj_new_int(deceleration);
+            ret[1] = mp_obj_new_tuple(2, accel);
+        }
         return mp_obj_new_tuple(3, ret);
     }
 
     // Set user settings
     speed = pb_obj_get_default_abs_int(speed_in, speed);
-    acceleration = pb_obj_get_default_abs_int(acceleration_in, acceleration);
     torque = pb_obj_get_default_abs_int(torque_in, torque);
 
+    // If single value is given for acceleration, use it for deceleration too.
+    if (mp_obj_is_int(acceleration_in) || mp_obj_is_float(acceleration_in)) {
+        acceleration = pb_obj_get_int(acceleration_in);
+        deceleration = acceleration;
+    }
+    // Otherwise, unpack acceleration and deceleration from tuple.
+    else {
+        mp_obj_t *values;
+        size_t n;
+        mp_obj_get_array(acceleration_in, &n, &values);
+        if (n != 2) {
+            pb_assert(PBIO_ERROR_INVALID_ARG);
+        }
+        acceleration = pb_obj_get_int(values[0]);
+        deceleration = pb_obj_get_int(values[1]);
+    }
+
+    // Set new values.
     pb_assert(pbio_control_settings_set_limits(&self->control->settings, speed, acceleration, deceleration, torque));
 
     return mp_const_none;

commit e53516c599b024642b38d1543c4ff155318edf4b

pybricks.common.Control.limits: Fix setter if acceleration=None.
This was broken in https://github.com/pybricks/pybricks-micropython/commit/fa37d043d7003bdb508f78bd88bf3baa2a954598

Fixes https://github.com/pybricks/support/issues/826

pybricks/common/pb_type_control.c
@@ -52,7 +52,7 @@ STATIC mp_obj_t common_Control_limits(size_t n_args, const mp_obj_t *pos_args, m
         PB_ARG_DEFAULT_NONE(acceleration),
         PB_ARG_DEFAULT_NONE(torque));
 
-    // Read current values. Deceleration is read but cannot be updated.
+    // Read current values.
     int32_t speed, acceleration, deceleration, torque;
     pbio_control_settings_get_limits(&self->control->settings, &speed, &acceleration, &deceleration, &torque);
 
@@ -74,7 +74,7 @@ STATIC mp_obj_t common_Control_limits(size_t n_args, const mp_obj_t *pos_args, m
         return mp_obj_new_tuple(3, ret);
     }
 
-    // Set user settings
+    // Set user settings if given, else keep using current value.
     speed = pb_obj_get_default_abs_int(speed_in, speed);
     torque = pb_obj_get_default_abs_int(torque_in, torque);
 
@@ -83,8 +83,8 @@ STATIC mp_obj_t common_Control_limits(size_t n_args, const mp_obj_t *pos_args, m
         acceleration = pb_obj_get_int(acceleration_in);
         deceleration = acceleration;
     }
-    // Otherwise, unpack acceleration and deceleration from tuple.
-    else {
+    // If something else is given, unpack acceleration and deceleration from tuple.
+    else if (acceleration_in != mp_const_none) {
         mp_obj_t *values;
         size_t n;
         mp_obj_get_array(acceleration_in, &n, &values);