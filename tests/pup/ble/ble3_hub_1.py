2ca7dca1c526b07e4e72bf2713bdf2404da6cc25
NStrijbosch <n.strijbosch@gmail.com>
2022-12-30 16:54:12 +0100

    tests/pup/ble: bidirectional communication tests
    This test uses the LED to indicate data dropouts.

---
+++ tests/pup/ble/ble3_hub_1.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2022 The Pybricks Authors
+
+"""
+Hardware Module: Any hub (ble3_hub_1.py) and any other hub (ble3_hub_2.py)
+
+Description: Tests broadcast bidirectional communication.
+In particular the LED on the hub will indicate if there are data dropouts
+"""
+
+from pybricks.hubs import ThisHub
+from pybricks.parameters import Color
+from pybricks.tools import wait
+from pybricks.experimental import Broadcast
+
+# Initialize this hub.
+hub = ThisHub()
+
+# Initialize broadcast with two topics.
+radio = Broadcast(topics=["1to2", "2to1"])
+
+index = 0
+last_data = 0
+count_no_data = 0
+
+while True:
+    data = radio.receive("2to1")
+    radio.send("1to2", index)
+
+    if data:
+        if last_data == data:
+            count_no_data += 1
+        else:
+            count_no_data = 0
+            last_data = data
+
+        hub.light.on(Color((count_no_data * 10) % 360))
+    else:
+        hub.light.on(Color.WHITE)
+
+    index += 1
+    wait(100)
