db707ed380c89383814acd8f7f739c1f914b48af
Laurens Valk <laurens@pybricks.com>
2023-01-13 12:23:10 +0100

    pybricks.tools: Make wait reusable.
    This can be used for any time-like awaitable.

--- pybricks/tools.h
+++ pybricks/tools.h
@@ -12,6 +12,10 @@
 
 bool pb_module_tools_run_loop_is_active();
 
+void pb_type_tools_wait_reset(void);
+
+mp_obj_t pb_type_tools_wait_new(mp_int_t duration);
+
 extern const mp_obj_type_t pb_type_StopWatch;
 
 #endif // PYBRICKS_PY_TOOLS
