2b3b4c64876ffdbf8bf0f1cd49a0af5ed1267ea9
David Lechner <david@pybricks.com>
2023-01-23 15:41:38 -0600

    bricks/_common/mpconfigport: disable gc module
    This reverts a change that was accidentally committed in 63e26377.

--- bricks/_common/mpconfigport.h
+++ bricks/_common/mpconfigport.h
@@ -81,7 +81,7 @@
 #define MICROPY_PY_BUILTINS_MIN_MAX             (1)
 #define MICROPY_PY___FILE__                     (0)
 #define MICROPY_PY_MICROPYTHON_MEM_INFO         (1)
-#define MICROPY_PY_GC                           (PYBRICKS_OPT_EXTRA_MOD)
+#define MICROPY_PY_GC                           (0)
 #define MICROPY_PY_ARRAY                        (0)
 #define MICROPY_PY_ATTRTUPLE                    (0)
 #define MICROPY_PY_COLLECTIONS                  (0)
