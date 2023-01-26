dbd1c76b79165931d6ad2be35cc6531a79aef5cd
Laurens Valk <laurens@pybricks.com>
2023-01-26 14:13:09 +0100

    pbio/test/servo: Simplify test.
    This is also a bit closer to a user program were there may be multiple moves in succession.

--- lib/pbio/test/src/test_servo.c
+++ lib/pbio/test/src/test_servo.c
@@ -28,78 +28,49 @@
 #include "../drv/clock/clock_test.h"
 #include "../drv/motor_driver/motor_driver_virtual_simulation.h"
 
-/**
- * Common test for pbio_servo_run_* functions.
- * @param [in]  pt      The Contiki proto-thread
- * @param [in]  name    The name of the test (i.e. __func__)
- * @param [in]  func    Callback for invoking pbio_servo_run_*
- */
-static PT_THREAD(test_servo_run_func(struct pt *pt, const char *name, pbio_error_t (*func)(pbio_servo_t *servo))) {
-    static pbdrv_motor_driver_dev_t *driver;
-    static pbio_servo_t *servo;
-    static struct timer timeout;
+static PT_THREAD(test_servo(struct pt *pt)) {
+    static pbio_servo_t *srv;
 
+    // Start motor driver simulation process.
     pbdrv_motor_driver_init_manual();
 
     PT_BEGIN(pt);
 
+    // Wait for motor simulation process to be ready.
     while (pbdrv_init_busy()) {
         PT_YIELD(pt);
     }
 
-    timer_set(&timeout, 10 * 1000);
-
-    tt_uint_op(pbdrv_motor_driver_get_dev(0, &driver), ==, PBIO_SUCCESS);
-
+    // Start motor control process.
     process_start(&pbio_motor_process);
     tt_want(process_is_running(&pbio_motor_process));
 
-    tt_uint_op(pbio_servo_get_servo(PBIO_PORT_ID_A, &servo), ==, PBIO_SUCCESS);
-    tt_uint_op(pbio_servo_setup(servo, PBIO_DIRECTION_CLOCKWISE, 1000, true), ==, PBIO_SUCCESS);
-    tt_uint_op(func(servo), ==, PBIO_SUCCESS);
+    // Initialize the servo.
+    tt_uint_op(pbio_servo_get_servo(PBIO_PORT_ID_A, &srv), ==, PBIO_SUCCESS);
+    tt_uint_op(pbio_servo_setup(srv, PBIO_DIRECTION_CLOCKWISE, 1000, true), ==, PBIO_SUCCESS);
 
-    while (!pbio_control_is_done(&servo->control) && !timer_expired(&timeout)) {
+    // Test running by angle.
+    tt_uint_op(pbio_servo_run_angle(srv, 500, 180, PBIO_CONTROL_ON_COMPLETION_HOLD), ==, PBIO_SUCCESS);
+    while (!pbio_control_is_done(&srv->control)) {
         pbio_test_clock_tick(1);
         PT_YIELD(pt);
     }
+    tt_want(pbio_control_is_done(&srv->control));
 
-    tt_want(pbio_control_is_done(&servo->control));
+    // Test running for time.
+    tt_uint_op(pbio_servo_run_time(srv, 500, 1000, PBIO_CONTROL_ON_COMPLETION_HOLD), ==, PBIO_SUCCESS);
+    while (!pbio_control_is_done(&srv->control)) {
+        pbio_test_clock_tick(1);
+        PT_YIELD(pt);
+    }
+    tt_want(pbio_control_is_done(&srv->control));
 
 end:
 
     PT_END(pt);
 }
 
-static pbio_error_t test_servo_run_angle_func(pbio_servo_t *servo) {
-    return pbio_servo_run_angle(servo, 500, 180, PBIO_CONTROL_ON_COMPLETION_HOLD);
-}
-
-static PT_THREAD(test_servo_run_angle(struct pt *pt)) {
-    static struct pt child;
-
-    PT_BEGIN(pt);
-
-    PT_SPAWN(pt, &child, test_servo_run_func(&child, __func__, test_servo_run_angle_func));
-
-    PT_END(pt);
-}
-
-static pbio_error_t test_servo_run_time_func(pbio_servo_t *servo) {
-    return pbio_servo_run_time(servo, 500, 1000, PBIO_CONTROL_ON_COMPLETION_HOLD);
-}
-
-static PT_THREAD(test_servo_run_time(struct pt *pt)) {
-    static struct pt child;
-
-    PT_BEGIN(pt);
-
-    PT_SPAWN(pt, &child, test_servo_run_func(&child, __func__, test_servo_run_time_func));
-
-    PT_END(pt);
-}
-
 struct testcase_t pbio_servo_tests[] = {
-    PBIO_PT_THREAD_TEST(test_servo_run_angle),
-    PBIO_PT_THREAD_TEST(test_servo_run_time),
+    PBIO_PT_THREAD_TEST(test_servo),
     END_OF_TESTCASES
 };
