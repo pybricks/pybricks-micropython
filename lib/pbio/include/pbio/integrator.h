pybricks/pybricks-micropython

commit 69910852d73ce6bfd5ae2286c15de1a81769028e

pbio/integrator: Avoid integration in deadzone around target.

lib/pbio/src/integrator.c
@@ -176,8 +176,9 @@ int32_t pbio_position_integrator_update(pbio_position_integrator_t *itg, int32_t
         // control, with a factor of 2 so we begin integrating a bit sooner.
         int32_t integral_range_upper = pbio_control_settings_div_by_gain(itg->settings->actuation_max, itg->settings->pid_kp) * 2;
 
-        // Add change if we are near target, or always if it decreases the integral magnitude
-        if (pbio_int_math_abs(position_remaining) <= integral_range_upper || decrease) {
+        // Add change if we are near (but not too near) target, or always if it decreases the integral magnitude.
+        if ((pbio_int_math_abs(position_remaining) >= itg->settings->integral_deadzone &&
+             pbio_int_math_abs(position_remaining) <= integral_range_upper) || decrease) {
             itg->count_err_integral += pbio_control_settings_mul_by_loop_time(error_now);
         }
 

lib/pbio/include/pbio/control_settings.h
@@ -91,6 +91,10 @@ typedef struct _pbio_control_settings_t {
      */
     int32_t pid_kd;
     /**
+     * Zone (angle) around the target within which the integral error should not grow.
+     */
+    int32_t integral_deadzone;
+    /**
      * Absolute bound on the rate at which the integrator accumulates errors.
      */
     int32_t integral_change_max;

lib/pbio/src/motor/servo_settings.c
@@ -243,6 +243,7 @@ pbio_error_t pbio_servo_load_settings(pbio_control_settings_t *settings, const p
     settings->stall_speed_limit = DEG_TO_MDEG(20);
     settings->stall_time = pbio_control_time_ms_to_ticks(200);
     settings->integral_change_max = DEG_TO_MDEG(15);
+    settings->integral_deadzone = DEG_TO_MDEG(8);
 
     // Device type specific speed, acceleration, and PD settings.
     switch (id) {

commit 55de59152f8c0e16912cca64e94bbe3a32f71551

pbio/integrator: Use target error for deadzone.
Instead of using the remaining angle on the trajectory, which is ultimately always zero.

This fixes integral control not working after the trajectory completes,
which was broken in https://github.com/pybricks/pybricks-micropython/commit/69910852d73ce6bfd5ae2286c15de1a81769028e.

Coincidentally, that also make the deadzone fix look better than it
really was (there was no more movement simply because I=0), so this
needs further work.

lib/pbio/include/pbio/integrator.h
@@ -48,7 +48,7 @@ uint32_t pbio_position_integrator_get_ref_time(pbio_position_integrator_t *itg, 
 void pbio_position_integrator_pause(pbio_position_integrator_t *itg, uint32_t time_now);
 void pbio_position_integrator_resume(pbio_position_integrator_t *itg, uint32_t time_now);
 void pbio_position_integrator_reset(pbio_position_integrator_t *itg, pbio_control_settings_t *settings, uint32_t time_now);
-int32_t pbio_position_integrator_update(pbio_position_integrator_t *itg, int32_t position_error, int32_t position_remaining);
+int32_t pbio_position_integrator_update(pbio_position_integrator_t *itg, int32_t position_error, int32_t target_error);
 bool pbio_position_integrator_stalled(pbio_position_integrator_t *itg, uint32_t time_now, int32_t speed_now, int32_t speed_ref);
 
 #endif // _PBIO_INTEGRATOR_H_

lib/pbio/src/control.c
@@ -101,8 +101,8 @@ void pbio_control_update(pbio_control_t *ctl, uint32_t time_now, pbio_control_st
     if (pbio_control_type_is_position(ctl)) {
 
         // Update count integral error and get current error state
-        int32_t position_remaining = pbio_angle_diff_mdeg(&ref_end.position, &ref->position);
-        integral_error = pbio_position_integrator_update(&ctl->position_integrator, position_error, position_remaining);
+        int32_t target_error = pbio_angle_diff_mdeg(&ref_end.position, &state->position);
+        integral_error = pbio_position_integrator_update(&ctl->position_integrator, position_error, target_error);
 
         // For position control, the proportional term is the real position error.
         position_error_used = position_error;

lib/pbio/src/integrator.c
@@ -151,7 +151,7 @@ void pbio_position_integrator_reset(pbio_position_integrator_t *itg, pbio_contro
 
 }
 
-int32_t pbio_position_integrator_update(pbio_position_integrator_t *itg, int32_t position_error, int32_t position_remaining) {
+int32_t pbio_position_integrator_update(pbio_position_integrator_t *itg, int32_t position_error, int32_t target_error) {
 
     // Previous error will be multiplied by time delta and then added to integral (unless we limit growth)
     int32_t error_now = itg->count_err_prev;
@@ -177,8 +177,8 @@ int32_t pbio_position_integrator_update(pbio_position_integrator_t *itg, int32_t
         int32_t integral_range_upper = pbio_control_settings_div_by_gain(itg->settings->actuation_max, itg->settings->pid_kp) * 2;
 
         // Add change if we are near (but not too near) target, or always if it decreases the integral magnitude.
-        if ((pbio_int_math_abs(position_remaining) >= itg->settings->integral_deadzone &&
-             pbio_int_math_abs(position_remaining) <= integral_range_upper) || decrease) {
+        if ((pbio_int_math_abs(target_error) >= itg->settings->integral_deadzone &&
+             pbio_int_math_abs(target_error) <= integral_range_upper) || decrease) {
             itg->count_err_integral += pbio_control_settings_mul_by_loop_time(error_now);
         }
 