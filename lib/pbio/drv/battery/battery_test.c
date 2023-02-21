2ef97a3a63dacd32dfe6baa0187440274f9a0266
Laurens Valk <laurens@pybricks.com>
2023-02-21 11:13:41 +0100

    drv/battery_test: Implement current, temperature.

--- lib/pbio/drv/battery/battery_test.c
+++ lib/pbio/drv/battery/battery_test.c
@@ -23,4 +23,14 @@ pbio_error_t pbdrv_battery_get_type(pbdrv_battery_type_t *value) {
     return PBIO_SUCCESS;
 }
 
+pbio_error_t pbdrv_battery_get_current_now(uint16_t *value) {
+    *value = 0;
+    return PBIO_ERROR_NOT_SUPPORTED;
+}
+
+pbio_error_t pbdrv_battery_get_temperature(uint32_t *value) {
+    *value = 0;
+    return PBIO_ERROR_NOT_SUPPORTED;
+}
+
 #endif // PBDRV_CONFIG_BATTERY_TEST
