698dd1ceaaee4d9b260410cb85d0003707654e1a
Laurens Valk <laurens@pybricks.com>
2023-02-21 12:04:25 +0100

    tests/motors/open_loop: Print state while moving.
    Now this test can also be used as a sanity check
    for simulation tests without blocking on something
    that may never complete (like running to an angle).

--- tests/motors/open_loop.py
+++ tests/motors/open_loop.py
@@ -12,13 +12,12 @@ print(version)
 motor = Motor(Port.A)
 
 # Allocate the data logs.
-DURATION = 12000
-motor.log.start(DURATION)
-motor.control.log.start(DURATION)
+motor.log.start(6000)
 
 for i in range(11):
+    print(motor.angle())
     motor.dc(i * 10)
-    wait(1000)
+    wait(500)
 
 # Wait so we can also log the stopped behavior.
 motor.dc(0)
@@ -28,5 +27,4 @@ motor.stop()
 # Transfer data logs.
 print("Transferring data...")
 motor.log.save("servo.txt")
-motor.control.log.save("control.txt")
 print("Done")
