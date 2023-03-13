2736eda139cf476f7dcbc76af06f3d4584afbe12
David Lechner <david@pybricks.com>
2023-03-13 13:19:37 -0500

    pbio/src/parent: document module
    Add doc comments for public members of the `parent` module.

--- lib/pbio/include/pbio/parent.h
+++ lib/pbio/include/pbio/parent.h
@@ -18,21 +18,41 @@
 
 #include <pbio/error.h>
 
-
+/**
+ * Callback function used by pbio_parent_set().
+ *
+ * Implementations should stop all actuation when this is called. For example,
+ * when used with motors, this should stop the motors.
+ *
+ * @param [in]  parent          The parent object.
+ * @param [in]  clear_parent    When true, the parent relationship is ending now.
+ * @returns                     Error code on failure or ::PBIO_SUCCESS.
+ */
 typedef pbio_error_t (*pbio_parent_stop_func_t)(void *parent, bool clear_parent);
 
+/** Opaque pointer for managing parent object relationships. */
 typedef struct _pbio_parent_t {
+    /** @private Pointer to the parent object or @c NULL if not currently linked. */
     void *parent_object;
+    /** @private Stop function callback that is called from pbio_parent_stop().  */
     pbio_parent_stop_func_t parent_stop_func;
 } pbio_parent_t;
 
-void pbio_parent_set(pbio_parent_t *parent, void *parent_object, pbio_parent_stop_func_t stop_func);
+/** @name Initialization Functions */
+/**@{*/
+void pbio_parent_set(pbio_parent_t *self, void *parent_object, pbio_parent_stop_func_t stop_func);
+/**@}*/
 
-bool pbio_parent_exists(const pbio_parent_t *parent);
+/** @name Status Functions */
+/**@{*/
+bool pbio_parent_exists(const pbio_parent_t *self);
+bool pbio_parent_equals(const pbio_parent_t *self, const void *candidate_object);
+/**@}*/
 
-bool pbio_parent_equals(const pbio_parent_t *parent, const void *candidate_object);
-
+/** @name Operation Functions */
+/**@{*/
 pbio_error_t pbio_parent_stop(pbio_parent_t *self, bool clear_parent);
+/**@}*/
 
 #endif // _PBIO_PARENT_H_
 
--- lib/pbio/src/parent.c
+++ lib/pbio/src/parent.c
@@ -9,19 +9,54 @@
 #include <pbio/error.h>
 #include <pbio/parent.h>
 
+/**
+ * Sets the parent of @p self and assigns a stop callback.
+ *
+ * Call pbio_parent_stop() with @p clear_parent set to @c true to release
+ * @p parent_object when no longer needed.
+ *
+ * @param [in]  self                An uninitialized ::pbio_parent_t struct of the child object.
+ * @param [in]  parent_object       The parent object.
+ * @param [in]  stop_func           The stop function callback.
+ */
 void pbio_parent_set(pbio_parent_t *self, void *parent_object, pbio_parent_stop_func_t stop_func) {
     self->parent_object = parent_object;
     self->parent_stop_func = stop_func;
 }
 
+/**
+ * Tests if parent object of @p self is currently assigned.
+ *
+ * @param [in]  self                The ::pbio_parent_t struct of the child object.
+ * @returns                         @c true if the parent is assigned, otherwise @c false.
+ */
 bool pbio_parent_exists(const pbio_parent_t *self) {
     return self->parent_object != NULL;
 }
 
+/**
+ * Tests if the parent of @p self is the same object as @p candidate_object.
+ *
+ * @param [in]  self                The ::pbio_parent_t struct of the child object.
+ * @param [in]  candidate_object    The other object to compare.
+ * @returns                         @c true if both objects are the same object, otherwise @c false.
+ */
 bool pbio_parent_equals(const pbio_parent_t *self, const void *candidate_object) {
     return self->parent_object == candidate_object;
 }
 
+/**
+ * Calls the stop function of the parent of @p self.
+ *
+ * If the parent object has already been released, this function does nothing.
+ *
+ * @param [in]  self                The ::pbio_parent_t struct of the child object.
+ * @param [in]  clear_parent        When @c true, releases the parent object.
+ * @returns                         The result of the stop callback function.
+ *
+ * \note Even if the stop callback function fails, the parent will still be
+ * released when @p clear_parent is @c true.
+ */
 pbio_error_t pbio_parent_stop(pbio_parent_t *self, bool clear_parent) {
     if (!pbio_parent_exists(self)) {
         // Nothing to do if there is no parent.
