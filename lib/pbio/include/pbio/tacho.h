58fc68411490eb6e685b6732a7eede65cb099017
David Lechner <david@pybricks.com>
2023-03-13 14:14:55 -0500

    pbio/tacho: group functions in docs
    This adds group names for functions for the tacho module.
    
    Also clean up includes and fix missing return type while we are touching
    this.

--- lib/pbio/include/pbio/tacho.h
+++ lib/pbio/include/pbio/tacho.h
@@ -15,22 +15,35 @@
 #ifndef _PBIO_TACHO_H_
 #define _PBIO_TACHO_H_
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #include <pbio/angle.h>
 #include <pbio/config.h>
-#include <pbdrv/counter.h>
+#include <pbio/error.h>
 #include <pbio/dcmotor.h>
+#include <pbio/port.h>
 
+/** A rotation sensor object. */
 typedef struct _pbio_tacho_t pbio_tacho_t;
 
 #if PBIO_CONFIG_TACHO
 
+/** @name Initialization Functions */
+/**@{*/
 pbio_error_t pbio_tacho_get_tacho(pbio_port_id_t port, pbio_tacho_t **tacho);
 pbio_error_t pbio_tacho_setup(pbio_tacho_t *tacho, pbio_direction_t direction, bool reset_angle);
+/**@}*/
 
+/** @name Status Functions */
+/**@{*/
 pbio_error_t pbio_tacho_get_angle(pbio_tacho_t *tacho, pbio_angle_t *angle);
+/**@}*/
+
+/** @name Operation Functions */
+/**@{*/
 pbio_error_t pbio_tacho_reset_angle(pbio_tacho_t *tacho, pbio_angle_t *reset_angle, bool reset_to_abs);
+/**@}*/
 
 #else
 
@@ -42,11 +55,11 @@ static inline pbio_error_t pbio_tacho_setup(pbio_tacho_t *tacho, pbio_direction_
     return PBIO_ERROR_NOT_SUPPORTED;
 }
 
-
 static inline pbio_error_t pbio_tacho_get_angle(pbio_tacho_t *tacho, pbio_angle_t *angle) {
     return PBIO_ERROR_NOT_SUPPORTED;
 }
-static inline pbio_tacho_reset_angle(pbio_tacho_t * tacho, pbio_angle_t * reset_angle, bool reset_to_abs) {
+
+static inline pbio_error_t pbio_tacho_reset_angle(pbio_tacho_t *tacho, pbio_angle_t *reset_angle, bool reset_to_abs) {
     return PBIO_ERROR_NOT_SUPPORTED;
 }
 
