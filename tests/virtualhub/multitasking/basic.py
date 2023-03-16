8e6b96b6873d0dc8baae2f53b3cd1297f568008a
David Lechner <david@pybricks.com>
2023-03-16 17:32:21 -0500

    pybricks.tools.task: implement all() and race()
    This implements task.all() and task.race() in the style of redux
    sagas effects.
    
    This is similar to, but much more simple than, asyncio.gather() and
    asyncio.wait(return_when=FIRST_COMPLETED). If any task raises an
    exception, all other tasks are canceled.
    
    The return value of task.all() works like asyncio.gather(). The return
    value of task.race() is similar except all canceled tasks will have
    a value of None.
    
    The run loop is modified to call the garbage collector after each loop.
    This helps keep memory from fragmenting and reduces time needed to
    allocate new memory in many cases.
    
    The run loop also has a timer added to call it at a specific interval
    rather than as fast as possible.

---
+++ tests/virtualhub/multitasking/basic.py
@@ -0,0 +1,174 @@
+from pybricks.tools import task
+
+
+class CallCounter:
+    def __init__(self):
+        self.count = 0
+
+    def __call__(self):
+        self.count += 1
+
+
+def count():
+    """
+    Count forever.
+    """
+    n = 0
+    while True:
+        yield n
+        n += 1
+
+
+def never_return(on_cancel):
+    try:
+        for i in count():
+            yield i
+    except GeneratorExit:
+        on_cancel()
+
+
+def return_after(n, on_cancel):
+    try:
+        for i in range(n):
+            yield i
+        return i
+    except GeneratorExit:
+        on_cancel()
+
+
+def throw_after(n, msg, on_cancel):
+    try:
+        for i in range(n):
+            yield i
+        raise RuntimeError(msg)
+    except GeneratorExit:
+        on_cancel()
+
+
+async def test_all_1():
+    print("test_all_1")
+
+    task1_cancel = CallCounter()
+    task2_cancel = CallCounter()
+
+    task1 = return_after(1, task1_cancel)
+    task2 = return_after(2, task2_cancel)
+
+    # should return [0, 1] - both allowed to complete
+    print(await task.all(task1, task2))
+
+    # neither should be canceled
+    print(task1_cancel.count, task2_cancel.count)
+
+    # both should raise StopIteration because they are done
+
+    try:
+        next(task1)
+    except StopIteration as e:
+        print(type(e), e.args)
+
+    try:
+        next(task2)
+    except StopIteration as e:
+        print(type(e), e.args)
+
+
+async def test_all_2():
+    print("test_all_2")
+
+    task1_cancel = CallCounter()
+    task2_cancel = CallCounter()
+
+    task1 = return_after(2, task1_cancel)
+    task2 = throw_after(1, "task2", task2_cancel)
+
+    # should throw RuntimeError("task2")
+    try:
+        await task.all(task1, task2)
+    except Exception as e:
+        print(type(e), e.args)
+
+    # task1 should be canceled
+    print(task1_cancel.count, task2_cancel.count)
+
+    # both should raise StopIteration because they are done
+
+    try:
+        next(task1)
+    except StopIteration as e:
+        print(type(e), e.args)
+
+    try:
+        next(task2)
+    except StopIteration as e:
+        print(type(e), e.args)
+
+
+async def test_race_1():
+    print("test_race_1")
+
+    task1_cancel = CallCounter()
+    task2_cancel = CallCounter()
+
+    task1 = return_after(1, task1_cancel)
+    task2 = return_after(2, task2_cancel)
+
+    # returns [0, None] - only first completes
+    print(await task.race(task1, task2))
+
+    # task2 should be canceled
+    print(task1_cancel.count, task2_cancel.count)
+
+    # both should raise StopIteration because they are done/canceled
+
+    try:
+        next(task1)
+    except StopIteration as e:
+        print(type(e), e.args)
+
+    try:
+        next(task2)
+    except StopIteration as e:
+        print(type(e), e.args)
+
+
+async def test_race_2():
+    print("test_race_2")
+
+    task1_cancel = CallCounter()
+    task2_cancel = CallCounter()
+
+    task1 = return_after(2, task1_cancel)
+    task2 = throw_after(1, "task2", task2_cancel)
+
+    # should throw RuntimeError("task2")
+    try:
+        await task.race(task1, task2)
+    except Exception as e:
+        print(type(e), e.args)
+
+    # task1 should be canceled
+    print(task1_cancel.count, task2_cancel.count)
+
+    # both should raise StopIteration because they are done
+
+    try:
+        next(task1)
+    except StopIteration as e:
+        print(type(e), e.args)
+
+    try:
+        next(task2)
+    except StopIteration as e:
+        print(type(e), e.args)
+
+
+async def main():
+    await test_all_1()
+    await test_all_2()
+    await test_race_1()
+    await test_race_2()
+
+
+# run as fast as possible for CI
+task.run(main(), loop_time=0)
