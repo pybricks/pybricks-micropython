7ae4230ae89e3c38d131dda6b35bc1fc42c2f085
Laurens Valk <laurens@pybricks.com>
2023-03-27 15:58:34 +0200

    pbio/test/servo: Add stall test.

--- lib/pbio/test/src/test_servo.c
+++ lib/pbio/test/src/test_servo.c
@@ -29,7 +29,7 @@
 #include "../drv/clock/clock_test.h"
 #include "../drv/motor_driver/motor_driver_virtual_simulation.h"
 
-static PT_THREAD(test_servo(struct pt *pt)) {
+static PT_THREAD(test_servo_basics(struct pt *pt)) {
 
     static struct timer timer;
 
@@ -47,11 +47,6 @@ static PT_THREAD(test_servo(struct pt *pt)) {
 
     // Start motor control process manually.
     pbio_motor_process_start();
-
-    // Initialize a servo that will stall.
-    tt_uint_op(pbio_servo_get_servo(PBIO_PORT_ID_C, &srv), ==, PBIO_SUCCESS);
-    tt_uint_op(pbio_servo_setup(srv, PBIO_DIRECTION_CLOCKWISE, 1000, true, 0), ==, PBIO_SUCCESS);
-    tt_uint_op(pbio_servo_run_forever(srv, -500), ==, PBIO_SUCCESS);
 
     // Initialize the servo.
     tt_uint_op(pbio_servo_get_servo(PBIO_PORT_ID_A, &srv), ==, PBIO_SUCCESS);
@@ -72,7 +67,60 @@ end:
     PT_END(pt);
 }
 
+static PT_THREAD(test_servo_stall(struct pt *pt)) {
+
+    static struct timer timer;
+    static pbio_servo_t *srv;
+
+    static bool stalled;
+    static uint32_t stall_duration;
+
+    // Start motor driver simulation process.
+    pbdrv_motor_driver_init_manual();
+
+    PT_BEGIN(pt);
+
+    // Wait for motor simulation process to be ready.
+    while (pbdrv_init_busy()) {
+        PT_YIELD(pt);
+    }
+
+    // Start motor control process manually.
+    pbio_motor_process_start();
+
+    // Initialize a servo that will stall.
+    tt_uint_op(pbio_servo_get_servo(PBIO_PORT_ID_C, &srv), ==, PBIO_SUCCESS);
+    tt_uint_op(pbio_servo_setup(srv, PBIO_DIRECTION_CLOCKWISE, 1000, true, 0), ==, PBIO_SUCCESS);
+    tt_uint_op(pbio_servo_run_forever(srv, -500), ==, PBIO_SUCCESS);
+
+    // Should not be stalled at first.
+    tt_uint_op(pbio_servo_is_stalled(srv, &stalled, &stall_duration), ==, PBIO_SUCCESS);
+    tt_want(!stalled);
+
+    // But after a we should be hitting the endpoint and stall.
+    pbio_test_sleep_ms(&timer, 2000);
+    tt_uint_op(pbio_servo_is_stalled(srv, &stalled, &stall_duration), ==, PBIO_SUCCESS);
+    tt_want(stalled);
+
+    // The same should be true after we turn around, which should immediately
+    // unstall (evaluated on next control loop, so >= 5ms).
+    tt_uint_op(pbio_servo_run_forever(srv, 500), ==, PBIO_SUCCESS);
+    pbio_test_sleep_ms(&timer, 5);
+    tt_uint_op(pbio_servo_is_stalled(srv, &stalled, &stall_duration), ==, PBIO_SUCCESS);
+    tt_want(!stalled);
+
+    // But after a we should be hitting the other endpoint and stall.
+    pbio_test_sleep_ms(&timer, 2000);
+    tt_uint_op(pbio_servo_is_stalled(srv, &stalled, &stall_duration), ==, PBIO_SUCCESS);
+    tt_want(stalled);
+
+end:
+
+    PT_END(pt);
+}
+
 struct testcase_t pbio_servo_tests[] = {
-    PBIO_PT_THREAD_TEST(test_servo),
+    PBIO_PT_THREAD_TEST(test_servo_basics),
+    PBIO_PT_THREAD_TEST(test_servo_stall),
     END_OF_TESTCASES
 };
