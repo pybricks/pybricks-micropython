pybricks/pybricks-micropython

commit 117212a37f5e900f5431cd1ac8285116d031377c

github: update azure library
CI started getting errors due to the azure-cosmosdb-table which has
been deprecated for a few years now. This makes updates to use the
replacement library.

.github/build-each-commit.py
@@ -5,12 +5,13 @@ import argparse
 import os
 import subprocess
 
+from azure.core.credentials import AzureNamedKeyCredential
+from azure.data.tables import TableClient, UpdateMode
 import git
-
-from azure.cosmosdb.table.tableservice import TableService
 
 STORAGE_ACCOUNT = os.environ.get("STORAGE_ACCOUNT")
 STORAGE_KEY = os.environ.get("STORAGE_KEY")
+STORAGE_URL = os.environ.get("STORAGE_URL")
 FIRMWARE_SIZE_TABLE = os.environ.get("FIRMWARE_SIZE_TABLE")
 
 PYBRICKS_PATH = os.environ.get("PYBRICKS_PATH", ".")
@@ -31,7 +32,11 @@ assert not pybricks.bare, "Repository not found"
 
 # if credentials were given, connect to remote database
 if STORAGE_ACCOUNT:
-    service = TableService(STORAGE_ACCOUNT, STORAGE_KEY)
+    firmware_size_table = TableClient(
+        STORAGE_URL,
+        FIRMWARE_SIZE_TABLE,
+        credential=AzureNamedKeyCredential(STORAGE_ACCOUNT, STORAGE_KEY),
+    )
 
 # build each commit starting with the oldest
 start_commit = (
@@ -41,12 +46,8 @@ start_commit = (
 )
 end_commit = args.commit
 
-for commit in reversed(
-    list(
-        pybricks.iter_commits(
-            f"{start_commit}..{end_commit}", ancestry_path=start_commit
-        )
-    )
+for commit in pybricks.iter_commits(
+    f"{start_commit}..{end_commit}", ancestry_path=start_commit, reverse=True
 ):
     print("Checking out", commit.hexsha[:8], f'"{commit.summary}"', flush=True)
     pybricks.git.checkout(commit.hexsha)
@@ -55,6 +56,9 @@ for commit in reversed(
     # update only required submodules
     pybricks.git.submodule("update", "--init", "micropython")
     if args.hub in ["cityhub", "movehub", "technichub", "primehub", "essentialhub"]:
+        pybricks.submodule("micropython").module().git.submodule(
+            "update", "--init", "lib/micropython-lib"
+        )
         pybricks.submodule("micropython").module().git.submodule(
             "update", "--init", "lib/stm32lib"
         )
@@ -80,11 +84,11 @@ for commit in reversed(
         )
         size = os.path.getsize(bin_path)
 
-        service.insert_or_merge_entity(
-            FIRMWARE_SIZE_TABLE,
+        firmware_size_table.upsert_entity(
             {
                 "PartitionKey": "size",
                 "RowKey": commit.hexsha,
                 args.hub: size,
             },
+            UpdateMode.REPLACE,
         )

.github/build-missing-commits.py
@@ -5,13 +5,14 @@ import os
 import subprocess
 import sys
 
+from azure.core.credentials import AzureNamedKeyCredential
+from azure.core.exceptions import ResourceNotFoundError
+from azure.data.tables import TableClient, UpdateMode
 import git
-
-from azure.common import AzureMissingResourceHttpError
-from azure.cosmosdb.table.tableservice import TableService
 
 STORAGE_ACCOUNT = os.environ["STORAGE_ACCOUNT"]
 STORAGE_KEY = os.environ["STORAGE_KEY"]
+STORAGE_URL = os.environ["STORAGE_URL"]
 CI_STATUS_TABLE = os.environ["CI_STATUS_TABLE"]
 FIRMWARE_SIZE_TABLE = os.environ["FIRMWARE_SIZE_TABLE"]
 
@@ -37,9 +38,19 @@ except Exception as e:
 
 assert not pybricks.bare, "Repository not found"
 
-service = TableService(STORAGE_ACCOUNT, STORAGE_KEY)
+ci_status_table = TableClient(
+    STORAGE_URL,
+    CI_STATUS_TABLE,
+    credential=AzureNamedKeyCredential(STORAGE_ACCOUNT, STORAGE_KEY),
+)
 
-start_hash = service.get_entity(CI_STATUS_TABLE, "build", "lastHash")["hash"]
+firmware_size_table = TableClient(
+    STORAGE_URL,
+    FIRMWARE_SIZE_TABLE,
+    credential=AzureNamedKeyCredential(STORAGE_ACCOUNT, STORAGE_KEY),
+)
+
+start_hash = ci_status_table.get_entity("build", "lastHash")["hash"]
 
 if start_hash == pybricks.commit(PYBRICKS_BRANCH).hexsha:
     print("Already up to date.")
@@ -48,19 +59,19 @@ if start_hash == pybricks.commit(PYBRICKS_BRANCH).hexsha:
 
 # Process the commits in the tree and log the data
 for commit in pybricks.iter_commits(
-    f"{start_hash}..{PYBRICKS_BRANCH}", ancestry_path=start_hash
+    f"{start_hash}..{PYBRICKS_BRANCH}", ancestry_path=start_hash, reverse=True
 ):
     print(f"trying {commit.hexsha}...")
 
     sizes = {}
 
     try:
-        entity = service.get_entity(FIRMWARE_SIZE_TABLE, "size", commit.hexsha)
+        entity = firmware_size_table.get_entity("size", commit.hexsha)
         # if entity is found but some hubs had null size, redo only those hubs
         hubs = [h for h in HUBS if entity.get(h) is None]
         # save existing sizes since we replace then entity rather than update later
-        sizes = {h: int(entity.get(h)) for h in HUBS if entity.get(h) is not None}
-    except AzureMissingResourceHttpError:
+        sizes = {h: int(entity[h]) for h in HUBS if entity.get(h) is not None}
+    except ResourceNotFoundError:
         # if there is no entry at all, build all hubs
         hubs = HUBS
 
@@ -76,6 +87,9 @@ for commit in pybricks.iter_commits(
     # update required submodules
     print("Checking out submodules")
     pybricks.git.submodule("update", "--init", "micropython")
+    pybricks.submodule("micropython").module().git.submodule(
+        "update", "--init", "lib/micropython-lib"
+    )
     pybricks.submodule("micropython").module().git.submodule(
         "update", "--init", "lib/stm32lib"
     )
@@ -115,15 +129,16 @@ for commit in pybricks.iter_commits(
         except FileNotFoundError:
             pass
 
-    service.insert_or_replace_entity(
-        FIRMWARE_SIZE_TABLE, {"PartitionKey": "size", "RowKey": commit.hexsha, **sizes}
+    firmware_size_table.upsert_entity(
+        {"PartitionKey": "size", "RowKey": commit.hexsha, **sizes},
+        UpdateMode.REPLACE,
     )
 
-service.update_entity(
-    CI_STATUS_TABLE,
+ci_status_table.update_entity(
     {
         "PartitionKey": "build",
         "RowKey": "lastHash",
         "hash": pybricks.commit(PYBRICKS_BRANCH).hexsha,
     },
+    UpdateMode.REPLACE,
 )

.github/download-commit-metadata.py
@@ -5,13 +5,15 @@ import json
 import os
 from pathlib import Path
 
-from azure.cosmosdb.table.tableservice import TableService
+from azure.core.credentials import AzureNamedKeyCredential
+from azure.data.tables import TableClient, EntityProperty
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
 GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
 STORAGE_ACCOUNT = os.environ["STORAGE_ACCOUNT"]
 STORAGE_KEY = os.environ["STORAGE_KEY"]
+STORAGE_URL = os.environ["STORAGE_URL"]
 FIRMWARE_SIZE_TABLE = os.environ["FIRMWARE_SIZE_TABLE"]
 
 BUILD_DIR = os.environ.get("BUILD_DIR", "build")
@@ -55,12 +57,17 @@ QUERY = """query {
 
 def main():
     # grab size data from Azure
-    service = TableService(STORAGE_ACCOUNT, STORAGE_KEY)
+    firmware_size_table = TableClient(
+        STORAGE_URL,
+        FIRMWARE_SIZE_TABLE,
+        credential=AzureNamedKeyCredential(STORAGE_ACCOUNT, STORAGE_KEY),
+    )
     sizes = {
-        item["RowKey"]: {k: v for k, v in item.items() if k in HUBS}
-        for item in service.query_entities(
-            FIRMWARE_SIZE_TABLE,
-            filter="PartitionKey eq 'size'",
+        item["RowKey"]: {
+            k: v.value if isinstance(v, EntityProperty) else v for k, v in item.items() if k in HUBS
+        }
+        for item in firmware_size_table.query_entities(
+            "PartitionKey eq 'size'",
             select=",".join(["RowKey"] + HUBS),
         )
     }

.github/workflows/build.yml
@@ -194,22 +194,21 @@ jobs:
         path: micropython/mpy-cross/build
     - name: Fix file permission
       run: chmod +x micropython/mpy-cross/build/mpy-cross
-    - name: Install python
-      uses: actions/setup-python@v4
-      with:
-        python-version: '3.10'
-    - name: Install Python packages
-      run:  python -m pip install azure-cosmosdb-table GitPython
     - name: Build firmware (pull request)
       if: ${{ github.base_ref != null }}
-      run: .github/build-each-commit.py ${{ matrix.hub }} ${{ github.sha }}
+      run: poetry run .github/build-each-commit.py ${{ matrix.hub }} ${{ github.sha }}
+    - run: pipx install poetry
+      if: ${{ github.base_ref == null && github.ref != 'refs/heads/master' }}
+    - run: poetry install --only=stats
+      if: ${{ github.base_ref == null && github.ref != 'refs/heads/master' }}
     - name: Build firmware (non-master branch)
       if: ${{ github.base_ref == null && github.ref != 'refs/heads/master' }}
       env:
         STORAGE_ACCOUNT: ${{ secrets.STORAGE_ACCOUNT }}
         STORAGE_KEY: ${{ secrets.STORAGE_KEY }}
+        STORAGE_URL: ${{ secrets.STORAGE_URL }}
         FIRMWARE_SIZE_TABLE: ${{ secrets.FIRMWARE_SIZE_TABLE }}
-      run: .github/build-each-commit.py ${{ matrix.hub }} ${{ github.ref_name }}
+      run: poetry run .github/build-each-commit.py ${{ matrix.hub }} ${{ github.ref_name }}
     - name: Build firmware (master branch)
       if: ${{ github.base_ref == null && github.ref == 'refs/heads/master' }}
       run: make -C bricks/${{ matrix.hub }}

.github/workflows/stats.yml
@@ -14,35 +14,31 @@ jobs:
     steps:
       - name: Install cross-compiler
         run: sudo apt-get update && sudo apt-get install --yes gcc-arm-none-eabi
-      - name: Install python
-        uses: actions/setup-python@v4
-        with:
-          python-version: '3.10'
-      - name: Install Python packages
-        run:  |
-          python -m pip install azure-cosmosdb-table GitPython plotly
-          python -m pip install gql[requests] --pre
       - name: Checkout code
         uses: actions/checkout@v3
         with:
           submodules: true
           fetch-depth: 0
+      - run: pipx install poetry
+      - run: poetry install --only=stats
       - name: Build missing commits
         env:
           STORAGE_ACCOUNT: ${{ secrets.STORAGE_ACCOUNT }}
           STORAGE_KEY: ${{ secrets.STORAGE_KEY }}
+          STORAGE_URL: ${{ secrets.STORAGE_URL }}
           CI_STATUS_TABLE: ${{ secrets.CI_STATUS_TABLE }}
           FIRMWARE_SIZE_TABLE: ${{ secrets.FIRMWARE_SIZE_TABLE }}
-        run: .github/build-missing-commits.py
+        run: poetry run .github/build-missing-commits.py
       - name: Download commit metadata
         env:
           STORAGE_ACCOUNT: ${{ secrets.STORAGE_ACCOUNT }}
           STORAGE_KEY: ${{ secrets.STORAGE_KEY }}
+          STORAGE_URL: ${{ secrets.STORAGE_URL }}
           FIRMWARE_SIZE_TABLE: ${{ secrets.FIRMWARE_SIZE_TABLE }}
           GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-        run: .github/download-commit-metadata.py
+        run: poetry run .github/download-commit-metadata.py
       - name: Build web pages
-        run: .github/build-stats-web.py
+        run: poetry run .github/build-stats-web.py
       - name: Install lftp
         if: ${{ success() }}
         run: sudo apt-get update && sudo apt-get install --yes lftp

pyproject.toml
@@ -10,25 +10,27 @@ python = ">=3.10,<3.11"
 
 
 [tool.poetry.dev-dependencies]
-azure-cosmosdb-table = "^1.0.6"
 black = "^22.3.0"
 eventlet = "^0.33.3"
 flake8 = "^3.8.3"
-GitPython = "^3.1.30"
-gql = {version = "3.0.0b0", extras = ["requests"], allow-prereleases = true}
 matplotlib = "^3.4.3"
 nbstripout = "^0.5.0"
 notebook = "^6.4.12"
-plotly = "^4.14.1"
 pybricks = {version = "^3.0.0a5", allow-prereleases = true}
 pybricksdev = "1.0.0a46"
 pycryptodomex = "^3.9.9"
-PyGithub = "^1.51"
 python-dotenv = "^0.15.0"
 python-socketio = "^5.7.2"
 pyusb = "^1.0.2"
 scipy = "^1.8.1"
 sympy = "^1.10.1"
+
+[tool.poetry.group.stats.dependencies]
+azure-data-tables = "^12.4.2"
+gql = {extras = ["requests"], version = "^3.4.0"}
+gitpython = "^3.1.31"
+plotly = "^5.14.1"
+pygithub = "^1.58.1"
 
 [build-system]
 requires = ["poetry>=0.12"]

commit d80df1a4c9b23853e274753fd009d25bf412ff75

github: fix uploading from build-each-commit.py
We missed a rename in 117212a3 which caused build sizes to not be
uploaded.

.github/build-each-commit.py
@@ -78,7 +78,7 @@ for commit in pybricks.iter_commits(
     )
 
     # upload firmware size
-    if "service" in globals():
+    if "firmware_size_table" in globals():
         bin_path = os.path.join(
             PYBRICKS_PATH, "bricks", args.hub, "build", "firmware-base.bin"
         )