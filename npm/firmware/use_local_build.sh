2cdd212f15246786b1fe8bb8be0ade3ccc15170c
Laurens Valk <laurens@pybricks.com>
2024-01-30 16:12:35 +0100

    npm/firmware: Add script to optionally use local builds.

---
+++ npm/firmware/use_local_build.sh
@@ -0,0 +1,6 @@
+make -C ../.. all -j
+cp ../../bricks/cityhub/build/firmware.zip build/cityhub.zip
+cp ../../bricks/technichub/build/firmware.zip build/technichub.zip
+cp ../../bricks/movehub/build/firmware.zip build/movehub.zip
+cp ../../bricks/primehub/build/firmware.zip build/primehub.zip
+cp ../../bricks/essentialhub/build/firmware.zip build/essentialhub.zip
