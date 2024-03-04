6eb4c96a9d7327803861b1333219000cfc8efa29
Laurens Valk <laurens@pybricks.com>
2024-03-04 15:06:23 +0100

    tests/virtualhub/motor: Test the car class.

--- tests/virtualhub/motor/car.py
+++ tests/virtualhub/motor/car.py
@@ -4,19 +4,28 @@ from pybricks.robotics import Car
 from pybricks.tools import wait
 
 # Set up all devices.
-wait(2000)
 front = Motor(Port.A, Direction.CLOCKWISE)
 rear = Motor(Port.B, Direction.CLOCKWISE)
 steer = Motor(Port.C, Direction.CLOCKWISE)
 car = Car(steer, [front, rear])
 
+# Simulated endstop is at 142 degrees, minus 10 degrees in the car class.
+TARGET = 132
 
 car.drive_power(50)
 wait(1000)
 
 car.steer(100)
 wait(2000)
+assert abs(steer.angle() - TARGET) < 5
 
 car.steer(-100)
+wait(2000)
+assert abs(steer.angle() - -TARGET) < 5
+
+car.steer(0)
+wait(2000)
+assert abs(steer.angle()) < 5
+
 car.drive_power(-100)
 wait(2000)
