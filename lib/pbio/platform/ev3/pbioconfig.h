911cdab7e06b92cff9311b5f4e4f30c66f57c754
Laurens Valk <laurens@pybricks.com>
2024-03-05 10:57:03 +0100

    bricks/ev3: New arm none port.
    This boots all the way up to MicroPython and outputs stdout over UART on sensor port 1.

---
+++ lib/pbio/platform/ev3/pbioconfig.h
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2019-2022 The Pybricks Authors
+
+#define PBIO_CONFIG_ENABLE_SYS              (1)
---
+++ lib/pbio/platform/ev3/pbsysconfig.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2020-2024 The Pybricks Authors
+
+#define PBSYS_CONFIG_MAIN                           (1)
+#define PBSYS_CONFIG_PROGRAM_LOAD                   (1)
+#define PBSYS_CONFIG_PROGRAM_LOAD_RAM_SIZE          (10 * 1024)
+#define PBSYS_CONFIG_PROGRAM_LOAD_ROM_SIZE          (PBDRV_CONFIG_BLOCK_DEVICE_TEST_SIZE)
+#define PBSYS_CONFIG_PROGRAM_LOAD_OVERLAPS_BOOTLOADER_CHECKSUM (0)
+#define PBSYS_CONFIG_PROGRAM_LOAD_USER_DATA_SIZE    (512)
+#define PBSYS_CONFIG_PROGRAM_LOAD_AUTO_START        (1)
+#define PBSYS_CONFIG_STATUS_LIGHT                   (0)
+#define PBSYS_CONFIG_STATUS_LIGHT_BATTERY           (0)
+#define PBSYS_CONFIG_PROGRAM_STOP                   (0)
