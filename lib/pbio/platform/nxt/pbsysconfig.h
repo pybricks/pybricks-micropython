9b37555da99a3a8c3aa0841573697e9435ca38fb
Laurens Valk <laurens@pybricks.com>
2024-03-22 19:52:03 +0100

    pbio/platform/nxt: Use standard pbsys.
    Use the test block device to run a basic program.

--- lib/pbio/platform/nxt/pbsysconfig.h
+++ lib/pbio/platform/nxt/pbsysconfig.h
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2020-2023 The Pybricks Authors
 
-#define PBSYS_CONFIG_BATTERY_CHARGER                (0)
-#define PBSYS_CONFIG_BLUETOOTH                      (0)
-#define PBSYS_CONFIG_HUB_LIGHT_MATRIX               (0)
-#define PBSYS_CONFIG_MAIN                           (0)
-#define PBSYS_CONFIG_PROGRAM_LOAD                   (0)
+#define PBSYS_CONFIG_MAIN                           (1)
+#define PBSYS_CONFIG_PROGRAM_LOAD                   (1)
+#define PBSYS_CONFIG_PROGRAM_LOAD_RAM_SIZE          (10 * 1024)
+#define PBSYS_CONFIG_PROGRAM_LOAD_ROM_SIZE          (PBDRV_CONFIG_BLOCK_DEVICE_TEST_SIZE)
+#define PBSYS_CONFIG_PROGRAM_LOAD_USER_DATA_SIZE    (512)
+#define PBSYS_CONFIG_PROGRAM_LOAD_AUTO_START        (1)
 #define PBSYS_CONFIG_STATUS_LIGHT                   (0)
 #define PBSYS_CONFIG_STATUS_LIGHT_BATTERY           (0)
 #define PBSYS_CONFIG_PROGRAM_STOP                   (1)
