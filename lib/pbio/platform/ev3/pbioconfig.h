8378675d40f26fedbce6961be7b38b0878a4d582
Laurens Valk <laurens@pybricks.com>
2024-06-08 21:07:30 +0200

    bricks/ev3: Enable some modules.

--- lib/pbio/platform/ev3/pbioconfig.h
+++ lib/pbio/platform/ev3/pbioconfig.h
@@ -1,4 +1,23 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2019-2022 The Pybricks Authors
+// Copyright (c) 2023-2024 The Pybricks Authors
+
+#define PBIO_CONFIG_BATTERY                 (1)
+#define PBIO_CONFIG_DCMOTOR                 (1)
+#define PBIO_CONFIG_DCMOTOR_NUM_DEV         (4)
+#define PBIO_CONFIG_DRIVEBASE_SPIKE         (0)
+#define PBIO_CONFIG_IMU                     (0)
+#define PBIO_CONFIG_LIGHT                   (0)
+#define PBIO_CONFIG_LOGGER                  (1)
+#define PBIO_CONFIG_MOTOR_PROCESS           (1)
+#define PBIO_CONFIG_SERVO                   (1)
+#define PBIO_CONFIG_SERVO_NUM_DEV           (4)
+#define PBIO_CONFIG_SERVO_EV3_NXT           (1)
+#define PBIO_CONFIG_SERVO_PUP               (0)
+#define PBIO_CONFIG_SERVO_PUP_MOVE_HUB      (0)
+#define PBIO_CONFIG_TACHO                   (1)
+
+#define PBIO_CONFIG_UARTDEV                 (0)
+#define PBIO_CONFIG_UARTDEV_NUM_DEV         (0)
+#define PBIO_CONFIG_UARTDEV_FIRST_PORT      PBIO_PORT_ID_1
 
 #define PBIO_CONFIG_ENABLE_SYS              (1)
