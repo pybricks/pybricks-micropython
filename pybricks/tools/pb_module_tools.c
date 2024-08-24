pybricks/pybricks-micropython

commit 5a0c7d5944028730ad8110bb6f1b134185bfdcc9

pybricks/tools/pb_module_tools: remove gc_collect() from run loop
gc_collect() can take a long time to run (milliseconds) which means
that it isn't possible to get smaller loop times than that.

This was here in the first place to help prevent fragmentation, but
there are alternative solutions, like setting gc.threshold() or manually
running gc.collect() in an application that can handle the delay.

pybricks/tools/pb_module_tools.c
@@ -239,8 +239,6 @@ static mp_obj_t pb_module_tools_run_task(size_t n_args, const mp_obj_t *pos_args
 
         while (mp_iternext(iterable) != MP_OBJ_STOP_ITERATION) {
 
-            gc_collect();
-
             if (loop_time == 0) {
                 continue;
             }

commit a017bae12586535eb6b8b80495fb9fc55dc89802

pybricks.tools.run_task: call MICROPY_VM_HOOK_LOOP
Since removing the gc_collect() from the run_task() loop, when setting
loop_time_ms to 0, it was possible that the contiki events were never
getting handled, causing random crashes due to watchdog timeout or
UART device failure.

This adds MICROPY_VM_HOOK_LOOP in the loop to ensure that background
events are always being handled even when running in a tight loop.

Fixes: 5a0c7d594 ("pybricks/tools/pb_module_tools: remove gc_collect() from run loop")

pybricks/tools/pb_module_tools.c
@@ -238,6 +238,7 @@ static mp_obj_t pb_module_tools_run_task(size_t n_args, const mp_obj_t *pos_args
     if (nlr_push(&nlr) == 0) {
 
         while (mp_iternext(iterable) != MP_OBJ_STOP_ITERATION) {
+            MICROPY_VM_HOOK_LOOP
 
             if (loop_time == 0) {
                 continue;