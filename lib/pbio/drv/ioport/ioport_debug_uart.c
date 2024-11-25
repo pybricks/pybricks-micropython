e88360abe5a4107f151f47cecf50bd07020c925b
Laurens Valk <laurens@pybricks.com>
2024-11-25 11:54:25 +0100

    pbio/drv/ioport_debug_uart: Follow uart updates.
    This can be used to debug non-blocking protothreads like the Bluetooth driver. We need to explicitly call back to the relevant process since the uart drivers are no longer broadcasting to every process.

--- lib/pbio/drv/ioport/ioport_debug_uart.c
+++ lib/pbio/drv/ioport/ioport_debug_uart.c
@@ -5,8 +5,6 @@
 
 #if PBDRV_CONFIG_IOPORT_DEBUG_UART
 
-#error "This driver can currently not be used."
-
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
@@ -22,16 +20,13 @@
 #error "PBDRV_CONFIG_IOPORT_DEBUG_UART requires exactly one port"
 #endif
 
-static char buffer[64];
-static pbdrv_uart_dev_t *debug_uart;
+// Use globals for simplified debug macro inside protothreads.
+struct pt debug_printf_thread;
+char debug_buffer[64];
+pbdrv_uart_dev_t *debug_uart;
+pbio_error_t debug_err;
 
-#define UART_TIMEOUT (250)
-
-static pbio_error_t pbdrv_ioport_init(void) {
-    // UART already initialized.
-    if (debug_uart) {
-        return PBIO_SUCCESS;
-    }
+pbio_error_t pbdrv_ioport_debug_uart_init(pbdrv_uart_poll_callback_t callback) {
 
     // Get the debug uart from last port.
     const pbdrv_ioport_pup_port_platform_data_t *port_data = &pbdrv_ioport_pup_platform_data.ports[PBDRV_CONFIG_IOPORT_NUM_DEV - 1];
@@ -40,6 +35,8 @@ static pbio_error_t pbdrv_ioport_init(void) {
         debug_uart = NULL;
         return err;
     }
+
+    pbdrv_uart_set_poll_callback(debug_uart, callback);
 
     // Enable and configure uart.
     const pbdrv_ioport_pup_pins_t *pins = &port_data->pins;
@@ -52,60 +49,11 @@ static pbio_error_t pbdrv_ioport_init(void) {
     return PBIO_SUCCESS;
 }
 
-pbio_error_t pbdrv_ioport_debug_uart_printf(const char *format, ...) {
-
-    // Get the debug uart.
-    pbio_error_t err = pbdrv_ioport_init();
-    if (err != PBIO_SUCCESS) {
-        return err;
-    }
-
-    // If still writing, just ignore this debug message.
-    err = pbdrv_uart_write_end(debug_uart);
-    if (err != PBIO_SUCCESS) {
-        return err;
-    }
-
-    // Buffer result.
+void pbdrv_ioport_debug_uart_printf_buffer(const char *format, ...) {
     va_list args;
     va_start(args, format);
-    vsnprintf(buffer, sizeof(buffer), format, args);
+    vsnprintf(debug_buffer, sizeof(debug_buffer), format, args);
     va_end(args);
-
-    // Write to uart.
-    return pbdrv_uart_write_begin(debug_uart, (uint8_t *)buffer, strlen(buffer), UART_TIMEOUT);
-}
-
-// Protothread set up here for simplified debug macro inside protothreads.
-struct pt printf_thread;
-
-PT_THREAD(pbdrv_ioport_debug_uart_printf_thread(struct pt *pt, const char *format, ...)) {
-    static pbio_error_t err;
-
-    PT_BEGIN(pt);
-
-    // Only print if port initialized.
-    err = pbdrv_ioport_init();
-    if (err != PBIO_SUCCESS) {
-        PT_EXIT(pt);
-    }
-
-    // Buffer result.
-    va_list args;
-    va_start(args, format);
-    vsnprintf(buffer, sizeof(buffer), format, args);
-    va_end(args);
-
-    // Wait for uart to be ready then write.
-    PT_WAIT_UNTIL((pt), (err = pbdrv_uart_write_begin(debug_uart, (uint8_t *)buffer, strlen(buffer), UART_TIMEOUT)) != PBIO_ERROR_AGAIN);
-    if (err != PBIO_SUCCESS) {
-        PT_EXIT(pt);
-    }
-
-    // Wait for uart to finish.
-    PT_WAIT_UNTIL((pt), (err = pbdrv_uart_write_end(debug_uart)) != PBIO_ERROR_AGAIN);
-
-    PT_END(pt);
 }
 
 #endif // PBDRV_CONFIG_IOPORT_DEBUG_UART
