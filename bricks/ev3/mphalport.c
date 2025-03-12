0564797a88b5f48a5468cd0d2d48bd0188000896
Laurens Valk <laurens@pybricks.com>
2025-03-12 12:05:23 +0100

    bricks/ev3/mphalport: Duplicate to tty for simpler debugging.
    Also works with mpremote if adding a slight delay before disconnecting.

--- bricks/ev3/mphalport.c
+++ bricks/ev3/mphalport.c
@@ -61,21 +61,42 @@ void mp_hal_delay_ms(mp_uint_t Delay) {
     } while (pbdrv_clock_get_ms() - start < Delay);
 }
 
+extern uint32_t pbdrv_usb_rx_data_available(void);
+
+extern int32_t pbdrv_usb_get_char(void);
+
 uintptr_t mp_hal_stdio_poll(uintptr_t poll_flags) {
-    return 0;
+
+    uintptr_t ret = 0;
+
+    if ((poll_flags & MP_STREAM_POLL_RD) && pbdrv_usb_rx_data_available() > 0) {
+        ret |= MP_STREAM_POLL_RD;
+    }
+
+    return ret;
 }
 
 int mp_hal_stdin_rx_chr(void) {
     int c;
-    while ((c = pbdrv_uart_debug_get_char()) == -1) {
+    while (((c = pbdrv_uart_debug_get_char()) == -1) && (c = pbdrv_usb_get_char()) == -1) {
         MICROPY_EVENT_POLL_HOOK;
     }
     return c;
 }
 
+extern uint32_t pbdrv_usb_write(const uint8_t *data, uint32_t size);
+
 void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
     pbdrv_uart_debug_printf("%.*s", len, str);
+    uint32_t done = 0;
+    while (done < len) {
+        done += pbdrv_usb_write((uint8_t *)str + done, len - done);
+        MICROPY_EVENT_POLL_HOOK;
+    }
 }
 
+extern void pbdrv_usb_tx_flush(void);
+
 void mp_hal_stdout_tx_flush(void) {
+    pbdrv_usb_tx_flush();
 }
