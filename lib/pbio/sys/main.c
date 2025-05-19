987468d276ada0ebaadad74c9fe4c9b2f7e4472b
grantimaharafoundation <michael@grantimaharafoundation.org>
2025-05-19 14:35:42 -0700

    Auto start program

--- lib/pbio/sys/main.c
+++ lib/pbio/sys/main.c
@@ -9,6 +9,7 @@
 
 #include <pbdrv/reset.h>
 #include <pbdrv/usb.h>
+#include <pbdrv/bluetooth.h>
 #include <pbio/main.h>
 #include <pbio/protocol.h>
 #include <pbsys/core.h>
@@ -84,6 +85,17 @@ int main(int argc, char **argv) {
 
     pbio_init();
     pbsys_init();
+
+    // Automatically start single button hubs
+    #if !PBSYS_CONFIG_USER_PROGRAM_AUTO_START
+    // Ensure the Bluetooth driver is fully ready before requesting the program start.
+    // Otherwise, the program will run briefly and then stop
+    // pbsys_init() starts Bluetooth initialization, but might not wait for it to be complete.
+    while (!pbdrv_bluetooth_is_ready()) {
+        pbio_do_one_event();
+    }
+    pbsys_main_program_request_start(PBIO_PYBRICKS_USER_PROGRAM_ID_FIRST_SLOT, PBSYS_MAIN_PROGRAM_START_REQUEST_TYPE_BOOT);
+    #endif
 
     // Keep loading and running user programs until shutdown is requested.
     while (!pbsys_status_test(PBIO_PYBRICKS_STATUS_SHUTDOWN_REQUEST)) {
