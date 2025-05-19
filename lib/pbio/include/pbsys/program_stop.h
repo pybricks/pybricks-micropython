e3f2cd333dc7cfb69a607e32401153ea08de66d3
grantimaharafoundation <michael@grantimaharafoundation.org>
2025-05-19 14:50:49 -0700

    Rework power button behavior on hubs without bluetooth button.
    - Old behavior:
      - Program does not auto start on boot
      - Program running: short press stops program, long press shuts down hub
      - Programming not running: short press tries to start program, long press shuts down hub
    
    - New behavior:
      - Program tries to auto start on boot
      - Program running: short press shuts down hub, long press stops program
      - Program not running: short press tries to start program, long press shuts down hub

--- lib/pbio/include/pbsys/program_stop.h
+++ lib/pbio/include/pbsys/program_stop.h
@@ -17,6 +17,7 @@
 
 #if PBSYS_CONFIG_PROGRAM_STOP
 
+void pbsys_program_stop(bool force_stop);
 pbio_button_flags_t pbsys_program_stop_get_buttons(void);
 void pbsys_program_stop_set_buttons(pbio_button_flags_t buttons);
 
