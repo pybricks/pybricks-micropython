f825897b0a233bc91d869f22bb00f260f0d0930b
Laurens Valk <laurens@pybricks.com>
2025-07-04 11:58:22 +0200

    !deleteme: Add placeholder program that reads/writes persistent data.
    This will let us test SPI read and write in full, even though program download is not yet implemented.

--- lib/pbio/sys/main.c
+++ lib/pbio/sys/main.c
@@ -92,7 +92,7 @@ int main(int argc, char **argv) {
     while (!pbsys_status_test(PBIO_PYBRICKS_STATUS_SHUTDOWN_REQUEST)) {
 
         #if PBSYS_CONFIG_USER_PROGRAM_AUTO_START
-        pbsys_main_program_request_start(PBIO_PYBRICKS_USER_PROGRAM_ID_REPL, PBSYS_MAIN_PROGRAM_START_REQUEST_TYPE_BOOT);
+        pbsys_main_program_request_start(PBIO_PYBRICKS_USER_PROGRAM_ID_FIRST_SLOT, PBSYS_MAIN_PROGRAM_START_REQUEST_TYPE_BOOT);
         #endif
 
         // Drives all processes while we wait for user input.
