9b18eb43db41ab6cfc1e01bb2dae710e08e0c70e
R <rqou@berkeley.edu>
2025-07-08 13:36:53 +0200

    pbio/drv/block_device/block_device_ev3: Implement skeleton for EV3 flash driver.
    This is derived from the W25Qxx_STM32 driver, but with most of the logic removed
    to make room for the EV3 implementation.

---
+++ lib/pbio/drv/block_device/block_device_ev3.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 The Pybricks Authors
+
+#ifndef _INTERNAL_PBDRV_BLOCK_DEVICE_EV3_H_
+#define _INTERNAL_PBDRV_BLOCK_DEVICE_EV3_H_
+
+#include <stdint.h>
+
+void pbdrv_block_device_ev3_spi_tx_complete();
+void pbdrv_block_device_ev3_spi_rx_complete();
+
+#endif // _INTERNAL_PBDRV_BLOCK_DEVICE_EV3_H_
