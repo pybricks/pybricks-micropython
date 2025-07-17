426bde8cf37155b6eab7b4b1526f7117b4d2409e
Laurens Valk <laurens@pybricks.com>
2025-07-17 14:06:36 +0200

    pbio/drv/button_resistor_ladder: Wait until ready.
    Higher level UI code becomes cleaner if buttons just work. If buttons aren't working, we shouldn't be booting.

--- lib/pbio/drv/button/button_resistor_ladder.c
+++ lib/pbio/drv/button/button_resistor_ladder.c
@@ -7,11 +7,45 @@
 
 #if PBDRV_CONFIG_BUTTON_RESISTOR_LADDER
 
+#include <stddef.h>
+
 #include <pbdrv/resistor_ladder.h>
 #include <pbio/button.h>
 #include <pbio/error.h>
+#include <pbio/os.h>
+
+#include "../core.h"
+
+static pbio_os_process_t pbdrv_button_init_process;
+
+/**
+ * Wait for the buttons to get ready before giving control to system code.
+ *
+ * The resistor ladders give unknown values for 10s of milliseconds after boot.
+ *
+ * This ensures that system level functions such as the UI don't need hacks to
+ * wait for buttons to start working.
+ */
+pbio_error_t pbdrv_button_init_process_thread(pbio_os_state_t *state, void *context) {
+
+    static pbio_os_timer_t timer;
+    pbdrv_resistor_ladder_ch_flags_t flags;
+
+    PBIO_OS_ASYNC_BEGIN(state);
+
+    while (pbdrv_resistor_ladder_get(0, &flags) != PBIO_SUCCESS || pbdrv_resistor_ladder_get(1, &flags) != PBIO_SUCCESS) {
+        PBIO_OS_AWAIT_MS(state, &timer, 10);
+    }
+    PBIO_OS_AWAIT_MS(state, &timer, 30);
+
+    pbdrv_init_busy_down();
+
+    PBIO_OS_ASYNC_END(PBIO_SUCCESS);
+}
 
 void pbdrv_button_init(void) {
+    pbdrv_init_busy_up();
+    pbio_os_process_start(&pbdrv_button_init_process, pbdrv_button_init_process_thread, NULL);
 }
 
 pbio_button_flags_t pbdrv_button_get_pressed(void) {
