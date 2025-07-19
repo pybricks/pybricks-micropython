pybricks/pybricks-micropython

commit ce4253aec5cc986ec6b6dac4df10dcdd8b1c49a6

pbio/sys: Move block device read to driver level.
This moves the platform-specific checksum and saving logic to the driver level.

This is a better fit generally, and it is needed for EV3 where the flash shares
the SPI bus with the ADC driver. When both reside at the pbdrv level, they can
run sequentially without holding up other drivers.

This also renames the public functions to more accurately represent what they
do. This can only be used to save the disk in one go, so we can't call them
generic storage functions.

Fixes https://github.com/pybricks/support/issues/2264

lib/pbio/drv/block_device/block_device_ev3.c
@@ -32,6 +32,34 @@
 #include <pbio/error.h>
 #include <pbio/int_math.h>
 
+static pbio_os_process_t pbdrv_block_device_ev3_init_process;
+
+static struct {
+    /**
+     * How much data to write on shutdown and load on the next boot. Includes
+     * the size of this field, because it is also saved.
+     */
+    uint32_t saved_size;
+    /**
+     * A copy of the data loaded from flash and application heap. The first
+     * portion of this, up to pbdrv_block_device_get_writable_size() bytes,
+     * gets saved to flash at shutdown.
+     */
+    uint8_t data[PBDRV_CONFIG_BLOCK_DEVICE_RAM_SIZE];
+} ramdisk __attribute__((section(".noinit"), used));
+
+uint32_t pbdrv_block_device_get_writable_size(void) {
+    return PBDRV_CONFIG_BLOCK_DEVICE_EV3_SIZE - sizeof(ramdisk.saved_size);
+}
+
+pbio_error_t pbdrv_block_device_get_data(uint8_t **data) {
+    *data = ramdisk.data;
+
+    // Higher level code can use the ramdisk data if initialization completed
+    // successfully. Otherwise it should reset to factory default data.
+    return pbdrv_block_device_ev3_init_process.err;
+}
+
 /**
  * SPI bus state.
  */
@@ -522,7 +550,7 @@ static uint8_t write_address[4] = {FLASH_CMD_WRITE_DATA};
 // Request sector erase at address. Buffer: erase command + address.
 static uint8_t erase_address[4] = {FLASH_CMD_ERASE_BLOCK};
 
-pbio_error_t pbdrv_block_device_read(pbio_os_state_t *state, uint32_t offset, uint8_t *buffer, uint32_t size) {
+static pbio_error_t pbdrv_block_device_read(pbio_os_state_t *state, uint32_t offset, uint8_t *buffer, uint32_t size) {
 
     static uint32_t size_done;
     static uint32_t size_now;
@@ -573,7 +601,7 @@ static pbio_error_t flash_wait_write(pbio_os_state_t *state) {
     PBIO_OS_ASYNC_END(PBIO_SUCCESS);
 }
 
-pbio_error_t pbdrv_block_device_store(pbio_os_state_t *state, uint8_t *buffer, uint32_t size) {
+pbio_error_t pbdrv_block_device_write_all(pbio_os_state_t *state, uint32_t used_data_size) {
 
     static pbio_os_state_t sub;
     static uint32_t offset;
@@ -581,12 +609,20 @@ pbio_error_t pbdrv_block_device_store(pbio_os_state_t *state, uint8_t *buffer, u
     static uint32_t size_done;
     pbio_error_t err;
 
+    // We're going to write the used portion of the ramdisk to flash. Includes
+    // the size field itself, so add it to the total write size.
+    uint8_t *buffer = (void *)&ramdisk;
+    uint32_t size = used_data_size + sizeof(ramdisk.saved_size);
+
     PBIO_OS_ASYNC_BEGIN(state);
 
     // Exit on invalid size.
     if (size == 0 || size > PBDRV_CONFIG_BLOCK_DEVICE_EV3_SIZE) {
         return PBIO_ERROR_INVALID_ARG;
     }
+
+    // Store the new size so we know how much to load on next boot.
+    ramdisk.saved_size = size;
 
     #if PBDRV_CONFIG_ADC_EV3
     // HACK
@@ -648,10 +684,9 @@ pbio_error_t pbdrv_block_device_store(pbio_os_state_t *state, uint8_t *buffer, u
     PBIO_OS_ASYNC_END(PBIO_SUCCESS);
 }
 
-static pbio_os_process_t pbdrv_block_device_ev3_init_process;
-
 pbio_error_t pbdrv_block_device_ev3_init_process_thread(pbio_os_state_t *state, void *context) {
     pbio_error_t err;
+    static pbio_os_state_t sub;
 
     PBIO_OS_ASYNC_BEGIN(state);
 
@@ -667,10 +702,24 @@ pbio_error_t pbdrv_block_device_ev3_init_process_thread(pbio_os_state_t *state, 
         return PBIO_ERROR_FAILED;
     }
 
-    // Initialization done.
+    // Read size of stored data.
+    PBIO_OS_AWAIT(state, &sub, err = pbdrv_block_device_read(&sub, 0, (uint8_t *)&ramdisk.saved_size, sizeof(ramdisk.saved_size)));
+    if (err != PBIO_SUCCESS) {
+        return err;
+    }
+
+    // Read the available data into RAM.
+    PBIO_OS_AWAIT(state, &sub, err = pbdrv_block_device_read(&sub, 0, (uint8_t *)&ramdisk, ramdisk.saved_size));
+
+    // Reading may fail with PBIO_ERROR_INVALID_ARG if the size is too big.
+    // This happens when the size value was uninitialized or another firmware
+    // was used before. We still want to proceed with the boot process. The
+    // higher level code sees this error when requesting the RAM disk. On
+    // failure, it can reset the user data to factory defaults, and save it
+    // properly on shutdown.
     pbdrv_init_busy_down();
 
-    PBIO_OS_ASYNC_END(PBIO_SUCCESS);
+    PBIO_OS_ASYNC_END(err);
 }
 
 void pbdrv_block_device_init(void) {

lib/pbio/drv/block_device/block_device_flash_stm32.c
@@ -21,38 +21,103 @@
 
 #include STM32_HAL_H
 
-void pbdrv_block_device_init(void) {
-}
-
 extern uint8_t _pbdrv_block_device_storage_start[];
 
-pbio_error_t pbdrv_block_device_read(pbio_os_state_t *state, uint32_t offset, uint8_t *buffer, uint32_t size) {
+static struct {
+    /**
+     * How much data to write on shutdown and load on the next boot. Includes
+     * the size of this field, because it is also saved.
+     */
+    uint32_t saved_size;
+    /**
+     * Checksum complement to satisfy bootloader requirements. This ensures
+     * that words in the scanned area still add up to precisely 0 after user
+     * data was written. Needs to be volatile as it gets optimized out when it
+     * is never read by our code.
+     */
+    volatile uint32_t checksum_complement;
+    /**
+     * A copy of the data loaded from flash and application heap. The first
+     * portion of this, up to pbdrv_block_device_get_writable_size() bytes,
+     * gets saved to flash at shutdown.
+     */
+    uint8_t data[PBDRV_CONFIG_BLOCK_DEVICE_RAM_SIZE];
+} ramdisk __attribute__((section(".noinit"), used));
 
-    // NB: This function is called as an awaitable for compatibility with other
-    // external storage mediums. It blocks during the memcpy, but we only use
-    // this at boot.
+const uint32_t header_size = sizeof(ramdisk.saved_size) + sizeof(ramdisk.checksum_complement);
 
-    // Exit on invalid size.
-    if (size == 0 || offset + size > PBDRV_CONFIG_BLOCK_DEVICE_FLASH_STM32_SIZE) {
-        return PBIO_ERROR_INVALID_ARG;
-    }
-
-    // Copy requested data to RAM.
-    memcpy(buffer, _pbdrv_block_device_storage_start + offset, size);
-
-    return PBIO_SUCCESS;
+uint32_t pbdrv_block_device_get_writable_size(void) {
+    return PBDRV_CONFIG_BLOCK_DEVICE_FLASH_STM32_SIZE - header_size;
 }
 
-typedef union _double_word_t {
-    uint8_t data[8];
-    uint64_t dword;
-} double_word_t;
+static pbio_error_t init_err;
 
-pbio_error_t pbdrv_block_device_store(pbio_os_state_t *state, uint8_t *buffer, uint32_t size) {
+void pbdrv_block_device_init(void) {
+
+    uint32_t size;
+
+    memcpy(&size, _pbdrv_block_device_storage_start, sizeof(size));
+
+    // Exit on invalid size.
+    if (size == 0 || size > PBDRV_CONFIG_BLOCK_DEVICE_FLASH_STM32_SIZE) {
+        // This error will be retrieved when higher level code requests the
+        // ramdisk, so that it can reset data to firmware defaults.
+        init_err = PBIO_ERROR_INVALID_ARG;
+        return;
+    }
+
+    // Load requested amount of data to RAM. Also re-reads the size value.
+    memcpy(&ramdisk, _pbdrv_block_device_storage_start, size);
+}
+
+pbio_error_t pbdrv_block_device_get_data(uint8_t **data) {
+    *data = ramdisk.data;
+    return init_err;
+}
+
+// Updates checksum in data map to satisfy bootloader requirements.
+static void pbdrv_block_device_update_ramdisk_size_and_checksum(uint32_t used_data_size) {
+
+    // Total size includes used data and header.
+    ramdisk.saved_size = used_data_size + header_size;
+
+    // Align writable data by a double word, to simplify checksum
+    // computation and storage drivers that write double words.
+    while (ramdisk.saved_size % 8) {
+        *((uint8_t *)&ramdisk + ramdisk.saved_size++) = 0;
+    }
+
+    // The area scanned by the bootloader adds up to 0 when all user data
+    // is 0xFFFFFFFF. So the bootloader value up until just before the user
+    // data is always 0 + the number of words in the scanned user data.
+    extern uint32_t _pbsys_storage_checked_size;
+    uint32_t checksize = (uint32_t)&_pbsys_storage_checked_size;
+    uint32_t checksum = checksize / sizeof(uint32_t);
+
+    // Don't count existing value.
+    ramdisk.checksum_complement = 0;
+
+    // Add checksum for each word in the written data and empty checked size.
+    for (uint32_t offset = 0; offset < checksize; offset += sizeof(uint32_t)) {
+        uint32_t *word = (uint32_t *)((uint8_t *)&ramdisk + offset);
+        // Assume that everything after written data is erased by the block
+        // device driver prior to writing.
+        checksum += offset < ramdisk.saved_size ? *word : 0xFFFFFFFF;
+    }
+
+    // Set the checksum complement to cancel out user data checksum.
+    ramdisk.checksum_complement = 0xFFFFFFFF - checksum + 1;
+}
+
+pbio_error_t pbdrv_block_device_write_all(pbio_os_state_t *state, uint32_t used_data_size) {
 
     // NB: This function is called as an awaitable for compatibility with other
     // external storage mediums. This implementation is blocking, but we only
     // use it during shutdown so this is acceptable.
+
+    // Account for header size and make valid checksum.
+    pbdrv_block_device_update_ramdisk_size_and_checksum(used_data_size);
+    uint32_t size = ramdisk.saved_size;
 
     static const uint32_t base_address = (uint32_t)(&_pbdrv_block_device_storage_start[0]);
 
@@ -103,7 +168,7 @@ pbio_error_t pbdrv_block_device_store(pbio_os_state_t *state, uint8_t *buffer, u
         __disable_irq();
 
         // Write the data and re-enable interrupts.
-        hal_err = HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, base_address + done, *(uint64_t *)(buffer + done));
+        hal_err = HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, base_address + done, *(uint64_t *)((void *)&ramdisk + done));
         __set_PRIMASK(irq);
         if (hal_err != HAL_OK) {
             HAL_FLASH_Lock();
@@ -111,7 +176,7 @@ pbio_error_t pbdrv_block_device_store(pbio_os_state_t *state, uint8_t *buffer, u
         }
 
         // Update write progress.
-        done += sizeof(double_word_t);
+        done += sizeof(uint64_t);
     }
 
     // Lock flash on completion.

lib/pbio/drv/block_device/block_device_w25qxx_stm32.c
@@ -23,6 +23,34 @@
 #include <pbio/error.h>
 #include <pbio/int_math.h>
 
+static pbio_os_process_t pbdrv_block_device_w25qxx_stm32_init_process;
+
+static struct {
+    /**
+     * How much data to write on shutdown and load on the next boot. Includes
+     * the size of this field, because it is also saved.
+     */
+    uint32_t saved_size;
+    /**
+     * A copy of the data loaded from flash and application heap. The first
+     * portion of this, up to pbdrv_block_device_get_writable_size() bytes,
+     * gets saved to flash at shutdown.
+     */
+    uint8_t data[PBDRV_CONFIG_BLOCK_DEVICE_RAM_SIZE];
+} ramdisk __attribute__((section(".noinit"), used));
+
+uint32_t pbdrv_block_device_get_writable_size(void) {
+    return PBDRV_CONFIG_BLOCK_DEVICE_W25QXX_STM32_SIZE - sizeof(ramdisk.saved_size);
+}
+
+pbio_error_t pbdrv_block_device_get_data(uint8_t **data) {
+    *data = ramdisk.data;
+
+    // Higher level code can use the ramdisk data if initialization completed
+    // successfully. Otherwise it should reset to factory default data.
+    return pbdrv_block_device_w25qxx_stm32_init_process.err;
+}
+
 /**
  * SPI bus state.
  */
@@ -336,7 +364,7 @@ static spi_command_t cmd_data_read = {
     .operation = SPI_RECV,
 };
 
-pbio_error_t pbdrv_block_device_read(pbio_os_state_t *state, uint32_t offset, uint8_t *buffer, uint32_t size) {
+static pbio_error_t pbdrv_block_device_read(pbio_os_state_t *state, uint32_t offset, uint8_t *buffer, uint32_t size) {
 
     static pbio_os_state_t sub;
     static uint32_t size_done;
@@ -433,7 +461,7 @@ static pbio_error_t flash_erase_or_write(pbio_os_state_t *state, uint32_t addres
     PBIO_OS_ASYNC_END(PBIO_SUCCESS);
 }
 
-pbio_error_t pbdrv_block_device_store(pbio_os_state_t *state, uint8_t *buffer, uint32_t size) {
+pbio_error_t pbdrv_block_device_write_all(pbio_os_state_t *state, uint32_t used_data_size) {
 
     static pbio_os_state_t sub;
     static uint32_t offset;
@@ -441,12 +469,20 @@ pbio_error_t pbdrv_block_device_store(pbio_os_state_t *state, uint8_t *buffer, u
     static uint32_t size_done;
     pbio_error_t err;
 
+    // We're going to write the used portion of the ramdisk to flash. Includes
+    // the size field itself, so add it to the total write size.
+    uint8_t *buffer = (void *)&ramdisk;
+    uint32_t size = used_data_size + sizeof(ramdisk.saved_size);
+
     PBIO_OS_ASYNC_BEGIN(state);
 
     // Exit on invalid size.
     if (size == 0 || size > PBDRV_CONFIG_BLOCK_DEVICE_W25QXX_STM32_SIZE) {
         return PBIO_ERROR_INVALID_ARG;
     }
+
+    // Store the new size so we know how much to load on next boot.
+    ramdisk.saved_size = size;
 
     // Erase sector by sector.
     for (offset = 0; offset < size; offset += FLASH_SIZE_ERASE) {
@@ -470,8 +506,6 @@ pbio_error_t pbdrv_block_device_store(pbio_os_state_t *state, uint8_t *buffer, u
 
     PBIO_OS_ASYNC_END(PBIO_SUCCESS);
 }
-
-static pbio_os_process_t pbdrv_block_device_w25qxx_stm32_init_process;
 
 pbio_error_t pbdrv_block_device_w25qxx_stm32_init_process_thread(pbio_os_state_t *state, void *context) {
 
@@ -497,10 +531,24 @@ pbio_error_t pbdrv_block_device_w25qxx_stm32_init_process_thread(pbio_os_state_t
         return PBIO_ERROR_FAILED;
     }
 
-    // Initialization done.
+    // Read size of stored data.
+    PBIO_OS_AWAIT(state, &sub, err = pbdrv_block_device_read(&sub, 0, (uint8_t *)&ramdisk.saved_size, sizeof(ramdisk.saved_size)));
+    if (err != PBIO_SUCCESS) {
+        return err;
+    }
+
+    // Read the available data into RAM.
+    PBIO_OS_AWAIT(state, &sub, err = pbdrv_block_device_read(&sub, 0, (uint8_t *)&ramdisk, ramdisk.saved_size));
+
+    // Reading may fail with PBIO_ERROR_INVALID_ARG if the size is too big.
+    // This happens when the size value was uninitialized or another firmware
+    // was used before. We still want to proceed with the boot process. The
+    // higher level code sees this error when requesting the RAM disk. On
+    // failure, it can reset the user data to factory defaults, and save it
+    // properly on shutdown.
     pbdrv_init_busy_down();
 
-    PBIO_OS_ASYNC_END(PBIO_SUCCESS);
+    PBIO_OS_ASYNC_END(err);
 }
 
 void pbdrv_block_device_init(void) {

commit 502343779645ca82fe19da9cb6b99dde9cf357d4

pbio/sys/storage_data: fix strict aliasing issues
Fix strict aliasing issues between block drivers and pbsys storage.

Casting a uint8_t* to a struct pointer violates strict aliasing rules,
which can lead to hard-to-find bugs. We need to ensure that the memory
being pointed to has the same alignment, etc. as the struct it is being
cast to. The usual way to do this is to use a union of the struct and
the other type we want to use (in this case, uint8_t array).

To do this, we need to move the storage data struct definition to a
header file that can be included by both pbdrv/block_device and
pbsys/storage.

Additionally, in block_device_test, the size of the ramdisk block was
wrong and was causing the NXT to crash sometime after starting the REPL.
It also incorrectly had the .noinit section attribute, but depends on
being initialized to zero, so that is removed.

Fixes: https://github.com/pybricks/support/issues/2272
Fixes: ce4253aec ("pbio/sys: Move block device read to driver level.")

lib/pbio/drv/block_device/block_device_ev3.c
@@ -20,13 +20,6 @@
 #include <stdint.h>
 #include <string.h>
 
-#include "../core.h"
-
-#include <pbdrv/block_device.h>
-#include <pbdrv/clock.h>
-#include <pbdrv/compiler.h>
-#include <pbdrv/gpio.h>
-
 #include <tiam1808/edma.h>
 #include <tiam1808/spi.h>
 #include <tiam1808/psc.h>
@@ -36,9 +29,15 @@
 #include <tiam1808/armv5/am1808/edma_event.h>
 #include <tiam1808/armv5/am1808/interrupt.h>
 
-#include "block_device_ev3.h"
+#include "../core.h"
 #include "../drv/gpio/gpio_ev3.h"
+#include "../sys/storage_data.h"
+#include "block_device_ev3.h"
 
+#include <pbdrv/block_device.h>
+#include <pbdrv/clock.h>
+#include <pbdrv/compiler.h>
+#include <pbdrv/gpio.h>
 #include <pbio/error.h>
 #include <pbio/int_math.h>
 #include <pbio/util.h>
@@ -831,7 +830,11 @@ static struct {
      * portion of this, up to pbdrv_block_device_get_writable_size() bytes,
      * gets saved to flash at shutdown.
      */
-    uint8_t data[PBDRV_CONFIG_BLOCK_DEVICE_RAM_SIZE];
+    union {
+        // ensure that data is properly aligned for pbsys_storage_data_map_t
+        pbsys_storage_data_map_t data_map;
+        uint8_t data[PBDRV_CONFIG_BLOCK_DEVICE_RAM_SIZE];
+    };
 } ramdisk __attribute__((section(".noinit"), used));
 
 uint32_t pbdrv_block_device_get_writable_size(void) {
@@ -840,8 +843,8 @@ uint32_t pbdrv_block_device_get_writable_size(void) {
 
 static pbio_error_t pbdrv_block_device_load_err = PBIO_ERROR_FAILED;
 
-pbio_error_t pbdrv_block_device_get_data(uint8_t **data) {
-    *data = ramdisk.data;
+pbio_error_t pbdrv_block_device_get_data(pbsys_storage_data_map_t **data) {
+    *data = &ramdisk.data_map;
 
     // Higher level code can use the ramdisk data if initialization completed
     // successfully. Otherwise it should reset to factory default data.

lib/pbio/drv/block_device/block_device_flash_stm32.c
@@ -10,9 +10,8 @@
 #include <stdint.h>
 #include <string.h>
 
-#include <contiki.h>
-
 #include "../core.h"
+#include "../sys/storage_data.h"
 
 #include <pbdrv/block_device.h>
 
@@ -41,7 +40,11 @@ static struct {
      * portion of this, up to pbdrv_block_device_get_writable_size() bytes,
      * gets saved to flash at shutdown.
      */
-    uint8_t data[PBDRV_CONFIG_BLOCK_DEVICE_RAM_SIZE];
+    union {
+        // ensure that data is properly aligned for pbsys_storage_data_map_t
+        pbsys_storage_data_map_t data_map;
+        uint8_t data[PBDRV_CONFIG_BLOCK_DEVICE_RAM_SIZE];
+    };
 } ramdisk __attribute__((section(".noinit"), used));
 
 const uint32_t header_size = sizeof(ramdisk.saved_size) + sizeof(ramdisk.checksum_complement);
@@ -70,8 +73,8 @@ void pbdrv_block_device_init(void) {
     memcpy(&ramdisk, _pbdrv_block_device_storage_start, size);
 }
 
-pbio_error_t pbdrv_block_device_get_data(uint8_t **data) {
-    *data = ramdisk.data;
+pbio_error_t pbdrv_block_device_get_data(pbsys_storage_data_map_t **data) {
+    *data = &ramdisk.data_map;
     return init_err;
 }
 

lib/pbio/drv/block_device/block_device_w25qxx_stm32.c
@@ -13,9 +13,8 @@
 
 #include STM32_HAL_H
 
-#include <contiki.h>
-
 #include "../core.h"
+#include "../sys/storage_data.h"
 #include "block_device_w25qxx_stm32.h"
 
 #include <pbdrv/block_device.h>
@@ -36,15 +35,19 @@ static struct {
      * portion of this, up to pbdrv_block_device_get_writable_size() bytes,
      * gets saved to flash at shutdown.
      */
-    uint8_t data[PBDRV_CONFIG_BLOCK_DEVICE_RAM_SIZE];
+    union {
+        // ensure that data is properly aligned for pbsys_storage_data_map_t
+        pbsys_storage_data_map_t data_map;
+        uint8_t data[PBDRV_CONFIG_BLOCK_DEVICE_RAM_SIZE];
+    };
 } ramdisk __attribute__((section(".noinit"), used));
 
 uint32_t pbdrv_block_device_get_writable_size(void) {
     return PBDRV_CONFIG_BLOCK_DEVICE_W25QXX_STM32_SIZE - sizeof(ramdisk.saved_size);
 }
 
-pbio_error_t pbdrv_block_device_get_data(uint8_t **data) {
-    *data = ramdisk.data;
+pbio_error_t pbdrv_block_device_get_data(pbsys_storage_data_map_t **data) {
+    *data = &ramdisk.data_map;
 
     // Higher level code can use the ramdisk data if initialization completed
     // successfully. Otherwise it should reset to factory default data.

lib/pbio/include/pbdrv/block_device.h
@@ -11,9 +11,10 @@
 
 #include <stdint.h>
 
+#include "../sys/storage_data.h"
+
 #include <pbdrv/config.h>
 #include <pbio/error.h>
-
 #include <pbio/os.h>
 
 #if PBDRV_CONFIG_BLOCK_DEVICE
@@ -28,7 +29,7 @@
  *                      ::PBIO_ERROR_IO (driver-specific error), though boot
  *                        does not proceed if this happens.
  */
-pbio_error_t pbdrv_block_device_get_data(uint8_t **data);
+pbio_error_t pbdrv_block_device_get_data(pbsys_storage_data_map_t **data);
 
 /**
  * Writes the "RAM Disk" to storage. May erase entire disk prior to writing.
@@ -57,7 +58,7 @@ uint32_t pbdrv_block_device_get_writable_size(void);
 
 #else
 
-static inline pbio_error_t pbdrv_block_device_get_data(uint8_t **data) {
+static inline pbio_error_t pbdrv_block_device_get_data(pbsys_storage_data_map_t **data) {
     return PBIO_ERROR_NOT_SUPPORTED;
 }
 

lib/pbio/platform/nxt/pbdrvconfig.h
@@ -11,8 +11,6 @@
 #define PBDRV_CONFIG_BLOCK_DEVICE                   (1)
 #define PBDRV_CONFIG_BLOCK_DEVICE_RAM_SIZE          (10 * 1024)
 #define PBDRV_CONFIG_BLOCK_DEVICE_TEST              (1)
-#define PBDRV_CONFIG_BLOCK_DEVICE_TEST_SIZE         (8 * 1024)
-#define PBDRV_CONFIG_BLOCK_DEVICE_TEST_SIZE_USER    (512)
 
 #define PBDRV_CONFIG_BUTTON                         (1)
 #define PBDRV_CONFIG_BUTTON_NXT                     (1)

lib/pbio/platform/nxt/pbsysconfig.h
@@ -11,7 +11,7 @@
 #define PBSYS_CONFIG_MAIN                           (1)
 #define PBSYS_CONFIG_STORAGE                        (1)
 #define PBSYS_CONFIG_STORAGE_NUM_SLOTS              (1)
-#define PBSYS_CONFIG_STORAGE_USER_DATA_SIZE         (PBDRV_CONFIG_BLOCK_DEVICE_TEST_SIZE_USER)
+#define PBSYS_CONFIG_STORAGE_USER_DATA_SIZE         (512)
 #define PBSYS_CONFIG_STATUS_LIGHT                   (0)
 #define PBSYS_CONFIG_STATUS_LIGHT_BATTERY           (0)
 #define PBSYS_CONFIG_STATUS_LIGHT_BLUETOOTH         (0)

lib/pbio/sys/storage.c
@@ -23,60 +23,9 @@
 #include "hmi.h"
 
 /**
- * Information about one code slot.
- *
- * A size of 0 means that this slot is not used. The offset indicates where
- * the program is stored in user storage.
- *
- * Code slots are *not* stored chronologically by slot id. Instead they are
- * stored consecutively as they are received, with the newest program last.
- *
- * If a slot is already in use and a new program should be loaded into it,
- * it is deleted by mem-moving any subsequent programs into its place, and
- * appending the new program to be last again. Since a user is typically only
- * iterating code in one slot, this is therefore usually the last stored
- * program. This means memmoves happen very little, only when needed.
- *
- */
-typedef struct {
-    uint32_t offset;
-    uint32_t size;
-} pbsys_storage_slot_info_t;
-
-/**
  * Slot at which incoming program data is currently being received.
  */
 static uint8_t incoming_slot = 0;
-
-/**
- * Map of loaded data. All data types are little-endian.
- */
-typedef struct {
-    /**
-     * End-user read-write accessible data. Everything after this is also
-     * user-readable but not writable.
-     */
-    uint8_t user_data[PBSYS_CONFIG_STORAGE_USER_DATA_SIZE];
-    /**
-     * First 8 symbols of the git hash of the firmware version used to create
-     * this data map. If this does not match the version of the running
-     * firmware, user data will be reset to 0.
-     */
-    char stored_firmware_hash[8];
-    /**
-     * System settings. Settings will be reset to defaults when the firmware
-     * version changes due to an update.
-     */
-    pbsys_storage_settings_t settings;
-    /**
-     * Size and offset info for each slot.
-     */
-    pbsys_storage_slot_info_t slot_info[PBSYS_CONFIG_STORAGE_NUM_SLOTS];
-    /**
-     * Data of the application program (code + heap).
-     */
-    uint8_t program_data[] __attribute__((aligned(sizeof(void *))));
-} pbsys_storage_data_map_t;
 
 /**
  * Map of loaded data. This is kept in memory between successive runs of the
@@ -399,7 +348,7 @@ static pbio_error_t pbsys_storage_deinit_process_thread(pbio_os_state_t *state, 
  */
 void pbsys_storage_init(void) {
 
-    pbio_error_t err = pbdrv_block_device_get_data((uint8_t **)&map);
+    pbio_error_t err = pbdrv_block_device_get_data(&map);
 
     // Test that storage successfully loaded and matches current firmware,
     // otherwise reset storage.

lib/pbio/sys/storage_data.h
@@ -1,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 The Pybricks Authors
+
+// Data structures for non-volatile storage.
+
+// NB: Normally, pbsys code isn't referenced by drivers, however this is a
+// special case where the block device driver needs to have the actual struct
+// definition in order to ensure correct memory layout and alignment.
+
+#ifndef _PBSYS_SYS_STORAGE_DATA_H_
+#define _PBSYS_SYS_STORAGE_DATA_H_
+
+#include <stdint.h>
+
+#include <pbsys/config.h>
+#include <pbsys/storage_settings.h>
+
+/**
+ * Information about one code slot.
+ *
+ * A size of 0 means that this slot is not used. The offset indicates where
+ * the program is stored in user storage.
+ *
+ * Code slots are *not* stored chronologically by slot id. Instead they are
+ * stored consecutively as they are received, with the newest program last.
+ *
+ * If a slot is already in use and a new program should be loaded into it,
+ * it is deleted by mem-moving any subsequent programs into its place, and
+ * appending the new program to be last again. Since a user is typically only
+ * iterating code in one slot, this is therefore usually the last stored
+ * program. This means memmoves happen very little, only when needed.
+ *
+ */
+typedef struct {
+    uint32_t offset;
+    uint32_t size;
+} pbsys_storage_slot_info_t;
+
+/**
+ * Map of loaded data. All data types are little-endian.
+ */
+typedef struct {
+    /**
+     * End-user read-write accessible data. Everything after this is also
+     * user-readable but not writable.
+     */
+    uint8_t user_data[PBSYS_CONFIG_STORAGE_USER_DATA_SIZE];
+    /**
+     * First 8 symbols of the git hash of the firmware version used to create
+     * this data map. If this does not match the version of the running
+     * firmware, user data will be reset to 0.
+     */
+    char stored_firmware_hash[8];
+    /**
+     * System settings. Settings will be reset to defaults when the firmware
+     * version changes due to an update.
+     */
+    pbsys_storage_settings_t settings;
+    /**
+     * Size and offset info for each slot.
+     */
+    pbsys_storage_slot_info_t slot_info[PBSYS_CONFIG_STORAGE_NUM_SLOTS];
+    /**
+     * Data of the application program (code + heap).
+     */
+    uint8_t program_data[] __attribute__((aligned(sizeof(void *))));
+} pbsys_storage_data_map_t;
+
+#endif // _PBSYS_SYS_STORAGE_DATA_H_

lib/pbio/drv/block_device/block_device_test.c
@@ -10,8 +10,9 @@
 #include <stdint.h>
 #include <string.h>
 
-#include <pbdrv/block_device.h>
+#include "../sys/storage_data.h"
 
+#include <pbio/os.h>
 #include <pbio/version.h>
 
 /**
@@ -59,23 +60,20 @@ static const uint8_t _program_data[] = {
 // version at the right place. FIXME: Move the git version to pybricks build
 // system, since it isn't actually the micropython git version.
 #include "genhdr/mpversion.h"
-#include <pbsys/storage.h>
+
 
 static struct {
-    uint8_t user_data[PBSYS_CONFIG_STORAGE_USER_DATA_SIZE];
-    char stored_firmware_hash[8];
-    pbsys_storage_settings_t settings;
-    uint32_t program_offset;
-    uint32_t program_size;
-    uint8_t program_data[sizeof(_program_data)];
-} ramdisk __attribute__((section(".noinit"), used)) = { 0 };
+    // ensure that data is properly aligned for pbsys_storage_data_map_t
+    pbsys_storage_data_map_t data_map;
+    uint8_t data[PBDRV_CONFIG_BLOCK_DEVICE_RAM_SIZE];
+} ramdisk;
 
 uint32_t pbdrv_block_device_get_writable_size(void) {
     return 0;
 }
 
-pbio_error_t pbdrv_block_device_get_data(uint8_t **data) {
-    *data = (void *)&ramdisk;
+pbio_error_t pbdrv_block_device_get_data(pbsys_storage_data_map_t **data) {
+    *data = &ramdisk.data_map;
 
     // Higher level code can use the ramdisk data if initialization completed
     // successfully. Otherwise it should reset to factory default data.
@@ -83,9 +81,9 @@ pbio_error_t pbdrv_block_device_get_data(uint8_t **data) {
 }
 
 void pbdrv_block_device_init(void) {
-    ramdisk.program_size = sizeof(_program_data);
-    memcpy(&ramdisk.stored_firmware_hash[0], MICROPY_GIT_HASH, sizeof(ramdisk.stored_firmware_hash));
-    memcpy(&ramdisk.program_data[0], _program_data, sizeof(_program_data));
+    ramdisk.data_map.slot_info[0].size = sizeof(_program_data);
+    memcpy(ramdisk.data_map.stored_firmware_hash, MICROPY_GIT_HASH, sizeof(ramdisk.data_map.stored_firmware_hash));
+    memcpy(ramdisk.data_map.program_data, _program_data, sizeof(_program_data));
 }
 
 // Don't store any data in this implementation.