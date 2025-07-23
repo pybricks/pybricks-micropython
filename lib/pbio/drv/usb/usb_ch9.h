709dff897c61e73f10935ac99e25b64f7df36947
R <rqou@berkeley.edu>
2025-07-23 18:27:59 +0100

    pbio/drv/usb/usb_ch9.h: Add WebUSB/Microsoft descriptors
    Although these are not standard USB descriptors, Pybricks does require them,
    and they logically fit in with the rest of the descriptor definitions in this file.

--- lib/pbio/drv/usb/usb_ch9.h
+++ lib/pbio/drv/usb/usb_ch9.h
@@ -80,6 +80,12 @@ PBDRV_USB_TYPE_PUNNING_HELPER(pbdrv_usb_setup_packet)
 #define DESC_TYPE_DEVICE_QUALIFIER              6
 #define DESC_TYPE_OTHER_SPEED_CONFIGURATION     7
 #define DESC_TYPE_INTERFACE_POWER               8
+// Binary Object Store, originally from the Wireless USB / USB 3.0 spec
+// These descriptors have been backported to USB 2 and are available to
+// devices which indicate a bcdDevice of at least 2.1.
+// They are used for WebUSB and Microsoft WinUSB driver installation.
+#define DESC_TYPE_BOS                           15
+#define DESC_TYPE_DEVICE_CAPABILITY             16
 
 // Device descriptor
 typedef struct PBDRV_PACKED {
@@ -159,5 +165,126 @@ typedef struct PBDRV_PACKED {
     uint8_t bReserved;
 } pbdrv_usb_dev_qualifier_desc_t;
 PBDRV_USB_TYPE_PUNNING_HELPER(pbdrv_usb_dev_qualifier_desc);
+
+// Binary Object Store
+typedef struct PBDRV_PACKED {
+    uint8_t bLength;
+    uint8_t bDescriptorType;
+    uint16_t wTotalLength;
+    uint8_t bNumDeviceCaps;
+} pbdrv_usb_bos_desc_t;
+
+typedef struct PBDRV_PACKED {
+    uint32_t u0;
+    uint16_t u1;
+    uint16_t u2;
+    uint8_t u3[8];
+} pbdrv_usb_uuid_t;
+
+// Platform-specific capabilities
+#define USB_DEVICE_CAPABILITY_TYPE_PLATFORM     5
+
+typedef struct PBDRV_PACKED {
+    uint8_t bLength;
+    uint8_t bDescriptorType;
+    uint8_t bDevCapabilityType;
+    uint8_t bReserved;
+    pbdrv_usb_uuid_t uuid;
+} pbdrv_usb_platform_caps_common_t;
+
+typedef struct PBDRV_PACKED {
+    pbdrv_usb_platform_caps_common_t hdr;
+    uint16_t bcdVersion;
+    uint8_t bVendorCode;
+    uint8_t iLandingPage;
+} pbdrv_usb_webusb_capability_t;
+
+// {3408b638-09a9-47a0-8bfd-a0768815b665}
+#define USB_PLATFORM_CAP_GUID_WEBUSB    {                           \
+        .u0 = 0x3408b638,                                           \
+        .u1 = 0x09a9,                                               \
+        .u2 = 0x47a0,                                               \
+        .u3 = {0x8b, 0xfd, 0xa0, 0x76, 0x88, 0x15, 0xb6, 0x65},     \
+}
+
+typedef struct PBDRV_PACKED {
+    pbdrv_usb_platform_caps_common_t hdr;
+    uint32_t dwWindowsVersion;
+    uint16_t wMSOSDescriptorSetTotalLength;
+    uint8_t bMS_VendorCode;
+    uint8_t bAltEnumCode;
+} pbdrv_usb_microsoft_20_capability_t;
+
+// {D8DD60DF-4589-4CC7-9CD2-659D9E648A9F}
+#define USB_PLATFORM_CAP_GUID_MS_20     {                           \
+        .u0 = 0xD8DD60DF,                                           \
+        .u1 = 0x4589,                                               \
+        .u2 = 0x4CC7,                                               \
+        .u3 = {0x9C, 0xD2, 0x65, 0x9D, 0x9E, 0x64, 0x8A, 0x9F},     \
+}
+
+// These are not technically part of USB Chapter 9, but
+// we need them and they logically fit with the rest of the structs.
+
+#define WEBUSB_REQ_GET_URL          2
+#define WEBUSB_DESC_TYPE_URL        3
+#define WEBUSB_URL_SCHEME_HTTP      0
+#define WEBUSB_URL_SCHEME_HTTPS     1
+
+typedef struct PBDRV_PACKED {
+    uint8_t bLength;
+    uint8_t bDescriptorType;
+    uint8_t bScheme;
+    uint8_t url[];
+} pbdrv_usb_webusb_url_desc_t;
+PBDRV_USB_TYPE_PUNNING_HELPER(pbdrv_usb_webusb_url_desc);
+
+// Microsoft wIndex values
+#define MS_OS_20_DESCRIPTOR_INDEX       7
+#define MS_OS_20_SET_ALT_ENUMERATION    8
+
+// Microsoft descriptor types
+#define MS_OS_20_SET_HEADER_DESCRIPTOR          0
+#define MS_OS_20_SUBSET_HEADER_CONFIGURATION    1
+#define MS_OS_20_SUBSET_HEADER_FUNCTION         2
+#define MS_OS_20_FEATURE_COMPATBLE_ID           3
+#define MS_OS_20_FEATURE_REG_PROPERTY           4
+#define MS_OS_20_FEATURE_MIN_RESUME_TIME        5
+#define MS_OS_20_FEATURE_MODEL_ID               6
+#define MS_OS_20_FEATURE_CCGP_DEVICE            7
+#define MS_OS_20_FEATURE_VENDOR_REVISION        8
+
+// We only need (and thus define) the following descriptor types
+
+typedef struct PBDRV_PACKED {
+    uint16_t wLength;
+    uint16_t wDescriptorType;
+    uint32_t dwWindowsVersion;
+    uint16_t wTotalLength;
+} pbdrv_usb_ms_20_desc_set_header_t;
+
+typedef struct PBDRV_PACKED {
+    uint16_t wLength;
+    uint16_t wDescriptorType;
+    uint8_t CompatibleID[8];
+    uint8_t SubCompatibleID[8];
+} pbdrv_usb_ms_20_compatible_t;
+
+typedef struct PBDRV_PACKED {
+    uint16_t wLength;
+    uint16_t wDescriptorType;
+    uint16_t wPropertyDataType;
+} pbdrv_usb_ms_20_reg_prop_hdr_t;
+
+#define MS_OS_20_REG_PROP_TYPE_REG_SZ           1
+#define MS_OS_20_REG_PROP_TYPE_REG_EXPAND_SZ    2
+#define MS_OS_20_REG_PROP_TYPE_REG_BINARY       3
+#define MS_OS_20_REG_PROP_TYPE_REG_DWORD_LE     4
+#define MS_OS_20_REG_PROP_TYPE_REG_DWORD_BE     5
+#define MS_OS_20_REG_PROP_TYPE_REG_LINK         6
+#define MS_OS_20_REG_PROP_TYPE_REG_MULTI_SZ     7
+
+// This is the minimum Windows version needed to support these descriptors
+#define MS_WINDOWS_VERSION_81       0x06030000
 
 #endif // _INTERNAL_PBDRV_USB_CH9_H_
