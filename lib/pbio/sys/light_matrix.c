6c8449c97e1681635fc8fc3dfb6d0b261c876438
Laurens Valk <laurens@pybricks.com>
2025-07-24 13:33:14 +0200

    pbio/sys/light_matrix: Fix animation update driver call.
    This shouldn't call directly to the driver, but should use the system level pixel setter as it does everywhere else in this module.

--- lib/pbio/sys/light_matrix.c
+++ lib/pbio/sys/light_matrix.c
@@ -126,19 +126,16 @@ static uint32_t pbsys_hub_light_matrix_user_program_animation_next(pbio_light_an
     // which we can cycle in 256 steps.
     static uint8_t cycle = 0;
 
-    pbdrv_led_array_dev_t *array;
-    if (pbdrv_led_array_get_dev(0, &array) == PBIO_SUCCESS) {
-        for (size_t i = 0; i < PBIO_ARRAY_SIZE(indexes); i++) {
-            // The pixels are spread equally across the pattern.
-            uint8_t offset = cycle + i * (UINT8_MAX / PBIO_ARRAY_SIZE(indexes));
-            uint8_t brightness = offset > 200 ? 0 : (offset < 100 ? offset : 200 - offset);
+    for (size_t i = 0; i < PBIO_ARRAY_SIZE(indexes); i++) {
+        // The pixels are spread equally across the pattern.
+        uint8_t offset = cycle + i * (UINT8_MAX / PBIO_ARRAY_SIZE(indexes));
+        uint8_t brightness = offset > 200 ? 0 : (offset < 100 ? offset : 200 - offset);
 
-            // Set the brightness for this pixel
-            pbdrv_led_array_set_brightness(array, indexes[i], brightness);
-        }
-        // This increment controls the speed of the pattern
-        cycle += 9;
+        // Set the brightness for this pixel
+        pbsys_hub_light_matrix_set_pixel(pbsys_hub_light_matrix, indexes[i] / 5, indexes[i] % 5, brightness);
     }
+    // This increment controls the speed of the pattern
+    cycle += 9;
 
     return 40;
 }
