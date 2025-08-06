6af34ee5f65b107cab2fa076b0062b425de8bbcd
R <rqou@berkeley.edu>
2025-08-06 02:39:13 +0100

    pbio/drv/rproc/rproc_ev3_pru1.h: Share with PRU project
    This makes it much easier to keep the projects in sync.

---
+++ lib/pbio/drv/rproc/rproc_ev3_pru1.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 The Pybricks Authors
+
+#ifndef _INTERNAL_PBDRV_RPROC_EV3_PRU1_H_
+#define _INTERNAL_PBDRV_RPROC_EV3_PRU1_H_
+
+#include <stdint.h>
+
+// This file is shared between the PRU1 source code
+// and the Pybricks source code. It defines the ABI
+// between the two codebases and must be kept in sync.
+
+#define PBDRV_RPROC_EV3_PRU1_NUM_PWM_CHANNELS   4
+
+typedef struct {
+    union {
+        // u32 used by the PRU codebase to get more-efficient codegen
+        uint32_t pwms;
+        // PWM duty cycle for each channel, range [0-255]
+        uint8_t pwm_duty_cycle[PBDRV_RPROC_EV3_PRU1_NUM_PWM_CHANNELS];
+    };
+} pbdrv_rproc_ev3_pru1_shared_ram_t;
+
+#endif // _INTERNAL_PBDRV_RPROC_EV3_PRU1_H_
