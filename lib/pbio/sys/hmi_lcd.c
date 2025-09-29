7f4bb69e617e4609a56655cc4fe36e8deda0f4b6
Laurens Valk <laurens@pybricks.com>
2025-09-29 16:25:20 +0200

    pbio/sys/hmi_lcd: Move splash screen.
    This doesn't belong in the driver. We can draw it
    with shapes now. We can keep this on while the
    user program runs. The user can delete it if they
    want.

--- lib/pbio/sys/hmi_lcd.c
+++ lib/pbio/sys/hmi_lcd.c
@@ -35,6 +35,61 @@
 #define DEBUG_PRINT(...)
 #endif
 
+// Scaling factors and functions to simplify drawing the logo.
+static float _scale;
+static uint32_t _offset_x;
+static uint32_t _offset_y;
+
+static uint32_t sx(uint32_t x) {
+    return x * _scale + _offset_x + 0.5f;
+}
+
+static uint32_t sy(uint32_t y) {
+    return y * _scale + _offset_y + 0.5f;
+}
+
+static uint32_t sr(uint32_t r) {
+    return r * _scale + 0.5f;
+}
+
+/**
+ * Draws the Pybricks logo on the screen.
+ *
+ * @param  x     [in] Horizontal offset from the left.
+ * @param  y     [in] Vertical offset from the top.
+ * @param  scale [in] Scale (1.0 is 154 x 84).
+ */
+static void draw_pybricks_logo(uint32_t x, uint32_t y, float scale) {
+    _offset_x = x;
+    _offset_y = y;
+    _scale = scale;
+
+    pbio_image_t *display = pbdrv_display_get_image();
+    pbio_image_fill(display, 0);
+
+    uint8_t v = pbdrv_display_get_max_value();
+
+    // Rounded rectangles making up the left and right side of the head.
+    pbio_image_fill_rounded_rect(display, sx(0), sy(0), sr(42), sr(84), sr(11), v);
+    pbio_image_fill_rounded_rect(display, sx(112), sy(0), sr(42), sr(84), sr(11), v);
+
+    // Forehead, main fill, and jaw.
+    pbio_image_fill_rect(display, sx(14), sy(0), sr(126), sr(14), v);
+    pbio_image_fill_rect(display, sx(14), sy(14), sr(126), sr(56), 0);
+    pbio_image_fill_rect(display, sx(28), sy(56), sr(98), sr(14), v);
+
+    // Eyes.
+    pbio_image_fill_circle(display, sx(49), sy(29), sr(10), v);
+    pbio_image_fill_circle(display, sx(106), sy(29), sr(10), v);
+
+    // Teeth.
+    for (uint32_t i = 0; i < 6; i++) {
+        pbio_image_fill_rect(display, sx(40 + 14 * i), sy(51), sr(4), sr(5), v);
+    }
+
+    pbdrv_display_update();
+}
+
 static void hmi_lcd_grid_show_pixel(uint8_t row, uint8_t col, bool on) {
     pbio_image_t *display = pbdrv_display_get_image();
     uint8_t value = on ? pbdrv_display_get_max_value(): 0;
@@ -42,24 +97,9 @@ static void hmi_lcd_grid_show_pixel(uint8_t row, uint8_t col, bool on) {
     const uint32_t width = size * 4 / 5;
     const uint32_t offset = (PBDRV_CONFIG_DISPLAY_NUM_COLS - (PBSYS_CONFIG_HMI_NUM_SLOTS * size)) / 2;
     pbio_image_fill_rect(display, col * size + offset, row * size, width, width, value);
-    pbdrv_display_update();
-}
-
-static void hmi_lcd_show_program_slot(void) {
-    pbio_image_t *display = pbdrv_display_get_image();
-    pbio_image_fill(display, 0);
-
-    for (uint8_t r = 0; r < PBSYS_CONFIG_HMI_NUM_SLOTS; r++) {
-        for (uint8_t c = 0; c < PBSYS_CONFIG_HMI_NUM_SLOTS; c++) {
-            bool is_on = r < 3 && c > 0 && c < 4;
-            is_on |= (r == 4 && c == pbsys_status_get_selected_slot());
-            hmi_lcd_grid_show_pixel(r, c, is_on);
-        }
-    }
 }
 
 void pbsys_hmi_init(void) {
-
 }
 
 void pbsys_hmi_deinit(void) {
@@ -72,12 +112,18 @@ static pbio_error_t run_ui(pbio_os_state_t *state, pbio_os_timer_t *timer) {
 
     PBIO_OS_ASYNC_BEGIN(state);
 
+    // Centered above 5 code slot indicators.
+    draw_pybricks_logo(27, 12, 0.8);
+
     for (;;) {
 
         DEBUG_PRINT("Start HMI loop\n");
 
-        // Visually indicate current state on supported hubs.
-        hmi_lcd_show_program_slot();
+        // Visually indicate current slot.
+        for (uint8_t c = 0; c < PBSYS_CONFIG_HMI_NUM_SLOTS; c++) {
+            hmi_lcd_grid_show_pixel(4, c, c == pbsys_status_get_selected_slot());
+        }
+        pbdrv_display_update();
 
         // Buttons could be pressed at the end of the user program, so wait for
         // a release and then a new press, or until we have to exit early.
@@ -151,10 +197,6 @@ static pbio_error_t run_ui(pbio_os_state_t *state, pbio_os_timer_t *timer) {
         }
         pbdrv_button_get_pressed();
     }));
-
-    // Clear UI from display to start user program.
-    pbio_image_fill(pbdrv_display_get_image(), 0);
-    pbdrv_display_update();
 
     // Start light or display animations.
     pbio_color_light_start_breathe_animation(pbsys_status_light_main, PBSYS_CONFIG_STATUS_LIGHT_STATE_ANIMATIONS_HUE);
