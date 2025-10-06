816ad14f063092ca06ddbbbcdc283f6d1c62899e
Laurens Valk <laurens@pybricks.com>
2025-10-06 16:23:09 +0200

    pbio/sys/core: Update to pbio/os.
    Also add a de-init to stop updating lights. Critical poll processes keep running.

--- lib/pbio/sys/core.c
+++ lib/pbio/sys/core.c
@@ -15,50 +15,54 @@
 #include <pbsys/host.h>
 #include <pbsys/status.h>
 
-
 #include "hmi.h"
 #include "light.h"
 #include "storage.h"
 #include "program_stop.h"
 
-PROCESS(pbsys_system_process, "System");
+static pbio_os_process_t pbsys_system_poll_process;
 
-PROCESS_THREAD(pbsys_system_process, ev, data) {
-    static struct etimer timer;
+static pbio_error_t pbsys_system_poll_process_thread(pbio_os_state_t *state, void *context) {
 
-    PROCESS_BEGIN();
+    static pbio_os_timer_t timer;
 
-    etimer_set(&timer, 50);
+    PBIO_OS_ASYNC_BEGIN(state);
+
+    pbio_os_timer_set(&timer, 50);
 
     for (;;) {
-        PROCESS_WAIT_EVENT();
+        PBIO_OS_AWAIT_UNTIL(state, pbio_os_timer_is_expired(&timer));
+        pbio_os_timer_extend(&timer);
 
-        if (ev == PROCESS_EVENT_TIMER && etimer_expired(&timer)) {
-            etimer_reset(&timer);
-            pbsys_battery_poll();
-            pbsys_program_stop_poll();
-            pbsys_status_light_poll();
+        pbsys_battery_poll();
+        pbsys_program_stop_poll();
+        pbsys_status_light_poll();
 
-            // Monitor USB state.
-            if (pbdrv_usb_connection_is_active()) {
-                pbsys_status_set(PBIO_PYBRICKS_STATUS_USB_HOST_CONNECTED);
-            } else {
-                pbsys_status_clear(PBIO_PYBRICKS_STATUS_USB_HOST_CONNECTED);
-            }
+        // keep the hub from resetting itself
+        pbdrv_watchdog_update();
 
-            // Monitor BLE state.
-            if (pbdrv_bluetooth_is_connected(PBDRV_BLUETOOTH_CONNECTION_PYBRICKS)) {
-                pbsys_status_set(PBIO_PYBRICKS_STATUS_BLE_HOST_CONNECTED);
-            } else {
-                pbsys_status_clear(PBIO_PYBRICKS_STATUS_BLE_HOST_CONNECTED);
-            }
+        if (pbsys_system_poll_process.request == PBIO_OS_PROCESS_REQUEST_TYPE_CANCEL) {
+            // After shutdown we only poll critical system processes.
+            continue;
+        }
 
-            // keep the hub from resetting itself
-            pbdrv_watchdog_update();
+        // Monitor USB state.
+        if (pbdrv_usb_connection_is_active()) {
+            pbsys_status_set(PBIO_PYBRICKS_STATUS_USB_HOST_CONNECTED);
+        } else {
+            pbsys_status_clear(PBIO_PYBRICKS_STATUS_USB_HOST_CONNECTED);
+        }
+
+        // Monitor BLE state.
+        if (pbdrv_bluetooth_is_connected(PBDRV_BLUETOOTH_CONNECTION_PYBRICKS)) {
+            pbsys_status_set(PBIO_PYBRICKS_STATUS_BLE_HOST_CONNECTED);
+        } else {
+            pbsys_status_clear(PBIO_PYBRICKS_STATUS_BLE_HOST_CONNECTED);
         }
     }
 
-    PROCESS_END();
+    // Unreachable
+    PBIO_OS_ASYNC_END(PBIO_ERROR_FAILED);
 }
 
 void pbsys_init(void) {
@@ -71,7 +75,7 @@ void pbsys_init(void) {
     pbsys_host_init();
     pbsys_status_light_init();
 
-    process_start(&pbsys_system_process);
+    pbio_os_process_start(&pbsys_system_poll_process, pbsys_system_poll_process_thread, NULL);
 
     while (pbio_busy_count_busy()) {
         pbio_os_run_processes_once();
@@ -79,6 +83,8 @@ void pbsys_init(void) {
 }
 
 void pbsys_deinit(void) {
+
+    pbio_os_process_make_request(&pbsys_system_poll_process, PBIO_OS_PROCESS_REQUEST_TYPE_CANCEL);
 
     pbsys_storage_deinit();
     pbsys_hmi_deinit();
