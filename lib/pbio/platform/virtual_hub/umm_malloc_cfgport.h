cda7489b4bb0796e57070e84fcd80b7718779d16
Laurens Valk <laurens@pybricks.com>
2025-11-11 11:23:59 +0100

    bricks/virtualhub: Enable screen attribute.

---
+++ lib/pbio/platform/virtual_hub/umm_malloc_cfgport.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 The Pybricks Authors
+//
+// Configuration for umm_malloc memory allocator for Virtual platform.
+// See lib/umm_malloc/src/umm_malloc_cfg.h for details.
+
+// We only use this for large allocations, like images and we have a lot of RAM
+// on the host, so we can use a larger block size to reduce overhead.
+#define UMM_BLOCK_BODY_SIZE 256
+
+#define UMM_INFO
+#define DBGLOG_ENABLE
