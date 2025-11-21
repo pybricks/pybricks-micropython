fe2848d9fd46d97d45514cfb48db36b9aad7e952
James Aguilar <aguilar.james@gmail.com>
2025-11-21 11:15:30 -0700

    pbdrv/uart_debug_first_port: Use lwrb.
    Using lwrb gets rid of our error-prone manual manipulation of the ring
    buffer, and apparently resolves some semi-corruption issues observed
    during high-volume logging.
    
    Also limits the number of bytes written per uart write to avoid
    timeouts.

--- lib/pbio/drv/uart/uart_debug_first_port.c
+++ lib/pbio/drv/uart/uart_debug_first_port.c
@@ -13,12 +13,12 @@
 #include <stdarg.h>
 #include <stdint.h>
 #include <string.h>
+#include <lwrb/lwrb.h>
 
 #define BUF_SIZE (1024)
 
-static uint8_t ring_buf[BUF_SIZE];
-static size_t ring_head = 0;
-static size_t ring_tail = 0;
+static uint8_t ring_buf_storage[BUF_SIZE];
+static lwrb_t ring_buffer;
 
 static pbdrv_uart_dev_t *debug_uart = NULL;
 
@@ -33,13 +33,13 @@ static pbdrv_uart_dev_t *debug_uart = NULL;
  * @param va_list The variable arguments, as a va_list.
  */
 void pbdrv_uart_debug_vprintf(const char *format, va_list args) {
-    char buf[BUF_SIZE];
-    vsnprintf(buf, sizeof(buf), format, args);
-    size_t len = strlen(buf);
-    for (size_t i = 0; i < len; i++) {
-        ring_buf[ring_head] = buf[i];
-        ring_head = (ring_head + 1) % BUF_SIZE;
+    if (!lwrb_is_ready(&ring_buffer)) {
+        lwrb_init(&ring_buffer, ring_buf_storage, sizeof(ring_buf_storage));
     }
+
+    char buf[256];
+    size_t len = vsnprintf(buf, sizeof(buf), format, args);
+    lwrb_write(&ring_buffer, (const uint8_t *)buf, len);
 
     if (!debug_uart) {
         // Not initialized yet, so just buffer for now.
@@ -85,9 +85,14 @@ static pbio_error_t pbdrv_uart_debug_process_thread(pbio_os_state_t *state, void
 
     static pbio_os_state_t child;
     static pbio_error_t err;
+    static uint8_t *write_buf;
     static size_t write_size;
 
     PBIO_OS_ASYNC_BEGIN(state);
+
+    if (!lwrb_is_ready(&ring_buffer)) {
+        lwrb_init(&ring_buffer, ring_buf_storage, sizeof(ring_buf_storage));
+    }
 
     while (pbdrv_uart_get_instance(0, &debug_uart) != PBIO_SUCCESS) {
         pbio_os_request_poll();
@@ -97,22 +102,26 @@ static pbio_error_t pbdrv_uart_debug_process_thread(pbio_os_state_t *state, void
     pbdrv_uart_set_baud_rate(debug_uart, 115200);
 
     for (;;) {
-        PBIO_OS_AWAIT_UNTIL(state, ring_head != ring_tail);
+        PBIO_OS_AWAIT_UNTIL(state, lwrb_get_full(&ring_buffer) > 0);
 
         // Write up to the end of the buffer without wrapping.
-        size_t end = ring_head > ring_tail ? ring_head : BUF_SIZE;
-        write_size = end - ring_tail;
-        PBIO_OS_AWAIT(state, &child, (err = pbdrv_uart_write(&child, debug_uart, &ring_buf[ring_tail], write_size, 100)));
-        ring_tail = (ring_tail + write_size) % BUF_SIZE;
+
+        write_buf = lwrb_get_linear_block_read_address(&ring_buffer);
+        write_size = lwrb_get_linear_block_read_length(&ring_buffer);
+        if (write_size > 256) {
+            // Avoid writing too much at once, because it can cause timeouts.
+            write_size = 256;
+        }
+        PBIO_OS_AWAIT(state, &child, (err = pbdrv_uart_write(&child, debug_uart, write_buf, write_size, 100)));
+        lwrb_skip(&ring_buffer, write_size);
 
         if (err != PBIO_SUCCESS) {
-            ring_head = snprintf((char *)ring_buf, BUF_SIZE, "UART debug write error %d\n", err);
-            ring_tail = 0;
-        }
-
-        // Poll to write again if not fully finished, i.e. when wrapping.
-        if (ring_head != ring_tail) {
-            pbio_os_request_poll();
+            // In most circumstances, we'll have cleared enough to write this
+            // error down, unless we're near the end of the ring buffer. If so,
+            // then we'll just have to be sad.
+            char errbuf[128];
+            snprintf(errbuf, sizeof(errbuf), "UART debug write error %d\n", err);
+            lwrb_write(&ring_buffer, (const uint8_t *)errbuf, strlen(errbuf));
         }
     }
 
@@ -124,7 +133,7 @@ static pbio_error_t pbdrv_uart_debug_process_thread(pbio_os_state_t *state, void
  * Checks if the UART debug is done writing.
  */
 bool pbdrv_uart_debug_is_done(void) {
-    return debug_uart != NULL && ring_head == ring_tail;
+    return debug_uart != NULL && lwrb_get_full(&ring_buffer) == 0;
 }
 
 void pbdrv_uart_debug_init(void) {
