c3099fc0a19448222cd5eeb7a7998935e23ed115
David Lechner <david@pybricks.com>
2025-11-30 21:48:35 -0600

    pbsys/battery: Add PBSYS_CONFIG_BATTERY option.
    Make enabling battery monitor configurable per platform. This is
    especially useful for bringing up new platforms so that they don't
    shut down before the battery driver is implemented.

--- lib/pbio/platform/nxt/pbsysconfig.h
+++ lib/pbio/platform/nxt/pbsysconfig.h
@@ -6,6 +6,8 @@
 #define PBSYS_CONFIG_FEATURE_BUILTIN_USER_PROGRAM_IMU_CALIBRATION  (0)
 #define PBSYS_CONFIG_FEATURE_PROGRAM_FORMAT_MULTI_MPY_V6           (1)
 #define PBSYS_CONFIG_FEATURE_PROGRAM_FORMAT_MULTI_MPY_V6_3_NATIVE  (0)
+#define PBSYS_CONFIG_BATTERY                        (1)
+#define PBSYS_CONFIG_BATTERY_CHARGER                (0)
 #define PBSYS_CONFIG_HMI                            (1)
 #define PBSYS_CONFIG_HMI_STOP_BUTTON                (1 << 2) // down
 #define PBSYS_CONFIG_HMI_NONE                       (1)
