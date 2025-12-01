8acbad7873036faffb9372cdae249a4c345406d8
Laurens Valk <laurens@pybricks.com>
2025-12-01 10:53:37 +0100

    pbio/drv/bluetooth_btstack: Enable BLE features selectively.
    This lets us eventually use this on hubs with btstack with classic only, like EV3.
    
    We would eventually enable classic versions of functions such as pbdrv_bluetooth_peripheral_scan_and_connect_func, so they are still implemented here rather than disabled as inlined from an include file.

--- lib/pbio/platform/test/pbdrvconfig.h
+++ lib/pbio/platform/test/pbdrvconfig.h
@@ -9,6 +9,7 @@
 
 #define PBDRV_CONFIG_BLUETOOTH                              (1)
 #define PBDRV_CONFIG_BLUETOOTH_BTSTACK                      (1)
+#define PBDRV_CONFIG_BLUETOOTH_BTSTACK_LE                   (1)
 #define PBDRV_CONFIG_BLUETOOTH_BTSTACK_CC2564C              (1)
 #define PBDRV_CONFIG_BLUETOOTH_BTSTACK_HUB_KIND             0xff
 
