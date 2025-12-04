6bc54faa80ea9304801a4393998f6e0dc727a62c
Nicolas Schodet <nico@ni.fr.eu.org>
2025-12-04 09:25:25 +0100

    pbio/drv/display: Improve conversion from HSV to pixel value.
    Use a mapping trying to match the real pixel color, select the nearest
    pixel value according to the input color.
    
    Small test program:
    
      from pybricks.hubs import ThisHub
      from pybricks.parameters import Color
      from pybricks.tools import wait
    
      hub = ThisHub()
    
      i = hub.screen
    
      for y in range(i.height):
          for x in range(i.width):
    	  i.draw_pixel(x, y, Color(0, y * 100 / (i.height - 1), x * 100 / (i.width - 1)))
    
      wait(50000)
    
    Closes: https://github.com/pybricks/support/issues/2442

--- lib/pbio/include/pbdrv/display.h
+++ lib/pbio/include/pbdrv/display.h
@@ -27,6 +27,15 @@ pbio_image_t *pbdrv_display_get_image(void);
 uint8_t pbdrv_display_get_max_value(void);
 
 /**
+ * Get the pixel value corresponding to a color given in HSV space.
+ * @param [in] h  Hue, 0 to 359 degrees.
+ * @param [in] s  Saturation, 0 to 100 percent.
+ * @param [in] v  Value, 0 to 100 percent.
+ * @return  Pixel value.
+ */
+uint8_t pbdrv_display_get_value_from_hsv(uint16_t h, uint8_t s, uint8_t v);
+
+/**
  * Update the display to show current content of image container.
  */
 void pbdrv_display_update(void);
@@ -38,6 +47,10 @@ static inline pbio_image_t *pbdrv_display_get_image(void) {
 }
 
 static inline uint8_t pbdrv_display_get_max_value(void) {
+    return 0;
+}
+
+static inline uint8_t pbdrv_display_get_value_from_hsv(uint16_t h, uint8_t s, uint8_t v) {
     return 0;
 }
 
