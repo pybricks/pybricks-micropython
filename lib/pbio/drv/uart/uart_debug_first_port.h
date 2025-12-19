0b13710604ac94e08912fc9cefe1d05aed104dbf
James Aguilar <aguilar.james@gmail.com>
2025-12-19 07:41:37 -0700

    pbio/drv/uart_debug_first_port: Log on panic.
    It's helpful when you crash to see what happens right before you
    crash. For example, if you add a log message "about to dereference
    sketchy pointer" and then you get a segfault, you'd love to know
    whether that message occurred. This change makes it so that at least
    on EV3, we will.
    
    This also adds "pbdrv_log" as an alias for pbdrv_uart_debug_printf.
    Usually, platforms make it easy to log debug messages, and we should
    be no different. C.f. `ESP_LOGI`, Zephyr's `LOG_ERR`, glog's
    `LOG(WARNING)` etc.
    
    Tested by adding the following code to pbdrv_init.
    
    ```
        pbdrv_log("test\n");
    
        lwrb_t* rb = (lwrb_t*)0x1281441490;
        pbdrv_log("%p\n", lwrb_get_free(rb));
    ```
    
    Before this, the "test" would not appear before the panic.
    After, it does. If a panic occurs while some messages are pending
    in the uart send state, some bytes may be duplicated on the log.

--- lib/pbio/drv/uart/uart_debug_first_port.h
+++ lib/pbio/drv/uart/uart_debug_first_port.h
@@ -19,6 +19,11 @@ bool pbdrv_uart_debug_is_done(void);
 
 void pbdrv_uart_debug_init(void);
 
+// Returns the next character that should be sent to the UART, or -1 if there
+// are not pending log messages. Used only by the panic handler to flush unsent
+// data before reboot.
+int pbdrv_uart_debug_next_char(void);
+
 #else // PBDRV_CONFIG_UART_DEBUG_FIRST_PORT
 
 #define pbdrv_uart_debug_printf(...)
@@ -28,6 +33,13 @@ void pbdrv_uart_debug_init(void);
 
 #define pbdrv_uart_debug_init()
 
+static inline int pbdrv_uart_debug_next_char(void) {
+    return -1;
+}
+
 #endif // PBDRV_CONFIG_UART_DEBUG_FIRST_PORT
+
+// Convenient shorthand for pbdrv_uart_debug_printf.
+#define pbdrv_dbg(...) pbdrv_uart_debug_printf(__VA_ARGS__)
 
 #endif // _INTERNAL_PBDRV_UART_DEBUG_FIRST_PORT_H_
