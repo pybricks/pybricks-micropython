42e8a8a6e5520e4b5c41b29511cf1dbe638c0c8a
Nicolas Schodet <nico@ni.fr.eu.org>
2025-12-27 19:31:30 +0100

    pbio/platform/nxt: Do not display error message on assertion failure.
    Synchronous screen update support will be removed.
    
    Refs: https://github.com/pybricks/support/issues/2425

--- lib/pbio/platform/nxt/nxos/assert.c
+++ lib/pbio/platform/nxt/nxos/assert.c
@@ -6,31 +6,11 @@
  * the terms of the GNU Public License (GPL) version 2.
  */
 
-#include <stdbool.h>
-#include <string.h>
-
-#include <pbio/image.h>
-
-#include <pbdrv/reset.h>
-#include <pbdrv/display.h>
-
-#include "nxos/util.h"
-#include "nxos/drivers/systick.h"
-
 #include "nxos/assert.h"
-
-#include "../../../drv/display/display_nxt.h"
 
 void nx_assert_error(const char *file, const int line,
 		     const char *expr, const char *msg) {
-  const char *basename = strrchr(file, '/');
-  basename = basename ? basename+1 : file;
-
-  pbio_image_t *display = pbdrv_display_get_image();
-  pbio_image_fill(display, 0);
-  pbio_image_print0(display, "** Assertion **\n");
-  pbio_image_printf(display, "%s:%d\n", basename, line);
-  pbio_image_printf(display, "%s\n", expr);
-  pbio_image_printf(display, "%s\n", msg);
-  pbdrv_display_nxt_sync_refresh();
+    while (1)
+    {
+    }
 }
