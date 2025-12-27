0903c67499db52f6d3660cb492b512d91bf4d5a6
Nicolas Schodet <nico@ni.fr.eu.org>
2025-12-27 19:29:35 +0100

    pbio/platform/nxt: Do not display error message on abort.
    Synchronous screen update support will be removed.
    
    Refs: https://github.com/pybricks/support/issues/2425

--- lib/pbio/platform/nxt/nxos/_abort.c
+++ lib/pbio/platform/nxt/nxos/_abort.c
@@ -9,29 +9,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#include <pbio/image.h>
-
-#include <pbdrv/display.h>
-
 #include "nxos/interrupts.h"
 
 #include "nxos/_abort.h"
 
-#include "../../../drv/display/display_nxt.h"
-
 void nx__abort(bool data, uint32_t pc, uint32_t cpsr) {
   nx_interrupts_disable();
-  pbio_image_t *display = pbdrv_display_get_image();
-  pbio_image_fill(display, 0);
-  if (data) {
-    pbio_image_print0(display, "Data");
-  } else {
-    pbio_image_print0(display, "Prefetch");
-  }
-  pbio_image_print0(display, " abort\nPC: ");
-  pbio_image_print_hex(display, pc);
-  pbio_image_print0(display, "\nCPSR: ");
-  pbio_image_print_hex(display, cpsr);
-  pbdrv_display_nxt_sync_refresh();
   while(1);
 }
