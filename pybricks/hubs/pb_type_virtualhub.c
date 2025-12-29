17a5b83d537e6649c00cfead265c11af38e3c902
Laurens Valk <laurens@pybricks.com>
2025-12-29 10:04:44 +0100

    pybricks.hubs.VirtualHub: Enable BLE features.
    This lets us test BLE features on the REPL as we enable the new Virtual Hub driver, so we can see it working.

--- pybricks/hubs/pb_type_virtualhub.c
+++ pybricks/hubs/pb_type_virtualhub.c
@@ -14,6 +14,10 @@
 
 #include <pybricks/util_mp/pb_obj_helper.h>
 #include <pybricks/util_mp/pb_kwarg_helper.h>
+#include <pybricks/util_pb/pb_error.h>
+
+#include <pybricks/common.h>
+#include <pybricks/hubs.h>
 
 #include <pybricks/common.h>
 #include <pybricks/hubs.h>
@@ -26,11 +30,33 @@ typedef struct _hubs_VirtualHub_obj_t {
     mp_obj_t light;
     mp_obj_t screen;
     mp_obj_t system;
+    #if PYBRICKS_PY_COMMON_BLE
+    mp_obj_t ble;
+    #endif
 } hubs_VirtualHub_obj_t;
 
 static mp_obj_t hubs_VirtualHub_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
+    PB_PARSE_ARGS_CLASS(n_args, n_kw, args,
+        PB_ARG_DEFAULT_OBJ(top_side, pb_type_Axis_Z_obj),
+        PB_ARG_DEFAULT_OBJ(front_side, pb_type_Axis_X_obj)
+        #if PYBRICKS_PY_COMMON_BLE
+        , PB_ARG_DEFAULT_NONE(broadcast_channel)
+        , PB_ARG_DEFAULT_OBJ(observe_channels, mp_const_empty_tuple_obj)
+        #endif
+        );
+
+    (void)top_side_in;
+    (void)front_side_in;
+
+
+
     hubs_VirtualHub_obj_t *self = mp_obj_malloc(hubs_VirtualHub_obj_t, type);
     self->battery = MP_OBJ_FROM_PTR(&pb_module_battery);
+
+    #if PYBRICKS_PY_COMMON_BLE
+    self->ble = pb_type_BLE_new(broadcast_channel_in, observe_channels_in);
+    #endif
+
     self->buttons = pb_type_Keypad_obj_new(MP_OBJ_FROM_PTR(self), pb_type_button_pressed_hub_single_button);
     // FIXME: Implement lights.
     // self->light = common_ColorLight_internal_obj_new(pbsys_status_light_main);
@@ -41,6 +67,9 @@ static mp_obj_t hubs_VirtualHub_make_new(const mp_obj_type_t *type, size_t n_arg
 
 static const pb_attr_dict_entry_t hubs_VirtualHub_attr_dict[] = {
     PB_DEFINE_CONST_ATTR_RO(MP_QSTR_battery, hubs_VirtualHub_obj_t, battery),
+    #if PYBRICKS_PY_COMMON_BLE
+    PB_DEFINE_CONST_ATTR_RO(MP_QSTR_ble, hubs_VirtualHub_obj_t, ble),
+    #endif
     PB_DEFINE_CONST_ATTR_RO(MP_QSTR_buttons, hubs_VirtualHub_obj_t, buttons),
     // PB_DEFINE_CONST_ATTR_RO(MP_QSTR_light, hubs_VirtualHub_obj_t, light),
     PB_DEFINE_CONST_ATTR_RO(MP_QSTR_screen, hubs_VirtualHub_obj_t, screen),
