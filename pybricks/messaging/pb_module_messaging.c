0b1bc47de7b6cc37d33bfca801004ae65a34f7ce
James Aguilar <aguilar.james@gmail.com>
2026-01-18 10:27:45 -0700

    fixup! pbdrv/bluetooth: RFCOMM socket API.
    Convert rfcomm_listen/connect into RFCOMMSocket. This
    object-oriented approach better matches what we do
    elsewhere in pybricks and the general pattern within
    Micropython overall.

--- pybricks/messaging/pb_module_messaging.c
+++ pybricks/messaging/pb_module_messaging.c
@@ -205,7 +205,115 @@ static void pb_type_messaging_rfcomm_socket_print(const mp_print_t *print, mp_ob
     mp_printf(print, "RFCOMMSocket(conn_id=%d)", self->conn.conn_id);
 }
 
+// Forward declaration of the RFCOMMSocket type
+static const mp_obj_type_t pb_type_messaging_rfcomm_socket;
+
+// Constructor for RFCOMMSocket
+static mp_obj_t pb_type_messaging_rfcomm_socket_make_new(const mp_obj_type_t* type, size_t n_args, size_t n_kw, const mp_obj_t* args) {
+    mp_arg_check_num(n_args, n_kw, 0, 0, false);
+    pb_type_messaging_rfcomm_socket_obj_t *self = mp_obj_malloc(pb_type_messaging_rfcomm_socket_obj_t, &pb_type_messaging_rfcomm_socket);
+    self->conn.conn_id = -1;
+    return MP_OBJ_FROM_PTR(self);
+}
+
+// RFCOMM connect method
+
+typedef struct {
+    pb_type_messaging_rfcomm_socket_obj_t* socket;
+    bdaddr_t bdaddr;
+    int32_t timeout;
+} pb_type_messaging_rfcomm_socket_connect_context_t;
+
+static pbio_error_t pb_type_messaging_rfcomm_socket_connect_iterate(pbio_os_state_t* state, mp_obj_t parent_obj) {
+    pb_type_messaging_rfcomm_socket_connect_context_t* context = (pb_type_messaging_rfcomm_socket_connect_context_t*)parent_obj;
+    return pbdrv_bluetooth_rfcomm_connect(state, context->bdaddr, context->timeout, &context->socket->conn);
+}
+
+static mp_obj_t pb_type_messaging_rfcomm_socket_connect_return_map(mp_obj_t context_obj) {
+    pb_type_messaging_rfcomm_socket_connect_context_t* context = (pb_type_messaging_rfcomm_socket_connect_context_t*)context_obj;
+    return MP_OBJ_FROM_PTR(context->socket);
+}
+
+static mp_obj_t pb_type_messaging_rfcomm_socket_connect(size_t n_args, const mp_obj_t* pos_args, mp_map_t* kw_args) {
+    PB_PARSE_ARGS_METHOD(n_args, pos_args, kw_args, pb_type_messaging_rfcomm_socket_obj_t, self, PB_ARG_REQUIRED(address), PB_ARG_DEFAULT_INT(timeout, 10000));
+
+    // Check if socket is already connected
+    if (pbdrv_bluetooth_rfcomm_is_connected(&self->conn)) {
+        mp_raise_msg(&mp_type_OSError, MP_ERROR_TEXT("socket is already connected"));
+    }
+
+    pb_type_messaging_rfcomm_socket_connect_context_t* context = m_new(pb_type_messaging_rfcomm_socket_connect_context_t, 1);
+    context->socket = self;
+
+    const char *address_str = mp_obj_str_get_str(address_in);
+    if (!pbdrv_bluetooth_str_to_bdaddr(address_str, context->bdaddr)) {
+        m_del(pb_type_messaging_rfcomm_socket_connect_context_t, context, 1);
+        mp_raise_ValueError(MP_ERROR_TEXT("invalid Bluetooth address format"));
+    }
+
+    context->timeout = mp_obj_get_int(timeout_in);
+    if (context->timeout < 0) {
+        m_del(pb_type_messaging_rfcomm_socket_connect_context_t, context, 1);
+        mp_raise_ValueError(MP_ERROR_TEXT("timeout must be non-negative"));
+    }
+
+    pb_type_async_t *awaitable_ptr = NULL;
+    pb_type_async_t config = {
+        .iter_once = pb_type_messaging_rfcomm_socket_connect_iterate,
+        .parent_obj = MP_OBJ_FROM_PTR(context),
+        .return_map = pb_type_messaging_rfcomm_socket_connect_return_map,
+    };
+    return pb_type_async_wait_or_await(&config, &awaitable_ptr, true);
+}
+static MP_DEFINE_CONST_FUN_OBJ_KW(pb_type_messaging_rfcomm_socket_connect_obj, 1, pb_type_messaging_rfcomm_socket_connect);
+
+// RFCOMM listen method
+
+typedef struct {
+    pb_type_messaging_rfcomm_socket_obj_t* socket;
+    int32_t timeout;
+} pb_type_messaging_rfcomm_socket_listen_context_t;
+
+static pbio_error_t pb_type_messaging_rfcomm_socket_listen_iterate(pbio_os_state_t* state, mp_obj_t parent_obj) {
+    pb_type_messaging_rfcomm_socket_listen_context_t* context = (pb_type_messaging_rfcomm_socket_listen_context_t*)parent_obj;
+    return pbdrv_bluetooth_rfcomm_listen(state, context->timeout, &context->socket->conn);
+}
+
+static mp_obj_t pb_type_messaging_rfcomm_socket_listen_return_map(mp_obj_t context_obj) {
+    pb_type_messaging_rfcomm_socket_listen_context_t* context = (pb_type_messaging_rfcomm_socket_listen_context_t*)context_obj;
+    return MP_OBJ_FROM_PTR(context->socket);
+}
+
+static mp_obj_t pb_type_messaging_rfcomm_socket_listen(size_t n_args, const mp_obj_t* pos_args, mp_map_t* kw_args) {
+    PB_PARSE_ARGS_METHOD(n_args, pos_args, kw_args, pb_type_messaging_rfcomm_socket_obj_t, self, PB_ARG_DEFAULT_INT(timeout, 0));
+
+    // Check if socket is already connected
+    if (pbdrv_bluetooth_rfcomm_is_connected(&self->conn)) {
+        mp_raise_msg(&mp_type_OSError, MP_ERROR_TEXT("socket is already connected"));
+    }
+
+    pb_type_messaging_rfcomm_socket_listen_context_t* context = m_new(pb_type_messaging_rfcomm_socket_listen_context_t, 1);
+    context->socket = self;
+
+    context->timeout = mp_obj_get_int(timeout_in);
+    if (context->timeout < 0) {
+        m_del(pb_type_messaging_rfcomm_socket_listen_context_t, context, 1);
+        mp_raise_ValueError(MP_ERROR_TEXT("timeout must be non-negative"));
+    }
+
+    pb_type_async_t *awaitable_ptr = NULL;
+    pb_type_async_t config = {
+        .iter_once = pb_type_messaging_rfcomm_socket_listen_iterate,
+        .parent_obj = MP_OBJ_FROM_PTR(context),
+        .return_map = pb_type_messaging_rfcomm_socket_listen_return_map,
+    };
+    return pb_type_async_wait_or_await(&config, &awaitable_ptr, true);
+}
+static MP_DEFINE_CONST_FUN_OBJ_KW(pb_type_messaging_rfcomm_socket_listen_obj, 0, pb_type_messaging_rfcomm_socket_listen);
+
 static const mp_rom_map_elem_t pb_type_messaging_rfcomm_socket_locals_dict_table[] = {
+    {MP_ROM_QSTR(MP_QSTR_connect), MP_ROM_PTR(&pb_type_messaging_rfcomm_socket_connect_obj)},
+    {MP_ROM_QSTR(MP_QSTR_listen), MP_ROM_PTR(&pb_type_messaging_rfcomm_socket_listen_obj)},
     {MP_ROM_QSTR(MP_QSTR_read), MP_ROM_PTR(&mp_stream_read_obj)},
     {MP_ROM_QSTR(MP_QSTR_readinto), MP_ROM_PTR(&mp_stream_readinto_obj)},
     {MP_ROM_QSTR(MP_QSTR_write), MP_ROM_PTR(&mp_stream_write_obj)},
@@ -213,106 +321,23 @@ static const mp_rom_map_elem_t pb_type_messaging_rfcomm_socket_locals_dict_table
 };
 static MP_DEFINE_CONST_DICT(pb_type_messaging_rfcomm_socket_locals_dict, pb_type_messaging_rfcomm_socket_locals_dict_table);
 
-static MP_DEFINE_CONST_OBJ_TYPE(pb_type_messaging_rfcomm_socket, MP_QSTR_RFCOMMSocket, MP_TYPE_FLAG_NONE, print, pb_type_messaging_rfcomm_socket_print, protocol, &pb_type_messaging_rfcomm_socket_stream_p, locals_dict, &pb_type_messaging_rfcomm_socket_locals_dict);
-
-static mp_obj_t pb_type_messaging_rfcomm_socket_new(const pbdrv_bluetooth_rfcomm_conn_t *conn) {
-    pb_type_messaging_rfcomm_socket_obj_t *self = mp_obj_malloc(pb_type_messaging_rfcomm_socket_obj_t, &pb_type_messaging_rfcomm_socket);
-    self->conn = *conn;
-    return MP_OBJ_FROM_PTR(self);
-}
-
-// RFCOMM connect
-
-typedef struct {
-    bdaddr_t bdaddr;
-    int32_t timeout;
-    pbdrv_bluetooth_rfcomm_conn_t conn;
-} pb_module_messaging_rfcomm_connect_context_t;
-
-static pbio_error_t pb_module_messaging_rfcomm_connect_iterate(pbio_os_state_t *state, mp_obj_t parent_obj) {
-    pb_module_messaging_rfcomm_connect_context_t *context = (pb_module_messaging_rfcomm_connect_context_t *)parent_obj;
-    return pbdrv_bluetooth_rfcomm_connect(state, context->bdaddr, context->timeout, &context->conn);
-}
-
-static mp_obj_t pb_module_messaging_rfcomm_connect_return_map(mp_obj_t context_obj) {
-    pb_module_messaging_rfcomm_connect_context_t *context = (pb_module_messaging_rfcomm_connect_context_t *)context_obj;
-    return pb_type_messaging_rfcomm_socket_new(&context->conn);
-}
-
-static mp_obj_t pb_module_messaging_rfcomm_connect(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
-    PB_PARSE_ARGS_FUNCTION(n_args, pos_args, kw_args,
-        PB_ARG_REQUIRED(address),
-        PB_ARG_DEFAULT_INT(timeout, 10000));
-
-    pb_module_messaging_rfcomm_connect_context_t *context = m_new(pb_module_messaging_rfcomm_connect_context_t, 1);
-
-    const char *address_str = mp_obj_str_get_str(address_in);
-    if (!pbdrv_bluetooth_str_to_bdaddr(address_str, context->bdaddr)) {
-        m_del(pb_module_messaging_rfcomm_connect_context_t, context, 1);
-        mp_raise_ValueError(MP_ERROR_TEXT("invalid Bluetooth address format"));
-    }
-
-    context->timeout = mp_obj_get_int(timeout_in);
-    if (context->timeout < 0) {
-        m_del(pb_module_messaging_rfcomm_connect_context_t, context, 1);
-        mp_raise_ValueError(MP_ERROR_TEXT("timeout must be non-negative"));
-    }
-
-    pb_type_async_t *awaitable_ptr = NULL;
-    pb_type_async_t config = {
-        .iter_once = pb_module_messaging_rfcomm_connect_iterate,
-        .parent_obj = MP_OBJ_FROM_PTR(context),
-        .return_map = pb_module_messaging_rfcomm_connect_return_map,
-    };
-    return pb_type_async_wait_or_await(&config, &awaitable_ptr, true);
-}
-static MP_DEFINE_CONST_FUN_OBJ_KW(pb_module_messaging_rfcomm_connect_obj, 1, pb_module_messaging_rfcomm_connect);
-
-// RFCOMM listen
-
-typedef struct {
-    int32_t timeout;
-    pbdrv_bluetooth_rfcomm_conn_t conn;
-} pb_module_messaging_rfcomm_listen_context_t;
-
-static pbio_error_t pb_module_messaging_rfcomm_listen_iterate(pbio_os_state_t *state, mp_obj_t parent_obj) {
-    pb_module_messaging_rfcomm_listen_context_t *context = (pb_module_messaging_rfcomm_listen_context_t *)parent_obj;
-    return pbdrv_bluetooth_rfcomm_listen(state, context->timeout, &context->conn);
-}
-
-static mp_obj_t pb_module_messaging_rfcomm_listen_return_map(mp_obj_t context_obj) {
-    pb_module_messaging_rfcomm_listen_context_t *context = (pb_module_messaging_rfcomm_listen_context_t *)context_obj;
-    return pb_type_messaging_rfcomm_socket_new(&context->conn);
-}
-
-static mp_obj_t pb_module_messaging_rfcomm_listen(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
-    PB_PARSE_ARGS_FUNCTION(n_args, pos_args, kw_args,
-        PB_ARG_DEFAULT_INT(timeout, 0));
-
-    pb_module_messaging_rfcomm_listen_context_t *context = m_new(pb_module_messaging_rfcomm_listen_context_t, 1);
-
-    context->timeout = mp_obj_get_int(timeout_in);
-    if (context->timeout < 0) {
-        m_del(pb_module_messaging_rfcomm_listen_context_t, context, 1);
-        mp_raise_ValueError(MP_ERROR_TEXT("timeout must be non-negative"));
-    }
-
-    pb_type_async_t *awaitable_ptr = NULL;
-    pb_type_async_t config = {
-        .iter_once = pb_module_messaging_rfcomm_listen_iterate,
-        .parent_obj = MP_OBJ_FROM_PTR(context),
-        .return_map = pb_module_messaging_rfcomm_listen_return_map,
-    };
-    return pb_type_async_wait_or_await(&config, &awaitable_ptr, true);
-}
-static MP_DEFINE_CONST_FUN_OBJ_KW(pb_module_messaging_rfcomm_listen_obj, 0, pb_module_messaging_rfcomm_listen);
+static MP_DEFINE_CONST_OBJ_TYPE(
+    pb_type_messaging_rfcomm_socket,
+    MP_QSTR_RFCOMMSocket,
+    MP_TYPE_FLAG_NONE,
+    make_new,
+    pb_type_messaging_rfcomm_socket_make_new,
+    print,
+    pb_type_messaging_rfcomm_socket_print,
+    protocol,
+    &pb_type_messaging_rfcomm_socket_stream_p,
+    locals_dict,
+    &pb_type_messaging_rfcomm_socket_locals_dict);
 
 static const mp_rom_map_elem_t messaging_globals_table[] = {
     {MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_messaging)},
     {MP_ROM_QSTR(MP_QSTR_local_address), MP_ROM_PTR(&pb_messaging_local_address_obj)},
     {MP_ROM_QSTR(MP_QSTR_bluetooth_scan), MP_ROM_PTR(&pb_messaging_bluetooth_scan_obj)},
-    {MP_ROM_QSTR(MP_QSTR_rfcomm_connect), MP_ROM_PTR(&pb_module_messaging_rfcomm_connect_obj)},
-    {MP_ROM_QSTR(MP_QSTR_rfcomm_listen), MP_ROM_PTR(&pb_module_messaging_rfcomm_listen_obj)},
     {MP_ROM_QSTR(MP_QSTR_RFCOMMSocket), MP_ROM_PTR(&pb_type_messaging_rfcomm_socket)},
 };
 static MP_DEFINE_CONST_DICT(pb_module_messaging_globals, messaging_globals_table);
