44a4ef8848fb60ab100ebdb5b0fddad9ec01eb5d
Laurens Valk <laurens@pybricks.com>
2026-02-20 16:27:14 +0100

    tests/virtualhub/basics: Restore test program.
    An alternate test was accidentally committed.

--- tests/virtualhub/basics/hello.py
+++ tests/virtualhub/basics/hello.py
@@ -1,11 +1,5 @@
 from pybricks.tools import wait
 
-from pybricks.hubs import VirtualHub
-
-hub = VirtualHub()
-hub.screen.print("HI")
-hub.screen.print("HI")
-
 print("\nHello")
-wait(10000)
+wait(1000)
 print("World\n")
