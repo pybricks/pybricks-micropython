11afd44846914dc3666d264ef8e62719d74cd636
Laurens Valk <laurens@pybricks.com>
2026-05-12 13:54:15 +0200

    pybricks.tools.AppData: Update for camera processors.
    This adds a general purpose API to enable
    one or more camera processors from the Pybricks Code
    interface.
    
    It is still possible to send or read raw bytes to
    allow for any other protocol.

--- pybricks/tools/pb_type_app_data.c
+++ pybricks/tools/pb_type_app_data.c
@@ -20,47 +20,68 @@
 #include <pybricks/util_mp/pb_obj_helper.h>
 #include <pybricks/util_pb/pb_error.h>
 
+typedef struct {
+    uint8_t mode;
+    size_t offset;
+    size_t size;
+} pb_type_app_data_mode_info_t;
+
 typedef struct _pb_type_app_data_obj_t {
     mp_obj_base_t base;
     pb_type_async_t *tx_iter;
-    mp_obj_t rx_format;
-    mp_obj_str_t rx_bytes_obj;
+    size_t rx_len;
+    size_t num_modes;
+    pb_type_app_data_mode_info_t *modes;
     uint8_t rx_buffer[] __attribute__((aligned(4)));
 } pb_type_app_data_obj_t;
+
 
 // pointer to dynamically allocated app_data singleton for driver callback.
 static pb_type_app_data_obj_t *app_data_instance;
 
 static pbio_error_t handle_incoming_app_data(uint16_t offset, uint32_t size, const uint8_t *data) {
     // Can't write if rx_buffer does not exist or isn't big enough.
-    if (!app_data_instance || offset + size > app_data_instance->rx_bytes_obj.len) {
+    if (!app_data_instance || offset + size > app_data_instance->rx_len) {
         return PBIO_ERROR_INVALID_ARG;
     }
     memcpy(app_data_instance->rx_buffer + offset, data, size);
     return PBIO_SUCCESS;
 }
 
-static mp_obj_t pb_type_app_data_get_bytes(mp_obj_t self_in) {
-    pb_type_app_data_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    // Don't return internal bytes object but make a copy so the user bytes
-    // object is constant as would be expected. Revisit: enable and return
-    // a memoryview, especially if using large buffers.
-    return mp_obj_new_bytes(self->rx_bytes_obj.data, self->rx_bytes_obj.len);
+static pb_type_app_data_mode_info_t *get_mode_info(pb_type_app_data_obj_t *self, mp_obj_t mode_in) {
+    size_t mode = mp_obj_get_int(mode_in);
+    for (size_t i = 0; i < self->num_modes; i++) {
+        if (self->modes[i].mode == mode) {
+            return &self->modes[i];
+        }
+    }
+    return NULL;
 }
-static MP_DEFINE_CONST_FUN_OBJ_1(pb_type_app_data_get_bytes_obj, pb_type_app_data_get_bytes);
 
-static mp_obj_t pb_type_app_data_get_values(mp_obj_t self_in) {
+static mp_obj_t pb_type_app_data_get_bytes(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+    PB_PARSE_ARGS_METHOD(n_args, pos_args, kw_args,
+        pb_type_app_data_obj_t, self,
+        PB_ARG_REQUIRED(mode),
+        PB_ARG_DEFAULT_NONE(index));
 
-    // Implementation in MicroPython is static, so import from ustruct.unpack.
-    mp_obj_t ustruct_unpack = pb_function_import_helper(MP_QSTR_ustruct, MP_QSTR_unpack);
+    pb_type_app_data_mode_info_t *mode_info = get_mode_info(self, mode_in);
+    if (!mode_info) {
+        mp_raise_ValueError(MP_ERROR_TEXT("invalid mode"));
+    }
 
-    // Host (sender) is responsible for making sure that each individual
-    // value remains valid, i.e. is written in a single chunk, since the
-    // following may allocate, and thus be updated between unpacking values.
-    pb_type_app_data_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    return mp_call_function_2(ustruct_unpack, self->rx_format, MP_OBJ_FROM_PTR(&self->rx_bytes_obj));
+    // No index, return all bytes for the mode.
+    if (index_in == mp_const_none) {
+        return mp_obj_new_bytes(&self->rx_buffer[mode_info->offset], mode_info->size);
+    }
+
+    // Index given, return single byte as integer.
+    size_t index = mp_obj_get_int(index_in);
+    if (index >= mode_info->size) {
+        mp_raise_ValueError(MP_ERROR_TEXT("index out of range"));
+    }
+    return mp_obj_new_int(self->rx_buffer[mode_info->offset + index]);
 }
-static MP_DEFINE_CONST_FUN_OBJ_1(pb_type_app_data_get_values_obj, pb_type_app_data_get_values);
+static MP_DEFINE_CONST_FUN_OBJ_KW(pb_type_app_data_get_bytes_obj, 1, pb_type_app_data_get_bytes);
 
 static pbio_error_t app_data_write_bytes_iterate_once(pbio_os_state_t *state, mp_obj_t parent_obj) {
     // No need to pass in buffered arguments since they were copied on the
@@ -95,35 +116,89 @@ static mp_obj_t pb_type_app_data_write_bytes(mp_obj_t self_in, mp_obj_t data_in)
 }
 static MP_DEFINE_CONST_FUN_OBJ_2(pb_type_app_data_write_bytes_obj, pb_type_app_data_write_bytes);
 
-static const mp_obj_str_t pb_const_empty_str_obj = {{&mp_type_str}, 0, 0, (const byte *)""};
+static mp_obj_t pb_type_app_data_configure(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+    PB_PARSE_ARGS_METHOD(n_args, pos_args, kw_args,
+        pb_type_app_data_obj_t, self,
+        PB_ARG_REQUIRED(mode),
+        PB_ARG_REQUIRED(parameter),
+        PB_ARG_REQUIRED(value));
+
+    size_t size;
+    const uint8_t *value = (const uint8_t *)mp_obj_str_get_data(value_in, &size);
+
+    // Create new bytes object with mode and size prepended.
+    vstr_t vstr;
+    vstr_init_len(&vstr, 3 + size);
+    vstr.buf[0] = 0x01;
+    vstr.buf[1] = mp_obj_get_int(mode_in);
+    vstr.buf[2] = mp_obj_get_int(parameter_in);
+    memcpy(vstr.buf + 3, value, size);
+    return pb_type_app_data_write_bytes(MP_OBJ_FROM_PTR(self), mp_obj_new_bytes_from_vstr(&vstr));
+}
+static MP_DEFINE_CONST_FUN_OBJ_KW(pb_type_app_data_configure_obj, 1, pb_type_app_data_configure);
+
 
 static mp_obj_t pb_type_app_data_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
 
     PB_PARSE_ARGS_CLASS(n_args, n_kw, args,
-        PB_ARG_DEFAULT_OBJ(rx_format, pb_const_empty_str_obj));
+        PB_ARG_REQUIRED(modes));
 
-    // Use ustruct.calcsize to parse user rx_format for size.
-    mp_obj_t ustruct_calcsize = pb_function_import_helper(MP_QSTR_ustruct, MP_QSTR_calcsize);
-    size_t size = mp_obj_get_int(mp_call_function_1(ustruct_calcsize, rx_format_in));
-
-    // Can only create one instance for now.
+    // Can only create one instance during initialization.
+    pb_module_tools_assert_blocking();
     if (app_data_instance) {
         mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("host rx_buffer already allocated"));
     }
 
-    // Use finalizer so we can deactivate the data callback when rx_buffer is garbage collected.
-    app_data_instance = mp_obj_malloc_var_with_finaliser(pb_type_app_data_obj_t, uint8_t, size, type);
-    app_data_instance->rx_format = rx_format_in;
+    // Unpack modes to find out required allocation size.
+    size_t alloc_size = 0;
+    size_t num_modes;
+    mp_obj_t *mode_items;
+    mp_obj_get_array(modes_in, &num_modes, &mode_items);
 
-    // Keep rx_buffer in bytes object rx_format for compatibility with unpack.
-    app_data_instance->rx_bytes_obj.base.type = &mp_type_bytes;
-    app_data_instance->rx_bytes_obj.len = size;
-    app_data_instance->rx_bytes_obj.data = app_data_instance->rx_buffer;
+    // Prepare mode setup command.
+    vstr_t mode_vstr;
+    vstr_init_len(&mode_vstr, 1 + num_modes);
+    mode_vstr.buf[0] = 0x00;
+
+    pb_type_app_data_mode_info_t *modes = m_new(pb_type_app_data_mode_info_t, num_modes);
+
+    for (size_t i = 0; i < num_modes; i++) {
+
+        // Expect each item to be a (mode, size) tuple.
+        mp_obj_t item = mode_items[i];
+        size_t pair_size;
+        mp_obj_t *pair_items;
+        mp_obj_get_array(item, &pair_size, &pair_items);
+        if (pair_size != 2 || mp_obj_get_int(pair_items[0]) > 255) {
+            mp_raise_TypeError(MP_ERROR_TEXT("modes must be a list of (mode, size) tuples"));
+        }
+
+        // Store mode info for easy reading/writing later.
+        pb_type_app_data_mode_info_t *mode_info = &modes[i];
+        mode_info->mode = mp_obj_get_int(pair_items[0]);
+        mode_info->offset = alloc_size;
+        mode_info->size = mp_obj_get_int(pair_items[1]);
+
+        // Mode command is just a concatenation of mode numbers.
+        mode_vstr.buf[i + 1] = mode_info->mode;
+
+        // Total allocation size.
+        alloc_size += mode_info->size;
+    }
+
+    // Use finalizer so we can deactivate the data callback when rx_buffer is garbage collected.
+    app_data_instance = mp_obj_malloc_var_with_finaliser(pb_type_app_data_obj_t, uint8_t, alloc_size, type);
+    app_data_instance->rx_len = alloc_size;
+    app_data_instance->num_modes = num_modes;
+    app_data_instance->modes = modes;
 
     // Activate callback now that we have allocated the rx_buffer.
     pbsys_command_set_write_app_data_callback(handle_incoming_app_data);
 
     app_data_instance->tx_iter = NULL;
+
+    // Send mode setup command to host.
+    pb_type_app_data_write_bytes(MP_OBJ_FROM_PTR(app_data_instance), mp_obj_new_bytes_from_vstr(&mode_vstr));
 
     return MP_OBJ_FROM_PTR(app_data_instance);
 }
@@ -141,8 +216,8 @@ static const mp_rom_map_elem_t pb_type_app_data_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR___del__),      MP_ROM_PTR(&pb_type_app_data_close_obj) },
     { MP_ROM_QSTR(MP_QSTR_close),        MP_ROM_PTR(&pb_type_app_data_close_obj) },
     { MP_ROM_QSTR(MP_QSTR_get_bytes),    MP_ROM_PTR(&pb_type_app_data_get_bytes_obj) },
-    { MP_ROM_QSTR(MP_QSTR_get_values),   MP_ROM_PTR(&pb_type_app_data_get_values_obj) },
-    { MP_ROM_QSTR(MP_QSTR_write_bytes),    MP_ROM_PTR(&pb_type_app_data_write_bytes_obj) },
+    { MP_ROM_QSTR(MP_QSTR_write_bytes),  MP_ROM_PTR(&pb_type_app_data_write_bytes_obj) },
+    { MP_ROM_QSTR(MP_QSTR_configure),    MP_ROM_PTR(&pb_type_app_data_configure_obj) },
 };
 static MP_DEFINE_CONST_DICT(pb_type_app_data_locals_dict, pb_type_app_data_locals_dict_table);
 
