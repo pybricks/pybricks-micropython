1edf69ed78bc3e2f2d82765b477db75bf6952c9f
Laurens Valk <laurens@pybricks.com>
2026-09-07 10:59:14 +0200

    tests/virtualhub/basics: Run a motor by default.
    Helpful for basic I/O in default debug program.

--- tests/virtualhub/basics/hello.py
+++ tests/virtualhub/basics/hello.py
@@ -1,5 +1,10 @@
 from pybricks.tools import wait
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port
 
+motor = Motor(Port.A)
+
+motor.run(200)
 print("\nHello")
 wait(1000)
 print("World\n")
